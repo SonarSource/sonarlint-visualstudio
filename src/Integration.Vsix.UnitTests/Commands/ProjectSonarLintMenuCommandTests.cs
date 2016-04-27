e0a50a7aa8b1e74c2aeba397594f0f5d00a55dba
Matthew John Cheetham <Matthew.Cheetham@microsoft.com>
2016-04-27 08:16:27 +0100

    Add missing copyright notice

--- src/Integration.Vsix.UnitTests/Commands/ProjectSonarLintMenuCommandTests.cs
+++ src/Integration.Vsix.UnitTests/Commands/ProjectSonarLintMenuCommandTests.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.VisualStudio.ComponentModelHost;
+﻿//-----------------------------------------------------------------------
+// <copyright file="ProjectSonarLintMenuCommandTests.cs" company="SonarSource SA and Microsoft Corporation">
+//   Copyright (c) SonarSource SA and Microsoft Corporation.  All rights reserved.
+//   Licensed under the MIT License. See License.txt in the project root for license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using Microsoft.VisualStudio.ComponentModelHost;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SonarLint.VisualStudio.Integration.Vsix;
