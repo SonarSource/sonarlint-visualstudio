be3786c529065b41b56caf3fe37eb9fecf3815a3
Tamas Vajk <tamas.vajk@sonarsource.com>
2016-06-22 17:05:52 +0200

    Updating version number to 2.4.0

--- src/AssemblyInfo.Shared.cs
+++ src/AssemblyInfo.Shared.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 
 
 // Note: keep the version number in sync with the VSIX (source.extension.manifest)
-[assembly: AssemblyVersion("2.3.0.0")]
-[assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyVersion("2.4.0.0")]
+[assembly: AssemblyFileVersion("2.4.0.0")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: ComVisible(false)]
