fe852e65da38c618192eba18406e9d0ff83b7c16
Tamas Vajk <tamas.vajk@sonarsource.com>
2016-10-13 12:05:21 +0200

    Add version changing .csproj

---
+++ build/README.md
@@ -0,0 +1,4 @@
+## How to increase SonarLint VS version
+
+1. Open `Version.props` and enter the new version
+2. Run `msbuild ChangeVersion.proj`
--- src/AssemblyInfo.Shared.cs
+++ src/AssemblyInfo.Shared.cs
@@ -1,4 +1,4 @@
-//-----------------------------------------------------------------------
+﻿//-----------------------------------------------------------------------
 // <copyright file="AssemblyInfo.Shared.cs" company="SonarSource SA and Microsoft Corporation">
 //   Copyright (c) SonarSource SA and Microsoft Corporation.  All rights reserved.
 //   Licensed under the MIT License. See License.txt in the project root for license information.
@@ -8,10 +8,10 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-
-// Note: keep the version number in sync with the VSIX (source.extension.manifest)
-[assembly: AssemblyVersion("2.8.0.0")]
-[assembly: AssemblyFileVersion("2.8.0.0")]
+// Note: keep the version numbers in sync with the VSIX (source.extension.manifest)
+[assembly: AssemblyVersion("2.8.0")]
+[assembly: AssemblyFileVersion("2.8.0.0")] // This should exactly match the VSIX version
+[assembly: AssemblyInformationalVersion("Version:2.8.0.0 Branch:not-set Sha1:not-set")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: ComVisible(false)]
