c37f1ab3d18fcfe6ce3e5c0435de12133828d695
Tom <thomas.verin@sonarsource.com>
2016-10-14 15:51:16 +0200

    QA: fix artifact path for promotion

--- build/qa.ps1
+++ build/qa.ps1
@@ -34,11 +34,11 @@ $basicAuthValue = "Basic $encodedCreds"
 $Headers = @{Authorization = $basicAuthValue}
 Invoke-WebRequest -UseBasicParsing -Uri "$url" -Headers $Headers -OutFile $fileName
 
-#write filename to a property file for promotion job
-$s="ARTIFACT=$fileName"
-$s | out-file -encoding utf8 -append ".\filename.properties"
+#write the version to a property file for promotion job
+$s="ARTIFACT=org/sonarsource/dotnet/SonarLint.VSIX/$version"
+$s | out-file -encoding utf8 -append ".\artifact.properties"
 #convert property file to unix for jenkins compatiblity
-Get-ChildItem .\filename.properties | ForEach-Object {
+Get-ChildItem .\artifact.properties | ForEach-Object {
   $contents = [IO.File]::ReadAllText($_) -replace "`r`n?", "`n"
   $utf8 = New-Object System.Text.UTF8Encoding $false
   [IO.File]::WriteAllText($_, $contents, $utf8)
