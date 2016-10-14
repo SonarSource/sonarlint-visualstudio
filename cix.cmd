cdfd3ba04456af3176dd16c48a7c23114e3421a2
Tamas Vajk <tamas.vajk@sonarsource.com>
2016-10-14 10:19:04 +0200

    Rename CIX scripts to BUILD scripts

--- cix.cmd
+++
@@ -1,3 +0,0 @@
-@PowerShell -NonInteractive -NoProfile -ExecutionPolicy Unrestricted -Command "& {.\build\cix.ps1; exit $LastExitCode }" 
-@echo From Cmd.exe: cix.ps1 exited with exit code %errorlevel%
-exit %errorlevel%
