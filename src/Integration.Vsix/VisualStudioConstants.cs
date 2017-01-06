5408dfecc552c0b2b0ab6438df3a1df48d62780b
Amaury Levé <amaury.leve@sonarsource.com>
2017-01-06 17:57:27 +0100

    Updates License headers

--- src/Integration.Vsix/VisualStudioConstants.cs
+++ src/Integration.Vsix/VisualStudioConstants.cs
@@ -1,21 +1,18 @@
 ﻿/*
- * SonarLint for VisualStudio
- * Copyright (C) 2015-2016 SonarSource SA
- * mailto:contact@sonarsource.com
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2017 SonarSource SA and Microsoft Corporation
+ * mailto: contact AT sonarsource DOT com
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
+ * Licensed under the MIT License.
+ * See LICENSE file in the project root for full license information.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 namespace SonarLint.VisualStudio.Integration.Vsix
