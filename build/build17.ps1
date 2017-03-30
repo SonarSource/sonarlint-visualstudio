0160174ea6f9596b4dcc3778a69f7b943fd6494d
Valeri Hristov <valeri.hristov@sonarsource.com>
2017-03-30 16:43:55 +0200

    enabled build on branches

--- build/build17.ps1
+++ build/build17.ps1
@@ -15,33 +15,23 @@ if ($env:IS_PULLREQUEST -eq "true") {
     Start-Process "build/vs2017.bat" -NoNewWindow -Wait
 
 } else {
-    if (($env:GITHUB_BRANCH -eq "master") -or ($env:GITHUB_BRANCH -eq "refs/heads/master")) {
-        write-host -f green "Building master branch"
+    #generate build version from the build number
+    $buildversion="$env:BUILD_NUMBER"
+    $branchName = "$env:GITHUB_BRANCH"
+    $sha1 = "$env:GIT_SHA1"
 
-        #generate build version from the build number
-        $buildversion="$env:BUILD_NUMBER"
+    write-host -f green "Building ${branchName} branch"
 
-        $branchName = "$env:GITHUB_BRANCH"
-        $sha1 = "$env:GIT_SHA1"
+    #Append build number to the versions
+    (Get-Content .\build\Version.props) -replace '<AssemblyFileVersion>\$\(MainVersion\)\.0</AssemblyFileVersion>', "<AssemblyFileVersion>`$(MainVersion).$buildversion</AssemblyFileVersion>" | Set-Content .\build\Version.props
+    (Get-Content .\build\Version.props) -replace '<AssemblyInformationalVersion>Version:\$\(AssemblyFileVersion\) Branch:not-set Sha1:not-set</AssemblyInformationalVersion>', "<AssemblyInformationalVersion>Version:`$(AssemblyFileVersion) Branch:$branchName Sha1:$sha1</AssemblyInformationalVersion>" | Set-Content .\build\Version.props
+    & $env:MSBUILD_PATH  .\build\ChangeVersion.proj
+    testExitCode
 
-        #Append build number to the versions
-        (Get-Content .\build\Version.props) -replace '<AssemblyFileVersion>\$\(MainVersion\)\.0</AssemblyFileVersion>', "<AssemblyFileVersion>`$(MainVersion).$buildversion</AssemblyFileVersion>" | Set-Content .\build\Version.props
-        (Get-Content .\build\Version.props) -replace '<AssemblyInformationalVersion>Version:\$\(AssemblyFileVersion\) Branch:not-set Sha1:not-set</AssemblyInformationalVersion>', "<AssemblyInformationalVersion>Version:`$(AssemblyFileVersion) Branch:$branchName Sha1:$sha1</AssemblyInformationalVersion>" | Set-Content .\build\Version.props
-        & $env:MSBUILD_PATH  .\build\ChangeVersion.proj
-        testExitCode
+    #build
+    & $env:NUGET_PATH restore .\src\SonarLint.VisualStudio.Integration.2017.sln
+    testExitCode
 
-        #build
-        & $env:NUGET_PATH restore .\src\SonarLint.VisualStudio.Integration.2017.sln
-        testExitCode
-
-        #build with VS2017
-        Start-Process "build/vs2017.bat" -NoNewWindow -Wait
-
-    } else {
-        write-host -f green "not on master"
-    }
-
+    #build with VS2017
+    Start-Process "build/vs2017.bat" -NoNewWindow -Wait
 }
-
-
-
