53e8e91a5cdd94a1e6303a8114573d001ae60c44
Valeri Hristov <valhristov@users.noreply.github.com>
2017-03-31 17:20:51 +0200

    Fix #132 - SonarLint for VS2017 no longer crashes on machines without VS2015

--- build/build15.ps1
+++ build/build15.ps1
@@ -40,42 +40,43 @@ if ($env:IS_PULLREQUEST -eq "true") {
     testExitCode
 
 } else {
-    if (($env:GITHUB_BRANCH -eq "master") -or ($env:GITHUB_BRANCH -eq "refs/heads/master")) {
-        write-host -f green "Building master branch"
+    #generate build version from the build number
+    $buildversion="$env:BUILD_NUMBER"
+    $branchName = "$env:GITHUB_BRANCH"
+    $sha1 = "$env:GIT_SHA1"
 
-        #generate build version from the build number
-        $buildversion="$env:BUILD_NUMBER"
+    write-host -f green "Building ${branchName} branch"
 
-        $branchName = "$env:GITHUB_BRANCH"
-        $sha1 = "$env:GIT_SHA1"
+    $isMaster = ($env:GITHUB_BRANCH -eq "master") -or ($env:GITHUB_BRANCH -eq "refs/heads/master");
 
-        #Append build number to the versions
-        (Get-Content .\build\Version.props) -replace '<AssemblyFileVersion>\$\(MainVersion\)\.0</AssemblyFileVersion>', "<AssemblyFileVersion>`$(MainVersion).$buildversion</AssemblyFileVersion>" | Set-Content .\build\Version.props
-        (Get-Content .\build\Version.props) -replace '<AssemblyInformationalVersion>Version:\$\(AssemblyFileVersion\) Branch:not-set Sha1:not-set</AssemblyInformationalVersion>', "<AssemblyInformationalVersion>Version:`$(AssemblyFileVersion) Branch:$branchName Sha1:$sha1</AssemblyInformationalVersion>" | Set-Content .\build\Version.props
-        & $env:MSBUILD_PATH  .\build\ChangeVersion.proj
-        testExitCode
+    #Append build number to the versions
+    (Get-Content .\build\Version.props) -replace '<AssemblyFileVersion>\$\(MainVersion\)\.0</AssemblyFileVersion>', "<AssemblyFileVersion>`$(MainVersion).$buildversion</AssemblyFileVersion>" | Set-Content .\build\Version.props
+    (Get-Content .\build\Version.props) -replace '<AssemblyInformationalVersion>Version:\$\(AssemblyFileVersion\) Branch:not-set Sha1:not-set</AssemblyInformationalVersion>', "<AssemblyInformationalVersion>Version:`$(AssemblyFileVersion) Branch:$branchName Sha1:$sha1</AssemblyInformationalVersion>" | Set-Content .\build\Version.props
+    & $env:MSBUILD_PATH  .\build\ChangeVersion.proj
+    testExitCode
 
+    if($isMaster)
+    {
         #start analysis
         downloadMSBuildSonarQubeScanner
         .\MSBuild.SonarQube.Runner begin /k:sonarlint-visualstudio /n:"SonarLint for Visual Studio" /v:master `
             /d:sonar.host.url=$env:SONAR_HOST_URL `
             /d:sonar.login=$env:SONAR_TOKEN 
         testExitCode
+    }
 
-        #build
-        & $env:NUGET_PATH restore .\src\SonarLint.VisualStudio.Integration.sln
-        testExitCode
-        & $env:MSBUILD_PATH .\src\SonarLint.VisualStudio.Integration.sln /p:configuration=Release /p:DeployExtension=false /p:ZipPackageCompressionLevel=normal /v:m /p:defineConstants=SignAssembly /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=$env:CERT_PATH
-        testExitCode
+    #build
+    & $env:NUGET_PATH restore .\src\SonarLint.VisualStudio.Integration.sln
+    testExitCode
+    & $env:MSBUILD_PATH .\src\SonarLint.VisualStudio.Integration.sln /p:configuration=Release /p:DeployExtension=false /p:ZipPackageCompressionLevel=normal /v:m /p:defineConstants=SignAssembly /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=$env:CERT_PATH
+    testExitCode
 
+    if ($isMaster)
+    {
         #end analysis
         .\MSBuild.SonarQube.Runner end /d:sonar.login=$env:SONAR_TOKEN
         testExitCode
-
-    } else {
-        write-host -f green "not on master"
     }
-
 }
 
 
