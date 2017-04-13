246bea4f7139f02d381d04309ca9b0b9e8b0342b
Amaury Levé <amaury.leve@sonarsource.com>
2017-04-13 12:05:55 +0200

    Add an empty line between header and first line of code

--- src/Progress/ProgressObservation/ViewModels/ProgressStepViewModel.cs
+++ src/Progress/ProgressObservation/ViewModels/ProgressStepViewModel.cs
@@ -17,6 +17,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+
 using SonarLint.VisualStudio.Progress.MVVM;
 using SonarLint.VisualStudio.Progress.Controller;
 using System;
