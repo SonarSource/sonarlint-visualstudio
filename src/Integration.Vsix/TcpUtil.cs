1d2f621283ccadbcfa777451f95e42a24e11ce64
dbmeneses <duarte.meneses@sonarsource.com>
2017-04-25 09:48:08 +0200

    SVS-147 Package SonarLint daemon with SonarLint for VS (#149)

---
+++ src/Integration.Vsix/TcpUtil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace SonarLint.VisualStudio.Integration.Vsix
+{
+    public static class TcpUtil
+    {
+        public static int FindFreePort(int startPort)
+        {
+            IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+            TcpConnectionInformation[] tcpConnInfoArray = ipGlobalProperties.GetActiveTcpConnections();
+            IPEndPoint[] ipEndPoint = ipGlobalProperties.GetActiveTcpListeners();
+
+            List<int> usedPorts = ipEndPoint.Select(x => x.Port)
+                .Concat(tcpConnInfoArray.Select(x => x.LocalEndPoint.Port)).ToList();
+
+            for (int i = startPort; i < UInt16.MaxValue; i++)
+            {
+                if (!usedPorts.Contains(i))
+                {
+                    return i;
+                }
+            }
+            throw new InvalidOperationException("Could not find any free TCP port over port " + startPort);
+        }
+    }
+}
