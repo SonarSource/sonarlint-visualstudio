595bedc146893443b9a4b84d9bb5c26067cf5475
Amaury Levé <amaury.leve@sonarsource.com>
2017-05-09 15:33:16 +0200

    Fix exit code not being properly read

--- build.cmd
+++ build.cmd
@@ -1,13 +1,15 @@
 @echo off
+setlocal ENABLEDELAYEDEXPANSION
+
 IF NOT "%NODE_LABELS%"=="%NODE_LABELS:vs2015=%" (
-	@PowerShell -NonInteractive -NoProfile -ExecutionPolicy Unrestricted -Command "& { .\build\build.ps1 -analyze -test -coverage -githubRepo $env:GITHUB_REPO -githubToken $env:GITHUB_TOKEN -githubPullRequest $env:PULL_REQUEST -isPullRequest $env:IS_PULLREQUEST -sonarQubeUrl $env:SONAR_HOST_URL -sonarQubeToken $env:SONAR_TOKEN -solutionName "SonarLint.VisualStudio.Integration.sln" -certificatePath $env:CERT_PATH; exit $LastExitCode }"
-	@echo From Cmd.exe: build.ps1 exited with exit code %errorlevel%
-	exit %errorlevel%
+	PowerShell -NonInteractive -NoProfile -ExecutionPolicy Unrestricted -Command ".\build\build.ps1 -analyze -test -coverage -githubRepo $env:GITHUB_REPO -githubToken $env:GITHUB_TOKEN -githubPullRequest $env:PULL_REQUEST -isPullRequest $env:IS_PULLREQUEST -sonarQubeUrl $env:SONAR_HOST_URL -sonarQubeToken $env:SONAR_TOKEN -solutionName "SonarLint.VisualStudio.Integration.sln" -certificatePath $env:CERT_PATH"
+	echo From Cmd.exe: build.ps1 exited with exit code !errorlevel!
+	exit !errorlevel!
 ) ELSE IF NOT "%NODE_LABELS%"=="%NODE_LABELS:vs2017=%" (
-	@PowerShell -NonInteractive -NoProfile -ExecutionPolicy Unrestricted -Command "& { .\build\build.ps1 -test -githubRepo $env:GITHUB_REPO -githubToken $env:GITHUB_TOKEN -githubPullRequest $env:PULL_REQUEST -isPullRequest $env:IS_PULLREQUEST -sonarQubeUrl $env:SONAR_HOST_URL -sonarQubeToken $env:SONAR_TOKEN -solutionName "SonarLint.VisualStudio.Integration.2017.sln" -certificatePath $env:CERT_PATH; exit $LastExitCode }"
-	@echo From Cmd.exe: build.ps1 exited with exit code %errorlevel%
-	exit %errorlevel%
+	PowerShell -NonInteractive -NoProfile -ExecutionPolicy Unrestricted -Command ".\build\build.ps1 -test -githubRepo $env:GITHUB_REPO -githubToken $env:GITHUB_TOKEN -githubPullRequest $env:PULL_REQUEST -isPullRequest $env:IS_PULLREQUEST -sonarQubeUrl $env:SONAR_HOST_URL -sonarQubeToken $env:SONAR_TOKEN -solutionName "SonarLint.VisualStudio.Integration.2017.sln" -certificatePath $env:CERT_PATH"
+	echo From Cmd.exe: build.ps1 exited with exit code !errorlevel!
+	exit !errorlevel!
 ) ELSE (
-    @echo ERROR: NODE_LABELS contains neither vs2015 nor vs2017
+    echo ERROR: NODE_LABELS contains neither vs2015 nor vs2017
     exit 1
 )
