af37acf3c04e0a0c5b1e8f13a2b1038befcc7e48
Amaury Levé <amaury.leve@sonarsource.com>
2017-05-16 15:22:14 +0200

    Provide a digital signature to the extension

--- build/build.ps1
+++ build/build.ps1
@@ -17,11 +17,48 @@ param (
     [string]$sonarQubeToken = $null,
 
     [string]$solutionName = "SonarLint.VisualStudio.Integration.sln",
-    [string]$certificatePath,
+    [string]$snkCertificatePath,
+
+    [string]$pfxCertificatePath,
+    [string]$pfxPassword,
 
     [parameter(ValueFromRemainingArguments = $true)] $badArgs)
 
 $ErrorActionPreference = "Stop"
+
+function Get-VsixSignTool() {
+    $vsixSignTool = Get-ChildItem (Resolve-RepoPath "packages") -Recurse -Include "vsixsigntool.exe" | Select-Object -First 1
+    if (!$vsixsigntool) {
+        throw "ERROR: Cannot find vsixsigntool.exe, please make sure the NuGet package is properly installed."
+    }
+
+    return $vsixsigntool.FullName
+}
+
+function ConvertTo-SignedExtension() {
+    Write-Header "Signing the VSIX extensions..."
+    $vsixSignTool = Get-VsixSignTool
+
+    $binariesFolder = Resolve-RepoPath "binaries"
+
+    if (!$pfxCertificatePath) {
+        throw "ERROR: Path to the PFX is not set."
+    }
+    if (!$pfxPassword) {
+        throw "ERROR: PFX password is not set."
+    }
+
+    $anyVsixFileFound = $false
+    Get-ChildItem $binariesFolder -Recurse -Include "*.vsix" | ForEach-Object {
+        $anyVsixFileFound = $true
+        & $vsixSignTool sign /f $pfxCertificatePath /p $pfxPassword /sha1 1a157a3bcf9a9e426330212b58a8d8f27a54eb68 $_
+        Test-ExitCode "ERROR: VSIX Extension signing FAILED."
+    }
+
+    if (!$anyVsixFileFound) {
+        throw "ERROR: Cannot find any VSIX file to sign in '${binariesFolder}'."
+    }
+}
 
 try {
     if ($badArgs -Ne $null) {
@@ -73,7 +110,7 @@ try {
             Build-Solution (Resolve-RepoPath $solutionRelativePath)
         }
         else {
-            Build-ReleaseSolution (Resolve-RepoPath $solutionRelativePath) $certificatePath
+            Build-ReleaseSolution (Resolve-RepoPath $solutionRelativePath) $snkCertificatePath
         }
     }
 
@@ -84,6 +121,8 @@ try {
     if (-Not $skippedAnalysis) {
         End-Analysis $sonarQubeToken
     }
+
+    ConvertTo-SignedExtension
 } catch {
     Write-Host $_
     exit 1
