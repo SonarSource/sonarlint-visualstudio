784877b0441666cef0a4bb65032d6f823489a457
Julien HENRY <henryju@yahoo.fr>
2017-07-07 09:48:55 +0200

    Update grpc dependency and protocol with server (+language)

--- src/Integration.Vsix/Protobuf/build.bat
+++ src/Integration.Vsix/Protobuf/build.bat
@@ -1,2 +1,2 @@
-set toolspath=..\..\..\packages\Grpc.Tools.1.2.2\tools\windows_x64
+set toolspath=..\..\..\packages\Grpc.Tools.1.4.1\tools\windows_x64
 %toolspath%\protoc.exe -I=. --csharp_out . --grpc_out . sonarlint-daemon.proto --plugin=protoc-gen-grpc=%toolspath%\grpc_csharp_plugin.exe
