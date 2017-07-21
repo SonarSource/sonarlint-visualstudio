9587cd570d6a87ba923538ab48cfb0eaba640246
Julien HENRY <henryju@yahoo.fr>
2017-07-21 10:33:23 +0200

    Improve coverage

--- src/Integration.Vsix.UnitTests/Settings/SonarLintSettingsTests.cs
+++ src/Integration.Vsix.UnitTests/Settings/SonarLintSettingsTests.cs
@@ -86,6 +86,17 @@ namespace SonarLint.VisualStudio.Integration.UnitTests.Settings
 
             // Assert
             actual2.Should().Be(expected2, "Did not return default value");
+
+            // Test case 3: invalid type
+            // Arrange
+            bool expected3 = true;
+            this.settingsStore.SetString(SonarLintSettings.SettingsRoot, "key3", "notboolean");
+
+            // Act
+            bool actual3 = testSubject.GetValueOrDefault("key3", expected3);
+
+            // Assert
+            actual3.Should().Be(expected3, "Did not return default value");
         }
 
         [TestMethod]
@@ -146,6 +157,204 @@ namespace SonarLint.VisualStudio.Integration.UnitTests.Settings
 
             // Act + Assert (no store -> no exception)
             testSubject.SetValue("key1", false);
+        }
+
+        [TestMethod]
+        public void IntegrationSettings_GetValueOrDefault_String()
+        {
+            // Arrange
+            SonarLintSettings testSubject = this.CreateTestSubject();
+
+            // Test case 1: exists -> value
+            // Arrange
+            string expected1 = "value";
+            this.settingsStore.SetString(SonarLintSettings.SettingsRoot, "key1", expected1);
+
+            // Act
+            string actual1 = testSubject.GetValueOrDefault("key1", "default");
+
+            // Assert
+            actual1.Should().Be(expected1, "Did not load existing value");
+
+            // Test case 2: does NOT exist -> default
+            // Arrange
+            string expected2 = "default";
+
+            // Act
+            string actual2 = testSubject.GetValueOrDefault("key2", expected2);
+
+            // Assert
+            actual2.Should().Be(expected2, "Did not return default value");
+
+            // Test case 3: invalid type
+            // Arrange
+            string expected3 = "default";
+            this.settingsStore.SetBoolean(SonarLintSettings.SettingsRoot, "key3", true);
+
+            // Act
+            string actual3 = testSubject.GetValueOrDefault("key3", expected3);
+
+            // Assert
+            actual3.Should().Be(expected3, "Did not return default value");
+        }
+
+        [TestMethod]
+        public void IntegrationSettings_GetValueOrDefault_String_NoStore()
+        {
+            // Arrange
+            string expected = "default";
+            SonarLintSettings testSubject;
+            using (new AssertIgnoreScope())
+            {
+                testSubject = this.CreateTestSubject(storeLoadFailure: true);
+            }
+
+            // Act
+            string actual = testSubject.GetValueOrDefault("key1", expected);
+
+            // Assert
+            actual.Should().Be(expected, "Did not return default value in case of missing setting store");
+        }
+
+        [TestMethod]
+        public void IntegrationSettings_SetValue_String()
+        {
+            // Arrange
+            const string propertyKey = "key1";
+            const string collection = SonarLintSettings.SettingsRoot;
+            SonarLintSettings testSubject = this.CreateTestSubject();
+
+            // Sanity
+            this.settingsStore.AssertCollectionPropertyCount(collection, 0);
+
+            // Test case 1: new property
+            // Act
+            testSubject.SetValue(propertyKey, "value");
+
+            // Assert
+            this.settingsStore.AssertCollectionPropertyCount(collection, 1);
+            this.settingsStore.AssertString(collection, propertyKey, "value");
+
+            // Test case 2: overwrite existing property
+            // Act
+            testSubject.SetValue(propertyKey, "value2");
+
+            // Assert
+            this.settingsStore.AssertCollectionPropertyCount(collection, 1);
+            this.settingsStore.AssertString(collection, propertyKey, "value2");
+        }
+
+        [TestMethod]
+        public void IntegrationSettings_SetValue_String_NoStore()
+        {
+            // Arrange
+            SonarLintSettings testSubject;
+            using (new AssertIgnoreScope())
+            {
+                testSubject = this.CreateTestSubject(storeLoadFailure: true);
+            }
+
+            // Act + Assert (no store -> no exception)
+            testSubject.SetValue("key1", "value");
+        }
+
+        [TestMethod]
+        public void IntegrationSettings_GetValueOrDefault_Int()
+        {
+            // Arrange
+            SonarLintSettings testSubject = this.CreateTestSubject();
+
+            // Test case 1: exists -> value
+            // Arrange
+            int expected1 = 1;
+            this.settingsStore.SetInt32(SonarLintSettings.SettingsRoot, "key1", expected1);
+
+            // Act
+            int actual1 = testSubject.GetValueOrDefault("key1", 0);
+
+            // Assert
+            actual1.Should().Be(expected1, "Did not load existing value");
+
+            // Test case 2: does NOT exist -> default
+            // Arrange
+            int expected2 = 2;
+
+            // Act
+            int actual2 = testSubject.GetValueOrDefault("key2", expected2);
+
+            // Assert
+            actual2.Should().Be(expected2, "Did not return default value");
+
+            // Test case 3: invalid type
+            // Arrange
+            int expected3 = 3;
+            this.settingsStore.SetBoolean(SonarLintSettings.SettingsRoot, "key3", true);
+
+            // Act
+            int actual3 = testSubject.GetValueOrDefault("key3", expected3);
+
+            // Assert
+            actual3.Should().Be(expected3, "Did not return default value");
+        }
+
+        [TestMethod]
+        public void IntegrationSettings_GetValueOrDefault_Int_NoStore()
+        {
+            // Arrange
+            int expected = 1;
+            SonarLintSettings testSubject;
+            using (new AssertIgnoreScope())
+            {
+                testSubject = this.CreateTestSubject(storeLoadFailure: true);
+            }
+
+            // Act
+            int actual = testSubject.GetValueOrDefault("key1", expected);
+
+            // Assert
+            actual.Should().Be(expected, "Did not return default value in case of missing setting store");
+        }
+
+        [TestMethod]
+        public void IntegrationSettings_SetValue_Int()
+        {
+            // Arrange
+            const string propertyKey = "key1";
+            const string collection = SonarLintSettings.SettingsRoot;
+            SonarLintSettings testSubject = this.CreateTestSubject();
+
+            // Sanity
+            this.settingsStore.AssertCollectionPropertyCount(collection, 0);
+
+            // Test case 1: new property
+            // Act
+            testSubject.SetValue(propertyKey, 1);
+
+            // Assert
+            this.settingsStore.AssertCollectionPropertyCount(collection, 1);
+            this.settingsStore.AssertInt(collection, propertyKey, 1);
+
+            // Test case 2: overwrite existing property
+            // Act
+            testSubject.SetValue(propertyKey, 2);
+
+            // Assert
+            this.settingsStore.AssertCollectionPropertyCount(collection, 1);
+            this.settingsStore.AssertInt(collection, propertyKey, 2);
+        }
+
+        [TestMethod]
+        public void IntegrationSettings_SetValue_Int_NoStore()
+        {
+            // Arrange
+            SonarLintSettings testSubject;
+            using (new AssertIgnoreScope())
+            {
+                testSubject = this.CreateTestSubject(storeLoadFailure: true);
+            }
+
+            // Act + Assert (no store -> no exception)
+            testSubject.SetValue("key1", 1);
         }
 
         #region Helpers
