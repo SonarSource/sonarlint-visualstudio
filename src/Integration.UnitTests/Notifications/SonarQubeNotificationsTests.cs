294e37a390e0fca0e5fc360faac770ee5a070c4c
Amaury Levé <amaury.leve@sonarsource.com>
2017-09-25 17:23:11 +0200

    Notifications (MMF-966)

---
+++ src/Integration.UnitTests/Notifications/SonarQubeNotificationsTests.cs
@@ -0,0 +1,165 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SonarQube.Client.Messages;
+using SonarQube.Client.Models;
+using SonarQube.Client.Services;
+
+namespace SonarLint.VisualStudio.Integration.Notifications.UnitTests
+{
+    [TestClass]
+    public class SonarQubeNotificationsTests
+    {
+        private Mock<ITimer> timerMock;
+        private Mock<INotificationIndicatorViewModel> modelMock;
+
+        private async Task<ISonarQubeService> GetConnectedService(params NotificationsResponse[] expectedEvents)
+        {
+            var successResponse = new HttpResponseMessage(HttpStatusCode.OK);
+            var client = new Mock<ISonarQubeClient>();
+            client.Setup(x => x.ValidateCredentialsAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(() => new Result<CredentialResponse>(successResponse, new CredentialResponse { IsValid = true }));
+
+            client.Setup(x => x.GetVersionAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(() => new Result<VersionResponse>(successResponse, new VersionResponse { Version = "6.6" }));
+
+            client.Setup(x => x.GetNotificationEventsAsync(It.IsAny<NotificationsRequest>(),
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(() => new Result<NotificationsResponse[]>(successResponse, expectedEvents));
+
+            var clientFactory = new Mock<ISonarQubeClientFactory>();
+            clientFactory.Setup(x => x.Create(It.IsAny<ConnectionRequest>())).Returns(client.Object);
+
+            var service = new SonarQubeService(clientFactory.Object);
+            await service.ConnectAsync(new ConnectionInformation(new Uri("http://mysq.com")), CancellationToken.None);
+            return service;
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            timerMock = new Mock<ITimer>();
+            modelMock = new Mock<INotificationIndicatorViewModel>();
+            modelMock.SetupAllProperties();
+        }
+
+        [TestMethod]
+        public async Task Start_Sets_IsVisible()
+        {
+            // Arrange
+            timerMock.Setup(mock => mock.Start());
+            var sqService = await GetConnectedService();
+
+            var model = modelMock.Object;
+            var notifications = new SonarQubeNotificationService(sqService, model, timerMock.Object);
+
+            // Act
+            await notifications.StartAsync("test", null);
+
+            // Assert
+            model.IsIconVisible.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public async Task Test_DefaultNotificationDate_IsOneDayAgo()
+        {
+            // Arrange
+            var sqService = await GetConnectedService();
+            using (var notifications = new SonarQubeNotificationService(sqService, modelMock.Object, timerMock.Object))
+            {
+                await notifications.StartAsync("test", null);
+
+                // Assert
+                notifications.GetNotificationData().LastNotificationDate
+                    .Should().BeCloseTo(DateTimeOffset.Now.AddDays(-1), (int)TimeSpan.FromSeconds(5).TotalMilliseconds);
+            }
+        }
+
+        [TestMethod]
+        public async Task Test_OldNotificationDate_IsSetToOneDayAgo()
+        {
+            // Arrange
+            var sqService = await GetConnectedService();
+            using (var notifications = new SonarQubeNotificationService(sqService, modelMock.Object, timerMock.Object))
+            {
+                var date = new NotificationData
+                {
+                    IsEnabled = true,
+                    LastNotificationDate =
+                       new DateTimeOffset(2000, 1, 1, 0, 0, 0, TimeSpan.FromHours(1))
+                };
+
+                await notifications.StartAsync("test", date);
+
+                // Assert
+                notifications.GetNotificationData().LastNotificationDate
+                    .Should().BeCloseTo(DateTimeOffset.Now.AddDays(-1), (int)TimeSpan.FromSeconds(5).TotalMilliseconds);
+            }
+        }
+
+        [TestMethod]
+        public async Task Test_NotificationDate_IsSetToDateOfMostRecentEvent()
+        {
+            // Arrange
+            var event1 = new NotificationsResponse
+            {
+                Category = "QUALITY_GATE",
+                Link = new Uri("http://foo.com"),
+                Date = new DateTimeOffset(2010, 1, 1, 14, 59, 59, TimeSpan.FromHours(2)),
+                Message = "foo",
+                Project = "test"
+            };
+
+            var event2 = new NotificationsResponse
+            {
+                Category = "QUALITY_GATE",
+                Link = new Uri("http://foo.com"),
+                Date = new DateTimeOffset(2010, 1, 1, 14, 59, 59, TimeSpan.FromHours(1)),
+                Message = "foo",
+                Project = "test"
+            };
+
+            var sqService = await GetConnectedService(event1, event2);
+            using (var notifications = new SonarQubeNotificationService(sqService, modelMock.Object, timerMock.Object))
+            {
+                var date = new NotificationData
+                {
+                    IsEnabled = true,
+                    LastNotificationDate =
+                       new DateTimeOffset(2000, 1, 1, 0, 0, 0, TimeSpan.FromHours(1))
+                };
+
+                await notifications.StartAsync("test", date);
+
+                // Assert
+                notifications.GetNotificationData().LastNotificationDate
+                    .Should().BeCloseTo(event2.Date, (int)TimeSpan.FromSeconds(5).TotalMilliseconds);
+            }
+        }
+    }
+}
---
+++ src/Integration.Vsix/SonarLintNotificationsPackage.cs
@@ -0,0 +1,132 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using SonarLint.VisualStudio.Integration.Notifications;
+using SonarQube.Client.Services;
+
+namespace SonarLint.VisualStudio.Integration.Vsix
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [Guid(PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    [ProvideAutoLoad(UIContextGuids.NoSolution)]
+    [ProvideAutoLoad(UIContextGuids.SolutionExists)]
+    public sealed class SonarLintNotificationsPackage : Package
+    {
+        /// <summary>
+        /// SonarLintNotifications GUID string.
+        /// </summary>
+        public const string PackageGuidString = "c26b6802-dd9c-4a49-b8a5-0ad8ef04c579";
+        private const string NotificationDataKey = "NotificationEventData";
+
+        private readonly IFormatter formatter = new BinaryFormatter();
+
+        private IActiveSolutionBoundTracker activeSolutionBoundTracker;
+        private ISonarQubeNotificationService notifications;
+        private NotificationData notificationData;
+        private NotificationIndicator notificationIcon;
+        private bool disposed;
+
+        protected override void Initialize()
+        {
+            AddOptionKey(NotificationDataKey);
+            base.Initialize();
+
+            var sonarqubeService = this.GetMefService<ISonarQubeService>();
+
+            notifications = new SonarQubeNotificationService(sonarqubeService,
+                new NotificationIndicatorViewModel(), new TimerWrapper { Interval = 60000 });
+
+            activeSolutionBoundTracker = this.GetMefService<IActiveSolutionBoundTracker>();
+            activeSolutionBoundTracker.SolutionBindingChanged += OnSolutionBindingChanged;
+        }
+
+        private void OnSolutionBindingChanged(object sender, ActiveSolutionBindingEventArgs binding)
+        {
+            if (notificationIcon == null)
+            {
+                notificationIcon = new NotificationIndicator();
+                notificationIcon.DataContext = notifications.Model;
+                VisualStudioStatusBarHelper.AddStatusBarIcon(notificationIcon);
+            }
+
+            if (binding.IsBound)
+            {
+                notifications.StartAsync(binding.ProjectKey, notificationData);
+            }
+            else
+            {
+                notifications.Stop();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+            base.Dispose(disposing);
+
+            if (notificationIcon != null)
+            {
+                VisualStudioStatusBarHelper.RemoveStatusBarIcon(notificationIcon);
+            }
+
+            activeSolutionBoundTracker.SolutionBindingChanged -= OnSolutionBindingChanged;
+
+            (notifications as IDisposable)?.Dispose();
+            disposed = true;
+        }
+
+        protected override void OnSaveOptions(string key, Stream stream)
+        {
+            if (key == NotificationDataKey)
+            {
+                formatter.Serialize(stream, notifications.GetNotificationData());
+            }
+        }
+
+        protected override void OnLoadOptions(string key, Stream stream)
+        {
+            if (key == NotificationDataKey)
+            {
+                try
+                {
+                    notificationData = formatter.Deserialize(stream) as NotificationData;
+                }
+                catch (Exception ex)
+                {
+                    VsShellUtils.WriteToSonarLintOutputPane(this,
+                        $"Failed to read notification data: {ex.Message}");
+                }
+            }
+        }
+    }
+}
