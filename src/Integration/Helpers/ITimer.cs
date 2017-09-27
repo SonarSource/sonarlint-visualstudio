9da8f0490af402dafd573007f415681d3b1e6dee
Amaury Levé <amaury.leve@sonarsource.com>
2017-09-27 18:21:25 +0200

    Refactor telemetry manager and add more tests

--- src/Integration/Helpers/ITimer.cs
+++ src/Integration/Helpers/ITimer.cs
@@ -18,12 +18,19 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+using System.Timers;
+
 namespace SonarLint.VisualStudio.Integration
 {
-    public interface ITelemetryManager
+    public interface ITimer
     {
-        bool IsAnonymousDataShared { get; }
-        void OptOut();
-        void OptIn();
+        event ElapsedEventHandler Elapsed;
+
+        bool AutoReset { get; set; }
+        double Interval { get; set; }
+
+        void Start();
+        void Stop();
+        void Close();
     }
 }
