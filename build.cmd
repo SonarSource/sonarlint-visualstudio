45bb4bab1e5d6d101acb537d4d0f5b9c39ee566f
Amaury Levé <amaury.leve@sonarsource.com>
2017-10-20 17:16:49 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -2,7 +2,8 @@
 setlocal ENABLEDELAYEDEXPANSION
 
 ECHO ======= Starting to wait
-timeout 30
+SLEEP 30
+
 IF NOT "%NODE_LABELS%"=="%NODE_LABELS:vs2015=%" (
 	PowerShell -NonInteractive -NoProfile -ExecutionPolicy Unrestricted -Command ".\build\build.ps1 -analyze -test -coverage -githubRepo $env:GITHUB_REPO -githubToken $env:GITHUB_TOKEN -githubPullRequest $env:PULL_REQUEST -isPullRequest $env:IS_PULLREQUEST -sonarQubeUrl $env:SONAR_HOST_URL -sonarQubeToken $env:SONAR_TOKEN -solutionName "SonarLint.VisualStudio.Integration.sln" -snkCertificatePath $env:CERT_PATH -pfxCertificatePath $env:PFX_PATH -pfxPassword $env:PFX_PASSWORD"
 	echo From Cmd.exe: build.ps1 exited with exit code !errorlevel!
