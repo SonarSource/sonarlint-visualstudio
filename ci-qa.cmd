a8f60b7c5f9182978e90a14209414ee6ba23d7fc
Amaury Levé <amaury.leve@sonarsource.com>
2017-11-17 11:27:06 +0100

    Rename build.ps1 and qa.ps1 (#436)

--- ci-qa.cmd
+++ ci-qa.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal ENABLEDELAYEDEXPANSION
 
-PowerShell -NonInteractive -NoProfile -ExecutionPolicy Unrestricted -Command ".\build\qa.ps1" 
+PowerShell -NonInteractive -NoProfile -ExecutionPolicy Unrestricted -Command ".\build\ci-qa.ps1" 
 echo From Cmd.exe: qa.ps1 exited with exit code !errorlevel!
 exit !errorlevel!
