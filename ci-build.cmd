adcf15e602d23969f9ae1095b612a2374d3f8586
Tom <thomas.verin@sonarsource.com>
2017-12-11 16:59:32 +0100

    use jenkinsfile declarative form

--- ci-build.cmd
+++ ci-build.cmd
@@ -1,6 +1,5 @@
 @echo off
 setlocal ENABLEDELAYEDEXPANSION
-
 IF NOT "%NODE_LABELS%"=="%NODE_LABELS:vs2015=%" (
 	PowerShell -NonInteractive -NoProfile -ExecutionPolicy Unrestricted -Command ".\build\ci-build.ps1 -analyze -test -coverage -githubRepo $env:GITHUB_REPO -githubToken $env:GITHUB_TOKEN -githubPullRequest $env:PULL_REQUEST -isPullRequest $env:IS_PULLREQUEST -sonarQubeUrl $env:SONAR_HOST_URL -sonarQubeToken $env:SONAR_TOKEN -solutionName "SonarLint.VisualStudio.Integration.sln" -snkCertificatePath $env:CERT_PATH -pfxCertificatePath $env:PFX_PATH -pfxPassword $env:PFX_PASSWORD"
 	echo From Cmd.exe: build.ps1 exited with exit code !errorlevel!
