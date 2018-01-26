abcba43c991d85f8704190865ea3aad5a967396f
duncanp-sonar <duncan.pocklington@sonarsource.com>
2018-01-26 17:54:03 +0000

    Added IConfigurationProvider (#513)
    * Added IConfigurationProvider
    * Changed existing code to use IConfigurationProvider instead of ISolutionBindingSerializer
    
    Relates to #508

---
+++ src/Integration/NewConnectedMode/LegacyConfigurationProviderAdapter.cs
@@ -0,0 +1,53 @@
+/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using SonarLint.VisualStudio.Integration.Persistence;
+
+namespace SonarLint.VisualStudio.Integration.NewConnectedMode
+{
+
+    // TODO: remove this class
+    // It's a temporary measure to keep the existing code working while
+    // refactoring/adding the new lightweight connected mode
+
+    internal class LegacyConfigurationProviderAdapter : IConfigurationProvider
+    {
+        private readonly ISolutionBindingSerializer legacySerializer;
+
+        public LegacyConfigurationProviderAdapter(ISolutionBindingSerializer legacySerializer)
+        {
+            if (legacySerializer == null)
+            {
+                throw new ArgumentNullException(nameof(legacySerializer));
+            }
+            this.legacySerializer = legacySerializer;
+        }
+
+        /// <summary>
+        /// Returns the currently bound project, or null if the current solution is
+        /// not bound, or if there is not a current solution
+        /// </summary>
+        public BoundSonarQubeProject GetBoundProject()
+        {
+            return legacySerializer.ReadSolutionBinding();
+        }
+    }
+}
