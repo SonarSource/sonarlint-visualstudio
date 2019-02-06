9cee1bee5ab545cee04ee682a15300f07d39f3a0
Valeri Hristov <valeri.hristov@sonarsource.com>
2019-02-06 18:42:24 +0100

    Removed AssemblyInfo.Shared.cs and Signing.cs references

--- SonarQube.Client/Properties/AssemblyInfo.cs
+++ SonarQube.Client/Properties/AssemblyInfo.cs
@@ -21,35 +21,16 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("SonarQube.Client")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SonarSource")]
-[assembly: AssemblyProduct("SonarQube.Client")]
-[assembly: AssemblyCopyright("Copyright © SonarSource SA 2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5e513fc3-bda7-40f2-8b6a-1bb9372acb99")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("5.0.0")]
+[assembly: AssemblyFileVersion("5.0.0.0")]
+[assembly: AssemblyInformationalVersion("Version:5.0.0.0 Branch:not-set Sha1:not-set")]
+[assembly: AssemblyConfiguration("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyProduct("SonarQube Web API Client for .NET")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyCompany("SonarSource")]
+[assembly: AssemblyCopyright("Copyright © SonarSource SA 2016-2019")]
+[assembly: AssemblyTrademark("")]
