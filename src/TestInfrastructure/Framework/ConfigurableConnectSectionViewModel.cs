2d72728ff077dc4210b1633d4aab9c5e10f0df4e
duncanp <duncan.pocklington@sonarsource.com>
2019-03-14 00:46:17 +0100

    Extracted the TeamExplorer code into a separate assembly
    * moved the TE-dependent tests to a new test assembly
    * remove the TE references from all of the other test and product projects
    
    TODO - fix up VS2017 projects

---
+++ src/TestInfrastructure/Framework/ConfigurableConnectSectionViewModel.cs
@@ -0,0 +1,38 @@
+/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using SonarLint.VisualStudio.Integration.Binding;
+using SonarLint.VisualStudio.Integration.State;
+using SonarLint.VisualStudio.Integration.TeamExplorer;
+using SonarLint.VisualStudio.Integration.WPF;
+using System;
+using System.Windows.Input;
+
+namespace SonarLint.VisualStudio.Integration.UnitTests
+{
+    internal class ConfigurableConnectSectionViewModel : IConnectSectionViewModel
+    {
+        public ICommand<BindCommandArgs> BindCommand { get; set; }
+        public ICommand<string> BrowseToUrlCommand { get; set; }
+        public ICommand ConnectCommand { get; set; }
+        public bool IsBusy { get; set; }
+        public TransferableVisualState State { get; set; }
+    }
+}
