61579f8cd771d045f8faaaaccce48b1c597d9523
Amaury Levé <amaury.leve@sonarsource.com>
2019-03-18 17:21:49 +0100

    Fix error MC3064 by changing accessiblity of some classes

--- src/Integration/TeamExplorer/ServerViewModel.cs
+++ src/Integration/TeamExplorer/ServerViewModel.cs
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * SonarLint for Visual Studio
  * Copyright (C) 2016-2018 SonarSource SA
  * mailto:info AT sonarsource DOT com
@@ -29,7 +29,7 @@ using SonarQube.Client.Models;
 
 namespace SonarLint.VisualStudio.Integration.TeamExplorer
 {
-    internal class ServerViewModel : ViewModelBase
+    public class ServerViewModel : ViewModelBase
     {
         private readonly ConnectionInformation connectionInformation;
         private readonly ObservableCollection<ProjectViewModel> projects = new ObservableCollection<ProjectViewModel>();
