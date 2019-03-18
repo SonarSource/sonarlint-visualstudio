ee2769a2d222b56ba24bd058f3eaa535bad39682
duncanp-sonar <duncan.pocklington@sonarsource.com>
2019-03-18 10:28:34 +0000

    Update and refactor build scripts

--- ci-build.cmd
+++ ci-build.cmd
@@ -1,11 +1,11 @@
 @echo off
 setlocal ENABLEDELAYEDEXPANSION
 IF NOT "%NODE_LABELS%"=="%NODE_LABELS:vs2015=%" (
-	PowerShell -NonInteractive -NoProfile -ExecutionPolicy Unrestricted -Command ".\build\ci-build.ps1 -analyze -test -coverage -githubRepo $env:GITHUB_REPO -githubToken $env:GITHUB_TOKEN -githubPullRequest $env:PULL_REQUEST -isPullRequest $env:IS_PULLREQUEST -sonarQubeUrl $env:SONAR_HOST_URL -sonarQubeToken $env:SONAR_TOKEN -solutionName "SonarLint.VisualStudio.Integration.sln" -snkCertificatePath $env:CERT_PATH -pfxCertificatePath $env:PFX_PATH -pfxPassword $env:PFX_PASSWORD"
+	PowerShell -NonInteractive -NoProfile -ExecutionPolicy Unrestricted -Command ".\build\ci-build.ps1 -vsTargetVersion vs2015"
 	echo From Cmd.exe: ci-build.ps1 exited with exit code !errorlevel!
 	exit !errorlevel!
 ) ELSE IF NOT "%NODE_LABELS%"=="%NODE_LABELS:vs2017=%" (
-	PowerShell -NonInteractive -NoProfile -ExecutionPolicy Unrestricted -Command ".\build\ci-build.ps1 -test -githubRepo $env:GITHUB_REPO -githubToken $env:GITHUB_TOKEN -githubPullRequest $env:PULL_REQUEST -isPullRequest $env:IS_PULLREQUEST -sonarQubeUrl $env:SONAR_HOST_URL -sonarQubeToken $env:SONAR_TOKEN -solutionName "SonarLint.VisualStudio.Integration.2017.sln" -snkCertificatePath $env:CERT_PATH -pfxCertificatePath $env:PFX_PATH -pfxPassword $env:PFX_PASSWORD"
+	PowerShell -NonInteractive -NoProfile -ExecutionPolicy Unrestricted -Command ".\build\ci-build.ps1 -vsTargetVersion vs2017"
 	echo From Cmd.exe: ci-build.ps1 exited with exit code !errorlevel!
 	exit !errorlevel!
 ) ELSE (
