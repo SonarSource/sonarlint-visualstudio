ba992d3c085dafe2ee34e8aa919abd5dd7d1c961
Amaury Levé <amaury.leve@sonarsource.com>
2019-03-21 17:17:06 +0100

    Make sure the sonarqube service doesn't fail when reaching 10k projects (#4)

--- SonarQube.Client/Requests/PagedRequestBase.cs
+++ SonarQube.Client/Requests/PagedRequestBase.cs
@@ -44,23 +44,29 @@ namespace SonarQube.Client.Requests
 
         public async override Task<TResponseItem[]> InvokeAsync(HttpClient httpClient, CancellationToken token)
         {
-            var result = new List<TResponseItem>();
+            var allResponseItems = new List<TResponseItem>();
 
             Result<TResponseItem[]> pageResult;
             do
             {
                 pageResult = await InvokeUncheckedAsync(httpClient, token);
-                pageResult.EnsureSuccess();
+                ValidateResult(pageResult, allResponseItems);
 
-                result.AddRange(pageResult.Value);
-
-                Logger.Debug($"Received {pageResult.Value.Length} items.");
+                if (pageResult.Value != null)
+                {
+                    allResponseItems.AddRange(pageResult.Value);
+                    Logger.Debug($"Received {pageResult.Value.Length} items.");
+                }
 
                 Page++;
             }
-            while (pageResult.Value.Length >= MaximumPageSize);
+            while (pageResult.Value != null &&
+                pageResult.Value.Length >= MaximumPageSize);
 
-            return result.ToArray();
+            return allResponseItems.ToArray();
         }
+
+        protected virtual void ValidateResult(Result<TResponseItem[]> pageResult, List<TResponseItem> allResponseItems) =>
+            pageResult.EnsureSuccess();
     }
 }
