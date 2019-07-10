fe98f36058fc1c9fdfc44c826ffde3d5d7f54fed
duncanp-sonar <duncan.pocklington@sonarsource.com>
2019-07-10 11:55:08 +0100

    Update daemon to version 4.3.0.2475
    Fixed #819
    
    Contains:
    * Cfamily plugin version 6.3.0.11371
    * JavaScript plugin version 4.0.0.5862

--- src/Integration.Vsix/SonarLintDaemon/SonarLintDaemon.cs
+++ src/Integration.Vsix/SonarLintDaemon/SonarLintDaemon.cs
@@ -41,7 +41,7 @@ namespace SonarLint.VisualStudio.Integration.Vsix
         private const string DaemonHost = "localhost";
         private const int DefaultDaemonPort = 8050;
 
-        internal /* for testing */ const string DefaultDaemonVersion = "4.0.2.2142";
+        internal /* for testing */ const string DefaultDaemonVersion = "4.3.0.2475";
         internal /* for testing */ const string SonarLintDownloadUrlEnvVar = "SONARLINT_DAEMON_DOWNLOAD_URL";
         internal /* for testing */ static readonly string DefaultDownloadUrl = string.Format("https://binaries.sonarsource.com/Distribution/sonarlint-daemon/sonarlint-daemon-{0}-windows.zip",
             DefaultDaemonVersion);
