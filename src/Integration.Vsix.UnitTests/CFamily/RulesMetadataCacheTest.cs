80705661cdee58bbe2a6891648132f57659ad843
Julien HENRY <julien.henry@sonarsource.com>
2019-07-25 18:14:24 +0200

    Create a singleton to cache rules metadata

---
+++ src/Integration.Vsix.UnitTests/CFamily/RulesMetadataCacheTest.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using EnvDTE;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SonarLint.VisualStudio.Integration.Vsix;
+using SonarLint.VisualStudio.Integration.Vsix.CFamily;
+using static SonarLint.VisualStudio.Integration.Vsix.CFamily.RulesLoader;
+
+namespace SonarLint.VisualStudio.Integration.UnitTests.CFamily
+{
+    [TestClass]
+    public class RulesMetadataCacheTest
+    {
+        [TestMethod]
+        public void Read_Rules()
+        {
+            RulesMetadataCache.Instance.AllRulesList.Should().HaveCount(410);
+        }
+
+        [TestMethod]
+        public void Read_Active_Rules()
+        {
+            RulesMetadataCache.Instance.ActiveRulesList.Should().HaveCount(255);
+        }
+
+        [TestMethod]
+        public void Read_Rules_Params()
+        {
+            Dictionary<string, string> parameters = null;
+            RulesMetadataCache.Instance.RulesParameters.TryGetValue("ClassComplexity", out parameters);
+            parameters.Should()
+                .Contain(new System.Collections.Generic.KeyValuePair<string, string>("maximumClassComplexityThreshold", "80"));
+
+        }
+
+        [TestMethod]
+        public void Read_Rules_Metadata()
+        {
+            RuleMetadata metadata = null;
+            RulesMetadataCache.Instance.RulesMetadata.TryGetValue("ClassComplexity", out metadata);
+            using (new AssertionScope())
+            {
+                metadata.Type.Should().Be(RuleType.CodeSmell);
+                metadata.DefaultSeverity.Should().Be(RuleSeverity.Critical);
+            }
+        }
+    }
+}
---
+++ src/Integration.Vsix/CFamily/RulesMetadataCache.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static SonarLint.VisualStudio.Integration.Vsix.CFamily.RulesLoader;
+
+namespace SonarLint.VisualStudio.Integration.Vsix.CFamily
+{
+    internal sealed class RulesMetadataCache
+    {
+        private static readonly Lazy<RulesMetadataCache> lazy = new Lazy<RulesMetadataCache>(() => new RulesMetadataCache());
+
+        public static RulesMetadataCache Instance { get { return lazy.Value; } }
+
+        public List<string> AllRulesList { get; }
+        public List<string> ActiveRulesList { get; }
+
+        public Dictionary<string, Dictionary<string, string>> RulesParameters { get; }
+
+        public Dictionary<string, RuleMetadata> RulesMetadata { get; }
+
+        private RulesMetadataCache()
+        {
+            AllRulesList = RulesLoader.ReadRulesList();
+            ActiveRulesList = RulesLoader.ReadActiveRulesList();
+            RulesParameters = AllRulesList
+                .ToDictionary(key => key, key => RulesLoader.ReadRuleParams(key));
+            RulesMetadata = AllRulesList
+                .ToDictionary(key => key, key => RulesLoader.ReadRuleMetadata(key));
+        }
+    }
+}
