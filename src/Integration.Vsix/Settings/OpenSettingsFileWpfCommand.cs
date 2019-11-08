aa69d9b9874e0876c89982268d0be7473d9fb8ba
duncanp <duncan.pocklington@sonarsource.com>
2019-11-08 11:46:26 +0000

    Bug #1071 Close Options dialogue when showing settings file
    NB changes to other settings (e.g. logging level) are not saved

--- src/Integration.Vsix/Settings/OpenSettingsFileWpfCommand.cs
+++ src/Integration.Vsix/Settings/OpenSettingsFileWpfCommand.cs
@@ -19,6 +19,8 @@
  */
 
 using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
 using System.Windows.Input;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
@@ -27,45 +29,74 @@ using SonarLint.VisualStudio.Integration.Vsix.Resources;
 namespace SonarLint.VisualStudio.Integration.Vsix
 {
     internal class OpenSettingsFileWpfCommand : ICommand
+    {
+        private readonly IServiceProvider serviceProvider;
+        private readonly IUserSettingsProvider userSettingsProvider;
+        private readonly ILogger logger;
+        private readonly IWin32Window win32Window;
+
+        [DllImport("user32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetAncestor(IntPtr hwnd, uint flags);
+        private const uint GetRootWindow = 2;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+        private const uint WM_CLOSE = 0x10;
+
+        public OpenSettingsFileWpfCommand(IServiceProvider serviceProvider, IUserSettingsProvider userSettingsProvider, IWin32Window win32Window, ILogger logger)
         {
-            private readonly IServiceProvider serviceProvider;
-            private readonly IUserSettingsProvider userSettingsProvider;
-            private readonly ILogger logger;
-
-            public OpenSettingsFileWpfCommand(IServiceProvider serviceProvider, IUserSettingsProvider userSettingsProvider, ILogger logger)
-            {
-                this.serviceProvider = serviceProvider;
-                this.userSettingsProvider = userSettingsProvider;
-                this.logger = logger;
-            }
-
-            #region ICommand methods
-
-            public event EventHandler CanExecuteChanged;
-
-            public bool CanExecute(object parameter) => true;
-
-            public void Execute(object parameter)
-            {
-                try
-                {
-                    userSettingsProvider.EnsureFileExists();
-                    OpenDocumentInVs(userSettingsProvider.SettingsFilePath);
-                }
-                catch (Exception ex) when (!ErrorHandler.IsCriticalException(ex))
-                {
-                    logger.WriteLine(Strings.ToolsOptions_ErrorOpeningSettingsFile, userSettingsProvider.SettingsFilePath, ex.Message);
-                }
-            }
-
-            protected virtual /* for testing */ void OpenDocumentInVs(string filePath)
-            {
-                // TryOpenDocument calls several other VS services. From a testing point of view, it's simpler to
-                // create a subclass and override this method.
-                var viewType = Guid.Empty;
-                VsShellUtilities.TryOpenDocument(serviceProvider, filePath, viewType, out var _, out var _, out var _);
-            }
-
-            #endregion ICommand methods
+            this.serviceProvider = serviceProvider;
+            this.userSettingsProvider = userSettingsProvider;
+            this.logger = logger;
+            this.win32Window = win32Window;
         }
+
+        #region ICommand methods
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter)
+        {
+            try
+            {
+                userSettingsProvider.EnsureFileExists();
+                OpenDocumentInVs(userSettingsProvider.SettingsFilePath);
+            }
+            catch (Exception ex) when (!ErrorHandler.IsCriticalException(ex))
+            {
+                logger.WriteLine(Strings.ToolsOptions_ErrorOpeningSettingsFile, userSettingsProvider.SettingsFilePath, ex.Message);
+            }
+        }
+
+        protected virtual /* for testing */ void OpenDocumentInVs(string filePath)
+        {
+            // TryOpenDocument calls several other VS services. From a testing point of view, it's simpler to
+            // create a subclass and override this method.
+            var viewType = Guid.Empty;
+            VsShellUtilities.TryOpenDocument(serviceProvider, filePath, viewType, out var _, out var _, out var _);
+
+            CloseOptionsDialog(win32Window);
+        }
+
+        #endregion ICommand methods
+
+        private static void CloseOptionsDialog(IWin32Window win32Window)
+        {
+            // There doesn't appear to be a VS API to close Tools, Options so 
+            // we're using PInvoke.
+            // NB this is equivalent to clicking Cancel i.e. any changed settings will
+            // not be saved.
+            var dialogHwnd = win32Window?.Handle ?? IntPtr.Zero;
+            if (dialogHwnd != IntPtr.Zero)
+            {
+                var topLevelHwnd = GetAncestor(dialogHwnd, GetRootWindow);
+                if (topLevelHwnd != IntPtr.Zero)
+                {
+                    SendMessage(topLevelHwnd, WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+                }
+            }
+        }
+    }
 }
