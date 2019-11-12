34a19b8b43f4f5a248ceb2b5667eb55477c1ea52
duncanp-sonar <duncan.pocklington@sonarsource.com>
2019-11-12 16:29:16 +0000

    Removed unused IQualityProfileProvider, implementations and tests (#1086)
    Fixed up remaining SonarAnalyzerConnectedWorkflowTests
    Fixes #1085

--- src/Integration.Vsix.UnitTests/Delegates/SonarAnalyzerConnectedWorkflowTests.cs
+++ src/Integration.Vsix.UnitTests/Delegates/SonarAnalyzerConnectedWorkflowTests.cs
@@ -20,14 +20,11 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using FluentAssertions;
 using Microsoft.CodeAnalysis;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using SonarAnalyzer.Helpers;
-using SonarLint.VisualStudio.Integration.Persistence;
-using SonarLint.VisualStudio.Integration.Rules;
 using SonarLint.VisualStudio.Integration.Vsix;
 using SonarLint.VisualStudio.Integration.Vsix.Suppression;
 
@@ -67,55 +64,15 @@ namespace SonarLint.VisualStudio.Integration.UnitTests
         #endregion
 
         #region VsixAnalyzerReportDiagnostic Tests
-        [TestMethod]
-        public void VsixAnalyzerReportDiagnostic_WhenRuleIsDisabledInQualityProfile_DoesNotCallReportDiagnostic()
-        {
-            // Arrange
-            var language = Language.CSharp;
-            var boundProject = new BoundSonarQubeProject { ProjectKey = "ProjectKey" };
-            var testSubject = CreateTestSubject(boundProject);
-            var diag = CreateFakeDiagnostic(LanguageNames.CSharp, true, "1");
-            var reportingContextMock = new Mock<IReportingContext>();
-            reportingContextMock.SetupGet(x => x.SyntaxTree).Returns(new Mock<SyntaxTree>().Object);
-            reportingContextMock.SetupGet(x => x.Diagnostic).Returns(diag);
-
-            // Act
-            testSubject.VsixAnalyzerReportDiagnostic(reportingContextMock.Object);
-
-            // Assert
-            reportingContextMock.Verify(x => x.ReportDiagnostic(It.IsAny<Diagnostic>()), Times.Never);
-        }
 
         [TestMethod]
-        public void VsixAnalyzerReportDiagnostic_WhenNoQualityProfileAndRuleIsNotInSonarWay_DoesNotCallReportDiagnostic()
+        public void VsixAnalyzerReportDiagnostic_WhenIssueIsSuppressed_DoesNotCallReportDiagnostic()
         {
             // Arrange
-            var language = Language.CSharp;
-            var boundProject = new BoundSonarQubeProject { ProjectKey = "ProjectKey" };
-            var testSubject = CreateTestSubject(boundProject);
-            var diag = CreateFakeDiagnostic(LanguageNames.CSharp, false, "1");
-            var reportingContextMock = new Mock<IReportingContext>();
-            reportingContextMock.SetupGet(x => x.SyntaxTree).Returns(new Mock<SyntaxTree>().Object);
-            reportingContextMock.SetupGet(x => x.Diagnostic).Returns(diag);
+            var testSubject = CreateTestSubject();
+            var diag = CreateFakeDiagnostic(LanguageNames.CSharp, "1");
+            var reportingContextMock = CreateReportingContext(diag);
 
-            // Act
-            testSubject.VsixAnalyzerReportDiagnostic(reportingContextMock.Object);
-
-            // Assert
-            reportingContextMock.Verify(x => x.ReportDiagnostic(It.IsAny<Diagnostic>()), Times.Never);
-        }
-
-        [TestMethod]
-        public void VsixAnalyzerReportDiagnostic_WhenRuleIsInQualityProfileButIssueSuppressed_DoesNotCallReportDiagnostic()
-        {
-            // Arrange
-            var language = Language.CSharp;
-            var boundProject = new BoundSonarQubeProject { ProjectKey = "ProjectKey" };
-            var testSubject = CreateTestSubject(boundProject);
-            var diag = CreateFakeDiagnostic(LanguageNames.CSharp, true, "1");
-            var reportingContextMock = new Mock<IReportingContext>();
-            reportingContextMock.SetupGet(x => x.SyntaxTree).Returns(new Mock<SyntaxTree>().Object);
-            reportingContextMock.SetupGet(x => x.Diagnostic).Returns(diag);
             this.suppressionHandlerMock.Setup(x => x.ShouldIssueBeReported(It.IsAny<SyntaxTree>(), It.IsAny<Diagnostic>()))
                 .Returns(false);
 
@@ -123,41 +80,17 @@ namespace SonarLint.VisualStudio.Integration.UnitTests
             testSubject.VsixAnalyzerReportDiagnostic(reportingContextMock.Object);
 
             // Assert
-            reportingContextMock.Verify(x => x.ReportDiagnostic(It.IsAny<Diagnostic>()), Times.Never);
+            VerifyDiagnostIsNotReported(reportingContextMock);
         }
 
         [TestMethod]
-        public void VsixAnalyzerReportDiagnostic_WhenNoQualityProfileAndRuleInSonarWayButIssueSuppressed_DoesNotCallReportDiagnostic()
+        public void VsixAnalyzerReportDiagnostic_WhenIssueIsNotSuppressed_CallsReportDiagnostic()
         {
             // Arrange
-            var language = Language.CSharp;
-            var boundProject = new BoundSonarQubeProject { ProjectKey = "ProjectKey" };
-            var testSubject = CreateTestSubject(boundProject);
-            var diag = CreateFakeDiagnostic(LanguageNames.CSharp, false, "1");
-            var reportingContextMock = new Mock<IReportingContext>();
-            reportingContextMock.SetupGet(x => x.SyntaxTree).Returns(new Mock<SyntaxTree>().Object);
-            reportingContextMock.SetupGet(x => x.Diagnostic).Returns(diag);
-            this.suppressionHandlerMock.Setup(x => x.ShouldIssueBeReported(It.IsAny<SyntaxTree>(), It.IsAny<Diagnostic>()))
-                .Returns(false);
+            var testSubject = CreateTestSubject();
+            var diag = CreateFakeDiagnostic(LanguageNames.CSharp, "1");
+            var reportingContextMock = CreateReportingContext(diag);
 
-            // Act
-            testSubject.VsixAnalyzerReportDiagnostic(reportingContextMock.Object);
-
-            // Assert
-            reportingContextMock.Verify(x => x.ReportDiagnostic(It.IsAny<Diagnostic>()), Times.Never);
-        }
-
-        [TestMethod]
-        public void VsixAnalyzerReportDiagnostic_WhenRuleIsInQualityProfileAndIssueNotSuppressed_CallsReportDiagnostic()
-        {
-            // Arrange
-            var language = Language.CSharp;
-            var boundProject = new BoundSonarQubeProject { ProjectKey = "ProjectKey" };
-            var testSubject = CreateTestSubject(boundProject);
-            var diag = CreateFakeDiagnostic(LanguageNames.CSharp, true, "1");
-            var reportingContextMock = new Mock<IReportingContext>();
-            reportingContextMock.SetupGet(x => x.SyntaxTree).Returns(new Mock<SyntaxTree>().Object);
-            reportingContextMock.SetupGet(x => x.Diagnostic).Returns(diag);
             this.suppressionHandlerMock.Setup(x => x.ShouldIssueBeReported(It.IsAny<SyntaxTree>(), diag))
                 .Returns(true);
 
@@ -165,44 +98,35 @@ namespace SonarLint.VisualStudio.Integration.UnitTests
             testSubject.VsixAnalyzerReportDiagnostic(reportingContextMock.Object);
 
             // Assert
-            reportingContextMock.Verify(x => x.ReportDiagnostic(diag), Times.Once);
+            VerifyDiagnostIsReported(reportingContextMock);
         }
 
-        [TestMethod]
-        public void VsixAnalyzerReportDiagnostic_WhenNoQualityProfileAndRuleInSonarWayAndIssueNotSuppressed_CallsReportDiagnostic()
-        {
-            // Arrange
-            var language = Language.CSharp;
-            var boundProject = new BoundSonarQubeProject { ProjectKey = "ProjectKey" };
-            var testSubject = CreateTestSubject(boundProject);
-            var diag = CreateFakeDiagnostic(LanguageNames.CSharp, true, "1");
-            var reportingContextMock = new Mock<IReportingContext>();
-            reportingContextMock.SetupGet(x => x.SyntaxTree).Returns(new Mock<SyntaxTree>().Object);
-            reportingContextMock.SetupGet(x => x.Diagnostic).Returns(diag);
-            this.suppressionHandlerMock.Setup(x => x.ShouldIssueBeReported(It.IsAny<SyntaxTree>(), diag))
-                .Returns(true);
-
-            // Act
-            testSubject.VsixAnalyzerReportDiagnostic(reportingContextMock.Object);
-
-            // Assert
-            reportingContextMock.Verify(x => x.ReportDiagnostic(diag), Times.Once);
-        }
         #endregion
 
-        private SonarAnalyzerConnectedWorkflow CreateTestSubject(BoundSonarQubeProject boundProject = null) =>
+        private SonarAnalyzerConnectedWorkflow CreateTestSubject() =>
             new SonarAnalyzerConnectedWorkflow(new AdhocWorkspace(), suppressionHandlerMock.Object);
 
-        private Diagnostic CreateFakeDiagnostic(string language, bool isInSonarWay = false, string suffix = "")
+        private Diagnostic CreateFakeDiagnostic(string language, string suffix = "")
         {
             var tags = new List<string> { language };
-            if (isInSonarWay)
-            {
-                tags.Add("SonarWay");
-            }
-
             return Diagnostic.Create($"id{suffix}", $"category{suffix}", "message", DiagnosticSeverity.Warning,
                 DiagnosticSeverity.Warning, true, 1, customTags: tags);
         }
+
+        private static Mock<IReportingContext> CreateReportingContext(Diagnostic diagnosticToReturn)
+        {
+            var reportingContextMock = new Mock<IReportingContext>();
+            reportingContextMock.SetupGet(x => x.SyntaxTree).Returns(new Mock<SyntaxTree>().Object);
+            reportingContextMock.SetupGet(x => x.Diagnostic).Returns(diagnosticToReturn);
+
+            return reportingContextMock;
+        }
+
+        private static void VerifyDiagnostIsNotReported(Mock<IReportingContext> reportingContextMock) =>
+            reportingContextMock.Verify(x => x.ReportDiagnostic(It.IsAny<Diagnostic>()), Times.Never);
+
+        private static void VerifyDiagnostIsReported(Mock<IReportingContext> reportingContextMock) =>
+            reportingContextMock.Verify(x => x.ReportDiagnostic(It.IsAny<Diagnostic>()), Times.Once);
     }
 }
+
