f3d171a4f14cc6b8cbaa476278f40eb49d7a270c
duncanp-sonar <duncan.pocklington@sonarsource.com>
2019-12-05 13:15:53 +0100

    Partial refactoring - make BindingProcessImpl and SolutionBindingOper… (#1124)
    * Partial refactoring - make BindingProcessImpl and SolutionBindingOperation unaware of RuleSets
    
    * fixed up unit tests
    * manual test of binding mixed solution works (C# + VB + C++)

--- src/Integration/Resources/Strings.Designer.cs
+++ src/Integration/Resources/Strings.Designer.cs
@@ -124,6 +124,15 @@ namespace SonarLint.VisualStudio.Integration.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Project-level binding is not required for project &apos;{0}&apos;.
+        /// </summary>
+        public static string Bind_Project_NotRequired {
+            get {
+                return ResourceManager.GetString("Bind_Project_NotRequired", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Ruleset binding: all projects need to be updated reference the generated ruleset..
         /// </summary>
         public static string Bind_Ruleset_AllProjectsNeedToBeUpdated {
