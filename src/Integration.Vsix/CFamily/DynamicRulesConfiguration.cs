681453e87ebf6200fc9d6ddb47a85ab6705202f5
duncanp-sonar <duncan.pocklington@sonarsource.com>
2019-12-06 16:30:06 +0000

    Code cleanup

--- src/Integration.Vsix/CFamily/DynamicRulesConfiguration.cs
+++ src/Integration.Vsix/CFamily/DynamicRulesConfiguration.cs
@@ -22,7 +22,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using SonarLint.VisualStudio.Core;
 
 namespace SonarLint.VisualStudio.Integration.Vsix.CFamily
 {
--- src/Integration/Persistence/FileBindingSerializer.cs
+++ src/Integration/Persistence/FileBindingSerializer.cs
@@ -24,7 +24,6 @@ using System.IO;
 using Microsoft.Alm.Authentication;
 using Newtonsoft.Json;
 using SonarLint.VisualStudio.Core.SystemAbstractions;
-using SonarLint.VisualStudio.Integration.Helpers;
 using SonarLint.VisualStudio.Integration.Resources;
 using SonarQube.Client.Helpers;
 
