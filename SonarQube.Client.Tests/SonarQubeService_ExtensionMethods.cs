8ad2d9ff65b21d7520975e69788d7d9044850d13
duncanp-sonar <duncan.pocklington@sonarsource.com>
2020-01-08 10:56:10 +0000

    Updated license headers for 2020 (#37)
    Fixes #36

--- SonarQube.Client.Tests/SonarQubeService_ExtensionMethods.cs
+++ SonarQube.Client.Tests/SonarQubeService_ExtensionMethods.cs
@@ -1,6 +1,6 @@
 ﻿/*
 * SonarQube Client
-* Copyright (C) 2016-2018 SonarSource SA
+* Copyright (C) 2016-2020 SonarSource SA
 * mailto:info AT sonarsource DOT com
 *
 * This program is free software; you can redistribute it and/or
