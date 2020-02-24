9693a85c3559c0aab8742c4f1583b0d0c55fae3c
Rita Gorokhod <rita.gorokhod@sonarsource.com>
2020-02-24 15:48:53 +0100

    Set release 4.19

--- src/AssemblyInfo.Shared.cs
+++ src/AssemblyInfo.Shared.cs
@@ -22,9 +22,9 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Note: keep the version numbers in sync with the VSIX (source.extension.manifest)
-[assembly: AssemblyVersion("4.18.0")]
-[assembly: AssemblyFileVersion("4.18.0.0")] // This should exactly match the VSIX version
-[assembly: AssemblyInformationalVersion("Version:4.18.0.0 Branch:not-set Sha1:not-set Configuration:not-set")]
+[assembly: AssemblyVersion("4.19.0")]
+[assembly: AssemblyFileVersion("4.19.0.0")] // This should exactly match the VSIX version
+[assembly: AssemblyInformationalVersion("Version:4.19.0.0 Branch:not-set Sha1:not-set Configuration:not-set")]
 [assembly: AssemblyConfiguration("")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyProduct("SonarLint for Visual Studio")]
