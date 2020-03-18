54a0fea847597a97cd9e90155c2f7cdb093f2d9e
duncanp-sonar <duncan.pocklington@sonarsource.com>
2020-03-18 15:31:41 +0000

    Apply issues filtering to the returned issues (#1276)
    * Apply issues filtering to the returned issues
    Relates to #1102
    
    TODO: the IssueFilter still needs to be implemented

---
+++ src/Integration.Vsix/SonarLintDaemon/DaemonIssueAdapter.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using SonarLint.VisualStudio.Core;
+
+namespace SonarLint.VisualStudio.Integration.Vsix
+{
+    internal class DaemonIssueAdapter : IFilterableIssue
+    {
+        private readonly string wholeLineText;
+        private readonly string lineHash;
+
+        public DaemonIssueAdapter(Sonarlint.Issue sonarLintIssue, string wholeLineText, string lineHash)
+        {
+            SonarLintIssue = sonarLintIssue ?? throw new ArgumentNullException(nameof(sonarLintIssue));
+
+            this.wholeLineText = wholeLineText;
+            this.lineHash = lineHash;
+        }
+
+        public Sonarlint.Issue SonarLintIssue { get; }
+
+        string IFilterableIssue.RuleId => SonarLintIssue.RuleKey;
+
+        string IFilterableIssue.FilePath => SonarLintIssue.FilePath;
+
+        string IFilterableIssue.ProjectGuid { get; } // TODO: remove if not needed by the issue matcher
+
+        int? IFilterableIssue.StartLine => SonarLintIssue.StartLine;
+
+        string IFilterableIssue.WholeLineText => wholeLineText;
+
+        string IFilterableIssue.LineHash => lineHash;
+    }
+}
+
+
