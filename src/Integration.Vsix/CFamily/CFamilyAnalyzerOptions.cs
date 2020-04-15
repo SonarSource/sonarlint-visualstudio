a6b970b5bb092fae46e6e89f033dd68e919385e6
Rita Gorokhod <60586879+rita-g-sonarsource@users.noreply.github.com>
2020-04-15 18:01:07 +0200

    Added IAnalyzerOptions interface (#1321)
    Relates to #1295

---
+++ src/Integration.Vsix/CFamily/CFamilyAnalyzerOptions.cs
@@ -0,0 +1,9 @@
+﻿using SonarLint.VisualStudio.Integration.Vsix.Analysis;
+
+namespace SonarLint.VisualStudio.Integration.Vsix.CFamily
+{
+    public class CFamilyAnalyzerOptions : IAnalyzerOptions
+    {
+        public bool RunReproducer { get; set; }
+    }
+}
