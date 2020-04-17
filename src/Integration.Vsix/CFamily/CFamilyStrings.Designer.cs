c67002333fb43291dc7f63fb94c7cafe8e936b09
Rita Gorokhod <rita.gorokhod@sonarsource.com>
2020-04-17 17:32:48 +0200

    Change message

--- src/Integration.Vsix/CFamily/CFamilyStrings.Designer.cs
+++ src/Integration.Vsix/CFamily/CFamilyStrings.Designer.cs
@@ -79,7 +79,7 @@ namespace SonarLint.VisualStudio.Integration.Vsix.CFamily {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Process failed, error code: {0}.
+        ///   Looks up a localized string similar to Process failed. Exit code: {0}.
         /// </summary>
         internal static string ERROR_ProcessRunner_Failed {
             get {
