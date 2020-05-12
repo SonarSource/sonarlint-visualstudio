c56188e23ecf9bca657f651b4a1e0eaea7ac6988
duncanp-sonar <duncan.pocklington@sonarsource.com>
2020-05-12 18:39:23 +0100

    Bump version to v5.3 (#47)

--- SonarQube.Client/Properties/AssemblyInfo.cs
+++ SonarQube.Client/Properties/AssemblyInfo.cs
@@ -23,9 +23,9 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("SonarQube.Client")]
 [assembly: Guid("5e513fc3-bda7-40f2-8b6a-1bb9372acb99")]
-[assembly: AssemblyVersion("5.2.0")]
-[assembly: AssemblyFileVersion("5.2.0.0")]
-[assembly: AssemblyInformationalVersion("Version:5.2.0.0 Branch:not-set Sha1:not-set")]
+[assembly: AssemblyVersion("5.3.0")]
+[assembly: AssemblyFileVersion("5.3.0.0")]
+[assembly: AssemblyInformationalVersion("Version:5.3.0.0 Branch:not-set Sha1:not-set")]
 [assembly: AssemblyConfiguration("")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyProduct("SonarQube Web API Client for .NET")]
