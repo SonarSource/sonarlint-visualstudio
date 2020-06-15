041fad2f19cd5fb36440b8be24d32c3b124053bc
duncanp-sonar <duncan.pocklington@sonarsource.com>
2020-06-15 17:02:51 +0100

    Refactor: removed error list code from TaggerProvider

---
+++ src/Integration.Vsix/ErrorList/ISonarErrorListDataSource.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using Microsoft.VisualStudio.Shell.TableManager;
+
+namespace SonarLint.VisualStudio.Integration.Vsix
+{
+    /// <summary>
+    /// Abstraction over the Error List, consumed by the rest of SLVS
+    /// </summary>
+    internal interface ISonarErrorListDataSource
+    {
+        void RefreshErrorList();
+
+        void AddFactory(ITableEntriesSnapshotFactory factory);
+
+        void RemoveFactory(ITableEntriesSnapshotFactory factory);
+    }
+}
