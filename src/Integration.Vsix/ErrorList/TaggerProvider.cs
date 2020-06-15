e29ba2dd224e5da411192863d98eb42347ac8abb
duncanp-sonar <duncan.pocklington@sonarsource.com>
2020-06-15 10:54:23 +0100

    Moved error list code to a separate subfolder

---
+++ src/Integration.Vsix/ErrorList/TaggerProvider.cs
@@ -0,0 +1,102 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.Shell.TableManager;
+
+namespace SonarLint.VisualStudio.Integration.Vsix
+{
+    partial class TaggerProvider : ITableDataSource, ISinkManagerRegister
+    {
+        private readonly ISet<SinkManager> managers = new HashSet<SinkManager>();
+
+        #region ITableDataSource members
+
+        public string DisplayName => "SonarLint";
+
+        public string Identifier => "SonarLint";
+
+        public string SourceTypeIdentifier => StandardTableDataSources.ErrorTableDataSource;
+
+        // Note: Error List is the only expected subscriber
+        public IDisposable Subscribe(ITableDataSink sink) => new SinkManager(this, sink);
+
+        #endregion
+
+        #region ISinkManagerRegister
+
+        public void AddSinkManager(SinkManager manager)
+        {
+            lock (managers)
+            {
+                managers.Add(manager);
+
+                foreach (var issueTracker in issueTrackers)
+                {
+                    manager.AddFactory(issueTracker.Factory);
+                }
+            }
+        }
+
+        public void RemoveSinkManager(SinkManager manager)
+        {
+            lock (managers)
+            {
+                managers.Remove(manager);
+            }
+        }
+
+        #endregion
+
+        public void UpdateAllSinks()
+        {
+            lock (managers)
+            {
+                foreach (var manager in managers)
+                {
+                    manager.UpdateSink();
+                }
+            }
+        }
+
+        public void AddFactory(SnapshotFactory factory)
+        {
+            lock (managers)
+            {
+                foreach (var manager in managers)
+                {
+                    manager.AddFactory(factory);
+                }
+            }
+        }
+
+        public void RemoveFactory(SnapshotFactory factory)
+        {
+            lock (managers)
+            {
+                foreach (var manager in managers)
+                {
+                    manager.RemoveFactory(factory);
+                }
+            }
+        }
+    }
+}
