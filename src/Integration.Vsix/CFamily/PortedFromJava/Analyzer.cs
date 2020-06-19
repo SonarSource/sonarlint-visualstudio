da4b5095aabe96cfd21707e9dab4b51906ac34b8
Rita Gorokhod <rita.gorokhod@sonarsource.com>
2020-06-19 06:50:28 +0200

    Add comment

--- src/Integration.Vsix/CFamily/PortedFromJava/Analyzer.cs
+++ src/Integration.Vsix/CFamily/PortedFromJava/Analyzer.cs
@@ -60,6 +60,7 @@ namespace SonarLint.VisualStudio.Integration.Vsix.CFamily
         public string File { get; set; } = "";
     }
 
+    [Serializable]
     internal class Response
     {
         public Message[] Messages { get; }
@@ -90,13 +91,14 @@ namespace SonarLint.VisualStudio.Integration.Vsix.CFamily
         }
     }
 
+    [Serializable]
     internal class Message : MessagePart
     {
-
         public string RuleKey { get; }
         public bool PartsMakeFlow { get; }
         public MessagePart[] Parts { get; }
 
+        // we expect the class to be serializable for the ease of testing, and therefore the ctor parameter names must match the property names
         public Message(string ruleKey, string filename, int line, int column, int endLine, int endColumn, string text, bool partsMakeFlow, MessagePart[] parts)
             : base(filename, line, column, endLine, endColumn, text)
         {
