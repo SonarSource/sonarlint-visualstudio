3dc436cc1cc9b3b4efb8cb30be6c8988841ddef6
duncanp-sonar <duncan.pocklington@sonarsource.com>
2020-08-12 17:38:45 +0100

    Added table list selection monitor

---
+++ src/IssueViz.UnitTests/TableControls/VSTableEventProcessorProviderTests.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * SonarQube Client
+ * Copyright (C) 2016-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using FluentAssertions;
+using Microsoft.VisualStudio.Shell.TableControl;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SonarLint.VisualStudio.IssueVisualization.TableControls;
+
+namespace SonarLint.VisualStudio.IssueVisualization.UnitTests.TableControls
+{
+    [TestClass]
+    public class VSTableEventProcessorProviderTests
+    {
+        [TestMethod]
+        public void GetProcessor_NullTableControl_ReturnsNull()
+        {
+            var testSubject = (ITableControlEventProcessorProvider)new VSTableEventProcessorProvider(Mock.Of<IIssueTablesSelectionMonitor>());
+
+            testSubject.GetAssociatedEventProcessor(null)
+                .Should().BeNull();
+        }
+
+        [TestMethod]
+        public void GetProcessor_ReturnsNewProcessorInstanceForEachCall()
+        {
+            var monitorMock = new Mock<IIssueTablesSelectionMonitor>();
+            var testSubject = (ITableControlEventProcessorProvider)new VSTableEventProcessorProvider(monitorMock.Object);
+
+            // First call
+            var processor1 = testSubject.GetAssociatedEventProcessor(Mock.Of<IWpfTableControl>());
+            processor1.Should().NotBeNull();
+            monitorMock.Verify(x => x.AddEventSource((IIssueTableEventSource)processor1), Times.Once);
+
+            var processor2 = testSubject.GetAssociatedEventProcessor(Mock.Of<IWpfTableControl>());
+            processor2.Should().NotBeNull();
+            monitorMock.Verify(x => x.AddEventSource((IIssueTableEventSource)processor2), Times.Once);
+
+            processor1.Should().NotBeSameAs(processor2);
+        }
+    }
+}
