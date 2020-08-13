aec07c29afa639ffda105f183137a4ff28f6aa06
Rita Gorokhod <rita.gorokhod@sonarsource.com>
2020-08-13 23:52:06 +0200

    Add command to open the tool window

---
+++ src/IssueViz/Commands/IssueVisualizationToolWindowCommand.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel.Design;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using SonarLint.VisualStudio.IssueVisualization.IssueVisualizationControl;
+using Task = System.Threading.Tasks.Task;
+
+namespace SonarLint.VisualStudio.IssueVisualization.Commands
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class IssueVisualizationToolWindowCommand
+    {
+        public static readonly Guid CommandSet = new Guid("FDEF405A-28C2-4AFD-A37B-49EF2B0D142E");
+        public const int CommandId = 0x0100;
+
+        public static IssueVisualizationToolWindowCommand Instance { get; private set; }
+
+        private readonly AsyncPackage package;
+
+        private IssueVisualizationToolWindowCommand(AsyncPackage package, IMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            var commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as IMenuCommandService;
+            Instance = new IssueVisualizationToolWindowCommand(package, commandService);
+        }
+
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var window = package.FindToolWindow(typeof(IssueVisualizationToolWindow), 0, true);
+
+            if (window?.Frame == null)
+            {
+                throw new NotSupportedException("Cannot create tool window");
+            }
+
+            var windowFrame = (IVsWindowFrame)window.Frame;
+            Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure(windowFrame.Show());
+        }
+    }
+}
---
+++ src/IssueViz/IssueVisualizationControl/IssueVisualizationToolWindow.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.Shell;
+using SonarLint.VisualStudio.IssueVisualization.Selection;
+
+namespace SonarLint.VisualStudio.IssueVisualization.IssueVisualizationControl
+{
+    [ExcludeFromCodeCoverage]
+    [Guid("bb3677d1-3b5c-45a3-8a3a-897108c3ba28")]
+    public class IssueVisualizationToolWindow : ToolWindowPane
+    {
+        public IssueVisualizationToolWindow(IServiceProvider serviceProvider) : base(null)
+        {
+            Caption = "SonarLint Issue Visualization";
+
+            var componentModel = serviceProvider.GetService(typeof(SComponentModel)) as IComponentModel;
+            var selectionService = componentModel.GetService<IAnalysisIssueSelectionService>();
+
+            //var viewModel = new IssueVisualizationViewModel(selectionService);
+            Content = new IssueVisualizationControl();
+        }
+    }
+}
