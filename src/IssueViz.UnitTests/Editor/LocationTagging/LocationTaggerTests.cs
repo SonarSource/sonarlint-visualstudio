c05547b1b882c55b09e1fa0b17d582111bddef56
duncanp-sonar <duncan.pocklington@sonarsource.com>
2020-09-02 18:43:51 +0100

    Added location tagger

---
+++ src/IssueViz.UnitTests/Editor/LocationTagging/LocationTaggerTests.cs
@@ -0,0 +1,188 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.Text;
+using Moq;
+using SonarLint.VisualStudio.Integration;
+using SonarLint.VisualStudio.IssueVisualization.Editor;
+using SonarLint.VisualStudio.IssueVisualization.Editor.LocationTagging;
+using SonarLint.VisualStudio.IssueVisualization.Models;
+
+namespace SonarLint.VisualStudio.IssueVisualization.UnitTests.Editor.LocationTagging
+{
+    [TestClass]
+    public class LocationTaggerTests
+    {
+        private const string ValidBufferDocName = "doc.txt";
+        private readonly ITextBuffer ValidBuffer = CreateBufferMock(ValidBufferDocName).Object;
+        private readonly IIssueLocationStore ValidStore = Mock.Of<IIssueLocationStore>();
+        private readonly IIssueSpanCalculator ValidSpanCalculator = Mock.Of<IIssueSpanCalculator>();
+        private readonly ILogger ValidLogger = Mock.Of<ILogger>();
+
+        [TestMethod]
+        public void Ctor_SubscribesToEvents()
+        {
+            var storeMock = new Mock<IIssueLocationStore>();
+            storeMock.SetupAdd(x => x.IssuesChanged += (sender, args) => { });
+
+            var bufferMock = CreateBufferMock(ValidBufferDocName);
+            bufferMock.SetupAdd(x => x.ChangedLowPriority += (sender, args) => { });
+
+            var testSubject = new LocationTagger(bufferMock.Object, storeMock.Object, ValidSpanCalculator, ValidLogger);
+
+            storeMock.VerifyAdd(x => x.IssuesChanged += It.IsAny<EventHandler<IssuesChangedEventArgs>>(), Times.Once);
+            bufferMock.VerifyAdd(x => x.ChangedLowPriority += It.IsAny<EventHandler<TextContentChangedEventArgs>>(), Times.Once);
+        }
+
+        [TestMethod]
+        public void Dispose_UnsubscribesFromEvents()
+        {
+            var storeMock = new Mock<IIssueLocationStore>();
+            storeMock.SetupAdd(x => x.IssuesChanged += (sender, args) => { });
+
+            var bufferMock = CreateBufferMock(ValidBufferDocName);
+            bufferMock.SetupAdd(x => x.ChangedLowPriority += (sender, args) => { });
+
+            var testSubject = new LocationTagger(bufferMock.Object, storeMock.Object, ValidSpanCalculator, ValidLogger);
+
+            testSubject.Dispose();
+
+            storeMock.VerifyRemove(x => x.IssuesChanged -= It.IsAny<EventHandler<IssuesChangedEventArgs>>(), Times.Once);
+            bufferMock.VerifyRemove(x => x.ChangedLowPriority -= It.IsAny<EventHandler<TextContentChangedEventArgs>>(), Times.Once);
+        }
+
+        [TestMethod]
+        public void Ctor_SetsFilePath()
+        {
+            var testSubject = new LocationTagger(ValidBuffer, ValidStore, ValidSpanCalculator, ValidLogger);
+            testSubject.FilePath.Should().Be(ValidBufferDocName);
+        }
+
+        [TestMethod]
+        public void Ctor_UpdatesTags()
+        {
+            var locVizWithSpan = CreateLocViz(new SnapshotSpan());
+
+            var storeMock = new Mock<IIssueLocationStore>();
+            storeMock.Setup(x => x.GetLocations(ValidBufferDocName)).Returns(new[] { locVizWithSpan });
+
+            var testSubject = new LocationTagger(ValidBuffer, storeMock.Object, ValidSpanCalculator, ValidLogger);
+
+            testSubject.locationTagSpans.Should().NotBeNull();
+            testSubject.locationTagSpans.Count.Should().Be(1);
+            testSubject.locationTagSpans[0].Tag.Location.Should().BeSameAs(locVizWithSpan);
+        }
+
+        [TestMethod]
+        public void OnIssuesChanged_NotTheTrackedFile_TagsChangedNotRaised()
+        {
+            var storeMock = new Mock<IIssueLocationStore>();
+            var testSubject = new LocationTagger(ValidBuffer, storeMock.Object, ValidSpanCalculator, ValidLogger);
+
+            SnapshotSpanEventArgs actualTagsChangedArgs = null;
+            testSubject.TagsChanged += (senders, args) => actualTagsChangedArgs = args;
+
+            storeMock.Raise(x => x.IssuesChanged += null, new IssuesChangedEventArgs(new string[] { "not the file being tracked.txt" }));
+
+            actualTagsChangedArgs.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void OnIssuesChanged_IsTheTrackedFile_TagsChangedIsRaised()
+        {
+            var storeMock = new Mock<IIssueLocationStore>();
+            var testSubject = new LocationTagger(ValidBuffer, storeMock.Object, ValidSpanCalculator, ValidLogger);
+
+            SnapshotSpanEventArgs actualTagsChangedArgs = null;
+            testSubject.TagsChanged += (senders, args) => actualTagsChangedArgs = args;
+
+            storeMock.Raise(x => x.IssuesChanged += null, new IssuesChangedEventArgs(new string[] { ValidBufferDocName }));
+
+            actualTagsChangedArgs.Should().NotBeNull();
+            // Changed span should be the whole snapshot
+            actualTagsChangedArgs.Span.Start.Position.Should().Be(0);
+            actualTagsChangedArgs.Span.End.Position.Should().Be(ValidBuffer.CurrentSnapshot.Length);
+        }
+
+        [TestMethod]
+        public void GetTags_NoTags_ReturnsEmpty()
+        {
+            var testSubject = new LocationTagger(ValidBuffer, ValidStore, ValidSpanCalculator, ValidLogger);
+
+            var validSpan = new Span(0, ValidBuffer.CurrentSnapshot.Length);
+            var inputSpans = new NormalizedSnapshotSpanCollection(ValidBuffer.CurrentSnapshot, validSpan);
+
+            testSubject.GetTags(inputSpans)
+                .Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void GetTags_EmptyInputSpan_ReturnsEmpty()
+        {
+            var testSubject = new LocationTagger(ValidBuffer, ValidStore, ValidSpanCalculator, ValidLogger);
+
+            var inputSpans = new NormalizedSnapshotSpanCollection();
+
+            testSubject.GetTags(inputSpans)
+                .Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void GetTags_NoOverlappingSpans_ReturnsEmpty()
+        {
+            //TOOD
+        }
+
+        [TestMethod]
+        public void GetTags_ReturnsMatchingTags()
+        {
+            //TOOD
+        }
+
+        private static Mock<ITextBuffer> CreateBufferMock(string filePath)
+        {
+            var snapshotMock = new Mock<ITextSnapshot>();
+            snapshotMock.Setup(x => x.Length).Returns(999);
+
+            var bufferMock = new Mock<ITextBuffer>();
+            bufferMock.Setup(x => x.CurrentSnapshot).Returns(snapshotMock.Object);
+
+            var properties = new Microsoft.VisualStudio.Utilities.PropertyCollection();
+            bufferMock.Setup(x => x.Properties).Returns(properties);
+
+            var docMock = new Mock<ITextDocument>();
+            docMock.Setup(x => x.FilePath).Returns(filePath);
+            properties[typeof(ITextDocument)] = docMock.Object;
+
+            return bufferMock;
+        }
+
+        private static IAnalysisIssueLocationVisualization CreateLocViz(SnapshotSpan? span)
+        {
+            var locVizMock = new Mock<IAnalysisIssueLocationVisualization>();
+            locVizMock.SetupProperty(x => x.Span);
+            locVizMock.Object.Span = span;
+            return locVizMock.Object;
+        }
+    }
+}
---
+++ src/IssueViz/Editor/LocationTagging/LocationTagger.cs
@@ -0,0 +1,188 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Tagging;
+using SonarLint.VisualStudio.Integration;
+using SonarLint.VisualStudio.IssueVisualization.Models;
+
+namespace SonarLint.VisualStudio.IssueVisualization.Editor.LocationTagging
+{
+    // TODO: track document renames: https://github.com/SonarSource/sonarlint-visualstudio/issues/1662
+
+    // Buffer tagger for Sonar issue locations
+    // * adds location tags for primary and secondary locations for a file
+    // * tracks buffer edits, re-calculate spans, and notifies the issues location store about the changes
+    internal class LocationTagger : ITagger<IIssueLocationTag>, IDisposable
+    {
+        private readonly ITextBuffer buffer;
+        private readonly IIssueLocationStore locationService;
+        private readonly IIssueSpanCalculator spanCalculator;
+        private readonly ILogger logger;
+
+        internal /* for testing */ IList<ITagSpan<IIssueLocationTag>> locationTagSpans;
+
+        public LocationTagger(ITextBuffer buffer, IIssueLocationStore locationService, IIssueSpanCalculator spanCalculator, ILogger logger)
+        {
+            this.buffer = buffer;
+            this.locationService = locationService;
+            this.spanCalculator = spanCalculator;
+            this.logger = logger;
+
+            FilePath = GetFileName(buffer);
+            UpdateTags();
+
+            locationService.IssuesChanged += OnIssuesChanged;
+            buffer.ChangedLowPriority += SafeOnBufferChange;
+        }
+
+        private void OnIssuesChanged(object sender, IssuesChangedEventArgs e)
+        {
+            if (!e.AnalyzedFiles.Contains(FilePath, StringComparer.OrdinalIgnoreCase))
+            {
+                return; // no changes in this file
+            }
+
+            UpdateTags();
+        }
+
+        private void UpdateTags()
+        {
+            var textSnapshot = buffer.CurrentSnapshot;
+
+            // Get the new locations and calculate the spans if necessary
+            var locations = locationService.GetLocations(FilePath);
+            EnsureSpansExist(locations, textSnapshot);
+
+            locationTagSpans = locations.Select(CreateTagSpan).ToList();
+
+            // Notify the editor that our set of tags has changed
+            var wholeSpan = new SnapshotSpan(textSnapshot, 0, textSnapshot.Length);
+            TagsChanged?.Invoke(this, new SnapshotSpanEventArgs(wholeSpan));
+        }
+
+        private void EnsureSpansExist(IEnumerable<IAnalysisIssueLocationVisualization> locVizs, ITextSnapshot currentSnapshot)
+        {
+            // All spans for issues in the primary file should have been calculated already (whether primary or secondary).
+            // However, there could be secondary locations relating to issues in other files that have not been calculated.
+            foreach (var locViz in locVizs)
+            {
+                if (!locViz.Span.HasValue)
+                {
+                    locViz.Span = spanCalculator.CalculateSpan(locViz.Location, currentSnapshot);
+                }
+            }
+        }
+
+        private static ITagSpan<IIssueLocationTag> CreateTagSpan(IAnalysisIssueLocationVisualization locViz) =>
+            new TagSpan<IIssueLocationTag>(locViz.Span.Value, new IssueLocationTag(locViz));
+
+        private string GetFileName(ITextBuffer textBuffer)
+        {
+            ITextDocument newTextDocument = null;
+            textBuffer.Properties?.TryGetProperty(typeof(ITextDocument), out newTextDocument);
+            return newTextDocument?.FilePath;
+        }
+
+        #region ITagger implementation
+
+        public event EventHandler<SnapshotSpanEventArgs> TagsChanged;
+
+        public IEnumerable<ITagSpan<IIssueLocationTag>> GetTags(NormalizedSnapshotSpanCollection spans)
+        {
+            if (spans.Count == 0 || locationTagSpans.Count == 0) { yield break; }
+
+            // If the requested snapshot isn't the same as our tags are on, translate our spans to the expected snapshot 
+            if (spans[0].Snapshot != locationTagSpans[0].Span.Snapshot)
+            {
+                TranslateTagSpans(spans[0].Snapshot);
+            }
+
+            // Find any tags in that overlap with that range
+            foreach (var tagSpan in locationTagSpans)
+            {
+                if (OverlapsExists(tagSpan.Span, spans))
+                {
+                    yield return tagSpan;
+                }
+            }
+        }
+
+        private void TranslateTagSpans(ITextSnapshot newSnapshot)
+        {
+            var translatedTagSpans = new List<ITagSpan<IIssueLocationTag>>();
+
+            foreach (var old in locationTagSpans)
+            {
+                var newSpan = old.Span.TranslateTo(newSnapshot, SpanTrackingMode.EdgeExclusive);
+                old.Tag.Location.Span = newSpan; // update the span stored in the location visualization 
+
+                translatedTagSpans.Add(new TagSpan<IIssueLocationTag>(newSpan, old.Tag));
+            }
+
+            locationTagSpans = translatedTagSpans;
+        }
+
+        private static bool OverlapsExists(Span span, NormalizedSnapshotSpanCollection spans) =>
+            spans.Any(x => x.OverlapsWith(span));
+
+        #endregion ITagger implementation
+
+        #region Buffer change tracking
+
+        internal /* for testing */ string FilePath { get; }
+
+        private void SafeOnBufferChange(object sender, TextContentChangedEventArgs e)
+        {
+            // Handles callback from VS. Suppress non-critical errors to prevent them
+            // propagating to VS, which would display a dialogue and disable the extension.
+            try
+            {
+                // The text buffer has been edited (i.e.text added, deleted or modified).
+                // The spans we have stored for location relate to the previous text buffer and
+                // are no longer valid, so we need to translate them to the equivalent spans
+                // in the new text buffer.
+                var newTextSnapshot = e.After;
+
+                TranslateTagSpans(newTextSnapshot);
+                locationService.Refresh(new string[] { FilePath });
+
+                var wholeSpan = new SnapshotSpan(newTextSnapshot, 0, newTextSnapshot.Length);
+                TagsChanged?.Invoke(this, new SnapshotSpanEventArgs(wholeSpan));
+            }
+            catch (Exception ex) when (!ErrorHandler.IsCriticalException(ex))
+            {
+                logger.WriteLine(Resources.ERR_HandlingBufferChange, ex.Message);
+            }
+        }
+
+        #endregion
+
+        public void Dispose()
+        {
+            locationService.IssuesChanged -= OnIssuesChanged;
+            buffer.ChangedLowPriority -= SafeOnBufferChange;
+        }
+    }
+}
