70d3f3f874e3e24f062c64844210c0a8493df287
duncanp-sonar <duncan.pocklington@sonarsource.com>
2020-09-07 10:55:24 +0100

    Added filtering tagger and error tagger (#1680)
    * Added error tagger provider and tests
    * Added filterable tagger and tests
    * Refactor common tagger provider tests into abstract base class

---
+++ src/IssueViz/Editor/ErrorTagging/ErrorTaggerProvider.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+using SonarLint.VisualStudio.IssueVisualization.Editor.LocationTagging;
+
+namespace SonarLint.VisualStudio.IssueVisualization.Editor.ErrorTagging
+{
+    [Export(typeof(ITaggerProvider))]
+    [ContentType("code")]
+    [TagType(typeof(IErrorTag))]
+    internal class ErrorTaggerProvider : ITaggerProvider
+    {
+        private readonly IBufferTagAggregatorFactoryService bufferTagAggregatorFactoryService;
+
+        [ImportingConstructor]
+        public ErrorTaggerProvider(IBufferTagAggregatorFactoryService bufferTagAggregatorFactoryService)
+        {
+            this.bufferTagAggregatorFactoryService = bufferTagAggregatorFactoryService;
+        }
+
+        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
+        {
+            if (buffer == null)
+            {
+                throw new ArgumentNullException(nameof(buffer));
+            }
+
+            var tagger = buffer.Properties.GetOrCreateSingletonProperty(typeof(ErrorTagger), () => CreateForBuffer(buffer));
+            return tagger as ITagger<T>;
+        }
+
+        private ErrorTagger CreateForBuffer(ITextBuffer buffer)
+        {
+            var aggregator = bufferTagAggregatorFactoryService.CreateTagAggregator<IIssueLocationTag>(buffer);
+            return new ErrorTagger(aggregator, buffer);
+        }
+    }
+}
