0f2c977e94b0f3f2306cca51e0456512f4cb1ac0
Rita Gorokhod <60586879+rita-g-sonarsource@users.noreply.github.com>
2020-10-15 16:45:12 +0200

    Add initial tool window (#1805)

---
+++ src/IssueViz/Hotspots/HotspotsControl/HotspotsToolWindow.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace SonarLint.VisualStudio.IssueVisualization.Hotspots.HotspotsControl
+{
+    [Guid("4BCD4392-DBCF-4AA2-9852-01129D229CD8")]
+    public class HotspotsToolWindow : ToolWindowPane
+    {
+        public HotspotsToolWindow()
+        {
+            Caption = Resources.HotspotsToolWindowCaption;
+
+            Content = new HotspotsControl(new HotspotsViewModel());
+        }
+    }
+}
