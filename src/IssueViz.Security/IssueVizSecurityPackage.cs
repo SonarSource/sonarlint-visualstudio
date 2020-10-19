b69368f31409c79777ece208e8c6830bad90deab
Rita Gorokhod <60586879+rita-g-sonarsource@users.noreply.github.com>
2020-10-19 20:09:51 +0200

    Add versioned Microsoft.VisualStudio.Shell (#1814)

--- src/IssueViz.Security/IssueVizSecurityPackage.cs
+++ src/IssueViz.Security/IssueVizSecurityPackage.cs
@@ -18,6 +18,8 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+extern alias versionSpecificShell;
+
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
