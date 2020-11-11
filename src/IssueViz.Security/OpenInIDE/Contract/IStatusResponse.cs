1fd31eda304fa053523657d492d84252a3e60e56
duncanp-sonar <duncan.pocklington@sonarsource.com>
2020-11-11 17:52:37 +0000

    Added http handler for the /status request

--- src/IssueViz.Security/OpenInIDE/Contract/IStatusResponse.cs
+++ src/IssueViz.Security/OpenInIDE/Contract/IStatusResponse.cs
@@ -20,27 +20,10 @@
 
 namespace SonarLint.VisualStudio.IssueVisualization.Security.OpenInIDE.Contract
 {
-    // TODO:
-    // IdeName: should we include the VS version and/or edition?
-    //  e.g. "Visual Studio", "Visual Studio 2015", "Visual Studio 2015 Community"
 
-    // InstanceName: should this be a standalone value, or should the server concatenate it
-    //  with the IdeName?
-
-    // * Icon - format? size in pixels?
 
     public interface IStatusResponse
     {
-        // TODO - up to the server team to decide if this field is required.
-        /// <summary>
-        /// Unique identifier for this instance of the IDE.
-        /// </summary>
-        /// <remarks>Allows the server to distinguish between responses from different IDEs.
-        /// The format is unimportant, but the id must be unique across:
-        /// * all instances and versions of the VS, and
-        /// * all other instances of SonarLint in other IDEs.</remarks>
-        string IdeInstanceId { get; }
-
         /// <summary>
         /// Short user-friendly name for the current IDE flavour.
         /// </summary
@@ -55,15 +38,9 @@ namespace SonarLint.VisualStudio.IssueVisualization.Security.OpenInIDE.Contract
         /// from other running instances.
         /// </summary
         /// <remarks>
-        /// This string should be the same for all instances of the current IDE flavour.
+        /// This string should be unique to the current VS instance
         /// Will be displayed by the server in the UI e.g. in a menu caption / tooltip
         /// </remarks>
-        string InstanceName { get; }
-
-        // TODO - byte array or encoded string?
-        /// <summary>
-        /// The icon to display for this IDE type
-        /// </summary>
-        byte[] Icon { get; }
+        string Description { get; }
     }
 }
