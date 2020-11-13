9160ee95722d69d4b7620f589818dabbdac93513
Rita Gorokhod <rita.gorokhod@sonarsource.com>
2020-11-13 20:13:47 +0100

    Implementation

--- src/IssueViz.Security/OpenInIDE/Api/StatusRequestHandler.cs
+++ src/IssueViz.Security/OpenInIDE/Api/StatusRequestHandler.cs
@@ -18,19 +18,38 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+using System;
 using System.ComponentModel.Composition;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 using SonarLint.VisualStudio.IssueVisualization.Security.OpenInIDE.Contract;
+using Task = System.Threading.Tasks.Task;
 
 namespace SonarLint.VisualStudio.IssueVisualization.Security.OpenInIDE.Api
 {
     [Export(typeof(IStatusRequestHandler))]
     internal class StatusRequestHandler : IStatusRequestHandler
     {
+        private readonly IVsShell vsShell;
+        private readonly IVsSolution vsSolution;
+
+        [ImportingConstructor]
+        public StatusRequestHandler([Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider)
+        {
+            vsShell = serviceProvider.GetService(typeof(SVsShell)) as IVsShell;
+            vsSolution = serviceProvider.GetService(typeof(SVsSolution)) as IVsSolution;
+        }
+
         Task<IStatusResponse> IStatusRequestHandler.GetStatusAsync()
         {
-            // TODO: fetch the real data
-            return Task.FromResult<IStatusResponse>(new StatusResponse("Visual Studio", "{unknown}"));
+            vsShell.GetProperty((int) __VSSPROPID5.VSSPROPID_AppBrandName, out var ideName);
+            ideName = ideName ?? "Microsoft Visual Studio";
+
+            vsSolution.GetProperty((int) __VSPROPID.VSPROPID_SolutionBaseName, out var ideInstance);
+            ideInstance = ideInstance ?? "";
+
+            return Task.FromResult<IStatusResponse>(new StatusResponse(ideName.ToString(), ideInstance.ToString()));
         }
     }
 }
