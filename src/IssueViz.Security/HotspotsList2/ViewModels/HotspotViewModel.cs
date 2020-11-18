ad6f2e716967dab099512c316a99774f8c8da8ba
Rita Gorokhod <rita.gorokhod@sonarsource.com>
2020-11-18 10:47:46 +0100

    Initial control

---
+++ src/IssueViz.Security/HotspotsList2/ViewModels/HotspotViewModel.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using SonarLint.VisualStudio.IssueVisualization.Models;
+
+namespace SonarLint.VisualStudio.IssueVisualization.Security.HotspotsList2.ViewModels
+{
+    internal interface IHotspotViewModel
+    {
+        IAnalysisIssueVisualization Hotspot { get; }
+        int Line { get; }
+        int Column { get; }
+    }
+
+    internal class HotspotViewModel : IHotspotViewModel
+    {
+        public HotspotViewModel(IAnalysisIssueVisualization hotspot)
+        {
+            Hotspot = hotspot;
+        }
+
+        public IAnalysisIssueVisualization Hotspot { get; }
+
+        // todo: take from span
+        public int Line => Hotspot.Issue.StartLine;
+
+        // todo: take from span
+        public int Column => Hotspot.Issue.StartLineOffset;
+    }
+}
---
+++ src/IssueViz.Security/HotspotsList2/ViewModels/HotspotsControlViewModel.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.ObjectModel;
+
+namespace SonarLint.VisualStudio.IssueVisualization.Security.HotspotsList2.ViewModels
+{
+    internal interface IHotspotsControlViewModel
+    {
+        ObservableCollection<IHotspotViewModel> Hotspots { get; }
+    }
+
+    internal class HotspotsControlViewModel : IHotspotsControlViewModel
+    {
+        public ObservableCollection<IHotspotViewModel> Hotspots { get; } = new ObservableCollection<IHotspotViewModel>();
+    }
+}
