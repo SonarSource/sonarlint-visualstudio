e46ab33fade5ae7522dae067574595cd347990c7
Rita Gorokhod <60586879+rita-g-sonarsource@users.noreply.github.com>
2020-12-08 16:51:34 +0100

    Fetch taint vulnerabilities (#68)

---
+++ SonarQube.Client/Api/V7_20/GetTaintVulnerabilitiesRequest.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * SonarQube Client
+ * Copyright (C) 2016-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using SonarQube.Client.Logging;
+using SonarQube.Client.Models;
+
+namespace SonarQube.Client.Api.V7_20
+{
+    public class GetTaintVulnerabilitiesRequest : IGetTaintVulnerabilitiesRequest
+    {
+        private readonly GetIssuesRequest getIssuesRequest = new GetIssuesRequest();
+
+        public string ProjectKey { get; set; }
+        public ILogger Logger { get; set; }
+
+        public async Task<SonarQubeIssue[]> InvokeAsync(HttpClient httpClient, CancellationToken token)
+        {
+            getIssuesRequest.Logger = Logger;
+            getIssuesRequest.ProjectKey = ProjectKey;
+            getIssuesRequest.Statuses = "OPEN,CONFIRMED,REOPENED";
+            getIssuesRequest.Types = "VULNERABILITY";
+
+            var vulnerabilities = await getIssuesRequest.InvokeAsync(httpClient, token);
+            WarnForApiLimit(vulnerabilities, getIssuesRequest);
+
+            var taintVulnerabilities = vulnerabilities
+                .Where(x => x.RuleId.Contains("security"))
+                .ToArray();
+
+            return taintVulnerabilities;
+        }
+
+        private void WarnForApiLimit(SonarQubeIssue[] issues, GetIssuesRequest request)
+        {
+            if (issues.Length == request.ItemsLimit)
+            {
+                Logger.Warning($"Sonar web API response limit reached ({request.ItemsLimit} items). Some vulnerabilities might not be shown.");
+            }
+        }
+    }
+}
