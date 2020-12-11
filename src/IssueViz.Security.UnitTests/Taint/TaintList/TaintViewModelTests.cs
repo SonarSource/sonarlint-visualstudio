d61e5b17855fbf417b6795b11661794353676825
duncanp-sonar <duncan.pocklington@sonarsource.com>
2020-12-11 14:35:26 +0000

    Added initial taint view model implementations
    Relates to #1985

---
+++ src/IssueViz.Security.UnitTests/Taint/TaintList/TaintViewModelTests.cs
@@ -0,0 +1,176 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.ComponentModel;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SonarLint.VisualStudio.Core.Analysis;
+using SonarLint.VisualStudio.IssueVisualization.Models;
+using SonarLint.VisualStudio.IssueVisualization.Security.Taint.TaintList.ViewModels;
+
+namespace SonarLint.VisualStudio.IssueVisualization.Security.UnitTests.Taint.TaintList
+{
+    [TestClass]
+    public class CommonIssueViewModelTests
+    {
+        [TestMethod]
+        public void Ctor_RegisterToIssueVizPropertyChangedEvent()
+        {
+            var issueViz = new Mock<IAnalysisIssueVisualization>();
+            issueViz.SetupAdd(x => x.PropertyChanged += null);
+
+            CreateTestSubject(issueViz.Object);
+
+            issueViz.VerifyAdd(x => x.PropertyChanged += It.IsAny<PropertyChangedEventHandler>(), Times.Once);
+        }
+
+        [TestMethod]
+        public void Dispose_UnregisterFromIssueVizPropertyChangedEvent()
+        {
+            var issueViz = new Mock<IAnalysisIssueVisualization>();
+            issueViz.SetupRemove(x => x.PropertyChanged -= null);
+
+            var testSubject = CreateTestSubject(issueViz.Object);
+            testSubject.Dispose();
+
+            issueViz.VerifyRemove(x => x.PropertyChanged -= It.IsAny<PropertyChangedEventHandler>(), Times.Once);
+        }
+
+        [TestMethod]
+        public void IssueVizPropertyChanged_UnknownProperty_NoPropertyChangedEvent()
+        {
+            var eventHandler = new Mock<PropertyChangedEventHandler>();
+            var issueViz = new Mock<IAnalysisIssueVisualization>();
+
+            var testSubject = CreateTestSubject(issueViz.Object);
+            testSubject.PropertyChanged += eventHandler.Object;
+
+            eventHandler.VerifyNoOtherCalls();
+
+            issueViz.Raise(x => x.PropertyChanged += null, new PropertyChangedEventArgs("some dummy property"));
+
+            eventHandler.VerifyNoOtherCalls();
+        }
+
+        [TestMethod]
+        public void IssueVizPropertyChanged_SpanProperty_RaisesPropertyChangedForLineAndColumn()
+        {
+            var eventHandler = new Mock<PropertyChangedEventHandler>();
+            var issueViz = new Mock<IAnalysisIssueVisualization>();
+
+            var testSubject = CreateTestSubject(issueViz.Object);
+            testSubject.PropertyChanged += eventHandler.Object;
+
+            eventHandler.VerifyNoOtherCalls();
+
+            issueViz.Raise(x => x.PropertyChanged += null, new PropertyChangedEventArgs(nameof(IAnalysisIssueVisualization.Span)));
+
+            eventHandler.Verify(x => x(testSubject,
+                    It.Is((PropertyChangedEventArgs args) => args.PropertyName == nameof(ITaintIssueViewModel.Line))),
+                Times.Once);
+
+            eventHandler.Verify(x => x(testSubject,
+                    It.Is((PropertyChangedEventArgs args) => args.PropertyName == nameof(ITaintIssueViewModel.Column))),
+                Times.Once);
+
+            eventHandler.VerifyNoOtherCalls();
+        }
+
+        [TestMethod]
+        public void Line_ReturnsDisplayPosition()
+        {
+            var issueViz = Mock.Of<IAnalysisIssueVisualization>();
+
+            var positionCalculatorMock = new Mock<IIssueVizDisplayPositionCalculator>();
+            positionCalculatorMock.Setup(x => x.GetLine(issueViz)).Returns(123);
+
+            // Act
+            var testSubject = CreateTestSubject(issueViz, positionCalculator: positionCalculatorMock.Object);
+
+            testSubject.Line.Should().Be(123);
+            positionCalculatorMock.VerifyAll();
+        }
+
+        [TestMethod]
+        public void Column_ReturnsDisplayPosition()
+        {
+            var issueViz = Mock.Of<IAnalysisIssueVisualization>();
+
+            var positionCalculatorMock = new Mock<IIssueVizDisplayPositionCalculator>();
+            positionCalculatorMock.Setup(x => x.GetColumn(issueViz)).Returns(999);
+
+            // Act
+            var testSubject = CreateTestSubject(issueViz, positionCalculator: positionCalculatorMock.Object);
+
+            testSubject.Column.Should().Be(999);
+        }
+
+        [TestMethod]
+        public void DisplayPath_IssueVizHasNoFilePath_ReturnsServerPath()
+        {
+            var issue = new Mock<IAnalysisIssue>();
+            issue.Setup(x => x.FilePath).Returns("\\some\\server\\path.cs");
+
+            var issueViz = new Mock<IAnalysisIssueVisualization>();
+            issueViz.Setup(x => x.CurrentFilePath).Returns((string)null);
+            issueViz.Setup(x => x.Issue).Returns(issue.Object);
+
+            var testSubject = CreateTestSubject(issueViz.Object);
+            testSubject.DisplayPath.Should().Be("path.cs");
+        }
+
+        [TestMethod]
+        public void DisplayPath_IssueVizHasFilePath_ReturnsFilePath()
+        {
+            var issueViz = new Mock<IAnalysisIssueVisualization>();
+            issueViz.Setup(x => x.CurrentFilePath).Returns("c:\\some\\local\\path.cs");
+
+            var testSubject = CreateTestSubject(issueViz.Object);
+            testSubject.DisplayPath.Should().Be("path.cs");
+        }
+
+        [TestMethod]
+        public void IssueVizPropertyChanged_CurrentFilePathProperty_RaisesPropertyChangedForDisplayPath()
+        {
+            var eventHandler = new Mock<PropertyChangedEventHandler>();
+            var issueViz = new Mock<IAnalysisIssueVisualization>();
+
+            var testSubject = CreateTestSubject(issueViz.Object);
+            testSubject.PropertyChanged += eventHandler.Object;
+
+            eventHandler.VerifyNoOtherCalls();
+
+            issueViz.Raise(x => x.PropertyChanged += null, new PropertyChangedEventArgs(nameof(IAnalysisIssueVisualization.CurrentFilePath)));
+
+            eventHandler.Verify(x => x(testSubject,
+                    It.Is((PropertyChangedEventArgs args) => args.PropertyName == nameof(ITaintIssueViewModel.DisplayPath))),
+                Times.Once);
+
+            eventHandler.VerifyNoOtherCalls();
+        }
+
+        private static TaintIssueViewModel CreateTestSubject(IAnalysisIssueVisualization issueViz, IIssueVizDisplayPositionCalculator positionCalculator = null)
+        {
+            positionCalculator ??= new IssueVizDisplayPositionCalculator();
+            return new TaintIssueViewModel(issueViz, positionCalculator);
+        }
+    }
+}
---
+++ src/IssueViz.Security/Taint/TaintList/ViewModels/TaintIssuesControlViewModel.cs
@@ -0,0 +1,116 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Windows.Data;
+using System.Windows.Input;
+using Microsoft.VisualStudio.PlatformUI;
+using Microsoft.VisualStudio.Shell;
+using SonarLint.VisualStudio.IssueVisualization.Editor;
+using SonarLint.VisualStudio.IssueVisualization.Models;
+
+namespace SonarLint.VisualStudio.IssueVisualization.Security.Taint.TaintList.ViewModels
+{
+    internal interface ITaintIssuesControlViewModel : IDisposable
+    {
+        // WIP: add selection handling
+        // WIP: add any other necessary commands
+
+        ICommand NavigateCommand { get; }
+
+        ObservableCollection<ITaintIssueViewModel> Issues { get; }
+    }
+
+    /// <summary>
+    /// View model that surfaces the data in the TaintStore
+    /// </summary>
+    /// <remarks>Monitors the taint store for changes and updates the view model accordingly.</remarks>
+    internal sealed class TaintIssuesControlViewModel : ITaintIssuesControlViewModel
+    {
+        private readonly object Lock = new object();
+        private readonly INotifyCollectionChanged observableIssuesCollection;
+
+        public ObservableCollection<ITaintIssueViewModel> Issues { get; } = new ObservableCollection<ITaintIssueViewModel>();
+
+        public ICommand NavigateCommand { get; }
+
+        public TaintIssuesControlViewModel(ITaintStore store, ILocationNavigator locationNavigator)
+        {
+
+            AllowMultiThreadedAccessToIssuesCollection();
+
+            var allIssues = store.GetAll();
+            observableIssuesCollection = allIssues;
+            observableIssuesCollection.CollectionChanged += TaintStore_CollectionChanged;
+
+            UpdateIssues(allIssues, Array.Empty<IAnalysisIssueVisualization>());
+
+            NavigateCommand = new DelegateCommand(
+                parameter => {
+                    var selected = (ITaintIssueViewModel)parameter;
+                    locationNavigator.TryNavigate(selected.TaintIssueViz);
+                },
+                parameter => parameter is ITaintIssueViewModel);
+        }
+
+        /// <summary>
+        /// Allow the observable collection <see cref="Issues"/> to be modified from a non-UI thread. 
+        /// </summary>
+        private void AllowMultiThreadedAccessToIssuesCollection()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            BindingOperations.EnableCollectionSynchronization(Issues, Lock);
+        }
+
+        private void UpdateIssues(IEnumerable<IAnalysisIssueVisualization> addedIssueVizs, IEnumerable<IAnalysisIssueVisualization> removedIssueVizs)
+        {
+            foreach (var added in addedIssueVizs)
+            {
+                Issues.Add(new TaintIssueViewModel(added));
+            }
+
+            var issueVizsToRemove = Issues.Where(x => removedIssueVizs.Contains(x.TaintIssueViz)).ToArray();
+
+            foreach (var removed in issueVizsToRemove)
+            {
+                Issues.Remove(removed);
+            }
+        }
+
+        private void TaintStore_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            var added = e.NewItems ?? Array.Empty<IAnalysisIssueVisualization>();
+            var removed = e.OldItems ?? Array.Empty<IAnalysisIssueVisualization>();
+
+            UpdateIssues(
+                added.Cast<IAnalysisIssueVisualization>(),
+                removed.Cast<IAnalysisIssueVisualization>());
+        }
+
+        public void Dispose()
+        {
+            observableIssuesCollection.CollectionChanged -= TaintStore_CollectionChanged;
+        }
+    }
+}
