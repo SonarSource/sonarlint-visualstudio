fe22c8d966ca8c52b3efd656518fc5c78506acdd
Rita Gorokhod <60586879+rita-g-sonarsource@users.noreply.github.com>
2020-12-15 09:42:45 +0100

    Add TaintIssuesBindingMonitor (#2017)

---
+++ src/IssueViz.Security.UnitTests/Taint/TaintIssuesBindingMonitorTests.cs
@@ -0,0 +1,85 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SonarLint.VisualStudio.Core.Binding;
+using SonarLint.VisualStudio.Integration.UnitTests;
+using SonarLint.VisualStudio.IssueVisualization.Security.Taint;
+
+namespace SonarLint.VisualStudio.IssueVisualization.Security.UnitTests.Taint
+{
+    [TestClass]
+    public class TaintIssuesBindingMonitorTests
+    {
+        [TestMethod]
+        public void MefCtor_CheckIsExported()
+        {
+            MefTestHelpers.CheckTypeCanBeImported<TaintIssuesBindingMonitor, ITaintIssuesBindingMonitor>(null, new[]
+            {
+                MefTestHelpers.CreateExport<IActiveSolutionBoundTracker>(Mock.Of<IActiveSolutionBoundTracker>()),
+                MefTestHelpers.CreateExport<ITaintIssuesSynchronizer>(Mock.Of<ITaintIssuesSynchronizer>())
+            });
+        }
+
+        [TestMethod]
+        public void Ctor_SubscribeToSolutionBindingUpdated()
+        {
+            var synchronizer = new Mock<ITaintIssuesSynchronizer>();
+            var activeSolutionBoundTracker = new Mock<IActiveSolutionBoundTracker>();
+
+            new TaintIssuesBindingMonitor(activeSolutionBoundTracker.Object, synchronizer.Object);
+
+            activeSolutionBoundTracker.Raise(x=> x.SolutionBindingUpdated += null, EventArgs.Empty);
+
+            synchronizer.Verify(x=> x.SynchronizeWithServer(), Times.Once);
+        }
+
+        [TestMethod]
+        public void Ctor_SubscribeToSolutionBindingChanged()
+        {
+            var synchronizer = new Mock<ITaintIssuesSynchronizer>();
+            var activeSolutionBoundTracker = new Mock<IActiveSolutionBoundTracker>();
+
+            new TaintIssuesBindingMonitor(activeSolutionBoundTracker.Object, synchronizer.Object);
+
+            activeSolutionBoundTracker.Raise(x => x.SolutionBindingChanged += null, new ActiveSolutionBindingEventArgs(BindingConfiguration.Standalone));
+
+            synchronizer.Verify(x => x.SynchronizeWithServer(), Times.Once);
+        }
+
+        [TestMethod]
+        public void Dispose_UnsubscribeFromEvents()
+        {
+            var synchronizer = new Mock<ITaintIssuesSynchronizer>();
+            var activeSolutionBoundTracker = new Mock<IActiveSolutionBoundTracker>();
+
+            var testSubject = new TaintIssuesBindingMonitor(activeSolutionBoundTracker.Object, synchronizer.Object);
+            testSubject.Dispose();
+
+            activeSolutionBoundTracker.Raise(x => x.SolutionBindingUpdated += null, EventArgs.Empty);
+            activeSolutionBoundTracker.Raise(x => x.SolutionBindingChanged += null, new ActiveSolutionBindingEventArgs(BindingConfiguration.Standalone));
+
+            synchronizer.Invocations.Count.Should().Be(0);
+        }
+    }
+}
---
+++ src/IssueViz.Security/Taint/TaintIssuesBindingMonitor.cs
@@ -0,0 +1,73 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using SonarLint.VisualStudio.Core.Binding;
+
+namespace SonarLint.VisualStudio.IssueVisualization.Security.Taint
+{
+    /// <summary>
+    /// Listens to binding changes and triggers fetching of taint vulnerabilities from the connected server.
+    /// </summary>
+    internal interface ITaintIssuesBindingMonitor : IDisposable
+    {
+    }
+
+    [Export(typeof(ITaintIssuesBindingMonitor))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    internal sealed class TaintIssuesBindingMonitor : ITaintIssuesBindingMonitor
+    {
+        private readonly IActiveSolutionBoundTracker activeSolutionBoundTracker;
+        private readonly ITaintIssuesSynchronizer taintIssuesSynchronizer;
+
+        [ImportingConstructor]
+        public TaintIssuesBindingMonitor(IActiveSolutionBoundTracker activeSolutionBoundTracker, ITaintIssuesSynchronizer taintIssuesSynchronizer)
+        {
+            this.activeSolutionBoundTracker = activeSolutionBoundTracker;
+            this.taintIssuesSynchronizer = taintIssuesSynchronizer;
+
+            activeSolutionBoundTracker.SolutionBindingChanged += ActiveSolutionBoundTracker_SolutionBindingChanged;
+            activeSolutionBoundTracker.SolutionBindingUpdated += ActiveSolutionBoundTracker_SolutionBindingUpdated;
+        }
+
+        private async void ActiveSolutionBoundTracker_SolutionBindingUpdated(object sender, EventArgs e)
+        {
+            await Sync();
+        }
+
+        private async void ActiveSolutionBoundTracker_SolutionBindingChanged(object sender, ActiveSolutionBindingEventArgs e)
+        {
+            await Sync();
+        }
+
+        private async Task Sync()
+        {
+            await taintIssuesSynchronizer.SynchronizeWithServer();
+        }
+
+        public void Dispose()
+        {
+            activeSolutionBoundTracker.SolutionBindingChanged -= ActiveSolutionBoundTracker_SolutionBindingChanged;
+            activeSolutionBoundTracker.SolutionBindingUpdated -= ActiveSolutionBoundTracker_SolutionBindingUpdated;
+        }
+    }
+}
