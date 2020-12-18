240b0388cd39867f17dafef660493ea7495df898
Rita Gorokhod <60586879+rita-g-sonarsource@users.noreply.github.com>
2020-12-18 18:30:47 +0100

    Add ShowInBrowser command (#2046)

---
+++ src/IssueViz.Security.UnitTests/ShowInBrowserServiceTests.cs
@@ -0,0 +1,108 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SonarLint.VisualStudio.Core.Binding;
+using SonarLint.VisualStudio.Integration.UnitTests;
+using SonarLint.VisualStudio.IssueVisualization.Security.SharedUI;
+using SonarQube.Client;
+
+namespace SonarLint.VisualStudio.IssueVisualization.Security.UnitTests
+{
+    [TestClass]
+    public class ShowInBrowserServiceTests
+    {
+        [TestMethod]
+        public void MefCtor_CheckIsExported()
+        {
+            MefTestHelpers.CheckTypeCanBeImported<ShowInBrowserService, IShowInBrowserService>(null, new[]
+            {
+                MefTestHelpers.CreateExport<ISonarQubeService>(Mock.Of<ISonarQubeService>()),
+                MefTestHelpers.CreateExport<IConfigurationProvider>(Mock.Of<IConfigurationProvider>())
+            });
+        }
+
+        [TestMethod]
+        [DataRow(null)]
+        [DataRow("")]
+        public void ShowIssue_NullIssueKey_ArgumentNullException(string issueKey)
+        {
+            var testSubject = CreateTestSubject();
+
+            Action act = () => testSubject.ShowIssue(issueKey);
+
+            act.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("issueKey");
+        }
+
+        [TestMethod]
+        public void ShowIssue_NotInConnectedMode_BrowserNotOpened()
+        {
+            var configurationProvider = new Mock<IConfigurationProvider>();
+            configurationProvider.Setup(x => x.GetConfiguration()).Returns(BindingConfiguration.Standalone);
+
+            var sonarQubeService = new Mock<ISonarQubeService>();
+            var showInBrowserAction = new Mock<Action<string>>();
+
+            var testSubject = CreateTestSubject(sonarQubeService.Object, configurationProvider.Object, showInBrowserAction.Object);
+            testSubject.ShowIssue("issue");
+
+            sonarQubeService.Invocations.Count.Should().Be(0);
+            showInBrowserAction.Invocations.Count.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void ShowIssue_InConnectedMode_BrowserOpened()
+        {
+            const string projectKey = "project key";
+            const string issueKey = "issue key";
+
+            var configurationProvider = new Mock<IConfigurationProvider>();
+            configurationProvider
+                .Setup(x => x.GetConfiguration())
+                .Returns(new BindingConfiguration(new BoundSonarQubeProject{ProjectKey = projectKey}, SonarLintMode.Connected, null));
+
+            var sonarQubeService = new Mock<ISonarQubeService>();
+            sonarQubeService
+                .Setup(x => x.GetViewIssueUrl(projectKey, issueKey))
+                .Returns(new Uri("http://localhost:123/expected/issue?id=1"));
+
+            var showInBrowserAction = new Mock<Action<string>>();
+
+            var testSubject = CreateTestSubject(sonarQubeService.Object, configurationProvider.Object, showInBrowserAction.Object);
+            testSubject.ShowIssue(issueKey);
+
+            showInBrowserAction.Verify(x=> x("http://localhost:123/expected/issue?id=1"));
+        }
+
+        private ShowInBrowserService CreateTestSubject(ISonarQubeService sonarQubeService = null,
+            IConfigurationProvider configurationProvider = null,
+            Action<string> showInBrowserAction = null)
+        {
+            sonarQubeService ??= Mock.Of<ISonarQubeService>();
+            configurationProvider ??= Mock.Of<IConfigurationProvider>();
+            showInBrowserAction ??= s => { };
+
+            return new ShowInBrowserService(sonarQubeService, configurationProvider, showInBrowserAction);
+        }
+    }
+}
--- src/IssueViz.Security.UnitTests/Taint/TaintList/TaintIssuesControlViewModelTests.cs
+++ src/IssueViz.Security.UnitTests/Taint/TaintList/TaintIssuesControlViewModelTests.cs
@@ -22,6 +22,7 @@ using System;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows.Data;
+using System.Windows.Input;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.VisualStudio.Text;
@@ -31,7 +32,9 @@ using SonarLint.VisualStudio.Infrastructure.VS.DocumentEvents;
 using SonarLint.VisualStudio.Integration.UnitTests;
 using SonarLint.VisualStudio.IssueVisualization.Editor;
 using SonarLint.VisualStudio.IssueVisualization.Models;
+using SonarLint.VisualStudio.IssueVisualization.Security.SharedUI;
 using SonarLint.VisualStudio.IssueVisualization.Security.Taint;
+using SonarLint.VisualStudio.IssueVisualization.Security.Taint.Models;
 using SonarLint.VisualStudio.IssueVisualization.Security.Taint.TaintList.ViewModels;
 
 namespace SonarLint.VisualStudio.IssueVisualization.Security.UnitTests.Taint.TaintList
@@ -178,10 +181,9 @@ namespace SonarLint.VisualStudio.IssueVisualization.Security.UnitTests.Taint.Tai
         public void NavigateCommand_CanExecute_NullParameter_False()
         {
             var locationNavigator = new Mock<ILocationNavigator>();
-
             var testSubject = CreateTestSubject(locationNavigator: locationNavigator.Object);
-            var result = testSubject.NavigateCommand.CanExecute(null);
-            result.Should().BeFalse();
+
+            VerifyCommandExecution(testSubject.NavigateCommand, null, false);
 
             locationNavigator.VerifyNoOtherCalls();
         }
@@ -190,10 +192,9 @@ namespace SonarLint.VisualStudio.IssueVisualization.Security.UnitTests.Taint.Tai
         public void NavigateCommand_CanExecute_ParameterIsNotTaintViewModel_False()
         {
             var locationNavigator = new Mock<ILocationNavigator>();
-
             var testSubject = CreateTestSubject(locationNavigator: locationNavigator.Object);
-            var result = testSubject.NavigateCommand.CanExecute("something");
-            result.Should().BeFalse();
+
+            VerifyCommandExecution(testSubject.NavigateCommand, "something", false);
 
             locationNavigator.VerifyNoOtherCalls();
         }
@@ -202,10 +203,9 @@ namespace SonarLint.VisualStudio.IssueVisualization.Security.UnitTests.Taint.Tai
         public void NavigateCommand_CanExecute_ParameterIsTaintViewModel_True()
         {
             var locationNavigator = new Mock<ILocationNavigator>();
-
             var testSubject = CreateTestSubject(locationNavigator: locationNavigator.Object);
-            var result = testSubject.NavigateCommand.CanExecute(Mock.Of<ITaintIssueViewModel>());
-            result.Should().BeTrue();
+
+            VerifyCommandExecution(testSubject.NavigateCommand, Mock.Of<ITaintIssueViewModel>(), true);
 
             locationNavigator.VerifyNoOtherCalls();
         }
@@ -224,6 +224,55 @@ namespace SonarLint.VisualStudio.IssueVisualization.Security.UnitTests.Taint.Tai
 
             locationNavigator.Verify(x => x.TryNavigate(issueViz), Times.Once);
             locationNavigator.VerifyNoOtherCalls();
+        }
+
+        [TestMethod]
+        public void ShowInBrowserCommand_CanExecute_NullParameter_False()
+        {
+            var showInBrowserService = new Mock<IShowInBrowserService>();
+            var testSubject = CreateTestSubject(showInBrowserService: showInBrowserService.Object);
+
+            VerifyCommandExecution(testSubject.ShowInBrowserCommand, null, false);
+
+            showInBrowserService.VerifyNoOtherCalls();
+        }
+
+        [TestMethod]
+        public void ShowInBrowserCommand_CanExecute_ParameterIsNotTaintViewModel_False()
+        {
+            var showInBrowserService = new Mock<IShowInBrowserService>();
+            var testSubject = CreateTestSubject(showInBrowserService: showInBrowserService.Object);
+
+            VerifyCommandExecution(testSubject.ShowInBrowserCommand, "something", false);
+
+            showInBrowserService.VerifyNoOtherCalls();
+        }
+
+        [TestMethod]
+        public void ShowInBrowserCommand_CanExecute_ParameterIsTaintViewModel_True()
+        {
+            var showInBrowserService = new Mock<IShowInBrowserService>();
+            var testSubject = CreateTestSubject(showInBrowserService: showInBrowserService.Object);
+
+            VerifyCommandExecution(testSubject.ShowInBrowserCommand, Mock.Of<ITaintIssueViewModel>(), true);
+
+            showInBrowserService.VerifyNoOtherCalls();
+        }
+
+        [TestMethod]
+        public void ShowInBrowserCommand_Execute_BrowserServiceIsCalled()
+        {
+            var issueViz = CreateIssueViz(issueKey: "issue key 123");
+            var viewModel = new Mock<ITaintIssueViewModel>();
+            viewModel.Setup(x => x.TaintIssueViz).Returns(issueViz);
+
+            var showInBrowserService = new Mock<IShowInBrowserService>();
+            var testSubject = CreateTestSubject(showInBrowserService: showInBrowserService.Object);
+
+            testSubject.ShowInBrowserCommand.Execute(viewModel.Object);
+
+            showInBrowserService.Verify(x => x.ShowIssue("issue key 123"), Times.Once);
+            showInBrowserService.VerifyNoOtherCalls();
         }
 
         [TestMethod]
@@ -277,7 +326,8 @@ namespace SonarLint.VisualStudio.IssueVisualization.Security.UnitTests.Taint.Tai
             ILocationNavigator locationNavigator = null,
             Mock<ITaintStore> store = null,
             IActiveDocumentTracker activeDocumentTracker = null,
-            IActiveDocumentLocator activeDocumentLocator = null)
+            IActiveDocumentLocator activeDocumentLocator = null,
+            IShowInBrowserService showInBrowserService = null)
         {
             originalCollection ??= new ObservableCollection<IAnalysisIssueVisualization>();
             var readOnlyWrapper = new ReadOnlyObservableCollection<IAnalysisIssueVisualization>(originalCollection);
@@ -287,19 +337,31 @@ namespace SonarLint.VisualStudio.IssueVisualization.Security.UnitTests.Taint.Tai
 
             activeDocumentTracker ??= Mock.Of<IActiveDocumentTracker>();
             activeDocumentLocator ??= Mock.Of<IActiveDocumentLocator>();
+            showInBrowserService ??= Mock.Of<IShowInBrowserService>();
 
             return new TaintIssuesControlViewModel(store.Object,
                 locationNavigator,
                 activeDocumentTracker,
-                activeDocumentLocator);
+                activeDocumentLocator,
+                showInBrowserService);
         }
 
-        private IAnalysisIssueVisualization CreateIssueViz(string filePath = "test.cpp")
+        private IAnalysisIssueVisualization CreateIssueViz(string filePath = "test.cpp", string issueKey = "issue key")
         {
+            var issue = new Mock<ITaintIssue>();
+            issue.Setup(x => x.IssueKey).Returns(issueKey);
+
             var issueViz = new Mock<IAnalysisIssueVisualization>();
             issueViz.Setup(x => x.CurrentFilePath).Returns(filePath);
+            issueViz.Setup(x => x.Issue).Returns(issue.Object);
 
             return issueViz.Object;
+        }
+
+        private void VerifyCommandExecution(ICommand command, object parameter, bool canExecute)
+        {
+            var result = command.CanExecute(parameter);
+            result.Should().Be(canExecute);
         }
     }
 }
