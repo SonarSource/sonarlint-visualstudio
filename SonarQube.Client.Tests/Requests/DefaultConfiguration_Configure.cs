13d9822090495e454717504ceacaa8bfe84df843
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-01-18 10:19:23 +0000

    Added unversioned request factory (#84)
    Added configuration for SonarCloud

--- SonarQube.Client.Tests/Requests/DefaultConfiguration_Configure.cs
+++ SonarQube.Client.Tests/Requests/DefaultConfiguration_Configure.cs
@@ -18,12 +18,12 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+using System.Diagnostics;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SonarQube.Client.Requests;
 using SonarQube.Client.Api;
+using SonarQube.Client.Requests;
 using SonarQube.Client.Tests.Infra;
-using System.Diagnostics;
 
 namespace SonarQube.Client.Tests.Requests
 {
@@ -31,7 +31,7 @@ namespace SonarQube.Client.Tests.Requests
     public class DefaultConfiguration_Configure
     {
         [TestMethod]
-        public void Configure_Writes_Debug_Messages()
+        public void ConfigureSonarQube_Writes_Debug_Messages()
         {
             var logger = new TestLogger();
 
@@ -64,12 +64,96 @@ namespace SonarQube.Client.Tests.Requests
                     "Registered SonarQube.Client.Api.V8_6.GetTaintVulnerabilitiesRequest for 8.6",
                 };
 
-            DefaultConfiguration.Configure(new RequestFactory(logger));
+            DefaultConfiguration.ConfigureSonarQube(new RequestFactory(logger));
 
             DumpDebugMessages(logger);
 
             logger.DebugMessages.Should().ContainInOrder(expected);
             logger.DebugMessages.Count.Should().Be(expected.Length);
+        }
+
+        [TestMethod]
+        public void ConfigureSonarCloud_Writes_Debug_Messages()
+        {
+            var logger = new TestLogger();
+
+            var expected = new string[]
+                {
+                    "Registered SonarQube.Client.Api.V2_10.GetVersionRequest",
+                    "Registered SonarQube.Client.Api.V3_30.ValidateCredentialsRequest",
+                    "Registered SonarQube.Client.Api.V5_10.GetLanguagesRequest",
+                    "Registered SonarQube.Client.Api.V5_40.GetModulesRequest",
+                    "Registered SonarQube.Client.Api.V5_50.GetRulesRequest",
+                    "Registered SonarQube.Client.Api.V5_50.DownloadStaticFile",
+                    "Registered SonarQube.Client.Api.V6_20.GetProjectsRequest",
+                    "Registered SonarQube.Client.Api.V6_30.GetPluginsRequest",
+                    "Registered SonarQube.Client.Api.V6_30.GetPropertiesRequest",
+                    "Registered SonarQube.Client.Api.V6_50.GetQualityProfileChangeLogRequest",
+                    "Registered SonarQube.Client.Api.V6_50.GetQualityProfilesRequest",
+                    "Registered SonarQube.Client.Api.V6_60.GetNotificationsRequest",
+                    "Registered SonarQube.Client.Api.V6_60.GetRoslynExportProfileRequest",
+                    "Registered SonarQube.Client.Api.V7_00.GetOrganizationsRequest",
+                    "Registered SonarQube.Client.Api.V7_20.GetIssuesRequestWrapper",
+                    "Registered SonarQube.Client.Api.V8_6.GetHotspotRequest",
+                    "Registered SonarQube.Client.Api.V8_6.GetTaintVulnerabilitiesRequest",
+                };
+
+            DefaultConfiguration.ConfigureSonarCloud(new UnversionedRequestFactory(logger));
+
+            DumpDebugMessages(logger);
+
+            logger.DebugMessages.Should().ContainInOrder(expected);
+            logger.DebugMessages.Count.Should().Be(expected.Length);
+        }
+
+        [TestMethod]
+        public void ConfigureSonarQube_CheckAllRequestsImplemented()
+        {
+            var testSubject = DefaultConfiguration.ConfigureSonarQube(new RequestFactory(new TestLogger()));
+            var serverInfo = new ServerInfo(null /* latest */, ServerType.SonarQube);
+
+            testSubject.Create<IDownloadStaticFile>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetHotspotRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetIssuesRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetLanguagesRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetModulesRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetNotificationsRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetOrganizationsRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetPluginsRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetProjectsRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetPropertiesRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetQualityProfileChangeLogRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetQualityProfilesRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetRoslynExportProfileRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetRulesRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetTaintVulnerabilitiesRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetVersionRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IValidateCredentialsRequest>(serverInfo).Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void ConfigureSonarCloud_CheckAllRequestsImplemented()
+        {
+            var testSubject = DefaultConfiguration.ConfigureSonarCloud(new UnversionedRequestFactory(new TestLogger()));
+            var serverInfo = new ServerInfo(null /* latest */, ServerType.SonarQube);
+
+            testSubject.Create<IDownloadStaticFile>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetHotspotRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetIssuesRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetLanguagesRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetModulesRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetNotificationsRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetOrganizationsRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetPluginsRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetProjectsRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetPropertiesRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetQualityProfileChangeLogRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetQualityProfilesRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetRoslynExportProfileRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetRulesRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetTaintVulnerabilitiesRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IGetVersionRequest>(serverInfo).Should().NotBeNull();
+            testSubject.Create<IValidateCredentialsRequest>(serverInfo).Should().NotBeNull();
         }
 
         private static void DumpDebugMessages(TestLogger logger)
