e3e7b280f58698677ccbc0e97fe1562b86168e46
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-01-18 08:28:02 +0000

    Added request factory selector (#83)

---
+++ SonarQube.Client.Tests/Requests/RequestFactorySelectorTests.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * SonarQube Client
+ * Copyright (C) 2016-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SonarQube.Client.Api;
+using SonarQube.Client.Requests;
+using SonarQube.Client.Tests.Infra;
+
+namespace SonarQube.Client.Tests.Requests
+{
+    [TestClass]
+    public class RequestFactorySelectorTests
+    {
+        [TestMethod]
+        public void Select_IsSonarQube_ReturnsSonarQubeFactory()
+        {
+            var logger = new TestLogger();
+            var testSubject = new RequestFactorySelector();
+
+            var actual = testSubject.Select(isSonarCloud: false, logger);
+
+            // Should be a configure SonarQube request factory
+            actual.Should().BeOfType<RequestFactory>();
+
+            var actualRequest = actual.Create<IGetVersionRequest>(new ServerInfo(new Version(6, 7), ServerType.SonarQube));
+            actualRequest.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void Select_IsSonarCloud_ReturnsSonarCloudFactory()
+        {
+            var logger = new TestLogger();
+            var testSubject = new RequestFactorySelector();
+
+            var actual = testSubject.Select(isSonarCloud: true, logger);
+
+            actual.Should().NotBeNull();
+
+            // TODO - validate returned factory
+        }
+    }
+}
--- SonarQube.Client.Tests/SonarQubeService_Miscellaneous.cs
+++ SonarQube.Client.Tests/SonarQubeService_Miscellaneous.cs
@@ -20,9 +20,13 @@
 
 using System;
 using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using SonarQube.Client.Api;
+using SonarQube.Client.Models;
 using SonarQube.Client.Requests;
 using SonarQube.Client.Tests.Infra;
 
@@ -38,16 +42,16 @@ namespace SonarQube.Client.Tests
 
             var logger = new TestLogger();
 
-            action = () => new SonarQubeService(null, new RequestFactory(logger), string.Empty, logger);
+            action = () => new SonarQubeService(null, Mock.Of<IRequestFactorySelector>(), string.Empty, logger);
             action.Should().ThrowExactly<ArgumentNullException>().And.ParamName.Should().Be("messageHandler");
 
             action = () => new SonarQubeService(new Mock<HttpClientHandler>().Object, null, string.Empty, logger);
-            action.Should().ThrowExactly<ArgumentNullException>().And.ParamName.Should().Be("requestFactory");
+            action.Should().ThrowExactly<ArgumentNullException>().And.ParamName.Should().Be("requestFactorySelector");
 
-            action = () => new SonarQubeService(new Mock<HttpClientHandler>().Object, new RequestFactory(logger), null, logger);
+            action = () => new SonarQubeService(new Mock<HttpClientHandler>().Object, Mock.Of<IRequestFactorySelector>(), null, logger);
             action.Should().ThrowExactly<ArgumentNullException>().And.ParamName.Should().Be("userAgent");
 
-            action = () => new SonarQubeService(new Mock<HttpClientHandler>().Object, new RequestFactory(logger), string.Empty, null);
+            action = () => new SonarQubeService(new Mock<HttpClientHandler>().Object, Mock.Of<IRequestFactorySelector>(), string.Empty, null);
             action.Should().ThrowExactly<ArgumentNullException>().And.ParamName.Should().Be("logger");
         }
 
@@ -66,6 +70,96 @@ namespace SonarQube.Client.Tests
             // 3. Special key is recognised
             CheckSpecialKeyIsRecognisedAndNullReturned("sonar.internal.testing.no.org");
             CheckSpecialKeyIsRecognisedAndNullReturned("SONAR.INTERNAL.TESTING.NO.ORG");
+        }
+
+        [TestMethod]
+        [DataRow("http://localhost", false)]
+        [DataRow("https://sonarcloud.io", true)]
+        public async Task SonarQubeService_UsesFactorySelector(string serverUrl, bool isSonarCloud)
+        {
+            var logger = new TestLogger();
+            var connectionInfo = new ConnectionInformation(new Uri(serverUrl));
+
+            var requestFactoryMock = CreateRequestFactory("1.2.3");
+            var selectorMock = new Mock<IRequestFactorySelector>();
+            selectorMock.Setup(x => x.Select(isSonarCloud, logger)).Returns(requestFactoryMock.Object);
+
+            var testSubject = new SonarQubeService(Mock.Of<HttpMessageHandler>(), selectorMock.Object, "user-agent", logger);
+            await testSubject.ConnectAsync(connectionInfo, CancellationToken.None);
+
+            selectorMock.Verify(x => x.Select(isSonarCloud, logger), Times.Once);
+            requestFactoryMock.Invocations.Count.Should().Be(2);
+
+            testSubject.IsConnected.Should().BeTrue();
+
+            var expectedServerType = isSonarCloud ? ServerType.SonarCloud : ServerType.SonarQube;
+            testSubject.ServerInfo.ServerType.Should().Be(expectedServerType);
+            testSubject.ServerInfo.Version.Should().Be(new Version(1, 2, 3));
+        }
+
+        [TestMethod]
+        public async Task SonarQubeService_FactorySelector_RequestsNewFactoryOnEachConnect()
+        {
+            var logger = new TestLogger();
+            var sonarQubeConnectionInfo = new ConnectionInformation(new Uri("http://sonarqube"));
+            var sonarCloudConnectionInfo = new ConnectionInformation(new Uri("https://sonarcloud.io"));
+
+            var qubeFactoryMock = CreateRequestFactory("1.2.3");
+            var cloudFactoryMock = CreateRequestFactory("9.9");
+
+            var selectorMock = new Mock<IRequestFactorySelector>();
+            selectorMock.Setup(x => x.Select(false /* isSonarCloud */, logger)).Returns(qubeFactoryMock.Object);
+            selectorMock.Setup(x => x.Select(true /* isSonarCloud */, logger)).Returns(cloudFactoryMock.Object);
+
+            var testSubject = new SonarQubeService(Mock.Of<HttpMessageHandler>(), selectorMock.Object, "user-agent", logger);
+
+            // 1. Connect to SonarQube
+            await testSubject.ConnectAsync(sonarQubeConnectionInfo, CancellationToken.None);
+
+            qubeFactoryMock.Invocations.Count.Should().Be(2);
+            cloudFactoryMock.Invocations.Count.Should().Be(0);
+            testSubject.IsConnected.Should().BeTrue();
+            testSubject.ServerInfo.ServerType.Should().Be(ServerType.SonarQube);
+
+            // 2. Disconnect
+            testSubject.Disconnect();
+            testSubject.IsConnected.Should().BeFalse();
+
+            // 3. Connect to SonarCloud
+            await testSubject.ConnectAsync(sonarCloudConnectionInfo, CancellationToken.None);
+
+            qubeFactoryMock.Invocations.Count.Should().Be(2);
+            cloudFactoryMock.Invocations.Count.Should().Be(2);
+            testSubject.IsConnected.Should().BeTrue();
+            testSubject.ServerInfo.ServerType.Should().Be(ServerType.SonarCloud);
+        }
+
+        private static Mock<IRequestFactory> CreateRequestFactory(string versionResponse)
+        {
+            var requestFactoryMock = new Mock<IRequestFactory>();
+
+            // ConnectAsync calls GetVersion and ValidateCredentials
+            var getVersionMock = CreateGetVersionRequest(versionResponse);
+            requestFactoryMock.Setup(x => x.Create<IGetVersionRequest>(It.IsAny<ServerInfo>())).Returns(getVersionMock.Object);
+
+            var validateCredentialsMock = CreateValidateCredsRequest(true);
+            requestFactoryMock.Setup(x => x.Create<IValidateCredentialsRequest>(It.IsAny<ServerInfo>())).Returns(validateCredentialsMock.Object);
+
+            return requestFactoryMock;
+        }
+
+        private static Mock<IGetVersionRequest> CreateGetVersionRequest(string response)
+        {
+            var requestMock = new Mock<IGetVersionRequest>();
+            requestMock.Setup(x => x.InvokeAsync(It.IsAny<HttpClient>(), CancellationToken.None)).Returns(Task.FromResult(response));
+            return requestMock;
+        }
+
+        private static Mock<IValidateCredentialsRequest> CreateValidateCredsRequest(bool response)
+        {
+            var requestMock = new Mock<IValidateCredentialsRequest>();
+            requestMock.Setup(x => x.InvokeAsync(It.IsAny<HttpClient>(), CancellationToken.None)).Returns(Task.FromResult(response));
+            return requestMock;
         }
 
         private static void CheckSameKeyReturnedAndNoLogOutput(string key)
