057a43db19bfb6adb0ab8d7c4691a19ff18d170b
Rita Gorokhod <60586879+rita-g-sonarsource@users.noreply.github.com>
2021-01-26 10:43:49 +0100

    Support regional datetime settings in Taint List (#2134)

---
+++ src/Core/WPF/DateTimeConverter.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SonarLint.VisualStudio.Core.WPF
+{
+    /// <summary>
+    /// Workaround for https://github.com/dotnet/wpf/issues/1946
+    /// Fixes SLVS #2126
+    /// </summary>
+    [ValueConversion(typeof(DateTime), typeof(string))]
+    public class DateTimeConverter : IValueConverter
+    {
+        private readonly CultureInfo userCulture;
+
+        public DateTimeConverter()
+            : this(CultureInfo.CurrentUICulture)
+        {
+        }
+
+        internal DateTimeConverter(CultureInfo userCulture)
+        {
+            this.userCulture = userCulture;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is DateTimeOffset))
+            {
+                throw new ArgumentException(nameof(value));
+            }
+
+            var date = (DateTimeOffset) value;
+            return date.ToString("G", userCulture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
