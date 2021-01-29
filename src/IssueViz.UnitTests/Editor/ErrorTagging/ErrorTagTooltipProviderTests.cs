98ba104c6ab7c9dc3f66d9681562e93444033998
Rita Gorokhod <rita.gorokhod@sonarsource.com>
2021-01-29 17:39:31 +0100

    Add help link to editor tooltip

---
+++ src/IssueViz.UnitTests/Editor/ErrorTagging/ErrorTagTooltipProviderTests.cs
@@ -0,0 +1,89 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using FluentAssertions;
+using Microsoft.VisualStudio.PlatformUI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.Core.Analysis;
+using SonarLint.VisualStudio.Infrastructure.VS;
+using SonarLint.VisualStudio.Integration.UnitTests;
+using SonarLint.VisualStudio.IssueVisualization.Editor.ErrorTagging;
+using Color = System.Drawing.Color;
+using IVsThemeColorProvider = SonarLint.VisualStudio.Infrastructure.VS.IVsThemeColorProvider;
+
+namespace SonarLint.VisualStudio.IssueVisualization.UnitTests.Editor.ErrorTagging
+{
+    [TestClass]
+    public class ErrorTagTooltipProviderTests
+    {
+        [TestMethod]
+        public void MefCtor_CheckIsExported()
+        {
+            MefTestHelpers.CheckTypeCanBeImported<ErrorTagTooltipProvider, IErrorTagTooltipProvider>(null,
+                new[]
+                {
+                    MefTestHelpers.CreateExport<IBrowserService>(Mock.Of<IBrowserService>()),
+                    MefTestHelpers.CreateExport<IVsThemeColorProvider>(Mock.Of<IVsThemeColorProvider>())
+                });
+        }
+
+        [TestMethod]
+        public void Create_CreatesTooltipWithHyperlink()
+        {
+            var issue = new Mock<IAnalysisIssueBase>();
+            issue.Setup(x => x.RuleKey).Returns("some rule");
+            issue.Setup(x => x.Message).Returns("some message");
+
+            var ruleHelpLinkProvider = new Mock<IRuleHelpLinkProvider>();
+            ruleHelpLinkProvider.Setup(x => x.GetHelpLink("some rule")).Returns("some url");
+
+            var browserService = new Mock<IBrowserService>();
+
+            var testSubject = new ErrorTagTooltipProvider(browserService.Object, Mock.Of<IVsThemeColorProvider>(), ruleHelpLinkProvider.Object);
+            var result = testSubject.Create(issue.Object);
+
+            result.Should().NotBeNull();
+            result.Should().BeOfType<TextBlock>();
+
+            var inlines = ((TextBlock)result).Inlines.ToList();
+            inlines[0].Should().BeOfType<Hyperlink>();
+            inlines[1].Should().BeOfType<Run>();
+            inlines[2].Should().BeOfType<Run>();
+
+            (inlines[1] as Run).Text.Should().Be(": ");
+            (inlines[2] as Run).Text.Should().Be("some message");
+
+            var hyperlink = (Hyperlink)inlines[0];
+            hyperlink.Command.Should().NotBeNull();
+            hyperlink.Command.CanExecute(null).Should().BeTrue();
+
+            browserService.Invocations.Count.Should().Be(0);
+            hyperlink.Command.Execute(null);
+            browserService.Verify(x=> x.Navigate("some url"), Times.Once);
+            browserService.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ src/IssueViz/Editor/ErrorTagging/ErrorTagTooltipProvider.cs
@@ -0,0 +1,97 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.ComponentModel.Composition;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using Microsoft.VisualStudio.PlatformUI;
+using Microsoft.VisualStudio.Shell;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.Core.Analysis;
+using SonarLint.VisualStudio.Infrastructure.VS;
+
+namespace SonarLint.VisualStudio.IssueVisualization.Editor.ErrorTagging
+{
+    /// <summary>
+    /// Creates a tooltip object for an IErrorTag
+    /// </summary>
+    internal interface IErrorTagTooltipProvider
+    {
+        object Create(IAnalysisIssueBase analysisIssueBase);
+    }
+
+    [Export(typeof(IErrorTagTooltipProvider))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    internal class ErrorTagTooltipProvider : IErrorTagTooltipProvider
+    {
+        private readonly IBrowserService browserService;
+        private readonly IVsThemeColorProvider vsThemeColorProvider;
+        private readonly IRuleHelpLinkProvider ruleHelpLinkProvider;
+
+        [ImportingConstructor]
+        public ErrorTagTooltipProvider(IBrowserService browserService, IVsThemeColorProvider vsThemeColorProvider)
+            : this(browserService, vsThemeColorProvider, new RuleHelpLinkProvider())
+        {
+        }
+
+        internal ErrorTagTooltipProvider(IBrowserService browserService, IVsThemeColorProvider vsThemeColorProvider, IRuleHelpLinkProvider ruleHelpLinkProvider)
+        {
+            this.browserService = browserService;
+            this.vsThemeColorProvider = vsThemeColorProvider;
+            this.ruleHelpLinkProvider = ruleHelpLinkProvider;
+        }
+
+        public object Create(IAnalysisIssueBase analysisIssueBase)
+        {
+            var ruleUrl = ruleHelpLinkProvider.GetHelpLink(analysisIssueBase.RuleKey);
+
+            var hyperLink = new Hyperlink
+            {
+                Inlines = {analysisIssueBase.RuleKey},
+                Foreground = GetVsThemedColor(EnvironmentColors.ControlLinkTextColorKey),
+                TextDecorations = null,
+                Command = new DelegateCommand(o => browserService.Navigate(ruleUrl))
+            };
+
+            var content = new TextBlock
+            {
+                Inlines =
+                {
+                    hyperLink,
+                    ": ",
+                    analysisIssueBase.Message
+                },
+                Foreground = GetVsThemedColor(EnvironmentColors.SystemCaptionTextBrushKey)
+            };
+
+
+            return content;
+        }
+
+        private Brush GetVsThemedColor(ThemeResourceKey resourceKey)
+        {
+            var textColor = vsThemeColorProvider.GetVsThemedColor(resourceKey);
+            var color = Color.FromArgb(textColor.A, textColor.R, textColor.G, textColor.B);
+            
+            return new SolidColorBrush(color);
+        }
+    }
+}
