2d8e0bfdd09a101be402a143c357faef58965a43
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-02-10 17:44:07 +0000

    Display notifications icon in VS2019
    Fixes #1751

--- src/Integration/Notifications/VisualStudioStatusBarHelper.cs
+++ src/Integration/Notifications/VisualStudioStatusBarHelper.cs
@@ -18,6 +18,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Windows;
 using System.Windows.Controls;
@@ -29,7 +30,6 @@ namespace SonarLint.VisualStudio.Integration.Notifications
     public static class VisualStudioStatusBarHelper
     {
         private const string SccStatusBarHostName = "PART_SccStatusBarHost";
-        private const string StatusBarPanelName = "StatusBarPanel";
 
         public static void RemoveStatusBarIcon(FrameworkElement statusBarIcon)
         {
@@ -44,10 +44,7 @@ namespace SonarLint.VisualStudio.Integration.Notifications
                 return;
             }
 
-            var root = VisualTreeHelper.GetChild(
-                VisualTreeHelper.GetChild(Application.Current.MainWindow, 0), 0);
-
-            var dockPanel = GetStatusBarPanel(root);
+            var dockPanel = FindSccStatusBarParentPanel(Application.Current.MainWindow);
             if (dockPanel == null)
             {
                 return;
@@ -59,17 +56,22 @@ namespace SonarLint.VisualStudio.Integration.Notifications
             dockPanel.Children.Insert(sourceControlHostIndex + 1, statusBarIcon);
         }
 
-        private static DockPanel GetStatusBarPanel(DependencyObject mainWindow)
+        private static DockPanel FindSccStatusBarParentPanel(Window window)
         {
-            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(mainWindow); i++)
-            {
-                var dockPanel = VisualTreeHelper.GetChild(mainWindow, i) as DockPanel;
-                if (dockPanel?.Name == StatusBarPanelName)
-                {
-                    return dockPanel;
-                }
-            }
-            return null;
+            // We want to add our icon as in the VS status bar as a sibling of the source control visuals.
+
+            // Assumptions:
+            // 1) there is a control named "PART_SccStatusBarHost"
+            // 2) that control is hosted in a dock panel 
+
+            // This approach is a bit ugly, but it's also the approach used in the extension -
+            // see https://github.com/github/VisualStudio/blob/824bab4ab2c3d8f6787cf38ff5ff9d4e9df00e98/src/GitHub.InlineReviews/Services/PullRequestStatusBarManager.cs#L171
+
+            // Note: the visual tree of VS2019 is different from that of VS2015 and VS2017, but they
+            // all have contain this named PART control. See #1751.
+
+            var statusBarPart = window?.Template?.FindName(SccStatusBarHostName,  window) as FrameworkElement;
+            return statusBarPart?.Parent as DockPanel;
         }
 
         private static int GetStatusBarSourceControlHostIndex(DockPanel dockPanel)
@@ -82,7 +84,9 @@ namespace SonarLint.VisualStudio.Integration.Notifications
                     return i;
                 }
             }
-            return 0;
+
+            Debug.Fail("Not expecting GetStatusBarSourceControlHostIndex to be called unless the expected element exists");
+            return -1;
         }
     }
 }
