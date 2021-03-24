4336d780726baa7d45f95dfc4e736028069f34ee
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-03-24 16:38:42 +0000

    WIP Add new project with extra VSIX and embed the new VSIX in the existing VSIX

---
+++ src/Payload/PathExporter.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.Composition;
+
+namespace Payload
+{
+
+    internal class PathExporter
+    {
+        [Export("SonarLint.Payload.ExtensionPath")]
+        public string ExtensionPath => System.IO.Path.GetDirectoryName(typeof(PathExporter).Assembly.Location);
+    }
+}
