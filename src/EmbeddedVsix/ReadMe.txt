1cfa01fafeb65b2433beaafc757e03a534750ee1
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-03-29 16:59:30 +0100

    Export eslintbridge server file path
    Relates to #2246

--- src/EmbeddedVsix/ReadMe.txt
+++ src/EmbeddedVsix/ReadMe.txt
@@ -8,3 +8,18 @@ the SLVS VSIX if the additional files are embedded directly in that VSIX ("out o
 
 The files are dynamically added at build time. This doesn't affect how the VSIX is built, and is done to stop the Solution Explorer
 from freezing due to large number of files that are included.
+
+
+This project has a couple of unusual characteristics that affect how it is built and used:
+
+* the contents of this project should be very stable; we should only need to rebuild it when the referenced version of the SonarJS plugin changes.
+* building this project can be slow (it creates a VSIX containing thousands of files, which is then installed in the Experimental hive).
+
+These characteristics mean we want to build the project as infrequently as possible.
+
+This in turn means that this project should NOT reference any other project in the solution since they all change
+more frequently than the embedded analyzer version.
+
+However, the other projects do need to know where some of the files are shipped in this VSIX are installed
+e.g. the eslint-bridge server. To avoid having to create references between the projects, we are exporting
+simple properties via MEF using specific contract names.
---
+++ src/ExtendedVsix.UnitTests/TypeScriptPathsProviderTests.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
+using System.IO;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SonarLint.VisualStudio.Integration.UnitTests;
+
+namespace SonarLint.VisualStudio.AdditionalFiles.UnitTests
+{
+    [TestClass]
+    public class TypeScriptPathsProviderTests
+    {
+        [TestMethod]
+        public void EsLintBridgeServerPath_IsExportedByContractName()
+        {
+            var catalog = new TypeCatalog(typeof(TypeScriptPathsProvider), typeof(TestPathImporter));
+            var importer = new SingleObjectImporter<TestPathImporter>();
+
+            CompositionBatch batch = new CompositionBatch();
+            batch.AddPart(importer);
+            using (CompositionContainer container = new CompositionContainer(catalog))
+            {
+                container.Compose(batch);
+            }
+
+            importer.Import.EsLintBridgeServerPath.Should().EndWith(@"ts\bin\server");
+            Path.IsPathRooted(importer.Import.EsLintBridgeServerPath).Should().BeTrue();
+        }
+    }
+
+    [Export]
+    internal class TestPathImporter
+    {
+        [Import("SonarLint.TypeScript.EsLintBridgeServerPath")]
+        public string EsLintBridgeServerPath { get; set; }
+    }
+}
