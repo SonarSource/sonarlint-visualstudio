6fa47550817db9cf132d8cdc55a5d714d9f9c4a9
Rita Gorokhod <60586879+rita-sonarsource@users.noreply.github.com>
2021-03-29 14:32:07 +0200

    Implement EslintBridgeClient with AnalyzeJs endpoint (#2258)

---
+++ src/TypeScript/EslintBridgeClient/Contract/AnalysisResponse.cs
@@ -0,0 +1,103 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace SonarLint.VisualStudio.TypeScript.EslintBridgeClient.Contract
+{
+    internal class AnalysisResponse
+    {
+        // Other fields omitted
+        // i.e. Hightlight[], HighlightedSymbol[], Metrics, CpdToken[]
+
+        [JsonProperty("issues")]
+        public IEnumerable<Issue> Issues { get; set; }
+
+        [JsonProperty("parsingError")]
+        public ParsingError ParsingError { get; set; }
+    }
+
+    internal class ParsingError
+    {
+        [JsonProperty("line")]
+        public int Line { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("code")]
+        public ParsingErrorCode Code { get; set; }
+    }
+
+    internal enum ParsingErrorCode
+    {
+        PARSING,
+        MISSING_TYPESCRIPT,
+        UNSUPPORTED_TYPESCRIPT,
+        FAILING_TYPESCRIPT,
+        GENERAL_ERROR
+    }
+
+    internal class Issue
+    {
+        [JsonProperty("line")]
+        public int Line { get; set; }
+
+        [JsonProperty("column")]
+        public int Column { get; set; }
+
+        [JsonProperty("endLine")]
+        public int EndLine { get; set; }
+
+        [JsonProperty("endColumn")]
+        public int EndColumn { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("ruleId")]
+        public string RuleId { get; set; }
+
+        [JsonProperty("secondaryLocations")]
+        public IssueLocation[] SecondaryLocations { get; set; }
+
+        [JsonProperty("cost")]
+        public int? Cost { get; set; }
+    }
+
+    internal class IssueLocation
+    {
+        [JsonProperty("line")]
+        public int Line { get; set; }
+
+        [JsonProperty("column")]
+        public int Column { get; set; }
+
+        [JsonProperty("endLine")]
+        public int EndLine { get; set; }
+
+        [JsonProperty("endColumn")]
+        public int EndColumn { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+    }
+}
