c0959bc1dd7bc6a37f8a77102b06e77b1e5be989
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-04-01 12:18:09 +0100

    Added separate interfaces for TS and JS

--- src/TypeScript.UnitTests/Rules/RuleDefinitionsProviderTests.cs
+++ src/TypeScript.UnitTests/Rules/RuleDefinitionsProviderTests.cs
@@ -28,26 +28,60 @@ using SonarLint.VisualStudio.TypeScript.Rules;
 namespace SonarLint.VisualStudio.TypeScript.UnitTests.Rules
 {
     [TestClass]
-    public class RuleDefinitionProviderTests
+    public class RuleDefinitionsProviderTests
     {
         [TestMethod]
-        public void MefCtor_CheckIsExported()
+        public void MefCtor_CheckInterfaceIsExported_TypeScript()
         {
-            var jsonFilePath = GetValidMetadataFilePath();
+            CheckTypeIsExported<ITypeScriptRuleDefinitionsProvider>();
+        }
 
-            MefTestHelpers.CheckTypeCanBeImported<RuleDefinitionProvider, IRuleDefinitionProvider>(null, new[]
+        [TestMethod]
+        public void MefCtor_CheckInterfaceIsExported_JavaScript()
+        {
+            CheckTypeIsExported<IJavaScriptRuleDefinitionsProvider>();
+        }
+
+        private static void CheckTypeIsExported<T>() where T : class
+        {
+            var jsonFilePath = GetRuleDefinitionFilePath("RuleDefns_Valid.json");
+
+            MefTestHelpers.CheckTypeCanBeImported<RuleDefinitionsProvider, T>(null, new[]
             {
-                MefTestHelpers.CreateExport<string>(jsonFilePath, RuleDefinitionProvider.RuleDefinitionsFilePathContractName)
+                MefTestHelpers.CreateExport<string>(jsonFilePath, RuleDefinitionsProvider.RuleDefinitionsFilePathContractName)
             });
         }
 
         [TestMethod]
-        public void GetAllRules_ReturnsExpectedDefinitions()
+        public void GetAllRules_SameProviderInstances_ReturnsExpectedRulesForLanguage()
         {
-            var jsonFilePath = GetValidMetadataFilePath();
+            var jsonFilePath = GetRuleDefinitionFilePath("RuleDefns_CheckLanguageFiltering.json");
 
-            var testSubject = new RuleDefinitionProvider(jsonFilePath);
-            var result = testSubject.GetAllRules().ToArray();
+            var testSubject = new RuleDefinitionsProvider(jsonFilePath);
+
+            // 1. TypeScript
+            var tsProvider = (ITypeScriptRuleDefinitionsProvider)testSubject;
+            var tsRuleKeys = tsProvider.GetDefinitions()
+                .Select(x => x.RuleKey);
+
+            tsRuleKeys.Should().BeEquivalentTo("typescript:S2092", "typescript:S3524", "TypeSCRIPT:S1135");
+
+            // 2. TypeScript
+            var jsProvider = (IJavaScriptRuleDefinitionsProvider)testSubject;
+            var jsRuleKeys = jsProvider.GetDefinitions()
+                .Select(x => x.RuleKey);
+
+            jsRuleKeys.Should().BeEquivalentTo("javascript:S1135", "JAVASCRIPT:xyz");
+        }
+
+        [TestMethod]
+        public void GetAllRules_ReturnsExpectedProperties()
+        {
+            // Checking the detailed definition properties for a single language
+
+            var jsonFilePath = GetRuleDefinitionFilePath("RuleDefns_CheckDetailedProperties.json");
+            var testSubject = (ITypeScriptRuleDefinitionsProvider)new RuleDefinitionsProvider(jsonFilePath);
+            var result = testSubject.GetDefinitions().ToArray();
 
             result.Should().HaveCount(2);
 
@@ -74,12 +108,12 @@ namespace SonarLint.VisualStudio.TypeScript.UnitTests.Rules
             result[1].ActivatedByDefault.Should().BeFalse();
         }
 
-        private static string GetValidMetadataFilePath()
+        private static string GetRuleDefinitionFilePath(string fileName)
         {
             var sampleJsonFile = Path.Combine(
-                Path.GetDirectoryName(typeof(RuleDefinitionProviderTests).Assembly.Location),
+                Path.GetDirectoryName(typeof(RuleDefinitionsProviderTests).Assembly.Location),
                 "" +
-                "Rules", "SampleRuleMetadata.json");
+                "Rules", fileName);
 
             File.Exists(sampleJsonFile).Should().BeTrue("Test setup error: could not find sample data file: " + sampleJsonFile);
             return sampleJsonFile;
