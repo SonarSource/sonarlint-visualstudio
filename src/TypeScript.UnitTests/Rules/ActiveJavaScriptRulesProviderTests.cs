5139c5d4c43741d6378f16654bbf91b47b044932
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-04-15 11:32:14 +0100

    Apply settings.json when calculating active JS rules
    Part of #771

--- src/TypeScript.UnitTests/Rules/ActiveJavaScriptRulesProviderTests.cs
+++ src/TypeScript.UnitTests/Rules/ActiveJavaScriptRulesProviderTests.cs
@@ -23,7 +23,9 @@ using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.Utilities;
 using Moq;
+using SonarLint.VisualStudio.Core;
 using SonarLint.VisualStudio.Integration.UnitTests;
 using SonarLint.VisualStudio.TypeScript.EslintBridgeClient.Contract;
 using SonarLint.VisualStudio.TypeScript.Rules;
@@ -33,17 +35,20 @@ namespace SonarLint.VisualStudio.TypeScript.UnitTests.Rules
     [TestClass]
     public class ActiveJavaScriptRulesProviderTests
     {
+        private static readonly IUserSettingsProvider EmptyUserSettingsProvider = CreateUserSettingsProvider(null);
+
         [TestMethod]
         public void MefCtor_CheckIsExported()
         {
             MefTestHelpers.CheckTypeCanBeImported<ActiveJavaScriptRulesProvider, IActiveJavaScriptRulesProvider>(null, new[]
             {
-                MefTestHelpers.CreateExport<IJavaScriptRuleDefinitionsProvider>(Mock.Of<IJavaScriptRuleDefinitionsProvider>())
+                MefTestHelpers.CreateExport<IJavaScriptRuleDefinitionsProvider>(Mock.Of<IJavaScriptRuleDefinitionsProvider>()),
+                MefTestHelpers.CreateExport<IUserSettingsProvider>(Mock.Of<IUserSettingsProvider>())
             });
         }
 
         [TestMethod]
-        public void Get_InactiveRulesAreNotReturned()
+        public void Get_DefaultRulesConfig_InactiveRulesAreNotReturned()
         {
             var ruleDefns = new RuleDefinitionsBuilder();
             ruleDefns.AddRule("active 1", activeByDefault: true);
@@ -51,7 +56,7 @@ namespace SonarLint.VisualStudio.TypeScript.UnitTests.Rules
             ruleDefns.AddRule("active 2", activeByDefault: true);
             ruleDefns.AddRule("inactive BBB", activeByDefault: false);
 
-            var testSubject = new ActiveJavaScriptRulesProvider(ruleDefns);
+            var testSubject = new ActiveJavaScriptRulesProvider(ruleDefns, EmptyUserSettingsProvider);
 
             var result = testSubject.Get().ToArray();
 
@@ -60,7 +65,7 @@ namespace SonarLint.VisualStudio.TypeScript.UnitTests.Rules
         }
 
         [TestMethod]
-        public void Get_HotspotsAreNotReturned()
+        public void Get_DefaultRulesConfig_HotspotsAreNotReturned()
         {
             // NOTE: there are currently no taint vulnerabilities in the SonarJS jar
             var ruleDefns = new RuleDefinitionsBuilder();
@@ -69,7 +74,7 @@ namespace SonarLint.VisualStudio.TypeScript.UnitTests.Rules
             ruleDefns.AddRule("hotspot", ruleType: RuleType.SECURITY_HOTSPOT);
             ruleDefns.AddRule("vuln", ruleType: RuleType.VULNERABILITY);
 
-            var testSubject = new ActiveJavaScriptRulesProvider(ruleDefns);
+            var testSubject = new ActiveJavaScriptRulesProvider(ruleDefns, EmptyUserSettingsProvider);
 
             var result = testSubject.Get().ToArray();
 
@@ -78,14 +83,14 @@ namespace SonarLint.VisualStudio.TypeScript.UnitTests.Rules
         }
 
         [TestMethod]
-        public void Get_RulesWithNullEslintKeysAreNotReturned()
+        public void Get_DefaultRulesConfig_RulesWithNullEslintKeysAreNotReturned()
         {
             var ruleDefns = new RuleDefinitionsBuilder();
             ruleDefns.AddRule("aaa");
             ruleDefns.AddRule(null);
             ruleDefns.AddRule("bbb");
 
-            var testSubject = new ActiveJavaScriptRulesProvider(ruleDefns);
+            var testSubject = new ActiveJavaScriptRulesProvider(ruleDefns, EmptyUserSettingsProvider);
 
             var result = testSubject.Get().ToArray();
 
@@ -94,7 +99,7 @@ namespace SonarLint.VisualStudio.TypeScript.UnitTests.Rules
         }
 
         [TestMethod]
-        public void Get_RulesWithConfigurations_ExpectedConfigsReturned()
+        public void Get_DefaultRulesConfig_RulesWithConfigurations_ExpectedConfigsReturned()
         {
             var config1 = new object();
             var config2 = "a default rule value";
@@ -102,12 +107,86 @@ namespace SonarLint.VisualStudio.TypeScript.UnitTests.Rules
             var ruleDefns = new RuleDefinitionsBuilder();
             ruleDefns.AddRule("aaa", configurations: new object[] { config1, config2 });
 
-            var testSubject = new ActiveJavaScriptRulesProvider(ruleDefns);
+            var testSubject = new ActiveJavaScriptRulesProvider(ruleDefns, EmptyUserSettingsProvider);
 
             var result = testSubject.Get().ToArray();
 
             CheckExpectedRuleKeys(result, "aaa");
-            result.First().Configurations.Should().ContainInOrder(config1, config2);
+            result[0].Configurations.Should().ContainInOrder(config1, config2);
+        }
+
+        [TestMethod]
+        [DataRow(true, true)]
+        [DataRow(false, false)]
+        [DataRow(false, true)]
+        [DataRow(true, false)]
+        public void Get_StandaloneMode_OverridesDefaultActivationLevel(
+            bool onByDefault, bool onInUserSettings)
+        {
+            // The value in the user settings should always override the default
+            var expectedRuleCount = onInUserSettings ? 1 : 0;
+
+            // Create definition and rule settings with a single rule
+            var ruleDefns = new RuleDefinitionsBuilder();
+            ruleDefns.AddRule(ruleKey: "javascript:rule1", activeByDefault: onByDefault, eslintKey: "esKey");
+
+            var userSettingsLevel = onInUserSettings ? RuleLevel.On : RuleLevel.Off;
+            var userConfig = new Dictionary<string, RuleConfig>
+                {
+                    {  "javascript:rule1", new RuleConfig { Level = userSettingsLevel } }
+                };
+            var userSettingsProvider = CreateUserSettingsProvider(userConfig);
+
+            var testSubject = new ActiveJavaScriptRulesProvider(ruleDefns, userSettingsProvider);
+
+            var result = testSubject.Get().ToArray();
+
+            result.Length.Should().Be(expectedRuleCount);
+        }
+
+        [TestMethod]
+        public void Get_StandaloneMode_IrrelevantSettingsAreIgnored()
+        {
+            var ruleDefns = new RuleDefinitionsBuilder();
+            ruleDefns.AddRule(ruleKey: "javascript:activeRule1", activeByDefault: true, eslintKey: "active1");
+
+            var userConfig = new Dictionary<string, RuleConfig>
+                {
+                    // Correct rule key but wrong repo -> ignored
+                    {  "wrongLanguage:activeRule1", new RuleConfig { Level = RuleLevel.Off } },
+
+                    // Right repo, but rule key doesn't match a defined rule -> ignored
+                    {  "javascript:unknownRule", new RuleConfig { Level = RuleLevel.On } }
+                };
+            var userSettingsProvider = CreateUserSettingsProvider(userConfig);
+
+            var testSubject = new ActiveJavaScriptRulesProvider(ruleDefns, userSettingsProvider);
+
+            var result = testSubject.Get().ToArray();
+
+            result.Length.Should().Be(1);
+            result[0].Key.Should().Be("active1");
+        }
+
+        [TestMethod]
+        public void Get_StandaloneMode_CannotEnableHotspotsOrRulesWithNullEslintKeys()
+        {
+            var ruleDefns = new RuleDefinitionsBuilder();
+            ruleDefns.AddRule(ruleKey: "javascript:hotspot1", activeByDefault: false, eslintKey: "hotspot1", ruleType: RuleType.SECURITY_HOTSPOT);
+            ruleDefns.AddRule(ruleKey: "javascript:S2260", activeByDefault: false, eslintKey: null);
+
+            var userConfig = new Dictionary<string, RuleConfig>
+                {
+                    {  "javascript:hotspot1", new RuleConfig { Level = RuleLevel.On } },
+                    {  "javascript:S2260", new RuleConfig { Level = RuleLevel.On } }
+                };
+            var userSettingsProvider = CreateUserSettingsProvider(userConfig);
+
+            var testSubject = new ActiveJavaScriptRulesProvider(ruleDefns, userSettingsProvider);
+
+            var result = testSubject.Get().ToArray();
+
+            result.Length.Should().Be(0);
         }
 
         private static void CheckExpectedRuleKeys(IEnumerable<Rule> result, params string[] expected) =>
@@ -122,12 +201,13 @@ namespace SonarLint.VisualStudio.TypeScript.UnitTests.Rules
 
             IEnumerable<RuleDefinition> IJavaScriptRuleDefinitionsProvider.GetDefinitions() => definitions;
 
-            public void AddRule(string eslintKey, bool activeByDefault = true, RuleType ruleType = RuleType.BUG,
-                object[] configurations = null)
+            public void AddRule(string eslintKey = "any", bool activeByDefault = true, RuleType ruleType = RuleType.BUG,
+                object[] configurations = null, string ruleKey = "any")
             {
                 configurations ??= Array.Empty<object>();
                 var newDefn = new RuleDefinition
                 {
+                    RuleKey = ruleKey,
                     EslintKey = eslintKey,
                     ActivatedByDefault = activeByDefault,
                     Type = ruleType,
@@ -136,6 +216,25 @@ namespace SonarLint.VisualStudio.TypeScript.UnitTests.Rules
 
                 definitions.Add(newDefn);
             }
+        }
+
+        private static IUserSettingsProvider CreateUserSettingsProvider(Dictionary<string, RuleConfig> rules)
+        {
+            var provider = new Mock<IUserSettingsProvider>();
+
+            var rulesSettings = new RulesSettings();
+            if (rules != null)
+            {
+                foreach (var item in rules)
+                {
+                    rulesSettings.Rules.Add(item.Key, item.Value);
+                }
+            }
+
+            var userSettings = new UserSettings(rulesSettings);
+            provider.Setup(x => x.UserSettings).Returns(userSettings);
+
+            return provider.Object;
         }
     }
 }
