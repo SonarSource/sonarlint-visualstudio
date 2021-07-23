328a3ce4f57b55f0e7f671aabd824bf7e5a0d7dd
Rita Gorokhod <60586879+rita-sonarsource@users.noreply.github.com>
2021-07-23 10:48:03 +0200

    Locate Compilation Database file (#2514)

---
+++ src/CFamily.UnitTests/CMake/CompilationDatabaseLocatorTests.cs
@@ -0,0 +1,216 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.IO;
+using System.IO.Abstractions;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Newtonsoft.Json;
+using SonarLint.VisualStudio.CFamily.CMake;
+using SonarLint.VisualStudio.Infrastructure.VS;
+using SonarLint.VisualStudio.Integration;
+using SonarLint.VisualStudio.Integration.UnitTests;
+
+namespace SonarLint.VisualStudio.CFamily.UnitTests.CMake
+{
+    [TestClass]
+    public class CompilationDatabaseLocatorTests
+    {
+        private const string RootDirectory = "dummy root";
+
+        [TestMethod]
+        public void MefCtor_CheckIsExported()
+        {
+            MefTestHelpers.CheckTypeCanBeImported<CompilationDatabaseLocator, ICompilationDatabaseLocator>(null, new[]
+            {
+                MefTestHelpers.CreateExport<IFolderWorkspaceService>(Mock.Of<IFolderWorkspaceService>()),
+                MefTestHelpers.CreateExport<ILogger>(Mock.Of<ILogger>())
+            });
+        }
+
+        [TestMethod]
+        [DataRow(null)]
+        [DataRow("")]
+        public void Locate_NoRootDirectory_Null(string rootDirectory)
+        {
+            var logger = new TestLogger();
+            var testSubject = CreateTestSubject(rootDirectory, logger: logger);
+
+            var result = testSubject.Locate();
+
+            result.Should().BeNull();
+            logger.AssertOutputStringExists(Resources.NoRootDirectory);
+        }
+
+        [TestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public void Locate_NoCMakeSettingsFile_ReturnsDefaultLocationIfItExists(bool defaultFileExists)
+        {
+            var activeConfiguration = CompilationDatabaseLocator.VSDefaultConfiguration;
+            var defaultLocation = GetDefaultDatabaseFileLocation(activeConfiguration);
+
+            var fileSystem = new Mock<IFileSystem>();
+            fileSystem.Setup(x => x.File.Exists(It.IsAny<string>())).Returns(false);
+            fileSystem.Setup(x => x.File.Exists(defaultLocation)).Returns(defaultFileExists);
+
+            var logger = new TestLogger();
+            var testSubject = CreateTestSubject(RootDirectory, fileSystem.Object, logger);
+
+            var result = testSubject.Locate();
+
+            if (defaultFileExists)
+            {
+                result.Should().Be(defaultLocation);
+                logger.AssertOutputStringExists(string.Format(Resources.FoundCompilationDatabaseFile, defaultLocation));
+            }
+            else
+            {
+                result.Should().BeNull();
+                logger.AssertOutputStringExists(string.Format(Resources.NoCompilationDatabaseFile, defaultLocation));
+            }
+
+            fileSystem.Verify(x=> x.File.Exists(defaultLocation), Times.Once);
+        }
+
+        [TestMethod]
+        public void Locate_HasCMakeSettingsFile_ActiveConfigurationDoesNotExist_Null()
+        {
+            var cmakeSettingsLocation = GetCmakeSettingsLocation(RootDirectory);
+
+            var fileSystem = new Mock<IFileSystem>();
+            SetupCMakeSettingsFileExists(fileSystem, cmakeSettingsLocation, new CMakeSettings());
+
+            var logger = new TestLogger();
+            var testSubject = CreateTestSubject(RootDirectory, fileSystem.Object, logger);
+
+            var result = testSubject.Locate();
+
+            result.Should().BeNull();
+            logger.AssertOutputStringExists(string.Format(Resources.NoBuildConfigInCMakeSettings,
+                CompilationDatabaseLocator.VSDefaultConfiguration, 
+                CompilationDatabaseLocator.CMakeSettingsFileName));
+
+            fileSystem.Verify(x=> x.File.ReadAllText(cmakeSettingsLocation), Times.Once);
+        }
+
+        [TestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public void Locate_HasCMakeSettingsFile_ReturnsConfiguredPathIfItExists(bool configuredPathExists)
+        {
+            var activeConfiguration = CompilationDatabaseLocator.VSDefaultConfiguration;
+            var cmakeSettings = CreateCMakeSettings(activeConfiguration, "folder");
+            var cmakeSettingsLocation = GetCmakeSettingsLocation(RootDirectory);
+
+            var fileSystem = new Mock<IFileSystem>();
+            SetupCMakeSettingsFileExists(fileSystem, cmakeSettingsLocation, cmakeSettings);
+
+            var compilationDatabaseFullLocation = Path.GetFullPath(
+                Path.Combine("folder", CompilationDatabaseLocator.CompilationDatabaseFileName));
+
+            fileSystem.Setup(x => x.File.Exists(compilationDatabaseFullLocation)).Returns(configuredPathExists);
+
+            var logger = new TestLogger();
+            var testSubject = CreateTestSubject(RootDirectory, fileSystem.Object, logger);
+
+            var result = testSubject.Locate();
+
+            if (configuredPathExists)
+            {
+                result.Should().Be(compilationDatabaseFullLocation);
+                logger.AssertOutputStringExists(string.Format(Resources.FoundCompilationDatabaseFile, compilationDatabaseFullLocation));
+            }
+            else
+            {
+                result.Should().BeNull();
+                logger.AssertOutputStringExists(string.Format(Resources.NoCompilationDatabaseFile, compilationDatabaseFullLocation));
+            }
+
+            fileSystem.Verify(x => x.File.Exists(compilationDatabaseFullLocation), Times.Once);
+        }
+
+        [TestMethod]
+        [DataRow("c:\\absolute\\", "c:\\absolute\\")]
+        [DataRow("relative", "relative")]
+        [DataRow("c:\\absolute\\${name}\\${projectDir}", "c:\\absolute\\x64-Debug\\dummy root")]
+        [DataRow("c:\\${projectDir}\\somefolder\\${name}\\sub", "c:\\dummy root\\somefolder\\x64-Debug\\sub")]
+        [DataRow("${projectDir}\\somefolder\\${name}\\sub", "dummy root\\somefolder\\x64-Debug\\sub")]
+        public void Locate_HasCMakeSettingsFile_ConfiguredPathHasParameters_ParametersReplaced(string configuredPath, string expectedPath)
+        {
+            var activeConfig = "x64-Debug";
+            var cmakeSettings = CreateCMakeSettings(activeConfig, configuredPath);
+            var cmakeSettingsLocation = Path.GetFullPath(Path.Combine(RootDirectory, CompilationDatabaseLocator.CMakeSettingsFileName));
+
+            var fileSystem = new Mock<IFileSystem>();
+            SetupCMakeSettingsFileExists(fileSystem, cmakeSettingsLocation, cmakeSettings);
+
+            var compilationDatabaseFullLocation = Path.GetFullPath(Path.Combine(expectedPath, CompilationDatabaseLocator.CompilationDatabaseFileName));
+            fileSystem.Setup(x => x.File.Exists(compilationDatabaseFullLocation)).Returns(true);
+
+            var testSubject = CreateTestSubject(RootDirectory, fileSystem.Object);
+
+            var result = testSubject.Locate();
+
+            result.Should().Be(compilationDatabaseFullLocation);
+
+            fileSystem.Verify(x => x.File.Exists(compilationDatabaseFullLocation), Times.Once);
+        }
+
+        private static CMakeSettings CreateCMakeSettings(string activeConfigurationName, string buildRoot) =>
+            new CMakeSettings
+            {
+                Configurations = new[]
+                {
+                    new CMakeBuildConfiguration
+                    {
+                        Name = activeConfigurationName,
+                        BuildRoot = buildRoot
+                    }
+                }
+            };
+
+        private static string GetCmakeSettingsLocation(string rootDirectory) => 
+            Path.GetFullPath(Path.Combine(rootDirectory, CompilationDatabaseLocator.CMakeSettingsFileName));
+
+        private static string GetDefaultDatabaseFileLocation(string activeBuildConfiguration) =>
+            Path.GetFullPath(Path.Combine(
+                string.Format(CompilationDatabaseLocator.DefaultLocationFormat,
+                    RootDirectory,
+                    activeBuildConfiguration),
+                CompilationDatabaseLocator.CompilationDatabaseFileName));
+
+        private void SetupCMakeSettingsFileExists(Mock<IFileSystem> fileSystem, string cmakeSettingsLocation, CMakeSettings cmakeSettings)
+        {
+            fileSystem.Setup(x => x.File.Exists(cmakeSettingsLocation)).Returns(true);
+            fileSystem.Setup(x => x.File.ReadAllText(cmakeSettingsLocation)).Returns(JsonConvert.SerializeObject(cmakeSettings));
+        }
+        private CompilationDatabaseLocator CreateTestSubject(string rootDirectory, IFileSystem fileSystem = null, ILogger logger = null)
+        {
+            var folderWorkspaceService = new Mock<IFolderWorkspaceService>();
+            folderWorkspaceService.Setup(x => x.FindRootDirectory()).Returns(rootDirectory);
+
+            logger ??= Mock.Of<ILogger>();
+
+            return new CompilationDatabaseLocator(folderWorkspaceService.Object, fileSystem, logger);
+        }
+    }
+}
---
+++ src/CFamily/CMake/CompilationDatabaseLocator.cs
@@ -0,0 +1,130 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.ComponentModel.Composition;
+using System.IO;
+using System.IO.Abstractions;
+using System.Linq;
+using Newtonsoft.Json;
+using SonarLint.VisualStudio.Infrastructure.VS;
+using SonarLint.VisualStudio.Integration;
+
+namespace SonarLint.VisualStudio.CFamily.CMake
+{
+    internal interface ICompilationDatabaseLocator
+    {
+        /// <summary>
+        /// Returns absolute path to the compilation database file of the currently active build configuration.
+        /// Returns null if the file was not found.
+        /// </summary>
+        string Locate();
+    }
+
+    [Export(typeof(ICompilationDatabaseLocator))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    internal class CompilationDatabaseLocator : ICompilationDatabaseLocator
+    {
+        internal const string CompilationDatabaseFileName = "compile_commands.json";
+        internal const string CMakeSettingsFileName = "CMakeSettings.json";
+        internal const string VSDefaultConfiguration = "x64-Debug";
+        internal const string DefaultLocationFormat = "{0}\\out\\build\\{1}";
+
+        private readonly IFolderWorkspaceService folderWorkspaceService;
+        private readonly IFileSystem fileSystem;
+        private readonly ILogger logger;
+
+        [ImportingConstructor]
+        public CompilationDatabaseLocator(IFolderWorkspaceService folderWorkspaceService, ILogger logger)
+            : this(folderWorkspaceService, new FileSystem(), logger)
+        {
+        }
+
+        public CompilationDatabaseLocator(IFolderWorkspaceService folderWorkspaceService, IFileSystem fileSystem, ILogger logger)
+        {
+            this.folderWorkspaceService = folderWorkspaceService;
+            this.fileSystem = fileSystem;
+            this.logger = logger;
+        }
+
+        public string Locate()
+        {
+            var databaseFolderPath = GetCompilationDatabaseFolderPath();
+
+            if (string.IsNullOrEmpty(databaseFolderPath))
+            {
+                return null;
+            }
+
+            var compilationDatabaseFilePath = Path.Combine(databaseFolderPath, CompilationDatabaseFileName);
+
+            if (!fileSystem.File.Exists(compilationDatabaseFilePath))
+            {
+                logger.WriteLine(Resources.NoCompilationDatabaseFile, compilationDatabaseFilePath);
+
+                return null;
+            }
+
+            logger.WriteLine(Resources.FoundCompilationDatabaseFile, compilationDatabaseFilePath);
+
+            return compilationDatabaseFilePath;
+        }
+
+        public string GetCompilationDatabaseFolderPath()
+        {
+            var rootDirectory = folderWorkspaceService.FindRootDirectory();
+
+            if (string.IsNullOrEmpty(rootDirectory))
+            {
+                logger.WriteLine(Resources.NoRootDirectory);
+                return null;
+            }
+
+            var cmakeSettingsFullPath = Path.GetFullPath(Path.Combine(rootDirectory, CMakeSettingsFileName));
+            var activeConfiguration = VSDefaultConfiguration;
+
+            if (!fileSystem.File.Exists(cmakeSettingsFullPath))
+            {
+                var defaultLocation = Path.GetFullPath(string.Format(DefaultLocationFormat, rootDirectory, activeConfiguration));
+
+                logger.WriteLine(Resources.NoCMakeSettings, CMakeSettingsFileName, rootDirectory, defaultLocation);
+
+                return defaultLocation;
+            }
+
+            logger.WriteLine(Resources.ReadingCMakeSettings, cmakeSettingsFullPath);
+
+            var settingsString = fileSystem.File.ReadAllText(cmakeSettingsFullPath);
+            var settings = JsonConvert.DeserializeObject<CMakeSettings>(settingsString);
+            var buildConfiguration = settings.Configurations?.FirstOrDefault(x => x.Name == activeConfiguration);
+
+            if (buildConfiguration == null)
+            {
+                logger.WriteLine(Resources.NoBuildConfigInCMakeSettings, activeConfiguration, CMakeSettingsFileName);
+                return null;
+            }
+
+            var databaseFolderPath = Path.GetFullPath(buildConfiguration.BuildRoot
+                .Replace("${projectDir}", rootDirectory)
+                .Replace("${name}", buildConfiguration.Name));
+
+            return databaseFolderPath;
+        }
+    }
+}
