5ff326cabf573062c6d4d9a2db87d04971538858
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-08-17 11:05:09 +0100

    Added service to return info about the VS installation

---
+++ src/Infrastructure.VS.UnitTests/VsInfoServiceTests.cs
@@ -0,0 +1,86 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SonarLint.VisualStudio.Integration;
+using SonarLint.VisualStudio.Integration.UnitTests;
+
+namespace SonarLint.VisualStudio.Infrastructure.VS.UnitTests
+{
+    [TestClass]
+    public class VsInfoServiceTests
+    {
+        [TestMethod]
+        public void MefCtor_CheckIsExported()
+        {
+            MefTestHelpers.CheckTypeCanBeImported<VsInfoService, IVsInfoService>(null, new[]
+            {
+                MefTestHelpers.CreateExport<SVsServiceProvider>(CreateConfiguredServiceProvider("anypath").Object),
+                MefTestHelpers.CreateExport<ILogger>(Mock.Of<ILogger>())
+            });
+        }
+
+        [TestMethod]
+        public void Create_VsShellCallSucceeds_ReturnsExpectedPath()
+        {
+            var logger = new TestLogger();
+            var serviceProvider = CreateConfiguredServiceProvider("c:\\test\\");
+
+            var testSubject = new VsInfoService(serviceProvider.Object, logger);
+
+            testSubject.InstallRootDir.Should().Be("c:\\test\\");
+            logger.AssertNoOutputMessages();
+        }
+
+        [TestMethod]
+        public void Create_VsShellCallFails_ReturnsNull()
+        {
+            var logger = new TestLogger();
+            var serviceProvider = CreateConfiguredServiceProvider("c:\\test\\", shellHrResult: -123);
+
+            var testSubject = new VsInfoService(serviceProvider.Object, logger);
+
+            testSubject.InstallRootDir.Should().BeNull();
+            logger.AssertPartialOutputStringExists("-123");
+        }
+
+        private Mock<IServiceProvider> CreateConfiguredServiceProvider(string installDirectory, int shellHrResult = VSConstants.S_OK)
+        {
+            object installDir = installDirectory;
+            var vsShell = new Mock<IVsShell>();
+            vsShell.Setup(x => x.GetProperty((int)__VSSPROPID2.VSSPROPID_InstallRootDir, out installDir)).Returns(shellHrResult);
+
+            var serviceProvider = new Mock<IServiceProvider>();
+            serviceProvider.Setup(x => x.GetService(typeof(SVsShell))).Returns(vsShell.Object);
+
+            return serviceProvider;
+        }
+    }
+}
---
+++ src/Infrastructure.VS/VsInfoService.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using SonarLint.VisualStudio.Integration;
+
+namespace SonarLint.VisualStudio.Infrastructure.VS
+{
+    /// <summary>
+    /// Returns information about the current VS installation
+    /// </summary>
+    public interface IVsInfoService
+    {
+        /// <summary>
+        /// Returns the root directory for executing VS installation
+        /// </summary>
+        string InstallRootDir { get; }
+    }
+
+    [Export(typeof(IVsInfoService))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    internal class VsInfoService : IVsInfoService
+    {
+        private readonly ILogger logger;
+
+        [ImportingConstructor]
+        public VsInfoService([Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider, ILogger logger)
+        {
+            this.logger = logger;
+            InstallRootDir = GetInstallRootDir(serviceProvider);
+        }
+
+        public string InstallRootDir { get; }
+
+        private string GetInstallRootDir(IServiceProvider serviceProvider)
+        {
+            IVsShell shell = serviceProvider.GetService(typeof(SVsShell)) as IVsShell;
+
+            object value;
+            int hr = shell.GetProperty((int)__VSSPROPID2.VSSPROPID_InstallRootDir, out value);
+            if (ErrorHandler.Succeeded(hr))
+            {
+                return value as string;
+            }
+
+            logger.WriteLine(Resources.FailedToFetchVsInstallRootDir, hr);
+            return null;
+        }
+    }
+}
