916ef7b3206feb3515196e66213e0af14de6ab2b
Rita Gorokhod <60586879+rita-sonarsource@users.noreply.github.com>
2021-09-03 17:54:24 +0200

    Support additional simple macros (#2635)

--- src/CFamily.UnitTests/CMake/MacroEvaluatorTests.cs
+++ src/CFamily.UnitTests/CMake/MacroEvaluatorTests.cs
@@ -34,10 +34,10 @@ namespace SonarLint.VisualStudio.CFamily.UnitTests.CMake
         [DataRow("wrongPrefix", "name")]
         [DataRow("wrongPrefix", "projectDir")]
         [DataRow("projectDir", "")] // projectDir is a name, not a prefix
-        public void Evaluate_ParametersReplaced(string macroPrefix, string macroName)
+        public void Evaluate_UnrecognizedParameters_Null(string macroPrefix, string macroName)
         {
             var testSubject = new MacroEvaluator();
-            var context = new EvaluationContext("activeConfig", "rootDir");
+            var context = CreateContext();
 
             var result = testSubject.TryEvaluate(macroPrefix, macroName, context);
 
@@ -45,25 +45,133 @@ namespace SonarLint.VisualStudio.CFamily.UnitTests.CMake
         }
 
         [TestMethod]
-        public void Evaluate_ProjectDir_ExpectedValueReturned()
+        [DataRow(null)]
+        [DataRow("")]
+        public void Evaluate_NoMacroName_Null(string macroName)
         {
             var testSubject = new MacroEvaluator();
-            var context = new EvaluationContext("any", "rootDir");
+            var context = CreateContext();
+
+            var result = testSubject.TryEvaluate(string.Empty, macroName, context);
+
+            result.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void Evaluate_WorkspaceRoot_ExpectedValueReturned()
+        {
+            var testSubject = new MacroEvaluator();
+            var context = CreateContext(rootDirectory: "rootDir");
+
+            var result = testSubject.TryEvaluate(string.Empty, "workspaceRoot", context);
+
+            result.Should().Be("rootDir");
+        }
+
+        [TestMethod]
+        public void Evaluate_ProjectFile_ExpectedValueReturned()
+        {
+            var testSubject = new MacroEvaluator();
+            var context = CreateContext(cmakeLists: "c:\\someFile.txt");
+
+            var result = testSubject.TryEvaluate(string.Empty, "projectFile", context);
+
+            result.Should().Be("c:\\someFile.txt");
+        }
+
+        [TestMethod]
+        [DataRow("c:\\someFile.txt", "c:\\")]
+        [DataRow("c:/someFile.txt", "c:\\")]
+        [DataRow("c:\\projectDir\\someFile.txt", "c:\\projectDir")]
+        [DataRow("c:\\projectDir\\sub\\someFile.txt", "c:\\projectDir\\sub")]
+        [DataRow("someFile.txt", "")]
+        public void Evaluate_ProjectDir_ExpectedValueReturned(string cmakeListsFilePath, string expected)
+        {
+            var testSubject = new MacroEvaluator();
+            var context = CreateContext(cmakeLists: cmakeListsFilePath);
 
             var result = testSubject.TryEvaluate(string.Empty, "projectDir", context);
 
-            result.Should().Be("rootDir");
+            result.Should().Be(expected);
+        }
+
+        [TestMethod]
+        [DataRow("c:\\someFile.txt", "c:\\")]
+        [DataRow("c:/someFile.txt", "c:\\")]
+        [DataRow("c:\\projectDir\\someFile.txt", "projectDir")]
+        [DataRow("c:\\projectDir\\sub\\someFile.txt", "sub")]
+        [DataRow("a\\someFile.txt", "a")]
+        [DataRow("a\\b\\someFile.txt", "b")]
+        [DataRow("a/someFile.txt", "a")]
+        public void Evaluate_ProjectDirName_ExpectedValueReturned(string cmakeListsFilePath, string expected)
+        {
+            var testSubject = new MacroEvaluator();
+            var context = CreateContext(cmakeLists: cmakeListsFilePath);
+
+            var result = testSubject.TryEvaluate(string.Empty, "projectDirName", context);
+
+            result.Should().Be(expected);
+        }
+
+        [TestMethod]
+
+        public void Evaluate_ProjectDirName_NoFolder_Null()
+        {
+            var testSubject = new MacroEvaluator();
+            var context = CreateContext(cmakeLists: "no folder.txt");
+
+            var result = testSubject.TryEvaluate(string.Empty, "projectDirName", context);
+
+            result.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void Evaluate_ThisFile_ExpectedValueReturned()
+        {
+            var testSubject = new MacroEvaluator();
+            var context = CreateContext(settingsJson: "c:\\someFile.txt");
+
+            var result = testSubject.TryEvaluate(string.Empty, "thisFile", context);
+
+            result.Should().Be("c:\\someFile.txt");
+        }
+
+        [TestMethod]
+        [DataRow("c:\\someFile.txt", "c:\\")]
+        [DataRow("c:/someFile.txt", "c:\\")]
+        [DataRow("c:\\projectDir\\someFile.txt", "c:\\projectDir")]
+        [DataRow("c:\\projectDir\\sub\\someFile.txt", "c:\\projectDir\\sub")]
+        [DataRow("someFile.txt", "")]
+        public void Evaluate_ThisFileDir_ExpectedValueReturned(string settingsFilePath, string expected)
+        {
+            var testSubject = new MacroEvaluator();
+            var context = CreateContext(settingsJson: settingsFilePath);
+
+            var result = testSubject.TryEvaluate(string.Empty, "thisFileDir", context);
+
+            result.Should().Be(expected);
         }
 
         [TestMethod]
         public void Evaluate_Name_ExpectedValueReturned()
         {
             var testSubject = new MacroEvaluator();
-            var context = new EvaluationContext("activeConfig", "any");
+            var context = CreateContext(activeConfig: "activeConfig");
 
             var result = testSubject.TryEvaluate(string.Empty, "name", context);
 
             result.Should().Be("activeConfig");
+        }
+
+        [TestMethod]
+        public void Evaluate_Generator_ExpectedValueReturned()
+        {
+            var testSubject = new MacroEvaluator();
+            var context = CreateContext(generator: "dummy-generator");
+
+            var result = testSubject.TryEvaluate(string.Empty, "generator", context);
+
+            result.Should().Be("dummy-generator");
         }
 
         [TestMethod]
@@ -76,7 +184,7 @@ namespace SonarLint.VisualStudio.CFamily.UnitTests.CMake
             envVariableProvider.Setup(x => x.TryGet("var name")).Returns("some value");
 
             var testSubject = new MacroEvaluator(envVariableProvider.Object);
-            var context = new EvaluationContext("any", "any");
+            var context = CreateContext();
 
             var result = testSubject.TryEvaluate(prefix, "var name", context);
 
@@ -91,7 +199,7 @@ namespace SonarLint.VisualStudio.CFamily.UnitTests.CMake
             var envVariableProvider = new Mock<IEnvironmentVariableProvider>();
 
             var testSubject = new MacroEvaluator(envVariableProvider.Object);
-            var context = new EvaluationContext("any", "any");
+            var context = CreateContext();
 
             var result = testSubject.TryEvaluate("env", macroName, context);
 
@@ -100,17 +208,11 @@ namespace SonarLint.VisualStudio.CFamily.UnitTests.CMake
             envVariableProvider.Invocations.Count.Should().Be(0);
         }
 
-        [TestMethod]
-        [DataRow(null)]
-        [DataRow("")]
-        public void Evaluate_NoMacroName_Null(string macroName)
-        {
-            var testSubject = new MacroEvaluator();
-            var context = new EvaluationContext("any", "any");
-
-            var result = testSubject.TryEvaluate(string.Empty, macroName, context);
-
-            result.Should().BeNull();
-        }
+        private EvaluationContext CreateContext(string activeConfig = "config",
+            string rootDirectory = "root",
+            string generator = "generator",
+            string cmakeLists = "c:\\someFile.txt",
+            string settingsJson = "c:\\someFile.txt") =>
+            new(activeConfig, rootDirectory, generator, cmakeLists, settingsJson);
     }
 }
