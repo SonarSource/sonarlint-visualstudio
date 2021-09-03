ff067b251b447ef4c6c27d876a71a8301e313b8a
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-09-03 09:27:41 +0100

    Added skeleton IMacroEvaluationService (#2626)
    * Added skeleton IMacroEvaluationService

--- src/CFamily.UnitTests/CMake/CompilationDatabaseLocatorTests.cs
+++ src/CFamily.UnitTests/CMake/CompilationDatabaseLocatorTests.cs
@@ -18,6 +18,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+using System;
 using System.IO;
 using System.IO.Abstractions;
 using FluentAssertions;
@@ -36,6 +37,9 @@ namespace SonarLint.VisualStudio.CFamily.UnitTests.CMake
     public class CompilationDatabaseLocatorTests
     {
         private const string RootDirectory = "dummy root";
+
+        // Macro evaluator that returns the input unchanged
+        private static readonly IMacroEvaluationService PassthroughMacroService = CreatePassthroughMacroService();
 
         [TestMethod]
         public void MefCtor_CheckIsExported()
@@ -67,7 +71,7 @@ namespace SonarLint.VisualStudio.CFamily.UnitTests.CMake
         {
             var activeConfiguration = "any";
             var configProvider = CreateConfigProvider(activeConfiguration);
-            var cmakeSettingsProvider = CreateCmakeSettingsProvider(null);
+            var cmakeSettingsProvider = CreateCMakeSettingsProvider(RootDirectory, null);
 
             var defaultLocation = GetDefaultDatabaseFileLocation(activeConfiguration);
 
@@ -88,14 +92,14 @@ namespace SonarLint.VisualStudio.CFamily.UnitTests.CMake
             }
 
             fileSystem.VerifyFileExistsCalledOnce(defaultLocation);
-            cmakeSettingsProvider.Verify(x=> x.Find(RootDirectory), Times.Once);
+            cmakeSettingsProvider.Verify(x => x.Find(RootDirectory), Times.Once);
         }
 
         [TestMethod]
         public void Locate_HasCMakeSettingsFile_ActiveConfigurationDoesNotExist_Null()
         {
             var configProvider = CreateConfigProvider("my-config");
-            var cmakeSettingsProvider = CreateCmakeSettingsProvider(
+            var cmakeSettingsProvider = CreateCMakeSettingsProvider(RootDirectory,
                 new CMakeSettingsSearchResult(new CMakeSettings(), "", ""));
 
             var logger = new TestLogger();
@@ -112,7 +116,7 @@ namespace SonarLint.VisualStudio.CFamily.UnitTests.CMake
         {
             var configProvider = CreateConfigProvider("my-config");
             var cmakeSettings = CreateCMakeSettings("my-config", buildRoot: null);
-            var cmakeSettingsProvider = CreateCmakeSettingsProvider(
+            var cmakeSettingsProvider = CreateCMakeSettingsProvider(RootDirectory,
                 new CMakeSettingsSearchResult(cmakeSettings, "", ""));
 
             var logger = new TestLogger();
@@ -131,7 +135,7 @@ namespace SonarLint.VisualStudio.CFamily.UnitTests.CMake
         {
             var configProvider = CreateConfigProvider("my-config");
             var cmakeSettings = CreateCMakeSettings("my-config", "folder");
-            var cmakeSettingsProvider = CreateCmakeSettingsProvider(
+            var cmakeSettingsProvider = CreateCMakeSettingsProvider(RootDirectory, 
                 new CMakeSettingsSearchResult(cmakeSettings, "", ""));
 
             var compilationDatabaseFullLocation = Path.GetFullPath(
@@ -140,7 +144,8 @@ namespace SonarLint.VisualStudio.CFamily.UnitTests.CMake
             var fileSystem = new Mock<IFileSystem>();
             fileSystem.SetFileExists(compilationDatabaseFullLocation, fileExists);
 
-            var testSubject = CreateTestSubject(RootDirectory, configProvider, cmakeSettingsProvider.Object, fileSystem.Object);
+            var testSubject = CreateTestSubject(RootDirectory, configProvider, cmakeSettingsProvider.Object, fileSystem.Object,
+                macroEvaluationService: PassthroughMacroService);
 
             var result = testSubject.Locate();
 
@@ -155,28 +160,46 @@ namespace SonarLint.VisualStudio.CFamily.UnitTests.CMake
         }
 
         [TestMethod]
-        [DataRow("c:\\absolute\\", "c:\\absolute\\")]
-        [DataRow("relative", "relative")]
-        [DataRow("c:\\absolute\\${name}\\${projectDir}", "c:\\absolute\\my-config\\dummy root")]
-        [DataRow("c:\\${projectDir}\\somefolder\\${name}\\sub", "c:\\dummy root\\somefolder\\my-config\\sub")]
-        [DataRow("${projectDir}\\somefolder\\${name}\\sub", "dummy root\\somefolder\\my-config\\sub")]
-        public void Locate_HasCMakeSettingsFile_ConfiguredPathHasParameters_ParametersReplaced(string configuredPath, string expectedPath)
+        public void Locate_MacroServiceIsCalled_EvaluatedPropertyIsNull_Null()
         {
-            var configProvider = CreateConfigProvider("my-config");
-            var cmakeSettings = CreateCMakeSettings("my-config", configuredPath);
-            var cmakeSettingsProvider = CreateCmakeSettingsProvider(
-                new CMakeSettingsSearchResult(cmakeSettings, "", ""));
+            var context = new MacroEvalContext(null, unevaluatedBuildRoot: "property that can't be evaluated");
 
-            var compilationDatabaseFullLocation = Path.GetFullPath(Path.Combine(expectedPath, CompilationDatabaseLocator.CompilationDatabaseFileName));
-            
-            var fileSystem = new Mock<IFileSystem>();
-            fileSystem.SetFileExists(compilationDatabaseFullLocation);
+            var result = context.TestSubject.Locate();
 
-            var testSubject = CreateTestSubject(RootDirectory, configProvider, cmakeSettingsProvider.Object, fileSystem.Object);
+            result.Should().BeNull();
+            context.MacroEvalService.Invocations.Count.Should().Be(1);
+            context.Logger.AssertOutputStringExists(string.Format(Resources.UnableToEvaluateBuildRootProperty, "property that can't be evaluated"));
+        }
 
-            var result = testSubject.Locate();
+        [TestMethod]
+        [DataRow("c:\\absolute", "c:\\absolute\\compile_commands.json")]
+        [DataRow("c:\\absolute\\", "c:\\absolute\\compile_commands.json")]
+        [DataRow("c:\\path", "c:\\path\\compile_commands.json")]
+        [DataRow("c:\\path\\", "c:\\path\\compile_commands.json")]
+        [DataRow("c:\\aaa\\..\\bbb", "c:\\bbb\\compile_commands.json")]  // non-canonical path
+        public void Locate_MacroServiceIsCalled_RootedPath_ExpectedValueIsReturn(string macroServiceReturnValue, string expectedResult)
+        {
+            var context = new MacroEvalContext(macroServiceReturnValue);
 
-            result.Should().Be(compilationDatabaseFullLocation);
+            var result = context.TestSubject.Locate();
+
+            result.Should().Be(expectedResult);
+            context.MacroEvalService.Invocations.Count.Should().Be(1);
+        }
+
+        [TestMethod]
+        [DataRow("relative", "relative\\compile_commands.json")]
+        [DataRow("relative\\", "relative\\compile_commands.json")]
+        [DataRow("aaa\\bbb\\.\\..\\ccc\\", "aaa\\ccc\\compile_commands.json")]
+        public void Locate_MacroServiceIsCalled_RelativePath_ExpectedValueIsReturn(string macroServiceReturnValue, string partialExpectedResult)
+        {
+            var context = new MacroEvalContext(macroServiceReturnValue);
+            var fullExpectedResult = Path.GetFullPath(partialExpectedResult);
+
+            var result = context.TestSubject.Locate();
+
+            result.Should().Be(fullExpectedResult);
+            context.MacroEvalService.Invocations.Count.Should().Be(1);
         }
 
         private static CMakeSettings CreateCMakeSettings(string activeConfigurationName, string buildRoot) =>
@@ -199,11 +222,12 @@ namespace SonarLint.VisualStudio.CFamily.UnitTests.CMake
                     activeBuildConfiguration),
                 CompilationDatabaseLocator.CompilationDatabaseFileName));
 
-        private CompilationDatabaseLocator CreateTestSubject(string rootDirectory, 
+        private static CompilationDatabaseLocator CreateTestSubject(string rootDirectory, 
             IBuildConfigProvider buildConfigProvider = null,
             ICMakeSettingsProvider cMakeSettingsProvider = null,
             IFileSystem fileSystem = null, 
-            ILogger logger = null)
+            ILogger logger = null,
+            IMacroEvaluationService macroEvaluationService = null)
         {
             var folderWorkspaceService = new Mock<IFolderWorkspaceService>();
             folderWorkspaceService.Setup(x => x.FindRootDirectory()).Returns(rootDirectory);
@@ -212,11 +236,12 @@ namespace SonarLint.VisualStudio.CFamily.UnitTests.CMake
             buildConfigProvider ??= Mock.Of<IBuildConfigProvider>();
             logger ??= Mock.Of<ILogger>();
             fileSystem ??= new FileSystem();
+            macroEvaluationService ??= Mock.Of<IMacroEvaluationService>();
 
-            return new CompilationDatabaseLocator(folderWorkspaceService.Object, buildConfigProvider, cMakeSettingsProvider, fileSystem, logger);
+            return new CompilationDatabaseLocator(folderWorkspaceService.Object, buildConfigProvider, cMakeSettingsProvider, macroEvaluationService, fileSystem, logger);
         }
 
-        private IBuildConfigProvider CreateConfigProvider(string activeConfiguration)
+        private static IBuildConfigProvider CreateConfigProvider(string activeConfiguration)
         {
             var provider = new Mock<IBuildConfigProvider>();
             provider.Setup(x => x.GetActiveConfig(It.IsAny<string>())).Returns(activeConfiguration);
@@ -224,12 +249,59 @@ namespace SonarLint.VisualStudio.CFamily.UnitTests.CMake
             return provider.Object;
         }
 
-        private static Mock<ICMakeSettingsProvider> CreateCmakeSettingsProvider(CMakeSettingsSearchResult result)
+        private static Mock<ICMakeSettingsProvider> CreateCMakeSettingsProvider(string rootDirectory, CMakeSettingsSearchResult result)
         {
             var cmakeSettingsProvider = new Mock<ICMakeSettingsProvider>();
-            cmakeSettingsProvider.Setup(x => x.Find(RootDirectory)).Returns(result);
+            cmakeSettingsProvider.Setup(x => x.Find(rootDirectory)).Returns(result);
 
             return cmakeSettingsProvider;
+        }
+
+        private static IMacroEvaluationService CreatePassthroughMacroService()
+        {
+            Func<string, string, string, string> passthrough = (input, _, _) => input;
+
+            var macroService = new Mock<IMacroEvaluationService>();
+            macroService.Setup(x => x.Evaluate(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(passthrough);
+
+            return macroService.Object;
+        }
+
+        /// <summary>
+        /// Sets up the file and config mocks so that the test subject will call the
+        /// macro evaluation service to process property
+        /// </summary>
+        private class MacroEvalContext
+        {
+            public MacroEvalContext(string macroServiceReturnValue, string unevaluatedBuildRoot = "any")
+            {
+                const string ActiveConfig = "my-active-config";
+                const string WorkspaceRootDir = "c:\\workspace root";
+
+                var configProvider = CreateConfigProvider(ActiveConfig);
+                var cmakeSettings = CreateCMakeSettings(ActiveConfig, unevaluatedBuildRoot);
+                var cmakeSettingsProvider = CreateCMakeSettingsProvider(WorkspaceRootDir,
+                    new CMakeSettingsSearchResult(cmakeSettings, "", ""));
+
+                // Treat all files as existing
+                var fileSystem = new Mock<IFileSystem>();
+                Func<string, bool> nonNullFilesExist = x => x != null;
+                fileSystem.Setup(x => x.File.Exists(It.IsAny<string>())).Returns(nonNullFilesExist);
+ 
+                MacroEvalService = new Mock<IMacroEvaluationService>();
+                MacroEvalService.Setup(x => x.Evaluate(unevaluatedBuildRoot, ActiveConfig, WorkspaceRootDir))
+                    .Returns(macroServiceReturnValue);
+
+                Logger = new TestLogger(logToConsole: true);
+
+                TestSubject = CreateTestSubject(WorkspaceRootDir, configProvider, cmakeSettingsProvider.Object,
+                    fileSystem.Object, Logger, MacroEvalService.Object);
+            }
+
+            public CompilationDatabaseLocator TestSubject { get; }
+            public Mock<IMacroEvaluationService> MacroEvalService { get; }
+            public TestLogger Logger { get; }
         }
     }
 }
---
+++ src/CFamily/CMake/MacroEvaluationService.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace SonarLint.VisualStudio.CFamily.CMake
+{
+    internal interface IMacroEvaluationService
+    {
+        /// <summary>
+        /// Evaluates any macros and environment variables in the input and returns the expanded
+        /// value
+        /// </summary>
+        /// <param name="input">The string to be evaluated</param>
+        /// <param name="activeConfiguration">The current build configuration</param>
+        /// <param name="workspaceRootDir">The workspace root directory</param>
+        /// <returns>The evaluated result, or null if the input contained properties that could not be evaluated.</returns>
+        string Evaluate(string input, string activeConfiguration, string workspaceRootDir);
+    }
+
+    internal class MacroEvaluationService : IMacroEvaluationService
+    {
+        public string Evaluate(string input, string activeConfiguration, string workspaceRootDir)
+        {
+            if (input == null)
+            {
+                return null;
+            }
+            
+            var output = input
+                .Replace("${projectDir}", workspaceRootDir)
+                .Replace("${name}", activeConfiguration);
+            return output;
+        }
+    }
+}
