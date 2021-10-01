8c2efd79ef61b8b80d3bc80363f4644185d29b59
Rita Gorokhod <60586879+rita-sonarsource@users.noreply.github.com>
2021-10-01 15:20:24 +0200

    Reference SonarLint.Secrets nuget package (#2686)

---
+++ src/CloudSecrets/Analyzer.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Threading;
+using SonarLint.Secrets.DotNet;
+using SonarLint.VisualStudio.Core.Analysis;
+
+namespace SonarLint.VisualStudio.CloudSecrets
+{
+    [Export(typeof(IAnalyzer))]
+    internal class Analyzer : IAnalyzer
+    {
+        private readonly DummySecretDetector dummySecretDetector;
+
+        public Analyzer()
+        {
+           dummySecretDetector = new DummySecretDetector();
+        }
+
+        public bool IsAnalysisSupported(IEnumerable<AnalysisLanguage> languages)
+        {
+            return false;
+        }
+
+        public void ExecuteAnalysis(string path, string charset, IEnumerable<AnalysisLanguage> detectedLanguages, IIssueConsumer consumer,
+            IAnalyzerOptions analyzerOptions, CancellationToken cancellationToken)
+        {
+        }
+    }
+}
