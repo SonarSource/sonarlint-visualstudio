728c9eec0ee814c556d2ec965cec49d15a539884
Rita Gorokhod <rita.gorokhod@sonarsource.com>
2021-10-05 08:56:00 +0200

    Add SecretsAnalyzer

---
+++ src/CloudSecrets.UnitTests/SecretsAnalyzerTests.cs
@@ -0,0 +1,208 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Threading;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SonarLint.Secrets.DotNet;
+using SonarLint.VisualStudio.Core.Analysis;
+using SonarLint.VisualStudio.Integration.UnitTests;
+
+namespace SonarLint.VisualStudio.CloudSecrets.UnitTests
+{
+    [TestClass]
+    public class SecretsAnalyzerTests
+    {
+        private const string ValidFilePath = "c:\\test";
+        private const string ValidFileContent = "this is a content";
+
+        [TestMethod]
+        public void MefCtor_CheckIsExported()
+        {
+            MefTestHelpers.CheckTypeCanBeImported<SecretsAnalyzer, IAnalyzer>(null, new[]
+            {
+                MefTestHelpers.CreateExport<IFileSystem>(Mock.Of<IFileSystem>()),
+                MefTestHelpers.CreateExport<IAnalysisStatusNotifier>(Mock.Of<IAnalysisStatusNotifier>()),
+            });
+        }
+
+        [TestMethod]
+        public void IsAnalysisSupported_NoLanguages_True()
+        {
+            var testSubject = CreateTestSubject();
+
+            var languages = Array.Empty<AnalysisLanguage>();
+            var result = testSubject.IsAnalysisSupported(languages);
+
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void IsAnalysisSupported_AnyLanguage_True()
+        {
+            var testSubject = CreateTestSubject();
+
+            var languages = new[] { (AnalysisLanguage)123456879 };
+            var result = testSubject.IsAnalysisSupported(languages);
+
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void ExecuteAnalysis_AllDetectorsAreCalled()
+        {
+            var fileSystem = SetupFileSystem(ValidFilePath, ValidFileContent);
+
+            var consumer = new Mock<IIssueConsumer>();
+
+            var secretDetectors = new[]
+            {
+                SetupSecretDetector(ValidFileContent),
+                SetupSecretDetector(ValidFileContent),
+                SetupSecretDetector(ValidFileContent)
+            };
+
+            ExecuteAnalysis(ValidFilePath, consumer.Object, fileSystem, secretDetectors: secretDetectors);
+
+            foreach (var detector in secretDetectors)
+            {
+                detector.Verify(x=> x.Find(ValidFileContent), Times.Once());
+            }
+        }
+
+        [TestMethod]
+        public void ExecuteAnalysis_ResponseWithNoIssues_ConsumerNotCalled()
+        {
+            var fileSystem = SetupFileSystem(ValidFilePath, ValidFileContent);
+
+            var consumer = new Mock<IIssueConsumer>();
+
+            var secretDetector = SetupSecretDetector(ValidFileContent);
+
+            ExecuteAnalysis(ValidFilePath, consumer.Object, fileSystem, secretDetectors: secretDetector);
+
+            secretDetector.Verify(x=> x.Find(ValidFileContent), Times.Once);
+            consumer.Invocations.Count.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void ExecuteAnalysis_ResponseWithIssues_ConsumerCalled()
+        {
+            var fileSystem = SetupFileSystem(ValidFilePath, ValidFileContent);
+
+            var consumer = new Mock<IIssueConsumer>();
+
+            var secretDetector = SetupSecretDetector(ValidFileContent, Mock.Of<ISecret>());
+
+            ExecuteAnalysis(ValidFilePath, consumer.Object, fileSystem, secretDetectors: secretDetector);
+
+            secretDetector.Verify(x => x.Find(ValidFileContent), Times.Once);
+            consumer.Verify(x => x.Accept(ValidFilePath, It.IsAny<IEnumerable<IAnalysisIssue>>()));
+        }
+
+        [TestMethod]
+        public void ExecuteAnalysis_CriticalException_ExceptionThrown()
+        {
+            var fileSystem = new Mock<IFileSystem>();
+            fileSystem.Setup(x => x.File.ReadAllText(ValidFilePath)).Throws<StackOverflowException>();
+
+            var secretDetector = SetupSecretDetector(ValidFileContent, Mock.Of<ISecret>());
+
+            Action act = () => ExecuteAnalysis(ValidFilePath, null, fileSystem.Object, secretDetectors: secretDetector);
+            act.Should().ThrowExactly<StackOverflowException>();
+        }
+
+        [TestMethod]
+        public void ExecuteAnalysis_AnalysisFailed_NotifiesThatAnalysisFailed()
+        {
+            var statusNotifier = new Mock<IAnalysisStatusNotifier>();
+            var exception = new NotImplementedException("this is a test");
+
+            var fileSystem = new Mock<IFileSystem>();
+            fileSystem.Setup(x => x.File.ReadAllText(ValidFilePath)).Throws(exception);
+
+            var consumer = new Mock<IIssueConsumer>();
+
+            ExecuteAnalysis(ValidFilePath, consumer.Object, fileSystem.Object, statusNotifier.Object);
+
+            statusNotifier.Verify(x => x.AnalysisStarted(ValidFilePath), Times.Once);
+            statusNotifier.Verify(x => x.AnalysisFailed(ValidFilePath, exception), Times.Once);
+            statusNotifier.VerifyNoOtherCalls();
+        }
+
+        [TestMethod]
+        public void ExecuteAnalysis_AnalysisFinished_NotifiesThatAnalysisFinished()
+        {
+            var fileSystem = SetupFileSystem(ValidFilePath, ValidFileContent);
+            var statusNotifier = new Mock<IAnalysisStatusNotifier>();
+
+            var consumer = new Mock<IIssueConsumer>();
+
+            var secrets = new[] { Mock.Of<ISecret>(), Mock.Of<ISecret>() };
+            var secretDetector = SetupSecretDetector(ValidFileContent, secrets);
+
+            ExecuteAnalysis(ValidFilePath, consumer.Object, fileSystem, statusNotifier.Object, secretDetector);
+
+            statusNotifier.Verify(x => x.AnalysisStarted(ValidFilePath), Times.Once);
+            statusNotifier.Verify(x => x.AnalysisFinished(ValidFilePath, secrets.Length, It.IsAny<TimeSpan>()), Times.Once);
+            statusNotifier.VerifyNoOtherCalls();
+        }
+
+        private void ExecuteAnalysis(string filePath, IIssueConsumer consumer, IFileSystem fileSystem, IAnalysisStatusNotifier statusNotifier = null, params Mock<ISecretDetector>[] secretDetectors)
+        {
+            var testSubject = CreateTestSubject(statusNotifier, fileSystem, secretDetectors);
+            testSubject.ExecuteAnalysis(filePath, "", Array.Empty<AnalysisLanguage>(), consumer, null, CancellationToken.None);
+        }
+
+        private SecretsAnalyzer CreateTestSubject(IAnalysisStatusNotifier statusNotifier = null,
+            IFileSystem fileSystem = null,
+            IEnumerable<Mock<ISecretDetector>> detectors = null)
+        {
+            statusNotifier ??= Mock.Of<IAnalysisStatusNotifier>();
+            fileSystem ??= Mock.Of<IFileSystem>();
+            detectors ??= Array.Empty<Mock<ISecretDetector>>();
+
+            return new SecretsAnalyzer(detectors.Select(x=> x.Object),
+                statusNotifier,
+                fileSystem);
+        }
+
+        private Mock<ISecretDetector> SetupSecretDetector(string input, params ISecret[] secrets)
+        {
+            var detector = new Mock<ISecretDetector>();
+            detector.Setup(x => x.Find(input)).Returns(secrets);
+
+            return detector;
+        }
+
+        private IFileSystem SetupFileSystem(string filePath, string content)
+        {
+            var fileSystem = new Mock<IFileSystem>();
+            fileSystem.Setup(x => x.File.ReadAllText(filePath)).Returns(content);
+
+            return fileSystem.Object;
+        }
+    }
+}
---
+++ src/CloudSecrets/SecretsAnalyzer.cs
@@ -0,0 +1,119 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Threading;
+using SonarLint.Secrets.DotNet;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.Core.Analysis;
+
+namespace SonarLint.VisualStudio.CloudSecrets
+{
+    [Export(typeof(IAnalyzer))]
+    internal class SecretsAnalyzer : IAnalyzer
+    {
+        private readonly IEnumerable<ISecretDetector> secretDetectors;
+        private readonly IAnalysisStatusNotifier analysisStatusNotifier;
+        private readonly IFileSystem fileSystem;
+
+        [ImportingConstructor]
+        public SecretsAnalyzer([ImportMany] IEnumerable<ISecretDetector> secretDetectors, IAnalysisStatusNotifier analysisStatusNotifier)
+            : this(secretDetectors, analysisStatusNotifier, new FileSystem())
+        {
+        }
+
+        internal SecretsAnalyzer(IEnumerable<ISecretDetector> secretDetectors,
+            IAnalysisStatusNotifier analysisStatusNotifier,
+            IFileSystem fileSystem)
+        {
+            this.secretDetectors = secretDetectors;
+            this.analysisStatusNotifier = analysisStatusNotifier;
+            this.fileSystem = fileSystem;
+        }
+
+        public bool IsAnalysisSupported(IEnumerable<AnalysisLanguage> languages)
+        {
+            return true;
+        }
+
+        public void ExecuteAnalysis(string filePath,
+            string charset,
+            IEnumerable<AnalysisLanguage> detectedLanguages,
+            IIssueConsumer consumer,
+            IAnalyzerOptions analyzerOptions,
+            CancellationToken cancellationToken)
+        {
+            analysisStatusNotifier.AnalysisStarted(filePath);
+
+            try
+            {
+                var stopwatch = Stopwatch.StartNew();
+
+                var fileContent = fileSystem.File.ReadAllText(filePath);
+
+                // todo: pass rules configuration if should run the detector? or should each detector expose metadata?
+                // todo: pass cancellation token that should be checked before running each detector? or should check the cancellation here?
+                var detectedSecrets = secretDetectors.SelectMany(x => x.Find(fileContent));
+                var issues = detectedSecrets.Select(x => CreateIssue(x, filePath)).ToArray();
+
+                analysisStatusNotifier.AnalysisFinished(filePath, issues.Length, stopwatch.Elapsed);
+
+                if (issues.Any())
+                {
+                    consumer.Accept(filePath, issues);
+                }
+            }
+            catch (Exception ex) when (!ErrorHandler.IsCriticalException(ex))
+            {
+                analysisStatusNotifier.AnalysisFailed(filePath, ex);
+            }
+        }
+
+        private IAnalysisIssue CreateIssue(ISecret secret, string filePath)
+        {
+            var ruleKey = secret.RuleKey;
+            var severity = AnalysisIssueSeverity.Major;
+            var message = "this is a secret";
+
+            var startLine = 0;
+            var endLine = 0;
+            var startLineOffset = 0;
+            var endLineOffset = 0;
+            string lineHash = null;
+
+            return new AnalysisIssue(ruleKey,
+                severity,
+                AnalysisIssueType.Vulnerability,
+                message,
+                filePath,
+                startLine,
+                endLine,
+                startLineOffset,
+                endLineOffset,
+                lineHash,
+                null);
+        }
+    }
+}
