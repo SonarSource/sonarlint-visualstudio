d5e01ba4d6c7140e9a727f3b2eff9b85b219db2f
Uğraş Ergün <ugras.ergun@sonarsource.com>
2022-01-27 17:17:12 +0100

    2834 Added interface

---
+++ src/Core/Analysis/IQuickFix.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonarLint.VisualStudio.Core.Analysis
+{
+    public interface IQuickFix
+    {
+        string Message { get; }
+        IReadOnlyList<IEdit> Edits { get; }
+    }
+
+    public interface IEdit
+    {
+        int StartLine { get; }
+        int StartColumn { get; }
+        int EndLine { get; }
+        int EndColumn { get; }
+        string Text { get; }
+    }
+}
