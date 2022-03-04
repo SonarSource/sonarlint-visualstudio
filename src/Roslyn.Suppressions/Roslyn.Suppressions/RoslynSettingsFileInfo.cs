0f88db65ecffd9e0b6a20053cc7da3f25fcf060d
Rita Gorokhod <60586879+rita-sonarsource@users.noreply.github.com>
2022-03-04 18:14:13 +0100

    Add helper class for RoslynSettings file location (#2903)

---
+++ src/Roslyn.Suppressions/Roslyn.Suppressions/RoslynSettingsFileInfo.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.IO;
+using SonarLint.VisualStudio.Core.Helpers;
+
+namespace SonarLint.VisualStudio.Roslyn.Suppressions
+{
+    internal static class RoslynSettingsFileInfo
+    {
+        public static readonly string Directory = Path.Combine(Path.GetTempPath(), "SLVS", "Roslyn");
+
+        /// <summary>
+        /// Returns the full file path for the given project key
+        /// </summary>
+        public static string GetSettingsFilePath(string sonarProjectKey)
+        {
+            var escapedName = PathHelper.EscapeFileName(sonarProjectKey);
+
+            var fileName = escapedName + ".json";
+
+            return Path.Combine(Directory, fileName);
+        }
+
+        /// <summary>
+        /// Returns an identifier for the project settings to which the settings file relates.
+        /// </summary>
+        /// <remarks> The identifier is *not* the actual project key since we can't recover it accurately from the file name </remarks>
+        public static string GetSettingsKey(string filePath)
+        {
+            return Path.GetFileNameWithoutExtension(filePath);
+        }
+    }
+}
