e37417cf9d1bbf7459a02f8b789efc0b87b8224d
Uğraş Ergün <ugras.ergun@sonarsource.com>
2022-03-08 23:53:19 +0100

    SuppressionExecutionContext added

---
+++ src/Roslyn.Suppressions/Roslyn.Suppressions.UnitTests/SuppressionExecutionContextTests.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Immutable;
+using System.Threading;
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SonarLint.VisualStudio.Roslyn.Suppressions.UnitTests
+{
+    [TestClass]
+    public class SuppressionExecutionContextTests
+    {
+        [TestMethod]
+        [DataRow(@"C:\project\.sonarlint\projectKey1\CSharp\SonarLint.xml", "projectKey1")]
+        [DataRow(@"C:\project\.sonarlint\projectKey2\VB\SonarLint.xml", "projectKey2")]
+        public void SonarProjectKey_PathIsValid_ReturnsKey(string path, string projectKey)
+        {
+            var additionalText = new ConcreteAdditionalText(path);
+
+            var testSubject = CreateTestSubject(additionalText);
+
+            testSubject.SonarProjectKey.Should().Be(projectKey);
+            testSubject.IsInConnectedMode.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void SonarProjectKey_PathIsNotValid_ReturnsNull()
+        {
+            var additionalText = new ConcreteAdditionalText(@"C:\project\projectKey1\CSharp\SonarLint.xml");
+
+            var testSubject = CreateTestSubject(additionalText);
+
+            testSubject.SonarProjectKey.Should().BeNull();
+            testSubject.IsInConnectedMode.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void SonarProjectKey_HasMixedPaths_ReturnsKey()
+        {
+            var additionalText1 = new ConcreteAdditionalText(@"C:\project\projectKey1\CSharp\SonarLint.xml");
+            var additionalText2 = new ConcreteAdditionalText(@"C:\project\.sonarlint\projectKey2\CSharp\SonarLint.xml");            
+
+            var testSubject = CreateTestSubject(additionalText1, additionalText2);
+
+            testSubject.SonarProjectKey.Should().Be("projectKey2");
+            testSubject.IsInConnectedMode.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void SonarProjectKey_HasNoPaths_ReturnsNull()
+        {
+            var testSubject = CreateTestSubject();
+
+            testSubject.SonarProjectKey.Should().BeNull();
+            testSubject.IsInConnectedMode.Should().BeFalse();
+        }
+
+        private SuppressionExecutionContext CreateTestSubject(params AdditionalText[] existingAdditionalFiles)
+        {
+            var analyzerOptions = CreateAnalyzerOptions(existingAdditionalFiles);
+            return new SuppressionExecutionContext(analyzerOptions);
+        }
+
+        private AnalyzerOptions CreateAnalyzerOptions(params AdditionalText[] existingAdditionalFiles)
+        {
+            return new AnalyzerOptions(existingAdditionalFiles.ToImmutableArray());
+        }
+
+    }
+
+    internal class ConcreteAdditionalText : AdditionalText
+    {
+        private readonly string text;
+
+        internal ConcreteAdditionalText(string path)
+        {
+            Path = path;
+        }
+
+        public override string Path { get; }
+
+        public override SourceText GetText(CancellationToken cancellationToken = default)
+        {
+            return SourceText.From(string.Empty);
+        }
+    }
+}
---
+++ src/Roslyn.Suppressions/Roslyn.Suppressions/SuppressionExecutionContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace SonarLint.VisualStudio.Roslyn.Suppressions
+{
+    public interface ISuppressionExecutionContext
+    {
+        bool IsInConnectedMode { get; }
+        string SonarProjectKey { get; }
+    }
+    internal class SuppressionExecutionContext : ISuppressionExecutionContext
+    {
+        private const string Exp = @"\\.sonarlint\\(?<sonarkey>[^\\/]+)\\(CSharp|VB)\\SonarLint.xml$";
+        private static readonly Regex SonarLintFileRegEx = new Regex(Exp, RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.CultureInvariant);
+        private readonly AnalyzerOptions analyzerOptions;
+
+
+        public SuppressionExecutionContext(AnalyzerOptions analyzerOptions)
+        {
+            this.analyzerOptions = analyzerOptions;
+
+            GetProjectKey();
+        }
+
+        private void GetProjectKey()
+        {
+            foreach (var item in analyzerOptions.AdditionalFiles)
+            {
+                var match = SonarLintFileRegEx.Match(item.Path);
+                if (match.Success)
+                {
+                    SonarProjectKey = match.Groups["sonarkey"].Value;
+                    return;
+                }
+            }
+        }
+
+        public bool IsInConnectedMode => SonarProjectKey != null;
+
+        public string SonarProjectKey { get; private set; } = null;
+    }
+}
