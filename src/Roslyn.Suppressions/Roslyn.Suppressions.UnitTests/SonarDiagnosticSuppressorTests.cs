0f07f5cdd5084b8e0db78581a02fb0a6e3be9ddc
Uğraş Ergün <ugras.ergun@sonarsource.com>
2022-03-10 16:18:07 +0100

    implemented suppressor

--- src/Roslyn.Suppressions/Roslyn.Suppressions.UnitTests/SonarDiagnosticSuppressorTests.cs
+++ src/Roslyn.Suppressions/Roslyn.Suppressions.UnitTests/SonarDiagnosticSuppressorTests.cs
@@ -18,8 +18,13 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+using System.Collections.Immutable;
+using System.Linq;
 using FluentAssertions;
+using Microsoft.CodeAnalysis;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SonarLint.VisualStudio.Integration;
 
 namespace SonarLint.VisualStudio.Roslyn.Suppressions.UnitTests
 {
@@ -29,7 +34,7 @@ namespace SonarLint.VisualStudio.Roslyn.Suppressions.UnitTests
         [TestMethod]
         public void Suppressor_ReturnsExpectedSuppressions()
         {
-            var testSubject = new SonarDiagnosticSuppressor();
+            var testSubject = CreateTestSubject();
 
             var actual = testSubject.SupportedSuppressions;
 
@@ -44,11 +49,137 @@ namespace SonarLint.VisualStudio.Roslyn.Suppressions.UnitTests
         public void Suppressors_AllSuppressorsReturnSameInstances()
         {
             // Perf - check every instance reuses the same set of descriptors
-            var supported1 = new SonarDiagnosticSuppressor().SupportedSuppressions;
-            var supported2 = new SonarDiagnosticSuppressor().SupportedSuppressions;
+            var supported1 = CreateTestSubject().SupportedSuppressions;
+            var supported2 = CreateTestSubject().SupportedSuppressions;
 
             // Have to use "Equals" to test identityt equality - see above test.
             supported1.Equals(supported2).Should().BeTrue();
         }
+
+        [TestMethod]
+        public void GetSuppressions_IsNotInConnectedMode_ShouldReturnEmpty()
+        {
+            var suppressionContext = CreateSuppressionContext(null);
+            var reportedDiagnostics = CreateReportedDiagnostics();
+
+            var testSubject = CreateTestSubject();
+
+            var suppressions = testSubject.GetSuppressions(reportedDiagnostics, suppressionContext.Object);
+
+            suppressions.Count().Should().Be(0);
+        }
+
+        [TestMethod]
+        public void GetSuppressions_HasSupressedDiagnostic_ShouldReturnSupressions()
+        {
+
+            var suppressionContext = CreateSuppressionContext("sonarKey");
+            var diag = CreateDiagnostic("S100");
+
+            var suppressionChecker = CreateSuppressionChecker("sonarKey", diag);
+            var container = CreateContainer(suppressionChecker);
+
+            var reportedDiagnostics = CreateReportedDiagnostics(diag);
+            
+            var testSubject = CreateTestSubject(container);
+
+            var suppressions = testSubject.GetSuppressions(reportedDiagnostics, suppressionContext.Object).ToList();
+
+            suppressions.Count().Should().Be(1);
+            suppressions[0].Descriptor.SuppressedDiagnosticId.Should().Be("S100");
+        }
+
+        [TestMethod]
+        public void GetSuppressions_HasMixedSupressedDiagnostic_ShouldReturnOnlySupressed()
+        {
+
+            var suppressionContext = CreateSuppressionContext("sonarKey");
+            var diag1 = CreateDiagnostic("S100");
+            var diag2 = CreateDiagnostic("S101");
+            var diag3 = CreateDiagnostic("S103");
+
+            var suppressionChecker = CreateSuppressionChecker("sonarKey", diag1, diag2);
+            var container = CreateContainer(suppressionChecker);
+
+            var reportedDiagnostics = CreateReportedDiagnostics(diag1, diag2, diag3);
+
+            var testSubject = CreateTestSubject(container);
+
+            var suppressions = testSubject.GetSuppressions(reportedDiagnostics, suppressionContext.Object).ToList();
+
+            suppressions.Count().Should().Be(2);
+            suppressions[0].Descriptor.SuppressedDiagnosticId.Should().Be("S100");
+            suppressions[1].Descriptor.SuppressedDiagnosticId.Should().Be("S101");
+        }
+
+        [TestMethod]
+        public void GetSuppressions_HasNotSupressedDiagnostic_ShouldReturnEmpty()
+        {
+            var suppressionContext = CreateSuppressionContext("sonarKey");
+            var diag = CreateDiagnostic("S100");
+
+            var reportedDiagnostics = CreateReportedDiagnostics(diag);
+
+            var testSubject = CreateTestSubject();
+
+            var suppressions = testSubject.GetSuppressions(reportedDiagnostics, suppressionContext.Object).ToList();
+
+            suppressions.Count().Should().Be(0);
+        }
+
+        private ISuppressionChecker CreateSuppressionChecker(string settingsKey, params Diagnostic[] diagnostics)
+        {
+            var suppressionChecker = new Mock<ISuppressionChecker>();
+            suppressionChecker.Setup(sc => sc.IsSuppressed(It.IsAny<Diagnostic>(), It.IsAny<string>())).Returns(false);
+
+            foreach (var diagnostic in diagnostics)
+            {
+                suppressionChecker.Setup(sc => sc.IsSuppressed(diagnostic, settingsKey)).Returns(true);
+            }
+
+            return suppressionChecker.Object;
+        }
+
+        private Mock<ISuppressionExecutionContext> CreateSuppressionContext(string settingsKey)
+        {
+            var context = new Mock<ISuppressionExecutionContext>();
+            context.SetupGet(c => c.SettingsKey).Returns(settingsKey);
+            context.SetupGet(c => c.IsInConnectedMode).Returns(settingsKey != null);
+
+            return context;
+        }
+
+        private Diagnostic CreateDiagnostic(string id)
+        {
+            var diagnostic = new Mock<Diagnostic>();
+            diagnostic.SetupGet(d => d.Id).Returns(id);
+
+            return diagnostic.Object;
+        }
+        
+       private ImmutableArray<Diagnostic> CreateReportedDiagnostics(params Diagnostic[] diagnostics)
+        {
+            return diagnostics.ToImmutableArray();
+        }
+
+        private SonarDiagnosticSuppressor CreateTestSubject(IContainer container = null)
+        {
+            container = container ?? CreateContainer();
+            return new SonarDiagnosticSuppressor(container);
+        }
+
+        private IContainer CreateContainer(ISuppressionChecker suppressionChecker = null, ILogger logger = null)
+        {
+            suppressionChecker = suppressionChecker ?? Mock.Of<ISuppressionChecker>();
+            logger = logger ?? Mock.Of<ILogger>();
+
+            var container = new Mock<IContainer>();
+            container.SetupGet(c => c.SuppressionChecker).Returns(suppressionChecker);
+            container.SetupGet(c => c.Logger).Returns(logger);
+
+            return container.Object;
+        }
+
+        
     }
 }
