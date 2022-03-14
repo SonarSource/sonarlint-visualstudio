eb568d6a26a5017c47a36b9c8a52e1d7b90e4a26
duncanp-sonar <duncan.pocklington@sonarsource.com>
2022-03-14 08:35:38 +0000

    Implement suppression checker (#2921)
    * Implement suppression checker
    Part of #2919

---
+++ src/Roslyn.Suppressions/Roslyn.Suppressions.UnitTests/SuppressionCheckerTests.cs
@@ -0,0 +1,402 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SonarLint.VisualStudio.Roslyn.Suppressions.Settings.Cache;
+using SonarQube.Client;
+using SonarQube.Client.Models;
+using static SonarLint.VisualStudio.Roslyn.Suppressions.UnitTests.TestHelper;
+
+namespace SonarLint.VisualStudio.Roslyn.Suppressions.UnitTests
+{
+    [TestClass]
+    public class SuppressionCheckerTests
+    {
+        // Cache that will throw if it is called
+        private static readonly ISettingsCache ThrowingSettingsCache = new Mock<ISettingsCache>(MockBehavior.Strict).Object;
+
+        private readonly Diagnostic ValidInSourceDiagnostic = CreateDiagnostic("any",
+            CreateSourceFileLocation("c:\\any.txt"));
+
+        [TestMethod]
+        public void IsSuppressed_NonSourceLocation_ReturnsFalse()
+        {
+            // Perf - should early-out for non-source location
+            var testSubject = CreateTestSubject(ThrowingSettingsCache);
+
+            var nonSourceDiagnostic = CreateDiagnostic("any", CreateNonSourceLocation());
+
+            // Act
+            var actual = testSubject.IsSuppressed(nonSourceDiagnostic, "any");
+
+            actual.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void IsSuppressed_NullDiagnosticLocation_ReturnsFalse()
+        {
+            // Perf - should early-out for null diagnostic location
+            var testSubject = CreateTestSubject(ThrowingSettingsCache);
+
+            var nullLocationDiagnostic = CreateDiagnostic("any", null);
+
+            // Act
+            var actual = testSubject.IsSuppressed(nullLocationDiagnostic, "any");
+
+            actual.Should().BeFalse();
+        }
+
+
+        [TestMethod]
+        public void IsSuppressed_NullSettings_ReturnsFalse()
+        {
+            var cache = CreateSettingsCache("settingsKey1", null);
+            var testSubject = CreateTestSubject(cache.Object);
+
+            // Act
+            var actual = testSubject.IsSuppressed(ValidInSourceDiagnostic, "settingsKey1");
+
+            actual.Should().Be(false);
+            CheckGetSettingsCalled(cache, "settingsKey1");        
+        }
+
+        [TestMethod]
+        public void IsSuppressed_EmptySettings_ReturnsFalse()
+        {
+            var cache = CreateSettingsCache("settingsKey1", new SonarQubeIssue[0]);
+
+            var testSubject = CreateTestSubject(cache.Object);
+
+            // Act
+            var actual = testSubject.IsSuppressed(ValidInSourceDiagnostic, "settingsKey1");
+
+            actual.Should().Be(false);
+            CheckGetSettingsCalled(cache, "settingsKey1");
+        }
+
+        [TestMethod]
+        public void IsSuppressed_HasIssues_NoMatches_ReturnsFalse()
+        {
+            var diagnostic = CreateDiagnostic("S111", CreateSourceFileLocation("c:\\myfile.cs"));
+            
+            var suppressedIssues = new SonarQubeIssue[]
+            {
+                CreateIssue("any", "S111", "c:\\wrongFile1.txt")
+            };
+
+            var cache = CreateSettingsCache("settingsKey", suppressedIssues);
+            var checksumCalculator = CreateChecksumCalculator("any");
+            var testSubject = CreateTestSubject(cache.Object, checksumCalculator.Object);
+
+            // Act
+            var actual = testSubject.IsSuppressed(diagnostic, "settingsKey");
+
+            actual.Should().BeFalse();
+            CheckGetSettingsCalled(cache, "settingsKey");
+        }
+
+        [TestMethod]
+        public void IsSuppressed_HasIssues_Matches_ReturnsTrue()
+        {
+            var location = CreateSourceFileLocation(DiagFileName, DiagFileText, DiagSelectedText);
+            var diagnostic = CreateDiagnostic(DiagRuleId, location);
+
+            var suppressedIssues = new SonarQubeIssue[]
+            {
+                 CreateIssueFromDiagnostic(diagnostic)
+            };
+
+            var checksumCalculator = CreateChecksumCalculator(DiagHash);
+
+            var cache = CreateSettingsCache("settingsKey", suppressedIssues);
+            var testSubject = CreateTestSubject(cache.Object, checksumCalculator.Object);
+
+            // Act
+            var actual = testSubject.IsSuppressed(diagnostic, "settingsKey");
+
+            actual.Should().BeTrue();
+            CheckGetSettingsCalled(cache, "settingsKey");
+        }
+
+        [TestMethod]
+        public void IsMatch_FilesAreDifferent_ReturnsFalse()
+        {
+            var checksumCalculator = CreateChecksumCalculator("hash");
+
+            var diag = CreateDiagnostic("S999", CreateSourceFileLocation("c:\\diagnosticFileName.cs", "all text", "all"));
+            // Sanity check the diagnostic location is on the expected line
+            diag.Location.GetLineSpan().StartLinePosition.Line.Should().Be(0, "Test setup error");
+
+            var issue = CreateIssue("any", "S999", "c:\\issueFile.cs", 1, "hash");
+            
+            SuppressionChecker.IsMatch(diag, issue, checksumCalculator.Object)
+                .Should().BeFalse();
+
+            checksumCalculator.Invocations.Count.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void IsMatch_RuleIdsAreDifferent_ReturnsFalse()
+        {
+            var checksumCalculator = CreateChecksumCalculator("hash");
+
+            var diag = CreateDiagnostic("S111", CreateSourceFileLocation("c:\\file.cs", "all text", "all"));
+            // Sanity check the diagnostic location is on the expected line
+            diag.Location.GetLineSpan().StartLinePosition.Line.Should().Be(0, "Test setup error");
+
+            var issue = CreateIssue("any", "S999", "c\\file.cs", 1, "hash");
+
+            SuppressionChecker.IsMatch(diag, issue, checksumCalculator.Object)
+                .Should().BeFalse();
+
+            checksumCalculator.Invocations.Count.Should().Be(0);
+
+        }
+
+        #region Matching tests
+
+        // Constants used in for the diagnostic to match against in IsMatch_ReturnsExpectedValue
+        // They are defined as constants here so we can use them in the [DataRow]s for the test.
+        private const string DiagFileName = "c:\\diag.txt";
+        private const string DiagRuleId = "S999";
+        private const string DiagHash = "diag hash";
+
+        private const string DiagFileText = "0\n1\n2\n3 text \n\n";
+        private const string DiagSelectedText = "text";
+        private const int DiagRoslynLineNumber = 3; // the 0-base line in which the word "text" appears
+        private const int MatchingSonarLineNumber = DiagRoslynLineNumber + 1;
+
+        [TestMethod]
+        [DataRow(DiagFileName, DiagRuleId, MatchingSonarLineNumber, DiagHash, true)]
+        [DataRow("c:\\wrong file name.cs", DiagRuleId, MatchingSonarLineNumber, DiagHash, false)]
+        [DataRow(DiagFileName, "wrong rule id", MatchingSonarLineNumber, DiagHash, false)]
+        [DataRow(DiagFileName, DiagRuleId, 999, "wrong hash", false)] // wrong line, wrong hash -> false
+        [DataRow(DiagFileName, DiagRuleId, MatchingSonarLineNumber, "wrong hash", true)] // right line, wrong hash -> true
+        [DataRow(DiagFileName, DiagRuleId, 888, DiagHash, true)] // wrong line, right hash -> true
+
+        // Special cases
+        [DataRow("C:\\DIAG.TXT", DiagRuleId, MatchingSonarLineNumber, DiagHash, true)] // case-insensitive
+        [DataRow(DiagFileName, "s999", MatchingSonarLineNumber, DiagHash, true)] // case-insensitive
+        public void IsMatch_ReturnsExpectedValue(string issueFile, string issueRuleId, int sonarIssueLine,
+            string issueHash, bool expected)
+        {
+            // The diagnostic is the same in every case
+            var checksumCalculator = CreateChecksumCalculator(DiagHash);
+            var diag = CreateDiagnostic(DiagRuleId, CreateSourceFileLocation(DiagFileName, DiagFileText, DiagSelectedText));
+            // Sanity check the diagnostic location is on the expected line
+            diag.Location.GetLineSpan().StartLinePosition.Line.Should().Be(DiagRoslynLineNumber, "Test setup error");
+
+            var issue = CreateIssue("any", issueRuleId, issueFile, sonarIssueLine, issueHash);
+
+            SuppressionChecker.IsMatch(diag, issue, checksumCalculator.Object)
+                .Should().Be(expected);
+        }
+
+        [TestMethod]
+        [DataRow(0, 0, true)]
+        [DataRow(1, 0, false)]
+        [DataRow(1, 1, false)]
+        [DataRow(2, 0, false)]
+        [DataRow(2, 2, false)]
+        public void IsMatch_IsRoslynFileLevelIssue_MatchesSonarFileLevelIssue(int roslynStartPosition, int length,
+            bool expected)
+        {
+            const string text = "000\n111\n222\n";
+            var selectedSpan = new TextSpan(roslynStartPosition, length);
+            var syntaxTree = Microsoft.CodeAnalysis.CSharp.CSharpSyntaxTree.ParseText(text, path: "path");
+            var location = Location.Create(syntaxTree, selectedSpan);
+            var diagnostic = CreateDiagnostic("id", location);
+
+            var sonarFileLevelIssue = CreateIssueWithTextRange("id", "path", "hash", null);
+
+            var actual = SuppressionChecker.IsMatch(diagnostic, sonarFileLevelIssue, Mock.Of<IChecksumCalculator>());
+
+            actual.Should().Be(expected);
+        }
+
+        [TestMethod]
+        public void IsMatch_RoslynFileLevelIssue_SonarNonFileLevelIssue_DoNotMatch()
+        {
+            const string text = "000\n111\n222\n";
+            var selectedSpan = new TextSpan(0, 0);
+            var syntaxTree = Microsoft.CodeAnalysis.CSharp.CSharpSyntaxTree.ParseText(text, path: "path");
+            var location = Location.Create(syntaxTree, selectedSpan);
+            var diagnostic = CreateDiagnostic("id", location);
+
+            var sonarFileLevelIssue = CreateIssueWithTextRange("id", "path", "hash", new IssueTextRange(1, 1, 0, 1));
+
+            var actual = SuppressionChecker.IsMatch(diagnostic, sonarFileLevelIssue, Mock.Of<IChecksumCalculator>());
+
+            actual.Should().BeFalse();
+        }
+
+        [TestMethod]
+        [DataRow(@"c:\same.txt", @"c:\same.txt", true)]
+        [DataRow(@"C:\SAME.TXT", @"c:\same.txt", true)]
+        [DataRow(@"differentExt.123", @"differentExt.999", false)]
+        [DataRow(@"partial\file.cs", @"c:aaa\partial\file.cs", true)]
+        [DataRow(@"aaa\partial\file.cs", @"partial\file.cs", false)]
+        public void IsMatch_IsSameFile(string issueFile, string diagFile, bool expected)
+        {
+            var checksumCalculator = CreateChecksumCalculator("a hash that won't match");
+
+            var diag = CreateDiagnostic(DiagRuleId, CreateSourceFileLocation(diagFile, DiagFileText, DiagSelectedText));
+
+            var issue = CreateIssueFromDiagnostic(diag, overrideFilePath: issueFile);
+
+            SuppressionChecker.IsMatch(diag, issue, checksumCalculator.Object)
+                .Should().Be(expected);
+        }
+
+        #endregion // Matching tests
+
+        private void CheckGetSettingsCalled(Mock<ISettingsCache> cache, string expectedSettingsKey) =>
+            cache.Verify(x => x.GetSettings(expectedSettingsKey), Times.Once());
+
+        private static SuppressionChecker CreateTestSubject(ISettingsCache cache = null,
+            IChecksumCalculator checksumCalculator = null)
+        {
+            cache ??= CreateSettingsCache("any", null).Object;
+            checksumCalculator ??= CreateChecksumCalculator("any").Object;
+
+            return new SuppressionChecker(cache, checksumCalculator);
+        }
+
+        private static Mock<ISettingsCache> CreateSettingsCache(string settingsKey, IEnumerable<SonarQubeIssue> suppressedIssues)
+        {
+            var cache = new Mock<ISettingsCache>();
+            cache.Setup(x => x.GetSettings(settingsKey)).Returns(suppressedIssues);
+            return cache;
+        }
+
+        private static Mock<IChecksumCalculator> CreateChecksumCalculator(string hashToReturn)
+        {
+            var calculator = new Mock<IChecksumCalculator>();
+            calculator.Setup(x => x.Calculate(It.IsAny<string>()))
+                .Returns(hashToReturn);
+
+            return calculator;
+        }
+
+        /// <summary>
+        /// Returns an issue with the same values as the supplied diagnostic.
+        /// </summary>
+        /// <remarks>
+        /// The issue will match the diagnostic, unless the file path is overridden.
+        /// The diagnostic must have a valid location and source tree.
+        /// </remarks>
+        private static SonarQubeIssue CreateIssueFromDiagnostic(Diagnostic diagnostic,
+            string overrideFilePath = null)
+        {
+            // The issue will match because the line number is the same (so the hash is irrelevant)
+            var sonarLine = diagnostic.Location.GetLineSpan().EndLinePosition.Line + 1;
+
+            var issuePath = overrideFilePath ?? diagnostic.Location.SourceTree.FilePath;
+            var issueHash = System.Guid.NewGuid().ToString();
+
+            return CreateIssue("any", diagnostic.Id, issuePath, sonarLine, issueHash);
+        }
+
+        private static SonarQubeIssue CreateIssueWithTextRange(string ruleId,
+            string path,
+            string hash,
+            IssueTextRange textRange)
+        {
+            return new SonarQubeIssue("any issue key",
+                path,
+                hash,
+                "message",
+                "moduleKey",
+                ruleId,
+                true,
+                SonarQubeIssueSeverity.Blocker,
+                DateTimeOffset.Now,
+                DateTimeOffset.Now,
+                textRange,
+                null);
+        }
+
+        #region Diagnostic helper methods
+
+        private static Location CreateNonSourceLocation()
+        {
+            var nonSourceLocation = Location.Create("dummyFilePath.cs", new TextSpan(), new LinePositionSpan());
+            nonSourceLocation.IsInSource.Should().BeFalse();
+            nonSourceLocation.Kind.Should().NotBe(LocationKind.SourceFile);
+            return nonSourceLocation;
+        }
+
+        /// <summary>
+        /// Returns a Location that is in a source file, backed by a valid syntax tree.
+        /// </summary>
+        /// <param name="fileText">The contents of the entire file (optional)</param>
+        /// <param name="selectedText">The specific text in the file the location points to (optional)</param>
+        private static Location CreateSourceFileLocation(string filePath,
+            string fileText = "any", string selectedText = "any")
+        {
+            // Mocking source location so that it appears to be backed by a real syntax tree, with
+            // the required "GetLineSpan()", "GetText().Lines" etc methods working correctly is hard.
+
+            // The simplest thing to do is create a real C# syntax tree. It doesn't matter if the
+            // contents of the fake file are not valid C# - the parser will still produce a valid
+            // document.
+
+            var syntaxTree = Microsoft.CodeAnalysis.CSharp.CSharpSyntaxTree.ParseText(fileText, path: filePath);
+            var selectedSpan = CreateSpan(fileText, selectedText);
+            var location = Location.Create(syntaxTree, selectedSpan);
+
+            // Sanity checks that the objects are set up as we expected
+            location.IsInSource.Should().BeTrue("Test setup error: location should be in source");
+            location.Kind.Should().Be(LocationKind.SourceFile, "Test setup error: LocationKind should be SourceFile");
+            syntaxTree.FilePath.Should().Be(filePath, "Test setup error: syntaxTree FilePath is not set correctly");
+            var lineSpan = location.GetLineSpan();
+            lineSpan.IsValid.Should().BeTrue("Test setup error: lineSpan is not valid");
+            var lineText = syntaxTree.GetText().Lines[lineSpan.EndLinePosition.Line].ToString();
+            lineText.Should().Contain(selectedText, "Test setup error: the fake location/syntax tree is not correctly constructed");
+
+            return location;
+        }
+
+        private static TextSpan CreateSpan(string documentText, string selectedText)
+        {
+            var start = documentText.IndexOf(selectedText);
+            start.Should().BeGreaterThan(-1, "Test setup error: selected text is not in the document text");
+            return new TextSpan(start, selectedText.Length);
+        }
+
+        private static Diagnostic CreateDiagnostic(string ruleId, Location loc)
+        {
+            var diagnostic = new Mock<Diagnostic>();
+
+            diagnostic.Setup(x => x.Id).Returns(ruleId);
+            diagnostic.Setup(x => x.Location).Returns(loc);
+
+            return diagnostic.Object;        
+        }
+
+        #endregion
+    }
+}
