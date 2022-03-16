36b3ce5fc7651b1ffc0dd16216fcaa4186d9d1f8
Rita Gorokhod <60586879+rita-sonarsource@users.noreply.github.com>
2022-03-16 15:17:06 +0100

    Add RunAsync method to ThreadHandling (#2933)

--- src/Core/IThreadHandling.cs
+++ src/Core/IThreadHandling.cs
@@ -60,6 +60,17 @@ namespace SonarLint.VisualStudio.Core
         T Run<T>(Func<Task<T>> asyncMethod);
 
         /// <summary>
+        /// See <see cref="RunAsync{T}"/>
+        /// </summary>
+        Task RunAsync(Func<Task> asyncMethod);
+
+        /// <summary>
+        /// Runs the asynchronous method to completion without blocking the calling thread.
+        /// </summary>
+        /// <remarks>Wrapper around <see cref="ThreadHelper.JoinableTaskFactory.RunAsync"/></remarks>
+        Task RunAsync<T>(Func<Task<T>> asyncMethod);
+
+        /// <summary>
         /// Switches to the background thread
         /// </summary>
         /// <remarks>Wrapper that calls <see cref="TaskScheduler.Default"/></remarks>
