b635af16a0582a0e7fa3ebfa34cec701c0089819
Uğraş Ergün <ugras.ergun@sonarsource.com>
2022-03-25 10:32:33 +0100

    added cr and removed unnecesary usings

--- src/Roslyn.Suppressions/Roslyn.Suppressions.UnitTests/TestHelper.cs
+++ src/Roslyn.Suppressions/Roslyn.Suppressions.UnitTests/TestHelper.cs
@@ -18,9 +18,6 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using System;
-using System.Globalization;
-using System.Threading;
 using SonarQube.Client.Models;
 
 namespace SonarLint.VisualStudio.Roslyn.Suppressions.UnitTests
