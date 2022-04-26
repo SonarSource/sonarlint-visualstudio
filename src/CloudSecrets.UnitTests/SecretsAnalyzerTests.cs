9dc501bdebd2122dc4f2e8c9d4a3979a5610881a
duncanp-sonar <duncan.pocklington@sonarsource.com>
2022-04-26 09:08:13 +0100

    Re-enable secrets analysis (#2988)
    * Update embedded secrets package to v1.0.1
    * Re-enable secrets analysis

--- src/CloudSecrets.UnitTests/SecretsAnalyzerTests.cs
+++ src/CloudSecrets.UnitTests/SecretsAnalyzerTests.cs
@@ -40,7 +40,7 @@ namespace SonarLint.VisualStudio.CloudSecrets.UnitTests
         private const string ValidFilePath = "c:\\test";
         private const string ValidFileContent = "this is a content";
 
-        [TestMethod, Ignore("Secrets not yet supported")] // __secrets
+        [TestMethod]
         public void MefCtor_CheckIsExported()
         {
             MefTestHelpers.CheckTypeCanBeImported<SecretsAnalyzer, IAnalyzer>(null, new[]
