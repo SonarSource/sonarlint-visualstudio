c84730b8fbec5b05f6935da99d49ba38fc8ec1c4
Rita Gorokhod <rita.gorokhod@sonarsource.com>
2022-05-13 13:48:43 +0200

    Display c# tags

---
+++ src/IssueViz/Editor/ErrorTagging/Inline/Roslyn/ErrorToSonarErrorTaggerProvider.cs
@@ -0,0 +1,70 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+using SonarLint.VisualStudio.Integration;
+
+namespace SonarLint.VisualStudio.IssueVisualization.Editor.ErrorTagging.Inline.Roslyn
+{
+    [Export(typeof(ITaggerProvider))]
+    [ContentType("text")]
+    [TagType(typeof(ISonarErrorTag))]
+    internal class ErrorToSonarErrorTaggerProvider : ITaggerProvider
+    {
+        private readonly IRoslynSonarErrorTagsProvider roslynSonarErrorTagsProvider;
+        private readonly ITaggableBufferIndicator taggableBufferIndicator;
+        private ILogger logger;
+
+        [ImportingConstructor]
+        public ErrorToSonarErrorTaggerProvider(IRoslynSonarErrorTagsProvider roslynSonarErrorTagsProvider,
+            ITaggableBufferIndicator taggableBufferIndicator, 
+            ILogger logger)
+        {
+            this.roslynSonarErrorTagsProvider = roslynSonarErrorTagsProvider;
+            this.taggableBufferIndicator = taggableBufferIndicator;
+            this.logger = logger;
+        }
+
+        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
+        {
+            if (buffer == null)
+            {
+                throw new ArgumentNullException(nameof(buffer));
+            }
+
+            if (!taggableBufferIndicator.IsTaggable(buffer))
+            {
+                return null;
+            }
+
+            var tagger = Create(buffer);
+            return tagger as ITagger<T>;
+        }
+
+        private ErrorToSonarErrorTagger Create(ITextBuffer textBuffer)
+        {
+            return new ErrorToSonarErrorTagger(textBuffer, roslynSonarErrorTagsProvider, logger);
+        }
+    }
+}
---
+++ src/IssueViz/Editor/ErrorTagging/Inline/Roslyn/RoslynSonarErrorTagsProvider.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using Microsoft.VisualStudio.Shell.TableManager;
+
+namespace SonarLint.VisualStudio.IssueVisualization.Editor.ErrorTagging.Inline.Roslyn
+{
+    public interface IRoslynSonarErrorTagsProvider
+    {
+        event EventHandler<RoslynIssuesChanged> RoslynIssuesChanged;
+    }
+
+    public class RoslynIssuesChanged
+    {
+        public ITableEntriesSnapshotFactory Factory { get; }
+
+        public RoslynIssuesChanged(ITableEntriesSnapshotFactory factory)
+        {
+            Factory = factory;
+        }
+    }
+
+    [Export(typeof(IRoslynSonarErrorTagsProvider))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    internal class RoslynSonarErrorTagsProvider : IRoslynSonarErrorTagsProvider, ITableDataSink, IDisposable
+    {
+        private readonly ITableManager tableManager;
+        private IDisposable sinkHandle;
+
+        [ImportingConstructor]
+        public RoslynSonarErrorTagsProvider(ITableManagerProvider tableManagerProvider)
+        {
+            tableManager = tableManagerProvider.GetTableManager(StandardTables.ErrorsTable);
+            tableManager.SourcesChanged += TableManager_SourcesChanged;
+
+            FetchSources();
+        }
+
+        private void FetchSources()
+        {
+            var roslynDataSource = tableManager.Sources.FirstOrDefault(x => x.DisplayName == "C#/VB Diagnostics Table Data Source");
+            sinkHandle?.Dispose();
+            sinkHandle = roslynDataSource?.Subscribe(this);
+        }
+
+        private void TableManager_SourcesChanged(object sender, EventArgs e)
+        {
+            FetchSources();
+        }
+        
+        public void Dispose()
+        {
+            tableManager.SourcesChanged -= TableManager_SourcesChanged;
+        }
+
+        public void AddEntries(IReadOnlyList<ITableEntry> newEntries, bool removeAllEntries = false)
+        {
+        }
+
+        public void RemoveEntries(IReadOnlyList<ITableEntry> oldEntries)
+        {
+        }
+
+        public void ReplaceEntries(IReadOnlyList<ITableEntry> oldEntries, IReadOnlyList<ITableEntry> newEntries)
+        {
+        }
+
+        public void RemoveAllEntries()
+        {
+        }
+
+        public void AddSnapshot(ITableEntriesSnapshot newSnapshot, bool removeAllSnapshots = false)
+        {
+        }
+
+        public void RemoveSnapshot(ITableEntriesSnapshot oldSnapshot)
+        {
+        }
+
+        public void RemoveAllSnapshots()
+        {
+        }
+
+        public void ReplaceSnapshot(ITableEntriesSnapshot oldSnapshot, ITableEntriesSnapshot newSnapshot)
+        {
+        }
+
+        public void AddFactory(ITableEntriesSnapshotFactory newFactory, bool removeAllFactories = false)
+        {
+        }
+
+        public void RemoveFactory(ITableEntriesSnapshotFactory oldFactory)
+        {
+        }
+
+        public void ReplaceFactory(ITableEntriesSnapshotFactory oldFactory, ITableEntriesSnapshotFactory newFactory)
+        {
+        }
+
+        public void FactorySnapshotChanged(ITableEntriesSnapshotFactory factory)
+        {
+            RoslynIssuesChanged?.Invoke(this, new RoslynIssuesChanged(factory));
+        }
+
+        public void RemoveAllFactories()
+        {
+        }
+
+        public bool IsStable { get; set; } = true;
+        public event EventHandler<RoslynIssuesChanged> RoslynIssuesChanged;
+    }
+}
