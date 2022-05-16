5f28dfe7b1b4175d48c15cba9ef7e8a4bce297f9
duncanp-sonar <duncan.pocklington@sonarsource.com>
2022-05-16 10:59:17 +0200

    Added header and footer adornments

---
+++ src/IssueViz/Editor/ErrorTagging/Adornment/ErrorLayer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace SonarLint.VisualStudio.IssueVisualization.Editor.ErrorTagging.Adornment
+{
+    internal class ErrorLayer
+    {
+        public const string LayerName = "SonarLint error adornments";
+
+        [Export(typeof(AdornmentLayerDefinition))]
+        [Name(LayerName)]
+        [Order(After = PredefinedAdornmentLayers.Caret)]
+        public AdornmentLayerDefinition editorAdornmentLayer = null;
+    }
+}
