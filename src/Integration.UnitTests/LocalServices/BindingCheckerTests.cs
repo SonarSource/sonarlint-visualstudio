316e253122adb8f3ffd40b816dbbc243d5576eb6
Rita Gorokhod <60586879+rita-sonarsource@users.noreply.github.com>
2022-06-17 14:57:11 +0200

    Add IBindingChecker(#3070)

---
+++ src/Integration.UnitTests/LocalServices/BindingCheckerTests.cs
@@ -0,0 +1,92 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace SonarLint.VisualStudio.Integration.UnitTests.LocalServices
+{
+    [TestClass]
+    public class BindingCheckerTests
+    {
+        [TestMethod]
+        public void IsBindingUpdateRequired_NoUnboundProjects_False()
+        {
+            var unboundProjects = Array.Empty<EnvDTE.Project>();
+
+            var testSubject = CreateTestSubject(unboundProjects);
+
+            var result = testSubject.IsBindingUpdateRequired();
+
+            result.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void IsBindingUpdateRequired_NoUnboundProjects_NoLogs()
+        {
+            var unboundProjects = Array.Empty<EnvDTE.Project>();
+            var logger = new TestLogger();
+
+            var testSubject = CreateTestSubject(unboundProjects, logger);
+
+            testSubject.IsBindingUpdateRequired();
+
+            logger.OutputStrings.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void IsBindingUpdateRequired_HasUnboundProjects_True()
+        {
+            var unboundProjects = new[] { new ProjectMock("unbound.csproj") };
+
+            var testSubject = CreateTestSubject(unboundProjects);
+
+            var result = testSubject.IsBindingUpdateRequired();
+
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void IsBindingUpdateRequired_HasUnboundProjects_UnboundProjectsWrittenToLog()
+        {
+            var unboundProjects = new[] { new ProjectMock("unbound.csproj") };
+            var logger = new TestLogger();
+
+            var testSubject = CreateTestSubject(unboundProjects, logger);
+
+            testSubject.IsBindingUpdateRequired();
+
+            logger.AssertOutputStrings(1);
+            logger.AssertPartialOutputStringExists("unbound.csproj");
+        }
+
+        private BindingChecker CreateTestSubject(EnvDTE.Project[] unboundProjects, ILogger logger = null)
+        {
+            var unboundProjectFinder = new Mock<IUnboundProjectFinder>();
+            unboundProjectFinder.Setup(x => x.GetUnboundProjects()).Returns(unboundProjects);
+
+            logger ??= new TestLogger();
+
+            return new BindingChecker(unboundProjectFinder.Object, logger);
+        }
+    }
+}
---
+++ src/Integration/LocalServices/IBindingChecker.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Linq;
+using SonarLint.VisualStudio.Integration.Resources;
+
+namespace SonarLint.VisualStudio.Integration
+{
+    interface IBindingChecker
+    {
+        /// <summary>
+        /// Returns true/false if the currently opened solution/folder is bound and requires re-binding
+        /// </summary>
+        bool IsBindingUpdateRequired();
+    }
+
+    internal class BindingChecker : IBindingChecker
+    {
+        private readonly IUnboundProjectFinder unboundProjectFinder;
+        private readonly ILogger logger;
+
+        public BindingChecker(IUnboundProjectFinder unboundProjectFinder, ILogger logger)
+        {
+            this.unboundProjectFinder = unboundProjectFinder;
+            this.logger = logger;
+        }
+
+        public bool IsBindingUpdateRequired()
+        {
+            var unboundProjects = unboundProjectFinder.GetUnboundProjects().ToArray();
+            var hasUnboundProjects = unboundProjects.Length > 0;
+
+            if (hasUnboundProjects)
+            {
+                logger.WriteLine(Strings.SonarLintFoundUnboundProjects, unboundProjects.Length, string.Join(", ", unboundProjects.Select(p => p.UniqueName)));
+            }
+
+            return hasUnboundProjects;
+        }
+    }
+}
