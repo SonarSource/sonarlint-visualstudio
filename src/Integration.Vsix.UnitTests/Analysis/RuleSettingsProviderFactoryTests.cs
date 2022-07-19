a605a7eeae75b98fb1cb16d94fd10a50ad5a3276
Rita Gorokhod <rita.gorokhod@sonarsource.com>
2022-07-19 08:28:15 +0200

    Add RuleSettingsProvider

---
+++ src/Integration.Vsix.UnitTests/Analysis/RuleSettingsProviderFactoryTests.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.Core.Binding;
+using SonarLint.VisualStudio.Integration.Vsix.Analysis;
+
+namespace SonarLint.VisualStudio.Integration.UnitTests.Analysis
+{
+    [TestClass]
+    public class RuleSettingsProviderFactoryTests
+    {
+        [TestMethod]
+        public void MefCtor_CheckIsExported()
+        {
+            MefTestHelpers.CheckTypeCanBeImported<RuleSettingsProviderFactory, IRuleSettingsProviderFactory>(null, new[]
+            {
+                MefTestHelpers.CreateExport<IActiveSolutionBoundTracker>(Mock.Of<IActiveSolutionBoundTracker>()),
+                MefTestHelpers.CreateExport<IUserSettingsProvider>(Mock.Of<IUserSettingsProvider>()),
+                MefTestHelpers.CreateExport<ILogger>(Mock.Of<ILogger>())
+            });
+        }
+
+        [TestMethod]
+        public void Get_CreatesRuleSettingsProvider()
+        {
+            var testSubject = new RuleSettingsProviderFactory(
+                Mock.Of<IActiveSolutionBoundTracker>(),
+                Mock.Of<IUserSettingsProvider>(),
+                Mock.Of<IRulesSettingsSerializer>(),
+                Mock.Of<ILogger>());
+
+            var result = testSubject.Get(Language.C);
+
+            result.Should().NotBeNull();
+        }
+    }
+}
