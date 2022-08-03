d9e6a9bdc948fc23ebf302199487ec084bfe4a1e
Rita Gorokhod <60586879+rita-sonarsource@users.noreply.github.com>
2022-08-03 15:09:40 +0200

    Use the same serverInfo in subsequent calculations (#130)

---
+++ SonarQube.Client.Tests/SonarQubeService_GetViewHotspotUrl_Disconnect.cs
@@ -0,0 +1,70 @@
+﻿/*
+ * SonarQube Client
+ * Copyright (C) 2016-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SonarQube.Client.Helpers;
+using SonarQube.Client.Logging;
+using SonarQube.Client.Requests;
+
+namespace SonarQube.Client.Tests
+{
+    [TestClass]
+    public class SonarQubeService_GetViewHotspotUrl_Disconnect : SonarQubeService_TestBase
+    {
+        [TestMethod]
+        [Description("Regression test for https://github.com/SonarSource/sonarlint-visualstudio/issues/3142")]
+        public async Task GetViewHotspotUrl_DisconnectedInTheMiddle_NoException()
+        {
+            await ConnectToSonarQube("3.3.0.0", serverUrl: "https://sonarcloud.io");
+
+            var result = service.GetViewHotspotUrl("myProject", "myHotspot");
+
+            result.Should().NotBeNull();
+            result.Should().BeEquivalentTo(new Uri("https://sonarcloud.io/project/security_hotspots?id=myProject&hotspots=myHotspot"));
+        }
+
+        protected internal override SonarQubeService CreateTestSubject()
+        {
+            return new DisconnectingService(messageHandler.Object, UserAgent, logger, requestFactorySelector, secondaryIssueHashUpdater.Object);
+        }
+
+        internal class DisconnectingService : SonarQubeService
+        {
+            internal DisconnectingService(HttpMessageHandler messageHandler, string userAgent, ILogger logger, IRequestFactorySelector requestFactorySelector, ISecondaryIssueHashUpdater secondaryIssueHashUpdater)
+                : base(messageHandler, userAgent, logger, requestFactorySelector, secondaryIssueHashUpdater)
+            {
+            }
+
+            protected override ServerInfo EnsureIsConnected()
+            {
+                var serverInfo = base.EnsureIsConnected();
+
+                // Simulate disconnecting immediately after calling Ensure
+                Disconnect();
+
+                return serverInfo;
+            }
+        }
+    }
+}
