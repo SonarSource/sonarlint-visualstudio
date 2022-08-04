b84aa92b6de7d9128a063592e4992d0f4fdf35ea
Rita Gorokhod <rita.gorokhod@sonarsource.com>
2022-08-04 13:08:26 +0200

    Re-analyze when user settings change only when in standalone mode

--- src/Integration.Vsix/Analysis/AnalysisStrings.Designer.cs
+++ src/Integration.Vsix/Analysis/AnalysisStrings.Designer.cs
@@ -19,7 +19,7 @@ namespace SonarLint.VisualStudio.Integration.Vsix.Analysis {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class AnalysisStrings {
@@ -79,7 +79,7 @@ namespace SonarLint.VisualStudio.Integration.Vsix.Analysis {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Note: user settings are ignored for languages that support Connected Mode i.e. C and C++.
+        ///   Looks up a localized string similar to User settings change is ignored: current workspace is in Connected Mode..
         /// </summary>
         internal static string ConfigMonitor_UserSettingsIgnoredForConnectedModeLanguages {
             get {
