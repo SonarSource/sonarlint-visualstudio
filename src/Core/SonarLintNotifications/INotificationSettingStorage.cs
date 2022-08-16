9cfb8a35041da8490e265f7033bbb574fdd9410c
Uğraş Ergün <ugras.ergun@sonarsource.com>
2022-08-16 14:47:14 +0200

    Notification setting Storage

---
+++ src/Core/SonarLintNotifications/INotificationSettingStorage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonarLint.VisualStudio.Core.SonarLintNotifications
+{
+    public enum StorageLocation
+    {
+        User,
+        VSInstance,
+        Solution
+    }
+
+    public interface INotificationSettingStorage
+    {
+        void SaveNotificationSetting(INotificationSetting setting, StorageLocation storageLocation);
+        void GetNotificationSetting(string notificationSettingKey, StorageLocation storageLocation);
+    }
+}
---
+++ src/Core/SonarLintNotifications/NotificationSetting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonarLint.VisualStudio.Core.SonarLintNotifications
+{
+    public interface INotificationSetting
+    {
+        string Key { get; }
+        bool DoNotShow { get; }
+    }
+
+    public class NotificationSetting : INotificationSetting
+    {
+        public string Key { get;  }
+        public bool DoNotShow { get; }
+    }
+}
