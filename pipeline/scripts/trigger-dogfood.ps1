3bae55d8fdc2f6d3218245be17cb78506a492812
Uğraş Ergün <ugras.ergun@sonarsource.com>
2022-09-29 14:38:00 +0200

    extracted powershell scripts

---
+++ pipeline/scripts/trigger-dogfood.ps1
@@ -0,0 +1,18 @@
+Write-Host "Current branch: $(Build.SourceBranch)"
+Write-Host "Trigger dogfood release flag; $(TriggerDogfoodReleaseIfMasterBranch)"
+
+if ("$(Build.SourceBranch)" -eq "refs/heads/master"){
+
+    if ("$(TriggerDogfoodReleaseIfMasterBranch)" -eq "true"){
+        Write-Host "Building master and trigger flag is true. Setting tag to trigger dogfood release pipeline."
+        Write-Host "##vso[build.addbuildtag]TriggerDogfoodVsixUpdate"
+    }
+    else{
+        Write-Host "Building master but trigger flag is false. Tag to trigger dogfood release pipeline will not be set."
+        Write-Host "##vso[build.addbuildtag]DoNot_TriggerDogfoodVsixUpdate_TriggerFlagIsFalse"
+    }
+}
+else{
+    Write-Host "Current branch is not master. Tag to trigger dogfood release pipeline will not be set."
+    Write-Host "##vso[build.addbuildtag]BuildTag DoNot_TriggerDogfoodVsixUpdate_NotMaster"
+}
