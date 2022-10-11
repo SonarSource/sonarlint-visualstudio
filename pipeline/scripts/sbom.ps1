cc36094a4f40151d8e790bcbd768e0b31cef48e0
duncanp-sonar <duncan.pocklington@sonarsource.com>
2022-10-11 16:22:41 +0100

    Test whether we need to install Gpg.Windows.x64 explicitly

--- pipeline/scripts/sbom.ps1
+++ pipeline/scripts/sbom.ps1
@@ -1,14 +1,16 @@
 # Calculate the file name
 $sbomName = "SonarLint.visualstudio.sbom-$env:SONAR_PROJECT_VERSION.$env:BUILD_BUILDID-$env:VSTARGETVERSION.json"
 Write-Host "SBOM file is: $sbomName"
-Write-Host "signkey file path is: $env:SIGNKEY_SECUREFILEPATH"
 
 Rename-Item -Path "binaries\bom.json" -NewName "${sbomName}"
 
 # Set the variable to it can be used by other tasks
 Write-Host "##vso[task.setvariable variable=SBOM_NAME;]$sbomName"
 
-Install-Package Gpg.Windows.x64
+# Note: we're assuming the PowerShell package Gpg.Windows.x64 is installed on the hosted agent.
+
+#check if this necessary... the log output for this step always has "WARNING: User declined to install package (Gpg.Windows.x64)"
+#Install-Package Gpg.Windows.x64
 gpg --batch --passphrase "$env:PGP_PASSPHRASE" --allow-secret-key-import --import "$env:SIGNKEY_SECUREFILEPATH"
 gpg --list-secret-keys
 cd binaries
