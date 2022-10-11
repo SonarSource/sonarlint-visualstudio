2beebc594bea5dbfae105949448f56437e9ab32e
duncanp-sonar <duncan.pocklington@sonarsource.com>
2022-10-11 15:34:44 +0100

    Fix SBOM generation

--- pipeline/scripts/sbom.ps1
+++ pipeline/scripts/sbom.ps1
@@ -1,6 +1,7 @@
 # Calculate the file name
-$sbomName = "SonarLint.visualstudio.sbom-$env:SONAR_PROJECT_VERSION.$env:BUILD_BUILDID-$(vsTargetVersion).json"
+$sbomName = "SonarLint.visualstudio.sbom-$env:SONAR_PROJECT_VERSION.$env:BUILD_BUILDID-$env:VSTARGETVERSION.json"
 Write-Host "SBOM file is: $sbomName"
+Write-Host "signkey file path is: $env:SIGNKEY_SECUREFILEPATH"
 
 Rename-Item -Path "binaries\bom.json" -NewName "${sbomName}"
 
@@ -8,9 +9,9 @@ Rename-Item -Path "binaries\bom.json" -NewName "${sbomName}"
 Write-Host "##vso[task.setvariable variable=SBOM_NAME;]$sbomName"
 
 Install-Package Gpg.Windows.x64 
-gpg --batch --passphrase $(PGP_PASSPHRASE) --allow-secret-key-import --import $(signKey.secureFilePath)
+gpg --batch --passphrase $env:PGP_PASSPHRASE --allow-secret-key-import --import "$env:SIGNKEY_SECUREFILEPATH"
 gpg --list-secret-keys
 cd binaries
 Write-Host "About to sign $sbomName"
-gpg --pinentry-mode loopback  --passphrase $(PGP_PASSPHRASE) --armor --detach-sig --default-key infra@sonarsource.com "$sbomName"
+gpg --pinentry-mode loopback  --passphrase $env:PGP_PASSPHRASE --armor --detach-sig --default-key infra@sonarsource.com "$sbomName"
 Write-Host "Signed $sbomName"
