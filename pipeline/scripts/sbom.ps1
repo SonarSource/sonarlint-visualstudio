d9c921c4d6790347098bc9ac6e1df9c467da354a
duncanp-sonar <duncan.pocklington@sonarsource.com>
2022-10-12 10:59:41 +0100

    Fix SBOM generation in pipeline (#3292)
    * Fix SBOM generation

--- pipeline/scripts/sbom.ps1
+++ pipeline/scripts/sbom.ps1
@@ -1,5 +1,5 @@
 # Calculate the file name
-$sbomName = "SonarLint.visualstudio.sbom-$env:SONAR_PROJECT_VERSION.$env:BUILD_BUILDID-$(vsTargetVersion).json"
+$sbomName = "SonarLint.visualstudio.sbom-$env:SONAR_PROJECT_VERSION.$env:BUILD_BUILDID-$env:VSTARGETVERSION.json"
 Write-Host "SBOM file is: $sbomName"
 
 Rename-Item -Path "binaries\bom.json" -NewName "${sbomName}"
@@ -7,10 +7,13 @@ Rename-Item -Path "binaries\bom.json" -NewName "${sbomName}"
 # Set the variable to it can be used by other tasks
 Write-Host "##vso[task.setvariable variable=SBOM_NAME;]$sbomName"
 
-Install-Package Gpg.Windows.x64 
-gpg --batch --passphrase $(PGP_PASSPHRASE) --allow-secret-key-import --import $(signKey.secureFilePath)
+# Note: we're assuming the PowerShell package Gpg.Windows.x64 is installed on the hosted agent.
+# If not, run the following:
+#Install-Package Gpg.Windows.x64 -Scope CurrentUser -Force
+
+gpg --batch --passphrase "$env:PGP_PASSPHRASE" --allow-secret-key-import --import "$env:SIGNKEY_SECUREFILEPATH"
 gpg --list-secret-keys
 cd binaries
 Write-Host "About to sign $sbomName"
-gpg --pinentry-mode loopback  --passphrase $(PGP_PASSPHRASE) --armor --detach-sig --default-key infra@sonarsource.com "$sbomName"
+gpg --pinentry-mode loopback  --passphrase "$env:PGP_PASSPHRASE" --armor --detach-sig --default-key infra@sonarsource.com "$sbomName"
 Write-Host "Signed $sbomName"
