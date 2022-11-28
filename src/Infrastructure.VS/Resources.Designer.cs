7cef15f8874328a21e129a4f338f88f962f5fc07
Uğraş Ergün <ugras.ergun@sonarsource.com>
2022-11-28 09:25:38 +0100

    Workspace Service

--- src/Infrastructure.VS/Resources.Designer.cs
+++ src/Infrastructure.VS/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace SonarLint.VisualStudio.Infrastructure.VS {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -66,6 +66,15 @@ namespace SonarLint.VisualStudio.Infrastructure.VS {
         internal static string FailedToCalculateVsVersion {
             get {
                 return ResourceManager.GetString("FailedToCalculateVsVersion", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to No open solution or folder has been found.
+        /// </summary>
+        internal static string NoOpenSolutionOrFolder {
+            get {
+                return ResourceManager.GetString("NoOpenSolutionOrFolder", resourceCulture);
             }
         }
         
