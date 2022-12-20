30fb3b6456c60aad0aea69c17e0737f0d8bdd22b
duncanp-sonar <duncan.pocklington@sonarsource.com>
2022-12-20 07:42:47 +0000

    Replace CFamily MSBuild task with exe (#3448)

---
+++ build/tools/CFamilyJarPreProcessor/Preprocessor.cs
@@ -0,0 +1,124 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Generic;
+using System.IO;
+
+namespace CFamilyJarPreProcessor
+{
+    internal class Preprocessor
+    {
+        // The txz archive containing the subprocess.exe
+        private const string WindowsTxzFilePattern = "clang*-win.txz";
+
+        // Sub-folder into which the tar file should be unzipped
+        private const string TarUnzipSubFolder = "tar_xz";
+
+        // List of patterns to match single files in the uncompressed output
+        private readonly string[] SingleFilePatterns = new string[]
+        {
+                "Sonar_way_profile.json",
+                "RulesList.json",
+                TarUnzipSubFolder + @"\subprocess.exe",
+                TarUnzipSubFolder + @"\LICENSE_THIRD_PARTY.txt"
+        };
+
+        // List of patterns to match multiple files in the uncompressed output
+        private readonly string[] MultipleFilesPatterns = new string[]
+        {
+            @"org\sonar\l10n\cpp\rules\params\*_params.json",
+            @"org\sonar\l10n\cpp\rules\cpp\*.json"
+        };
+
+        private readonly ILogger logger;
+        public Preprocessor(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        /// <summary>
+        /// Downloads plugin, extracts files, and copies the requires file to the output folder 
+        /// </summary>
+        /// <remarks>The method is lazy i.e. won't download the plugin if it exists, won't copy a file
+        /// unless it is newer than the target file.</remarks>
+        public void Execute(string downloadUrl, string destinationDir)
+        {
+            var pluginFileName = Common.ExtractPluginFileNameFromUrl(downloadUrl, logger);
+
+            // Ensure working directories exists
+            var localWorkingFolder = Common.GetLocalBuildTimePluginCacheDir("SLVS_CFamily_Build");
+            var perVersionPluginFolder = Path.Combine(localWorkingFolder, Path.GetFileNameWithoutExtension(pluginFileName));
+            Common.EnsureWorkingDirectoryExist(perVersionPluginFolder, logger);
+
+            // Download and unzip the jar
+            var jarFilePath = Path.Combine(perVersionPluginFolder, pluginFileName);
+            Common.DownloadJarFile(downloadUrl, jarFilePath, logger);
+            Common.UnzipJar(jarFilePath, perVersionPluginFolder, logger);
+
+            // Uncompress and extract the windows tar archive to get the subprocess exe
+            var tarFilePath = Common.FindSingleFile(perVersionPluginFolder, WindowsTxzFilePattern, logger);
+            var tarSubFolder = Path.Combine(perVersionPluginFolder, TarUnzipSubFolder);
+            Common.UncompressAndUnzipTgx(tarFilePath, tarSubFolder, logger);
+
+            // Locate the required files from the uncompressed jar and tar
+            var fileList = FindFiles(perVersionPluginFolder);
+
+            // Copy the files to the output directory
+            CopyFilesToOutputDirectory(fileList, destinationDir);
+        }
+
+        private List<string> FindFiles(string searchRoot)
+        {
+            var files = new List<string>();
+
+            files.AddRange(Common.FindSingleFiles(searchRoot, SingleFilePatterns, logger));
+            files.AddRange(Common.FindMultipleFiles(searchRoot, MultipleFilesPatterns, logger));
+
+            return files;
+        }
+
+        private void CopyFilesToOutputDirectory(IList<string> files, string destinationDir)
+        {
+            Common.EnsureWorkingDirectoryExist(destinationDir, logger);
+
+            foreach(var file in files)
+            {
+                CopyIfNewer(file, destinationDir);
+            }
+        }
+
+        private void CopyIfNewer(string file, string destinationDir)
+        {
+            // Overwrite if newer
+            var sourceFileInfo = new FileInfo(file);
+            var destinationFileInfo = new FileInfo(Path.Combine(destinationDir, sourceFileInfo.Name));
+
+            if (!destinationFileInfo.Exists || sourceFileInfo.LastWriteTimeUtc > destinationFileInfo.LastWriteTimeUtc)
+            {
+                logger.LogMessage($"  Copying file: {file}");
+                File.Copy(sourceFileInfo.FullName, destinationFileInfo.FullName, true);
+            }
+            else
+            {
+                logger.LogMessage($"  Skipping copying file - not newer: {file}");
+            }
+        }
+    }
+}
---
+++ build/tools/CFamilyJarPreProcessor/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace CFamilyJarPreProcessor
+{
+    internal static class Program
+    {
+        static int Main(string[] args)
+        {
+            ILogger logger = new ConsoleLogger();
+
+            if (args.Length != 2)
+            {
+                logger.LogError("Expected parameters: [plugin download url] [full path to destination directory]");
+                return -1;
+            }
+
+            var downloadUrl = args[0];
+            var destinationDir = args[1];
+
+            var preprocessor = new Preprocessor(logger);
+            
+            preprocessor.Execute(downloadUrl, destinationDir);
+
+            return 0;
+        }
+    }
+}
