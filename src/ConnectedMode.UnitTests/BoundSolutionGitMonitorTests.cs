2acdd54bd50eb8c33b850484b72e00417c49fb0c
Uğraş Ergün <ugras.ergun@sonarsource.com>
2022-12-28 17:09:56 +0100

    Added tests

--- src/ConnectedMode.UnitTests/BoundSolutionGitMonitorTests.cs
+++ src/ConnectedMode.UnitTests/BoundSolutionGitMonitorTests.cs
@@ -18,8 +18,10 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+using System;
 using SonarLint.VisualStudio.Core;
 using SonarLint.VisualStudio.Integration.UnitTests;
+using static SonarLint.VisualStudio.ConnectedMode.BoundSolutionGitMonitor;
 
 namespace SonarLint.VisualStudio.ConnectedMode.UnitTests
 {
@@ -31,6 +33,89 @@ namespace SonarLint.VisualStudio.ConnectedMode.UnitTests
         {
             MefTestHelpers.CheckTypeCanBeImported<BoundSolutionGitMonitor, IBoundSolutionGitMonitor>(
                 MefTestHelpers.CreateExport<IGitWorkspaceService>());
+        }
+
+        [TestMethod]
+        public void Initialize_ForwardsLowLevelEvent()
+        {
+            string repoPath = "some path";
+            int counter = 0;
+
+            var gitWorkspaceService = CreateGitWorkSpaceService(repoPath);
+
+            var gitEventsMonitor = new Mock<IGitEvents>();
+
+            GitEventFactory gitEventFactory = (string path) =>
+            {
+                if (path != repoPath)
+                {
+                    throw new Exception();
+                }
+
+                return gitEventsMonitor.Object;
+            };
+            BoundSolutionGitMonitor testSubject = CreateTestSubject(gitWorkspaceService, gitEventFactory);
+
+            testSubject.HeadChanged += (o, e) => counter++;
+
+            gitEventsMonitor.Raise(em => em.HeadChanged += null, null, null);
+
+            counter.Should().Be(1);
+        }
+
+        [TestMethod]
+        public void Refresh_ChangesLowLevelMonitor()
+        {
+            string originalPath = "original path";
+            string newPath = "new path";
+
+            int counter = 0;
+
+            var gitWorkspaceService = CreateGitWorkSpaceService(originalPath);
+
+            var originalEventsMonitor = new Mock<IGitEvents>();
+            var newEventsMonitor = new Mock<IGitEvents>();
+
+            GitEventFactory gitEventFactory = (string path) =>
+            {
+                if (path != originalPath && path != newPath)
+                {
+                    throw new Exception();
+                }
+
+                return path == originalPath ? originalEventsMonitor.Object : newEventsMonitor.Object;
+            };
+
+            BoundSolutionGitMonitor testSubject = CreateTestSubject(gitWorkspaceService, gitEventFactory);
+            testSubject.HeadChanged += (o, e) => counter++;
+
+            newEventsMonitor.Raise(em => em.HeadChanged += null, null, null);
+            counter.Should().Be(0);
+
+            originalEventsMonitor.Raise(em => em.HeadChanged += null, null, null);
+            counter.Should().Be(1);
+
+            gitWorkspaceService.Setup(ws => ws.GetRepoRoot()).Returns(newPath);
+
+            testSubject.Refresh();
+
+            originalEventsMonitor.Raise(em => em.HeadChanged += null, null, null);
+            counter.Should().Be(1);
+
+            newEventsMonitor.Raise(em => em.HeadChanged += null, null, null);
+            counter.Should().Be(2);
+        }
+
+        private Mock<IGitWorkspaceService> CreateGitWorkSpaceService(string repoPath)
+        {
+            var gitWorkspaceService = new Mock<IGitWorkspaceService>();
+            gitWorkspaceService.Setup(ws => ws.GetRepoRoot()).Returns(repoPath);
+            return gitWorkspaceService;
+        }
+        
+        private BoundSolutionGitMonitor CreateTestSubject(Mock<IGitWorkspaceService> gitWorkspaceService, GitEventFactory gitEventFactory)
+        {
+            return new BoundSolutionGitMonitor(gitWorkspaceService.Object, gitEventFactory);
         }
     }
 }
