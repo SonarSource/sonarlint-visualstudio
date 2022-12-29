d2c6a2087e719c2d47a230d0e0d27ec8b94ebb0a
Uğraş Ergün <ugras.ergun@sonarsource.com>
2022-12-29 14:24:07 +0100

    added cache and tests

--- src/ConnectedMode.UnitTests/StatefulServerBranchProviderTests.cs
+++ src/ConnectedMode.UnitTests/StatefulServerBranchProviderTests.cs
@@ -18,7 +18,10 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+using System.Threading;
+using System.Threading.Tasks;
 using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.Core.Binding;
 using SonarLint.VisualStudio.Integration.UnitTests;
 
 namespace SonarLint.VisualStudio.ConnectedMode.UnitTests
@@ -30,7 +33,75 @@ namespace SonarLint.VisualStudio.ConnectedMode.UnitTests
         public void MefCtor_CheckIsExported()
         {
             MefTestHelpers.CheckTypeCanBeImported<StatefulServerBranchProvider, IStatefulServerBranchProvider>(
-                MefTestHelpers.CreateExport<IServerBranchProvider>());
+                MefTestHelpers.CreateExport<IServerBranchProvider>(),
+                MefTestHelpers.CreateExport<IActiveSolutionBoundTracker>());
+        }
+
+        [TestMethod]
+        public async Task GetServerBranchNameAsync_WhenCalled_UsesCache()
+        {
+            var serverBranchProvider = CreateServerBranchProvider("Branch");
+
+            var activeSolutionBoundTracker = Mock.Of<IActiveSolutionBoundTracker>();
+
+            var testSubject = CreateTestSubject(serverBranchProvider.Object, activeSolutionBoundTracker);
+
+            //first call: Should use IServerBranchProvider
+            var serverBranch = await testSubject.GetServerBranchNameAsync(CancellationToken.None);
+
+            serverBranch.Should().Be("Branch");
+
+            serverBranchProvider.Verify(sbp => sbp.GetServerBranchNameAsync(It.IsAny<CancellationToken>()), Times.Once);
+
+            serverBranchProvider.Setup(sbp => sbp.GetServerBranchNameAsync(It.IsAny<CancellationToken>())).ReturnsAsync("NewBranch");
+
+            //second call: Should use cache
+            serverBranch = await testSubject.GetServerBranchNameAsync(CancellationToken.None);
+
+            serverBranch.Should().Be("Branch");
+
+            serverBranchProvider.Verify(sbp => sbp.GetServerBranchNameAsync(It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task GetServerBranchNameAsync_WhenSolutionBindingChanged_ClearCache()
+        {
+            var serverBranchProvider = CreateServerBranchProvider("Branch");
+
+            var activeSolutionBoundTracker = new Mock<IActiveSolutionBoundTracker>();
+
+            var testSubject = CreateTestSubject(serverBranchProvider.Object, activeSolutionBoundTracker.Object);
+
+            //first call: Should use IServerBranchProvider
+            var serverBranch = await testSubject.GetServerBranchNameAsync(CancellationToken.None);
+
+            serverBranch.Should().Be("Branch");
+
+            serverBranchProvider.Verify(sbp => sbp.GetServerBranchNameAsync(It.IsAny<CancellationToken>()), Times.Once);
+
+            serverBranchProvider.Setup(sbp => sbp.GetServerBranchNameAsync(It.IsAny<CancellationToken>())).ReturnsAsync("NewBranch");
+
+            activeSolutionBoundTracker.Raise(asbt => asbt.PreSolutionBindingChanged += null, null, null);
+
+            //second call: Should use cache
+            serverBranch = await testSubject.GetServerBranchNameAsync(CancellationToken.None);
+
+            serverBranch.Should().Be("NewBranch");
+
+            serverBranchProvider.Verify(sbp => sbp.GetServerBranchNameAsync(It.IsAny<CancellationToken>()), Times.Exactly(2));
+        }
+
+        private Mock<IServerBranchProvider> CreateServerBranchProvider(string branchName)
+        {
+            var serverBranchProvider = new Mock<IServerBranchProvider>();
+            serverBranchProvider.Setup(sbp => sbp.GetServerBranchNameAsync(It.IsAny<CancellationToken>())).ReturnsAsync(branchName);
+
+            return serverBranchProvider;
+        }
+
+        private StatefulServerBranchProvider CreateTestSubject(IServerBranchProvider provider, IActiveSolutionBoundTracker tracker)
+        {
+            return new StatefulServerBranchProvider(provider, tracker);
         }
     }
 }
