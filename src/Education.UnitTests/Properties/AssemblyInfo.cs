d1fc55d60b53dc990cf6ca58e175acadf3111dbc
Leyla Büchel <leyla.buechel@sonarsource.com>
2023-01-10 15:22:54 +0100

    Add new assembly for Education

---
+++ src/Education.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SonarLint.VisualStudio.Education.UnitTests")]
+[assembly: Guid("62a00556-28d6-4485-a9e0-c88e406123dc")]
