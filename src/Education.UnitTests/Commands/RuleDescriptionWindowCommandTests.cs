25919d0a5c357f6cd0583b17f2dec1bb306f369e
Leyla <54935347+bigfluffycookie@users.noreply.github.com>
2023-01-23 16:20:44 +0100

    Add new commmand "Open rule issue" which opens an empty tool window (#3541)
    * Add new commmand "Open rule issue" which opens an empty tool window
    
    * Add tests for rule description command
    
    * Add explicit reference to some frameworks in .cproj file
    
    * Add composition reference
    
    * Add even more references.
    
    * Switch out reference to Integration.UnitTests with TestInfrastructure
    
    * Pr fixes

---
+++ src/Education.UnitTests/Commands/RuleDescriptionWindowCommandTests.cs
@@ -0,0 +1,131 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.ComponentModel.Design;
+using FluentAssertions;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.Integration;
+using SonarLint.VisualStudio.Integration.UnitTests;
+using SonarLint.VisualStudio.Education;
+using SonarLint.VisualStudio.Education.Commands;
+
+using ThreadHelper = SonarLint.VisualStudio.Integration.UnitTests.ThreadHelper;
+
+namespace Education.UnitTests.Commands
+{
+    [TestClass]
+    public class RuleDescriptionWindowCommandTests
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            ThreadHelper.SetCurrentThreadAsUIThread();
+        }
+
+        [TestMethod]
+        public void Ctor_ArgsCheck()
+        {
+            var toolWindowService = Mock.Of<IToolWindowService>();
+            var commandService = Mock.Of<IMenuCommandService>();
+            var logger = Mock.Of<ILogger>();
+            
+            Action act = () => new RuleDescriptionWindowCommand(null, toolWindowService, logger);
+            act.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("commandService");
+
+            act = () => new RuleDescriptionWindowCommand(commandService, null, logger);
+            act.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("toolWindowService");
+
+            act = () => new RuleDescriptionWindowCommand(commandService, toolWindowService, null);
+            act.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("logger");
+        }
+
+        [TestMethod]
+        public void Ctor_CommandAddedToMenu()
+        {
+            var commandService = new Mock<IMenuCommandService>();
+
+            new RuleDescriptionWindowCommand(commandService.Object, Mock.Of<IToolWindowService>(), Mock.Of<ILogger>());
+
+            var guid = new Guid("80127033-1819-4996-8C45-E9C96F75E2A8");
+            var id = 0x100;
+
+            commandService.Verify(x =>
+                    x.AddCommand(It.Is((MenuCommand c) =>
+                        c.CommandID.Guid == guid &&
+                        c.CommandID.ID == id)),
+                Times.Once);
+
+            commandService.VerifyNoOtherCalls();
+        }
+
+        [TestMethod]
+        public void Execute_ServiceCalled()
+        {
+            var logger = new TestLogger(logToConsole: true);
+            var toolwindowServiceMock = new Mock<IToolWindowService>();
+
+            var testSubject = new RuleDescriptionWindowCommand(Mock.Of<IMenuCommandService>(), toolwindowServiceMock.Object, logger);
+
+            // Act
+            testSubject.Execute(null, null);
+
+            toolwindowServiceMock.Verify(x => x.Show(RuleDescriptionToolWindow.ToolWindowId), Times.Once);
+            logger.AssertNoOutputMessages();
+        }
+
+        [TestMethod]
+        public void Execute_NonCriticalException_IsSuppressed()
+        {
+            var logger = new TestLogger(logToConsole: true);
+            var toolwindowServiceMock = new Mock<IToolWindowService>();
+            toolwindowServiceMock.Setup(x => x.Show(RuleDescriptionToolWindow.ToolWindowId)).Throws(new InvalidOperationException("thrown by test"));
+
+            var testSubject = new RuleDescriptionWindowCommand(Mock.Of<IMenuCommandService>(), toolwindowServiceMock.Object, logger);
+
+            // Act
+            testSubject.Execute(null, null);
+
+            toolwindowServiceMock.Verify(x => x.Show(RuleDescriptionToolWindow.ToolWindowId), Times.Once);
+            logger.AssertPartialOutputStringExists("thrown by test");
+        }
+
+        [TestMethod]
+        public void Execute_CriticalException_IsNotSuppressed()
+        {
+            var logger = new TestLogger(logToConsole: true);
+            var toolwindowServiceMock = new Mock<IToolWindowService>();
+            toolwindowServiceMock.Setup(x => x.Show(RuleDescriptionToolWindow.ToolWindowId)).Throws(new StackOverflowException("thrown by test"));
+
+            var testSubject = new RuleDescriptionWindowCommand(Mock.Of<IMenuCommandService>(), toolwindowServiceMock.Object, logger);
+
+            // Act
+            Action act = () => testSubject.Execute(null, null);
+
+            act.Should().ThrowExactly<StackOverflowException>().And.Message.Should().Be("thrown by test");
+            logger.AssertNoOutputMessages();
+        }
+    }
+}
