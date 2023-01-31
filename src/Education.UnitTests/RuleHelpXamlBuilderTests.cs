905e94a61716ab5878c234c0dc6ae942c5ea42bf
duncanp-sonar <duncan.pocklington@sonarsource.com>
2023-01-31 21:57:25 +0000

    Add test to parse all rule resources

---
+++ src/Education.UnitTests/RuleHelpXamlBuilderTests.cs
@@ -0,0 +1,98 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.Education.XamlGenerator;
+using SonarLint.VisualStudio.Rules;
+
+namespace SonarLint.VisualStudio.Education.UnitTests
+{
+    [TestClass]
+    public class RuleHelpXamlBuilderTests
+    {
+        private static readonly Assembly ResourceAssembly = typeof(RuleMetadataProvider).Assembly;
+
+        [TestMethod]
+        public void CreateXamlString_CheckAllEmbedded()
+        {            
+            // Performance: this test is loading and parsing nearly 2000 files,
+            // but is still only takes a few hundred milliseconds.
+            var resourceNames = ResourceAssembly.GetManifestResourceNames()
+                .Where(x => x.EndsWith(".desc"));
+
+            // Sanity check - should have checked at least 1500 rules
+            resourceNames.Count().Should().BeGreaterThan(1500);
+
+            Console.WriteLine("Checking xaml creation. Count = " + resourceNames.Count());
+            var failures = resourceNames.Where(x => !ProcessResource(x))
+                .ToArray();
+
+            failures.Should().HaveCount(0);
+        }
+
+        private static bool ProcessResource(string fullResourceName)
+        {
+            var testSubject = new RuleHelpXamlBuilder();
+
+            try
+            {
+                var input = ReadResource(fullResourceName);
+                (var language, var ruleKey) = GetLanguageAndKeyFromResourceName(fullResourceName);
+                var ruleHelp = new RuleHelp(language, ruleKey, input);
+
+                var xamlString = testSubject.CreateXamlString(ruleHelp);
+                Console.WriteLine($"{language.Name} {ruleKey}: size = {xamlString.Length}");
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed: " + fullResourceName);
+                Console.WriteLine("    " + ex.Message);
+                return false;
+            }
+        }
+
+        private static string ReadResource(string fullResourceName)
+        {
+            using var stream = new StreamReader(ResourceAssembly.GetManifestResourceStream(fullResourceName));
+            return stream.ReadToEnd();
+        }
+
+        private static (Language Language, string ruleKey) GetLanguageAndKeyFromResourceName(string fullResourceName)
+        {
+            // Names are expected to be in the format:
+            //   SonarLint.VisualStudio.Rules.Embedded.{language key}.{rule key}
+            // e.g. SonarLint.VisualStudio.Rules.Embedded.cpp.S101.desc
+            var parts = fullResourceName.Split('.');
+            var languageKey = parts[parts.Length - 3];
+            var ruleKey = parts[parts.Length - 2];
+
+            var language = Language.GetLanguageFromLanguageKey(languageKey);
+            return (language, ruleKey);
+        }
+    }
+}
