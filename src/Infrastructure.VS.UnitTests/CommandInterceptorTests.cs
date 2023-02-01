bc51ee0a07c57e9ebe2ad25a79e86ed20a09f1bd
Leyla <54935347+bigfluffycookie@users.noreply.github.com>
2023-02-01 11:44:58 +0100

    Add a command interceptor class (#3592)
    * Add a command interceptor class
    
    * Update tests
    
    * Fix code smell
    
    * change all guids and cmd ids to non zero values
    
    * Change Command Interceptor class from internal to public.
    
    * Move files to Infrastructure.Vs
    
    * Fix namespaces

---
+++ src/Infrastructure.VS.UnitTests/CommandInterceptorTests.cs
@@ -0,0 +1,116 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.ComponentModel.Design;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SonarLint.VisualStudio.Integration.UnitTests;
+
+namespace SonarLint.VisualStudio.Infrastructure.VS.UnitTests
+{
+    [TestClass]
+    public class CommandInterceptorTests
+    {
+        [TestMethod]
+        public void Exec_CommandProgressionStops_ReturnsZero()
+        {
+            var guid = new Guid("A057C7F2-E3D3-414F-93C9-014E16122582");
+            var commandID = new CommandID(guid, 1);
+
+            bool funcCalled = false;
+            var func = () =>
+            {
+                funcCalled = true;
+                return CommandProgression.Stop;
+            };
+
+            var testSubject = new CommandInterceptor(commandID, func, new NoOpThreadHandler());
+
+            var result = testSubject.Exec(ref guid, (uint)commandID.ID, 0, IntPtr.Zero, IntPtr.Zero);
+
+            result.Should().Be(0);
+            funcCalled.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Exec_CommandProgressionContinue_ReturnsMSOCMDERR_E_FIRST()
+        {
+            var guid = new Guid("A057C7F2-E3D3-414F-93C9-014E16122582");
+            var commandID = new CommandID(guid, 0);
+
+            bool funcCalled = false;
+            var func = () =>
+            {
+                funcCalled = true;
+                return CommandProgression.Continue;
+            };
+
+            var testSubject = new CommandInterceptor(commandID, func, new NoOpThreadHandler());
+
+            var result = testSubject.Exec(ref guid, (uint)commandID.ID, 0, IntPtr.Zero, IntPtr.Zero);
+
+            result.Should().Be(-2147221248);
+            funcCalled.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Exec_CommandIdDoesNotMatch_ReturnsMSOCMDERR_E_FIRST()
+        {
+            var guid = new Guid("A057C7F2-E3D3-414F-93C9-014E16122582");
+            var commandID = new CommandID(guid, 1);
+
+            bool funcCalled = false;
+            var func = () =>
+            {
+                funcCalled = true;
+                return CommandProgression.Continue;
+            };
+
+            var testSubject = new CommandInterceptor(commandID, func, new NoOpThreadHandler());
+
+            var result = testSubject.Exec(ref guid, nCmdID:(uint)5, 0, IntPtr.Zero, IntPtr.Zero);
+
+            result.Should().Be(-2147221248);
+            funcCalled.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Exec_GuidDoesNotMatch_ReturnsMSOCMDERR_E_FIRST()
+        {
+            var guid = new Guid("A057C7F2-E3D3-414F-93C9-014E16122582");
+            var commandID = new CommandID(new Guid("21364CA3-5CA0-41F0-885E-CA2396506132"), 1);
+
+            bool funcCalled = false;
+            var func = () =>
+            {
+                funcCalled = true;
+                return CommandProgression.Continue;
+            };
+
+            var testSubject = new CommandInterceptor(commandID, func, new NoOpThreadHandler());
+
+            var result = testSubject.Exec(ref guid, (uint)commandID.ID, 0, IntPtr.Zero, IntPtr.Zero);
+
+            result.Should().Be(-2147221248);
+            funcCalled.Should().BeFalse();
+        }
+    }
+}
