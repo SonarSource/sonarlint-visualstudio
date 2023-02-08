829698a3b0d172e17ce7848a5c013512372c047e
georgii-borovinskikh-sonarsource <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2023-02-08 15:59:41 +0100

    Implementation for SSE session manager and event pump, interface declaration for event filter  (#3576)
    * Add reference to the System.Threading.Channel package
    
    * Add ServerEventFilteredChannel and it's concrete export types, change Publish to be synchronous, add doc remarks about thread safety
    
    * Move sse channel classes&interfaces + channels nuget pkg to core, remove mef exporter inheritors for channel impl, add tests, fix comments
    
    * add sse session manager implementation
    
    * rename ssechannel
    
    * Remove filter interface
    
    * Move publisher management from SSESessionManager to SSESessionFactory
    
    * Change EventPump into a transient instance and rename to SSESession
    
    * change ISSEStream to ISSEStreamReader
    
    ---------
    
    Co-authored-by: Rita Gorokhod <60586879+rita-sonarsource@users.noreply.github.com>
    Co-authored-by: Rita Gorokhod <rita.gorokhod@sonarsource.com>

---
+++ src/ConnectedMode/ServerSentEvents/SSESessionManager.cs
@@ -0,0 +1,91 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Threading;
+using SonarLint.VisualStudio.Core.Binding;
+
+namespace SonarLint.VisualStudio.ConnectedMode.ServerSentEvents
+{
+    /// <summary>
+    /// Reacts to project changes and opens/closes Server Sent Events sessions
+    /// </summary>
+    internal interface ISSESessionManager : IDisposable
+    {
+    }
+
+    [Export(typeof(ISSESessionManager))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    internal sealed class SSESessionManager : ISSESessionManager
+    {
+        private readonly IActiveSolutionBoundTracker activeSolutionBoundTracker;
+        private readonly ISSESessionFactory sseSessionFactory;
+
+        private ISSESession currentSession;
+
+        private bool disposed;
+
+        [ImportingConstructor]
+        public SSESessionManager(IActiveSolutionBoundTracker activeSolutionBoundTracker, ISSESessionFactory sseSessionFactory)
+        {
+            this.activeSolutionBoundTracker = activeSolutionBoundTracker;
+            this.sseSessionFactory = sseSessionFactory;
+
+            activeSolutionBoundTracker.SolutionBindingChanged += SolutionBindingChanged;
+        }
+
+        public void Dispose()
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+            activeSolutionBoundTracker.SolutionBindingChanged -= SolutionBindingChanged;
+            sseSessionFactory.Dispose();
+            EndCurrentSession();
+            disposed = true;
+        }
+
+        private void SolutionBindingChanged(object sender, ActiveSolutionBindingEventArgs activeSolutionBindingEventArgs)
+        {
+            EndCurrentSession();
+
+            var bindingConfiguration = activeSolutionBindingEventArgs.Configuration;
+            var isInConnectedMode = !bindingConfiguration.Equals(BindingConfiguration.Standalone);
+
+            if (!isInConnectedMode)
+            {
+                return;
+            }
+
+            currentSession = sseSessionFactory.Create(bindingConfiguration.Project.ProjectKey);
+
+            currentSession.PumpAllAsync().Forget();
+        }
+
+        private void EndCurrentSession()
+        {
+            currentSession?.Dispose();
+            currentSession = null;
+        }
+    }
+}
