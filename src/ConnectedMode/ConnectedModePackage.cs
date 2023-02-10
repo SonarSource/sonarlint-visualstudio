e86dc7be50d1aa9c79bf6cb60523bf83cdea85c4
Rita Gorokhod <60586879+rita-sonarsource@users.noreply.github.com>
2023-02-10 12:07:20 +0100

    Initialize SSESessionManager (#3666)

---
+++ src/ConnectedMode/ConnectedModePackage.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.Shell;
+using SonarLint.VisualStudio.ConnectedMode.ServerSentEvents;
+using SonarLint.VisualStudio.Core.Binding;
+using SonarLint.VisualStudio.Integration;
+using Task = System.Threading.Tasks.Task;
+
+namespace SonarLint.VisualStudio.ConnectedMode
+{
+    [ExcludeFromCodeCoverage]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [ProvideAutoLoad(BoundSolutionUIContext.GuidString, PackageAutoLoadFlags.BackgroundLoad)]
+    [Guid("dd3427e0-7bb2-4a51-b00a-ddae2c32c7ef")]
+    public sealed class ConnectedModePackage : AsyncPackage
+    {
+        private ISSESessionManager sseSessionManager;
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            var componentModel = await GetServiceAsync(typeof(SComponentModel)) as IComponentModel;
+            var logger = componentModel.GetService<ILogger>();
+
+            logger.WriteLine(Resources.Package_Initializing);
+
+            sseSessionManager = componentModel.GetService<ISSESessionManager>();
+
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            logger.WriteLine(Resources.Package_Initialized);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                sseSessionManager?.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
