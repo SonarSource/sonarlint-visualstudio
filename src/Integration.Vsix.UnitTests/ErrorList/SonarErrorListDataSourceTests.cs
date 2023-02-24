b70f833b0e86cacef1371daaeb6f11c1d067d51c
duncanp-sonar <duncan.pocklington@sonarsource.com>
2023-02-24 10:37:36 +0000

    Fix VS2019 build break
    - SuppressionState string constant is not available in VS2019.3

--- src/Integration.Vsix.UnitTests/ErrorList/SonarErrorListDataSourceTests.cs
+++ src/Integration.Vsix.UnitTests/ErrorList/SonarErrorListDataSourceTests.cs
@@ -55,6 +55,18 @@ namespace SonarLint.VisualStudio.Integration.UnitTests.ErrorList
             mockTableManagerProvider.Setup(x => x.GetTableManager(StandardTables.ErrorsTable)).Returns(mockTableManager.Object);
         }
 
+#if VS2022
+        [TestMethod]
+        public void CheckRedefinedSuppressionStateColumnName()
+        {
+            // Sanity check that our definition of the column name matches the VS version.
+
+            // We can only run this test for the VS2022 build - the whole point in having our own constant
+            // is because it doesn't exist in VS2019.3 (v16.3).
+            SonarErrorListDataSource.RedefinedSuppressionStateColumnName.Should().Be(StandardTableColumnDefinitions.SuppressionState);
+        }
+#endif
+
         [TestMethod]
         public void MefCtor_CheckExports()
         {
