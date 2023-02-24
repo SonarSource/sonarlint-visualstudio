6fa47f486db2230538deb5dd938d33890f13707e
Leyla <54935347+bigfluffycookie@users.noreply.github.com>
2023-02-24 12:00:20 +0100

    Change errorTagger to filter suppressed issues- (#3785)
    * Change errorTagger to filter suppressed issues-
    
    * change error tagger
    
    * Replace IIAnalysisIssueLocationVisualization with IAnalysisIssueVisualization

--- src/IssueViz/Editor/ErrorTagging/ErrorTagger.cs
+++ src/IssueViz/Editor/ErrorTagging/ErrorTagger.cs
@@ -45,9 +45,8 @@ namespace SonarLint.VisualStudio.IssueVisualization.Editor.ErrorTagging
         }
 
         protected override IEnumerable<IMappingTagSpan<IIssueLocationTag>> Filter(IEnumerable<IMappingTagSpan<IIssueLocationTag>> trackedTagSpans) =>
-            trackedTagSpans.Where(x => IsValidPrimaryLocation(x.Tag.Location));
+            trackedTagSpans.Where(x => x.Tag.Location is IAnalysisIssueVisualization issueViz && !issueViz.IsSuppressed && IsValidPrimaryLocation(issueViz));
 
-        private static bool IsValidPrimaryLocation(IAnalysisIssueLocationVisualization locViz) =>
-            locViz is IAnalysisIssueVisualization && locViz.Span.IsNavigable();
+        private static bool IsValidPrimaryLocation(IAnalysisIssueVisualization issueViz) => issueViz.Span.IsNavigable();
     }
 }
