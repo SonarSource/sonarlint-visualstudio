87baa03f84d1b2277070c1a7793908b69a1fb1f7
Leyla Büchel <leyla.buechel@sonarsource.com>
2023-02-27 10:46:31 +0100

    Add IssueServerEventsListener

---
+++ src/ConnectedMode.UnitTests/Suppressions/IssueServerEventsListenerTests.cs
@@ -0,0 +1,103 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Threading.Tasks;
+using SonarLint.VisualStudio.ConnectedMode.Suppressions;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.Integration;
+using SonarLint.VisualStudio.TestInfrastructure;
+using SonarQube.Client.Models.ServerSentEvents.ClientContract;
+
+namespace SonarLint.VisualStudio.ConnectedMode.UnitTests.Suppressions
+{
+    [TestClass]
+    public class IssueServerEventsListenerTests
+    {
+        [TestMethod]
+        public void MefCtor_CheckIsExported()
+        {
+            MefTestHelpers.CheckTypeCanBeImported<IssueServerEventsListener, IIssueServerEventsListener>(
+                MefTestHelpers.CreateExport<IIssueServerEventSource>(),
+                MefTestHelpers.CreateExport<IThreadHandling>(),
+                MefTestHelpers.CreateExport<ILogger>());
+        }
+
+        [TestMethod]
+        public void OnEvent_FailureToProcessIssueEvent_CriticalException_StopsListeningToServerEventsSource()
+        {
+            var issueServerEventSource = new Mock<IIssueServerEventSource>();
+            issueServerEventSource
+                .Setup(x => x.GetNextEventOrNullAsync())
+                .Throws(new StackOverflowException("this is a test"));
+
+            var testSubject = CreateTestSubject(issueServerEventSource: issueServerEventSource.Object);
+
+            Func<Task> func = async () => await testSubject.ListenAsync();
+
+            func.Should().Throw<StackOverflowException>().And.Message.Should().Be("this is a test");
+            issueServerEventSource.Verify(x => x.GetNextEventOrNullAsync(), Times.Once);
+            issueServerEventSource.VerifyNoOtherCalls();
+        }
+
+        [TestMethod]
+        public async Task Dispose_StopsListeningToServerEventsSource()
+        {
+            var issueServerEventSource = SetupIssueServerEventSource();
+            var testSubject = CreateTestSubject(issueServerEventSource: issueServerEventSource.Object);
+
+            var listenTask = testSubject.ListenAsync();
+            testSubject.Dispose();
+
+            await listenTask;
+
+            issueServerEventSource.Verify(x => x.GetNextEventOrNullAsync(), Times.Once);
+        }
+
+        private static Mock<IIssueServerEventSource> SetupIssueServerEventSource(params IIssueChangedServerEvent[] serverEvents)
+        {
+            var issueServerEventSource = new Mock<IIssueServerEventSource>();
+
+            var sequence = issueServerEventSource.SetupSequence(x => x.GetNextEventOrNullAsync());
+
+            foreach (var serverEvent in serverEvents)
+            {
+                sequence.ReturnsAsync(serverEvent);
+            }
+
+            // Signal that the task is finished
+            sequence.ReturnsAsync((IIssueChangedServerEvent)null);
+
+            return issueServerEventSource;
+        }
+
+        private static IssueServerEventsListener CreateTestSubject(
+            IIssueServerEventSource issueServerEventSource = null,
+            IThreadHandling threadHandling = null,
+            ILogger logger = null)
+        {
+            issueServerEventSource ??= Mock.Of<IIssueServerEventSource>();
+            threadHandling ??= new NoOpThreadHandler();
+            logger ??= Mock.Of<ILogger>();
+
+            return new IssueServerEventsListener(issueServerEventSource, threadHandling, logger);
+        }
+    }
+}
---
+++ src/ConnectedMode/Suppressions/IssueServerEventsListener.cs
@@ -0,0 +1,91 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.ComponentModel.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.Integration;
+using SonarQube.Client.Models.ServerSentEvents.ClientContract;
+
+namespace SonarLint.VisualStudio.ConnectedMode.Suppressions
+{
+    /// <summary>
+    /// Handles <see cref="IIssueServerEvent"/> coming from the server.
+    /// </summary>
+    internal interface IIssueServerEventsListener : IDisposable
+    {
+        Task ListenAsync();
+    }
+
+    [Export(typeof(IIssueServerEventsListener))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    internal sealed class IssueServerEventsListener : IIssueServerEventsListener
+    {
+        private readonly IIssueServerEventSource issueServerEventSource;
+        private readonly IThreadHandling threadHandling;
+        private readonly ILogger logger;
+        private readonly CancellationTokenSource cancellationTokenSource;
+
+        [ImportingConstructor]
+        public IssueServerEventsListener(IIssueServerEventSource issueServerEventSource,
+            IThreadHandling threadHandling,
+            ILogger logger)
+        {
+            this.issueServerEventSource = issueServerEventSource;
+            this.threadHandling = threadHandling;
+            this.logger = logger;
+
+            cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        public async Task ListenAsync()
+        {
+            await threadHandling.SwitchToBackgroundThread();
+
+            while (!cancellationTokenSource.IsCancellationRequested)
+            {
+                try
+                {
+                    var issueServerEvent = await issueServerEventSource.GetNextEventOrNullAsync();
+
+                    if(issueServerEvent == null)
+                    {
+                        // Will return null when issueServerEventSource is disposed
+                        return;
+                    }
+
+                    // TODO: Implement handling of event.
+                }
+                catch (Exception ex) when (!ErrorHandler.IsCriticalException(ex))
+                {
+                    logger.LogVerbose($"[IssueServerEventsListener] Failed to handle issue event: {ex}");
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            cancellationTokenSource.Cancel();
+            cancellationTokenSource?.Dispose();
+        }
+    }
+}
