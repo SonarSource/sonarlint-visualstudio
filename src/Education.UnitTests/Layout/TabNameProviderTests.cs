641225d25144cdeb6028f390a617637d8b1b451d
georgii-borovinskikh-sonarsource <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2023-03-06 15:30:28 +0100

    Rich rule description logical representation and visual representation generation (#3815)
    * Rich rule description and visual representation generation

---
+++ src/Education.UnitTests/Layout/TabNameProviderTests.cs
@@ -0,0 +1,121 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SonarLint.VisualStudio.Education.Layout.Tabs;
+
+namespace SonarLint.VisualStudio.Education.UnitTests.Layout
+{
+    [TestClass]
+    public class TabNameProviderTests
+    {
+        [TestMethod]
+        public void GetTabButtonName_ReturnsCorrectName()
+        {
+            TabNameProvider.GetTabButtonName("myg_roup", "veryfirsttab").Should().Be("myg_roup__veryfirsttab__Button");
+        }
+
+        [DataTestMethod]
+        [DataRow(null, "xaxaxa")]
+        [DataRow( "xaxaxa", null)]
+        public void GetTabButtonName_NullArguments_Throws(string tabGroup, string tabName)
+        {
+            Action act = () =>  TabNameProvider.GetTabButtonName(tabGroup, tabName);
+
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [DataTestMethod]
+        [DataRow("aB", "xax__axa")]
+        [DataRow("xax__axa", "aB")]
+        public void GetTabButtonName_ArgumentContainsSeparator_Throws(string tabGroup, string tabName)
+        {
+            Action act = () => TabNameProvider.GetTabButtonName(tabGroup, tabName);
+
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [TestMethod]
+        public void GetTabSectionName_ReturnsCorrectName()
+        {
+            TabNameProvider.GetTabSectionName("myg_roup", "veryfirsttab").Should().Be("myg_roup__veryfirsttab__Section");
+        }
+
+        [DataTestMethod]
+        [DataRow(null, "xaxaxa")]
+        [DataRow("xaxaxa", null)]
+        public void GetTabSectionName_NullArguments_Throws(string tabGroup, string tabName)
+        {
+            Action act = () => TabNameProvider.GetTabSectionName(tabGroup, tabName);
+
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [DataTestMethod]
+        [DataRow("aB", "xax__axa")]
+        [DataRow("xax__axa", "aB")]
+        public void GetTabSectionName_ArgumentContainsSeparator_Throws(string tabGroup, string tabName)
+        {
+            Action act = () => TabNameProvider.GetTabSectionName(tabGroup, tabName);
+
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [TestMethod]
+        public void GetTabIdentifier_SectionName_ReturnsCorrectTabGroupAndTabName()
+        {
+            var (tabGroup, tabName) = TabNameProvider.GetTabIdentifier("Myg_roup__myTab__Section");
+
+            tabGroup.Should().Be("Myg_roup");
+            tabName.Should().Be("myTab");
+        }
+
+        [TestMethod]
+        public void GetTabIdentifier_ButtonName_ReturnsCorrectTabGroupAndTabName()
+        {
+            var (tabGroup, tabName) = TabNameProvider.GetTabIdentifier("Myg_roup__myTab__Button");
+
+            tabGroup.Should().Be("Myg_roup");
+            tabName.Should().Be("myTab");
+        }
+
+        [DataTestMethod]
+        [DataRow("a__b__c__")]
+        [DataRow("a__b__c__d")]
+        [DataRow("a__b__c__d__e")]
+        [DataRow("a__b_c")]
+        public void GetTabIdentifier_IncorrectFormat_Throws(string name)
+        {
+            Action act = () => TabNameProvider.GetTabIdentifier(name);
+
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [TestMethod]
+        public void GetTabIdentifier_NullArgument_Throws()
+        {
+            Action act = () => TabNameProvider.GetTabIdentifier(null);
+
+            act.Should().Throw<ArgumentNullException>();
+        }
+    }
+}
---
+++ src/Education/XamlParser/IXamlBlockContent.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Windows.Documents;
+
+namespace SonarLint.VisualStudio.Education.XamlParser
+{
+
+    /// <summary>
+    /// Represents a XAML entity that is parseable into Block object
+    /// </summary>
+    internal interface IXamlBlockContent
+    {
+        Block GetObjectRepresentation();
+    }
+
+    internal class GeneratedXamlBlockContent : IXamlBlockContent
+    {
+        public Block GetObjectRepresentation()
+        {
+            // todo XamlReader.Parse(xamlString)
+            throw new System.NotImplementedException();
+        }
+    }
+
+    internal class StaticXamlBlockContent : IXamlBlockContent
+    {
+        public Block GetObjectRepresentation()
+        {
+            // todo something like XamlReader.Load(someXamlResource)
+            throw new System.NotImplementedException();
+        }
+    }
+}
