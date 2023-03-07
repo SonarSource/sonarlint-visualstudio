1dd0c1061fb6be8d912df71d78b993280f371ecf
Leyla <54935347+bigfluffycookie@users.noreply.github.com>
2023-03-07 11:55:35 +0100

    Implement server issue store read side. (#3839)

---
+++ src/ConnectedMode/Suppressions/ServerIssuesStore.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using SonarQube.Client.Models;
+
+namespace SonarLint.VisualStudio.ConnectedMode.Suppressions
+{
+    [Export(typeof(IServerIssuesStore))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    internal class ServerIssuesStore : IServerIssuesStore
+    {
+        private IEnumerable<SonarQubeIssue> serverIssues;
+
+        public event EventHandler ServerIssuesChanged;
+
+        public IEnumerable<SonarQubeIssue> Get()
+        {
+            return serverIssues ?? Enumerable.Empty<SonarQubeIssue>();
+        }
+    }
+}
