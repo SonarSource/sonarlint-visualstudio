645ac6eddf2878274e7f8407f7b8a66080de5906
georgii-borovinskikh-sonarsource <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2023-03-09 16:49:53 +0100

    Add static xaml storage, fix slightly incorrect html (#3853)

---
+++ src/Education/XamlGenerator/StaticXamlStorage.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Threading;
+
+namespace SonarLint.VisualStudio.Education.XamlGenerator
+{
+    internal interface IStaticXamlStorage
+    {
+        string EducationPrinciplesHeader { get; }
+        string EducationPrinciplesDefenseInDepth { get; }
+        string EducationPrinciplesNeverTrustUserInput { get; }
+        string HowToFixItFallbackContext { get; }
+    }
+
+    internal class StaticXamlStorage : IStaticXamlStorage
+    {
+        private readonly Lazy<string> educationPrinciplesHeader;
+        private readonly Lazy<string> educationPrinciplesDefenseInDepth;
+        private readonly Lazy<string> educationPrinciplesNeverTrustUserInput;
+        private readonly Lazy<string> howToFixItFallbackContext;
+
+        public StaticXamlStorage(IRuleHelpXamlTranslator ruleHelpXamlTranslator)
+        {
+            educationPrinciplesHeader = new Lazy<string>(() => ruleHelpXamlTranslator.TranslateHtmlToXaml(StaticHtmlSnippets.EducationPrinciplesHeader), LazyThreadSafetyMode.None);
+            educationPrinciplesDefenseInDepth = new Lazy<string>(() => ruleHelpXamlTranslator.TranslateHtmlToXaml(StaticHtmlSnippets.EducationPrinciplesDefenseInDepth), LazyThreadSafetyMode.None);
+            educationPrinciplesNeverTrustUserInput = new Lazy<string>(() => ruleHelpXamlTranslator.TranslateHtmlToXaml(StaticHtmlSnippets.EducationPrinciplesNeverTrustUserInput), LazyThreadSafetyMode.None);
+            howToFixItFallbackContext = new Lazy<string>(() => ruleHelpXamlTranslator.TranslateHtmlToXaml(StaticHtmlSnippets.HowToFixItFallbackContext), LazyThreadSafetyMode.None);
+        }
+
+        public string EducationPrinciplesHeader => educationPrinciplesHeader.Value;
+        public string EducationPrinciplesDefenseInDepth => educationPrinciplesDefenseInDepth.Value;
+        public string EducationPrinciplesNeverTrustUserInput => educationPrinciplesNeverTrustUserInput.Value;
+        public string HowToFixItFallbackContext => howToFixItFallbackContext.Value;
+    }
+}
