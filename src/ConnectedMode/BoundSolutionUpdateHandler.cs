d39996d9f0bb034e479a7efde678e53be1518d4d
Leyla <54935347+bigfluffycookie@users.noreply.github.com>
2023-03-13 13:34:28 +0100

    Add BoundSolutionUpdateHandler (#3854)
    * Add BoundSolutionUpdateHandler
    
    * Rebase and fix whitespace
    
    * Pr fixes

---
+++ src/ConnectedMode/BoundSolutionUpdateHandler.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Threading;
+using SonarLint.VisualStudio.ConnectedMode.Suppressions;
+using SonarLint.VisualStudio.Core.Binding;
+
+namespace SonarLint.VisualStudio.ConnectedMode
+{
+    [Export(typeof(BoundSolutionUpdateHandler))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    internal sealed class BoundSolutionUpdateHandler : IDisposable
+    {
+        private readonly IActiveSolutionBoundTracker activeSolutionBoundTracker;
+        private readonly ISuppressionIssueStoreUpdater suppressionIssueStoreUpdater;
+
+        private bool disposed;
+
+        [ImportingConstructor]
+        public BoundSolutionUpdateHandler(IActiveSolutionBoundTracker activeSolutionBoundTracker, ISuppressionIssueStoreUpdater suppressionIssueStoreUpdater)
+        {
+            this.activeSolutionBoundTracker = activeSolutionBoundTracker;
+            this.suppressionIssueStoreUpdater = suppressionIssueStoreUpdater;
+
+            this.activeSolutionBoundTracker.SolutionBindingChanged += OnSolutionBindingChanged;
+            this.activeSolutionBoundTracker.SolutionBindingUpdated += OnSolutionBindingUpdated;
+        }
+
+        private void OnSolutionBindingUpdated(object sender, EventArgs e) => TriggerUpdate();
+
+        private void OnSolutionBindingChanged(object sender, ActiveSolutionBindingEventArgs e) => TriggerUpdate();
+
+        private void TriggerUpdate() => suppressionIssueStoreUpdater.UpdateAllServerSuppressionsAsync().Forget();
+
+        public void Dispose()
+        {
+            if (!disposed)
+            {
+                this.activeSolutionBoundTracker.SolutionBindingChanged -= OnSolutionBindingChanged;
+                this.activeSolutionBoundTracker.SolutionBindingUpdated -= OnSolutionBindingUpdated;
+                disposed = true;
+            }
+        }
+    }
+}
