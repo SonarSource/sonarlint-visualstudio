ccf14e3899b9d08609e7969ea24ab17e9b546ee8
georgii-borovinskikh-sonarsource <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2023-03-31 15:34:08 +0200

    add nesting FlDocScrollViewer that fixes scrolling (#4010)

---
+++ src/Education/Controls/NestingFlowDocumentScrollViewer.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SonarLint.VisualStudio.Education.Controls
+{
+    public class NestingFlowDocumentScrollViewer : FlowDocumentScrollViewer
+    {
+        protected override void OnPreviewMouseWheel(MouseWheelEventArgs e)
+        {
+            base.OnPreviewMouseWheel(e);
+            if (!ReferenceEquals(e.OriginalSource, this))
+            {
+                this.OnMouseWheel(e);
+            }
+        }
+    }
+}
