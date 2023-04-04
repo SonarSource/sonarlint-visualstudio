2a9006d2759fed5ad7b8795fc3ee2ced7c083fff
duncanp-sonar <duncan.pocklington@sonarsource.com>
2023-04-04 14:38:25 +0100

    Handle null collections when parsing server rule info (#4031)
    * Handle null collections when processing server rule info
    
    Last part of the fix for #3973

---
+++ src/Rules.UnitTests/RuleInfoTests.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SonarLint.VisualStudio.Rules.UnitTests
+{
+    [TestClass]
+    public class RuleInfoTests
+    {
+        [TestMethod]
+        public void Ctor_NullCollectionsAreAllowed_SetToEmptyCollections()
+        {
+            var testSubject = new RuleInfo(
+                languageKey: null,
+                fullRuleKey: null,
+                description: null,
+                name: null,
+                RuleIssueSeverity.Unknown,
+                RuleIssueType.Unknown,
+                isActiveByDefault: false,
+                tags: null,
+                descriptionSections: null,
+                educationPrinciples: null,
+                htmlNote: null);
+
+            testSubject.Tags.Should().NotBeNull();
+            testSubject.DescriptionSections.Should().NotBeNull();
+            testSubject.EducationPrinciples.Should().NotBeNull();
+
+            testSubject.Tags.Should().HaveCount(0);
+            testSubject.DescriptionSections.Should().HaveCount(0);
+            testSubject.EducationPrinciples.Should().HaveCount(0);
+        }
+    }
+}
