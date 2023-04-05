e19f9fe1986418dcbd51d075ee8659f9881245e7
Leyla <54935347+bigfluffycookie@users.noreply.github.com>
2023-04-05 10:20:55 +0200

    Display rule cross reference as Hyperlink in rule description (#4039)
    * Display rule cross reference as Hyperlink in rule description
    
    * pr fixes

--- src/Education.UnitTests/RuleHelpXamlTranslatorTests.cs
+++ src/Education.UnitTests/RuleHelpXamlTranslatorTests.cs
@@ -78,5 +78,19 @@ namespace SonarLint.VisualStudio.Education.UnitTests
 
             result.Should().Be(expectedText);
         }
+
+        [TestMethod]
+        public void TranslateHtmlToXaml_CrossReferenceRule_AddsHyperLink()
+        {
+            var testSubject = new RuleHelpXamlTranslator();
+
+            var htmlText = "Texty text {rule:cpp:S1564} blabla";
+
+            var expectedText = "<Paragraph>Texty text <Hyperlink NavigateUri=\"sonarlintrulecrossref://cpp/S1564\">cpp:S1564</Hyperlink> blabla</Paragraph>";
+
+            var result = testSubject.TranslateHtmlToXaml(htmlText);
+
+            result.Should().Be(expectedText);
+        }
     }
 }
