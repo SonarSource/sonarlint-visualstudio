a1ef0c1f74adef96a38977941eaf156cd7de0547
Leyla <54935347+bigfluffycookie@users.noreply.github.com>
2023-04-26 14:39:04 +0200

    Create .targets file in ImportBefore (#4128)
    * Create .targets file in ImportBefore
    
    * Add embedded text file with the import before targets content.
    
    * Pr fixePr fixes
    
    * Add empty line

---
+++ src/ConnectedMode/Binding/ImportBeforeFileGenerator.cs
@@ -0,0 +1,104 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.IO;
+using System.IO.Abstractions;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.Integration;
+
+namespace SonarLint.VisualStudio.ConnectedMode.Binding
+{
+    internal class ImportBeforeFileGenerator
+    {
+        private readonly IFileSystem fileSystem;
+        private readonly ILogger logger;
+
+        private const string targetsFileName = "SonarLint.targets";
+        private const string resourcePath = "SonarLint.VisualStudio.ConnectedMode.Embedded.SonarLintTargets.xml";
+
+        public ImportBeforeFileGenerator(ILogger logger) : this(logger, new FileSystem()) { }
+
+        public ImportBeforeFileGenerator(ILogger logger, IFileSystem fileSystem)
+        {
+            this.logger = logger;
+            this.fileSystem = fileSystem;
+
+            WriteTargetsFileToDiskIfNotExists();
+        }
+
+        private void WriteTargetsFileToDiskIfNotExists()
+        {
+            var fileContent = GetTargetFileContent();
+            var pathToImportBefore = GetPathToImportBefore();
+            var fullPath = Path.Combine(pathToImportBefore, targetsFileName);
+
+            logger.LogVerbose(Resources.ImportBeforeFileGenerator_CheckingIfFileExists, fullPath);
+
+            try
+            {
+                if (!fileSystem.Directory.Exists(pathToImportBefore))
+                {
+                    logger.LogVerbose(Resources.ImportBeforeFileGenerator_CreatingDirectory, pathToImportBefore);
+                    fileSystem.Directory.CreateDirectory(pathToImportBefore);
+                }
+
+                if (fileSystem.File.Exists(fullPath) && fileSystem.File.ReadAllText(fullPath) == fileContent)
+                {
+                    logger.LogVerbose(Resources.ImportBeforeFileGenerator_FileAlreadyExists);
+                    return;
+                }
+
+                logger.LogVerbose(Resources.ImportBeforeFileGenerator_WritingTargetFileToDisk);
+                fileSystem.File.WriteAllText(fullPath, fileContent);
+            }
+            catch (Exception ex) when (!ErrorHandler.IsCriticalException(ex))
+            {
+                logger.WriteLine(Resources.ImportBeforeFileGenerator_FailedToWriteFile, ex.Message);
+                logger.LogVerbose(Resources.ImportBeforeFileGenerator_FailedToWriteFile_Verbose, ex.ToString());
+            }
+        }
+
+        private string GetTargetFileContent()
+        {
+            using (var stream = GetType().Assembly.GetManifestResourceStream(resourcePath))
+            {
+                if (stream != null)
+                {
+                    using (var reader = new StreamReader(stream))
+                    {
+                        var data = reader.ReadToEnd();
+                        return data;
+                    }
+                }
+            }
+
+            return "";
+        }
+
+        private string GetPathToImportBefore()
+        {
+            var localAppData = Environment.GetEnvironmentVariable("LOCALAPPDATA");
+            var pathToImportBefore = Path.Combine(localAppData, "Microsoft", "MSBuild", "Current", "Microsoft.Common.targets", "ImportBefore");
+
+            return pathToImportBefore;
+        }
+    }
+}
