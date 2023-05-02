e40fce0e7f463f5d9537801ef7043ef0616e4b67
Leyla <54935347+bigfluffycookie@users.noreply.github.com>
2023-05-02 16:05:09 +0200

    Add GlobalConfigGenerator Class (#4158)
    * Add GlobalConfigGenerator class
    
    * Add global config generator tests
    
    * pr fixes
    
    * Fix tests

---
+++ src/ConnectedMode.UnitTests/Binding/GlobalConfigGeneratorTests.cs
@@ -0,0 +1,141 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SonarLint.VisualStudio.ConnectedMode.Binding;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.Core.CSharpVB;
+using SonarQube.Client.Models;
+
+namespace SonarLint.VisualStudio.ConnectedMode.UnitTests.Binding
+{
+    [TestClass]
+    public class GlobalConfigGeneratorTests
+    {
+        [TestMethod]
+        public void Generate_RulesAreNull_ThrowsNullException()
+        {
+            var generator = new GlobalConfigGenerator();
+
+            Action act = () => generator.Generate(null);
+            act.Should().ThrowExactly<ArgumentNullException>().And.ParamName.Should().Be("rules");
+        }
+
+        [TestMethod]
+        public void Generate_MultipleRules_RulesAreSorted()
+        {
+            var generator = new GlobalConfigGenerator();
+            var rules = new List<SonarQubeRule>()
+            {
+                CreateRule("3", ""),
+                CreateRule("5", ""),
+                CreateRule("2", ""),
+                CreateRule("8", ""),
+            };
+
+            var result = generator.Generate(rules);
+
+            var sb = new StringBuilder();
+            sb.AppendLine("is_global=true");
+            sb.AppendLine("global_level=500000");
+            sb.AppendLine(GetRuleString("2", "suggestion"));
+            sb.AppendLine(GetRuleString("3", "suggestion"));
+            sb.AppendLine(GetRuleString("5", "suggestion"));
+            sb.AppendLine(GetRuleString("8", "suggestion"));
+
+            result.Should().Be(sb.ToString());
+        }
+
+        [TestMethod]
+        public void Generate_GlobalConfigSettingsAreCorrect()
+        {
+            var generator = new GlobalConfigGenerator();
+            var rules = new List<SonarQubeRule>() { };
+
+            var result = generator.Generate(rules);
+
+            var sb = new StringBuilder();
+            sb.AppendLine("is_global=true");
+            sb.AppendLine("global_level=500000");
+
+            result.Should().Be(sb.ToString());
+        }
+
+        [TestMethod]
+        [DataRow(RuleAction.Info, "suggestion")]
+        [DataRow(RuleAction.Warning, "warning")]
+        [DataRow(RuleAction.None, "none")]
+        [DataRow(RuleAction.Error, "error")]
+        [DataRow(RuleAction.Hidden, "silent")]
+        public void GetActionText_Valid(RuleAction action, string expected)
+        {
+            GlobalConfigGenerator.GetActionText(action).Should().Be(expected);
+        }
+
+        [TestMethod]
+        public void GetActionText_Invalid()
+        {
+            Action act = () => GlobalConfigGenerator.GetActionText((RuleAction)(-1));
+            act.Should().Throw<NotSupportedException>();
+        }
+
+        [TestMethod]
+        [DataRow(SonarQubeIssueSeverity.Info, RuleAction.Info)]
+        [DataRow(SonarQubeIssueSeverity.Minor, RuleAction.Info)]
+        [DataRow(SonarQubeIssueSeverity.Major, RuleAction.Warning)]
+        [DataRow(SonarQubeIssueSeverity.Critical, RuleAction.Warning)]
+        public void GetVSSeverity_NotBlocker_CorrectlyMapped(SonarQubeIssueSeverity sqSeverity, RuleAction expectedVsSeverity)
+        {
+            var testSubject = new GlobalConfigGenerator();
+
+            testSubject.GetVsSeverity(sqSeverity).Should().Be(expectedVsSeverity);
+        }
+
+        [TestMethod]
+        [DataRow(true, RuleAction.Error)]
+        [DataRow(false, RuleAction.Warning)]
+        public void GetVSSeverity_Blocker_CorrectlyMapped(bool shouldTreatBlockerAsError, RuleAction expectedVsSeverity)
+        {
+            var envSettingsMock = new Mock<IEnvironmentSettings>();
+            envSettingsMock.Setup(x => x.TreatBlockerSeverityAsError()).Returns(shouldTreatBlockerAsError);
+
+            var testSubject = new GlobalConfigGenerator(envSettingsMock.Object);
+
+            testSubject.GetVsSeverity(SonarQubeIssueSeverity.Blocker).Should().Be(expectedVsSeverity);
+        }
+
+        [TestMethod]
+        [DataRow(SonarQubeIssueSeverity.Unknown)]
+        [DataRow((SonarQubeIssueSeverity)(-1))]
+        public void GetVSSeverity_Invalid_Throws(SonarQubeIssueSeverity sqSeverity)
+        {
+            Action act = () => new GlobalConfigGenerator().GetVsSeverity(sqSeverity);
+            act.Should().Throw<NotSupportedException>();
+        }
+
+        private static SonarQubeRule CreateRule(string ruleKey, string repoKey, bool isActive = true, SonarQubeIssueSeverity sqSeverity = SonarQubeIssueSeverity.Info) =>
+            new SonarQubeRule(ruleKey, repoKey, isActive, sqSeverity, new Dictionary<string, string>(), SonarQubeIssueType.Unknown, null, null, null, null, null, null);
+
+        private string GetRuleString(string expectedKey, string expectedSeverity) =>
+            $"dotnet_diagnostic.{expectedKey}.severity = {expectedSeverity}";
+    }
+}
---
+++ src/ConnectedMode/Binding/GlobalConfigGenerator.cs
@@ -0,0 +1,117 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.Core.CSharpVB;
+using SonarQube.Client.Models;
+
+namespace SonarLint.VisualStudio.ConnectedMode.Binding
+{
+    public interface IGlobalConfigGenerator
+    {
+        string Generate(IEnumerable<SonarQubeRule> rules);
+    }
+
+    internal class GlobalConfigGenerator : IGlobalConfigGenerator
+    {
+        private readonly IEnvironmentSettings environmentSettings;
+
+        private static readonly string inactiveRuleActionText = GetActionText(RuleAction.None);
+
+        public GlobalConfigGenerator() : this(new EnvironmentSettings()) { }
+
+        public GlobalConfigGenerator(IEnvironmentSettings environmentSettings)
+        {
+            this.environmentSettings = environmentSettings;
+        }
+
+        public string Generate(IEnumerable<SonarQubeRule> rules)
+        {
+            if (rules == null)
+            {
+                throw new ArgumentNullException(nameof(rules));
+            }
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine("is_global=true");
+            sb.AppendLine("global_level=500000");
+
+            var sortedRules = rules.OrderBy(r => r.Key);
+
+            foreach (var rule in sortedRules)
+            {
+                var severityText = (rule.IsActive) ? GetActionText(GetVsSeverity(rule.Severity)) : inactiveRuleActionText;
+
+                sb.AppendLine($"dotnet_diagnostic.{rule.Key}.severity = {severityText}");
+            }
+
+            return sb.ToString();
+        }
+
+        internal /* for testing */ RuleAction GetVsSeverity(SonarQubeIssueSeverity sqSeverity)
+        {
+            switch (sqSeverity)
+            {
+                case SonarQubeIssueSeverity.Info:
+                case SonarQubeIssueSeverity.Minor:
+                    return RuleAction.Info;
+
+                case SonarQubeIssueSeverity.Major:
+                case SonarQubeIssueSeverity.Critical:
+                    return RuleAction.Warning;
+
+                case SonarQubeIssueSeverity.Blocker:
+                    return environmentSettings.TreatBlockerSeverityAsError() ? RuleAction.Error : RuleAction.Warning;
+
+                default:
+                    throw new NotSupportedException($"Unsupported SonarQube issue severity: {sqSeverity}");
+            }
+        }
+        // See https://learn.microsoft.com/en-us/dotnet/fundamentals/code-analysis/configuration-options#severity-level
+        internal /* for testing */ static string GetActionText(RuleAction ruleAction)
+        {
+            switch (ruleAction)
+            {
+                case RuleAction.None:
+                    return "none";
+
+                case RuleAction.Info:
+                    return "suggestion";
+
+                case RuleAction.Warning:
+                    return "warning";
+
+                case RuleAction.Error:
+                    return "error";
+
+                case RuleAction.Hidden:
+                    return "silent";
+
+                default:
+                    throw new NotSupportedException($"{ruleAction} is not a supported RuleAction.");
+            }
+        }
+    }
+}
