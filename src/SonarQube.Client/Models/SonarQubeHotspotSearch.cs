7116acf89e2db1178ae4c0f5dd1852649070051b
Uğraş Ergün <96827714+ugras-ergun-sonarsource@users.noreply.github.com>
2023-05-31 16:14:20 +0200

    Get hotspots from server in SQ Client (#4251)

---
+++ src/SonarQube.Client/Models/SonarQubeHotspotSearch.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace SonarQube.Client.Models
+{
+    public class SonarQubeHotspotSearch
+    {
+        public SonarQubeHotspotSearch(string hotspotKey, string componentKey, string filePath, string projectKey, string status, string resolution, IssueTextRange textRange, string ruleKey)
+        {
+            this.HotspotKey = hotspotKey;
+            this.ComponentKey = componentKey;
+            this.FilePath = filePath;
+            this.ProjectKey = projectKey;
+            this.Status = status;
+            this.Resolution = resolution;
+            this.TextRange = textRange;
+            this.RuleKey = ruleKey;
+        }
+
+        public string HotspotKey { get; }
+        public string ComponentKey { get; }
+        public string FilePath { get; }
+        public string ProjectKey { get; }
+        public string Status { get; }
+        public string Resolution { get; }
+        public IssueTextRange TextRange { get; }
+        public string RuleKey { get; }
+    }
+}
