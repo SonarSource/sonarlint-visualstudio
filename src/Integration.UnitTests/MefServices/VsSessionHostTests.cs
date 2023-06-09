f723338a6513e159c98c615ab17dfa4acf6e8d59
duncanp-sonar <duncan.pocklington@sonarsource.com>
2023-06-09 06:41:47 +0100

    Change ICredentialStoreService to not be an ILocalService
    - can now only be created using MEF
    - prep for moving the implementation from Integration to ConnectedMode
    
    Part of #4320

--- src/Integration.UnitTests/MefServices/VsSessionHostTests.cs
+++ src/Integration.UnitTests/MefServices/VsSessionHostTests.cs
@@ -355,7 +355,6 @@ namespace SonarLint.VisualStudio.Integration.UnitTests.TeamExplorer
             shell.RegisterPropertyGetter((int)__VSSPROPID2.VSSPROPID_InstallRootDir, () => this.TestContext.TestRunDirectory);
             this.serviceProvider.RegisterService(typeof(SVsShell), shell);
             this.serviceProvider.RegisterService(typeof(SVsSolution), new Mock<IVsSolution>().Object);
-            this.serviceProvider.RegisterService(typeof(ICredentialStoreService), new Mock<ICredentialStoreService>().Object);
 
             var testSubject = CreateTestSubject(new ConfigurableActiveSolutionTracker());
 
@@ -389,7 +388,6 @@ namespace SonarLint.VisualStudio.Integration.UnitTests.TeamExplorer
                 this.stepRunner,
                 this.sonarQubeServiceMock.Object,
                 tracker ?? new ConfigurableActiveSolutionTracker(),
-                Mock.Of<ICredentialStoreService>(),
                 Mock.Of<IProjectToLanguageMapper>(),
                 this.configProvider,
                 Mock.Of<ILogger>(),
