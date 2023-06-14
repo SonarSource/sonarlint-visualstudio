7cde51806b8ab950eba3b8596e4966887f937164
Leyla <54935347+bigfluffycookie@users.noreply.github.com>
2023-06-14 09:54:45 +0200

    Add MefFactory (#4349)
    * Add MefFactory
    
    * Fix task reference
    
    * fix whie space

---
+++ src/Integration/Helpers/MefFactory.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.Shell;
+using SonarLint.VisualStudio.Core;
+
+using Task = System.Threading.Tasks.Task;
+
+namespace SonarLint.VisualStudio.Integration.Helpers
+{
+    [Export(typeof(IMefFactory))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    public class MefFactory : IMefFactory
+    {
+        private readonly IServiceProvider serviceProvider;
+
+        private readonly IThreadHandling threadHandling;
+
+        [ImportingConstructor]
+        public MefFactory([Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider, IThreadHandling threadHandling)
+        {
+            this.serviceProvider = serviceProvider;
+            this.threadHandling = threadHandling;
+        }
+
+        public async Task<T> CreateAsync<T>() where T : class
+        {
+            T instance = null;
+
+            await threadHandling.RunOnUIThread(() =>
+                {
+                    var componentModel = serviceProvider.GetService(typeof(SComponentModel)) as IComponentModel;
+                    instance = componentModel.GetService<T>();
+                });
+
+            return instance;
+        }
+    }
+}
