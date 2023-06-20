3f746d5261c79c435b255bf3a5c09adec15d99d0
duncanp-sonar <duncan.pocklington@sonarsource.com>
2023-06-20 11:04:08 +0100

    Simplify code

--- src/ConnectedMode.UnitTests/Migration/RoslynProjectProviderTests.cs
+++ src/ConnectedMode.UnitTests/Migration/RoslynProjectProviderTests.cs
@@ -22,7 +22,6 @@ using System;
 using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.VisualStudio.LanguageServices;
-using Microsoft.VisualStudio.Shell.Interop;
 using SonarLint.VisualStudio.ConnectedMode.Migration;
 using SonarLint.VisualStudio.Integration;
 using SonarLint.VisualStudio.TestInfrastructure;
@@ -30,110 +29,102 @@ using SonarLint.VisualStudio.TestInfrastructure;
 namespace SonarLint.VisualStudio.ConnectedMode.UnitTests.Migration
 {
     [TestClass]
-    public class RoslynProjectWalkerTests
+    public class RoslynProjectProviderTests
     {
         [TestMethod]
         [Ignore] // VsWorkspace is not mockable -> can't create an export
         public void MefCtor_CheckIsExported()
         {
-            MefTestHelpers.CheckTypeCanBeImported<RoslynProjectWalker, IRoslynProjectWalker>(
-                MefTestHelpers.CreateExport<VisualStudioWorkspace>(),
-                MefTestHelpers.CreateExport<ILogger>());
+            MefTestHelpers.CheckTypeCanBeImported<RoslynProjectProvider, IRoslynProjectProvider>(
+                MefTestHelpers.CreateExport<VisualStudioWorkspace>());
         }
 
         [TestMethod]
         public void MefCtor_CheckTypeIsNonShared()
-            => MefTestHelpers.CheckIsNonSharedMefComponent<RoslynProjectWalker>();
+            => MefTestHelpers.CheckIsNonSharedMefComponent<RoslynProjectProvider>();
 
         [TestMethod]
-        public void Enumerate_NullSolution_NoError()
+        public void Get_NullSolution_NoError()
         {
             var workspace = new Mock<IVisualStudioWorkspace>();
             workspace.Setup(x => x.CurrentSolution).Returns((Solution)null);
 
             var testSubject = CreateTestSubject(workspace.Object);
 
-            var actual = testSubject.Walk().ToArray();
+            var actual = testSubject.Get().ToArray();
 
+            actual.Should().NotBeNull();
             actual.Should().BeEmpty();
             workspace.Verify(x => x.CurrentSolution, Times.AtLeastOnce());
         }
 
         [TestMethod]
-        public void Enumerate_NoProjects_NoError()
+        public void Get_NoProjects_NoError()
         {
             var workspace = CreateWorkspace(/* no projects */);
             var testSubject = CreateTestSubject(workspace.Object);
 
-            var actual = testSubject.Walk().ToArray();
+            var actual = testSubject.Get().ToArray();
 
+            actual.Should().NotBeNull();
             actual.Should().BeEmpty();
             workspace.Verify(x => x.CurrentSolution, Times.AtLeastOnce());
         }
 
         [TestMethod]
-        public void Enumerate_HasProjects_ProjectsWithHierachiesReturned()
+        public void Get_HasProjects_ReturnsExpectedProjects()
         {
-            var guid2 = Guid.NewGuid(); var hierarchy2 = Mock.Of<IVsHierarchy>();
-            var guid4 = Guid.NewGuid(); var hierarchy4 = Mock.Of<IVsHierarchy>();
+            var project1 = CreateProjectInfo();
+            var project2 = CreateProjectInfo();
+            var project3 = CreateProjectInfo();
 
-            var mapping = new Tuple<Guid, IVsHierarchy>[]
-                {
-                    new (Guid.NewGuid(), null),
-                    new (guid2, hierarchy2),
-                    new (Guid.NewGuid(), null),
-                    new (guid4, hierarchy4),
-                };
-
-            var workspace = CreateWorkspace(mapping);
+            var workspace = CreateWorkspace(project1, project2, project3);
 
             var testSubject = CreateTestSubject(workspace.Object);
 
             // Act
-            var actual = testSubject.Walk().ToArray();
+            var actual = testSubject.Get().ToArray();
 
-            actual.Should().HaveCount(2);
-            actual.Should().ContainInOrder(hierarchy2, hierarchy4);
+            actual.Should().HaveCount(3);
+            var actualIds = actual.Select(x => x.Id).ToArray();
+            
+            actualIds.Should().ContainInOrder(project1.Id, project2.Id, project3.Id);
         }
-        private static RoslynProjectWalker CreateTestSubject(IVisualStudioWorkspace workspace = null, ILogger logger = null)
+
+        private static RoslynProjectProvider CreateTestSubject(IVisualStudioWorkspace workspace = null)
         {
             workspace ??= Mock.Of<IVisualStudioWorkspace>();
-            logger ??= new TestLogger(logToConsole: true);
-            return new RoslynProjectWalker(workspace, logger);
+            return new RoslynProjectProvider(workspace);
         }
 
-        private static Mock<IVisualStudioWorkspace> CreateWorkspace(params Tuple<Guid, IVsHierarchy>[] projectGuidToHierachyMap)
+        private static Mock<IVisualStudioWorkspace> CreateWorkspace(params ProjectInfo[] projectInfos)
         {
-            projectGuidToHierachyMap ??= Array.Empty<Tuple<Guid, IVsHierarchy>>();
-
             // Create a real solution with a list of projects
-            var solution = CreateSolution(projectGuidToHierachyMap);
+            var solution = CreateSolution(projectInfos);
 
             var workspace = new Mock<IVisualStudioWorkspace>();
             workspace.Setup(x => x.CurrentSolution).Returns(solution);
 
-            // Setup up the ProjectId -> IVsHierarchy lookup
-            workspace.Setup(x => x.GetHierarchy(It.IsAny<ProjectId>()))
-                .Returns<ProjectId>(projecId => projectGuidToHierachyMap.FirstOrDefault(x => projecId.Id == x.Item1).Item2);
             return workspace;
         }
 
-        private static Solution CreateSolution(params Tuple<Guid, IVsHierarchy>[] projectGuidToHierachyMap)
+        private static Solution CreateSolution(params ProjectInfo[] projectInfos)
         {
             // We can't directly create or mock a Roslyn Solution. However, we can 
             // create a real Roslyn AdhocWorkspace and return it's Solution
             AdhocWorkspace adhocWorkspace = new AdhocWorkspace();
 
-            var projects = projectGuidToHierachyMap.Select(x => CreateProjectInfo(x.Item1)).ToArray();
-
-            var slnInfo = SolutionInfo.Create(SolutionId.CreateNewId(), VersionStamp.Default, null, 
-                projects);
-            var solution = adhocWorkspace.AddSolution(slnInfo);
+            var slnInfo = SolutionInfo.Create(SolutionId.CreateNewId(), VersionStamp.Default, null,
+                projectInfos);
+            adhocWorkspace.AddSolution(slnInfo);
 
             return adhocWorkspace.CurrentSolution;
         }
 
-        private static ProjectInfo CreateProjectInfo(Guid guid)
-             => ProjectInfo.Create(ProjectId.CreateFromSerialized(guid), VersionStamp.Default, guid.ToString(), guid.ToString(), LanguageNames.CSharp);
+        private static ProjectInfo CreateProjectInfo()
+        {
+            var guid = Guid.NewGuid();
+            return ProjectInfo.Create(ProjectId.CreateFromSerialized(guid), VersionStamp.Default, guid.ToString(), guid.ToString(), LanguageNames.CSharp);
+        }
     }
 }
