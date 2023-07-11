b3d39372a20430015c0b9af429052dc47a1dbbf1
georgii-borovinskikh-sonarsource <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2023-07-11 16:04:19 +0200

    Implement HotspotMatcher (#4492)
    * Implement HotspotMatcher

--- src/IssueViz.Security/Hotspots/HotspotMatcher.cs
+++ src/IssueViz.Security/Hotspots/HotspotMatcher.cs
@@ -18,7 +18,9 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+using System;
 using System.ComponentModel.Composition;
+using SonarLint.VisualStudio.Core.Helpers;
 using SonarLint.VisualStudio.IssueVisualization.Models;
 using SonarQube.Client.Models;
 
@@ -30,11 +32,28 @@ namespace SonarLint.VisualStudio.IssueVisualization.Security.Hotspots
     }
 
     [Export(typeof(IHotspotMatcher))]
-    internal class NoOpHotspotMatcher : IHotspotMatcher
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    internal class HotspotMatcher : IHotspotMatcher
     {
         public bool IsMatch(IAnalysisIssueVisualization localHotspotVisualization, SonarQubeHotspot serverHotspot)
         {
-            return false;
+            if (!StringComparer.OrdinalIgnoreCase.Equals(localHotspotVisualization.RuleId, serverHotspot.Rule.RuleKey)
+                || !PathHelper.IsServerFileMatch(localHotspotVisualization.FilePath, serverHotspot.FilePath))
+            {
+                return false;
+            }
+
+            if (!string.IsNullOrEmpty(serverHotspot.LineHash)
+                && StringComparer.Ordinal.Equals(localHotspotVisualization.LineHash, serverHotspot.LineHash))
+            {
+                return true;
+            }
+
+            // Line hashes are missing from the server response at the moment this code is written,
+            // which is how I got the idea from other SL flavors to also compare issue message to reduce the amount of false-negative matches.
+            // However, there's nothing in principal stopping hashes from being adding to the server response later, so we also check them above.
+            return localHotspotVisualization.StartLine == serverHotspot.TextRange.StartLine 
+                   || StringComparer.Ordinal.Equals(localHotspotVisualization.Issue.PrimaryLocation.Message, serverHotspot.Message);
         }
     }
 }
