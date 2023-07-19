ddc38a85f6e881371394842c7d2b29766165d575
duncanp-sonar <duncan.pocklington@sonarsource.com>
2023-07-19 12:02:52 +0100

    Add DocumentClosedHandler
    Part of #4508

---
+++ src/IssueViz.Security.UnitTests/Hotspots/DocumentClosedHandlerTests.cs
@@ -0,0 +1,116 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.IssueVisualization.Security.Hotspots;
+using SonarLint.VisualStudio.TestInfrastructure;
+
+namespace SonarLint.VisualStudio.IssueVisualization.Security.UnitTests.Hotspots
+{
+    [TestClass]
+    public class DocumentClosedHandlerTests
+    {
+        [TestMethod]
+        public void MefCtor_CheckIsExported()
+            =>  MefTestHelpers.CheckTypeCanBeImported<DocumentClosedHandler, DocumentClosedHandler>(
+                    MefTestHelpers.CreateExport<IDocumentEvents>(),
+                    MefTestHelpers.CreateExport<ILocalHotspotsStore>(),
+                    MefTestHelpers.CreateExport<IThreadHandling>());
+
+        [TestMethod]
+        public void CheckIsSharedMefComponent()
+            => MefTestHelpers.CheckIsSingletonMefComponent<DocumentClosedHandler>();
+
+        [TestMethod]
+        public void DocumentIsClosed_StoreUpdateIsTriggered()
+        {
+            var docEvents = new Mock<IDocumentEvents>();
+            var store = new Mock<ILocalHotspotsStore>();
+
+            var testSubject = CreateTestSubject(docEvents.Object, store.Object);
+            store.Invocations.Should().BeEmpty();
+
+            // Act
+            RaiseDocClosedEvent(docEvents, "c:\\a file.txt");
+            store.Verify(x => x.RemoveForFile("c:\\a file.txt"), Times.Once());
+        }
+
+        [TestMethod]
+        public void DocumentIsClosed_UpdateIsOnBackgroundThread()
+        {
+            var callSequence = new List<string>();
+
+            var docEvents = new Mock<IDocumentEvents>();
+            var store = new Mock<ILocalHotspotsStore>();
+            var threadHandling = new Mock<IThreadHandling>();
+
+            store.Setup(x => x.RemoveForFile(It.IsAny<string>()))
+                .Callback<string>(x => callSequence.Add("RemoveForFile"));
+
+            threadHandling.Setup(x => x.RunOnBackgroundThread(It.IsAny<Func<Task<bool>>>()))
+                .Returns((Func<Task<bool>> action) =>
+                {
+                    callSequence.Add("RunOnBackgroundThread");
+                    return action();
+                });
+            
+            var testSubject = CreateTestSubject(docEvents.Object, store.Object, threadHandling.Object);
+            store.Invocations.Should().BeEmpty();
+
+            // Act
+            RaiseDocClosedEvent(docEvents, "any");
+
+            callSequence.Should().ContainInOrder("RunOnBackgroundThread", "RemoveForFile");
+        }
+
+        [TestMethod]
+        public void Dispose_EventHandlerIsUnregistered()
+        {
+            var docEvents = new Mock<IDocumentEvents>();
+            var store = new Mock<ILocalHotspotsStore>();
+
+            var testSubject = CreateTestSubject(docEvents.Object, store.Object);
+            docEvents.VerifyAdd(x => x.DocumentClosed += It.IsAny<EventHandler<DocumentClosedEventArgs>>(), Times.Once);
+
+            testSubject.Dispose();
+            docEvents.VerifyRemove(x => x.DocumentClosed -= It.IsAny<EventHandler<DocumentClosedEventArgs>>(), Times.Once);
+        }
+
+        private void RaiseDocClosedEvent(Mock<IDocumentEvents> docEvents, string filePath)
+            => docEvents.Raise(x => x.DocumentClosed += null, new DocumentClosedEventArgs(filePath));
+
+        private DocumentClosedHandler CreateTestSubject(IDocumentEvents documentEvents = null,
+            ILocalHotspotsStore store = null,
+            IThreadHandling threadHandling = null)
+        {
+            documentEvents ??= Mock.Of<IDocumentEvents>();
+            store ??= Mock.Of<ILocalHotspotsStore>();
+            threadHandling ??= new NoOpThreadHandler();
+
+            return new DocumentClosedHandler(documentEvents, store, threadHandling);
+        }
+    }
+}
