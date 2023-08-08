108f98746a3b4328c26821f52a7621bf19b01bee
Leyla Büchel <leyla.buechel@sonarsource.com>
2023-08-08 09:45:45 +0200

    Fix line text being omitted if line has 0 subpieces

--- src/Education.UnitTests/XamlGenerator/DiffTranslatorTests.cs
+++ src/Education.UnitTests/XamlGenerator/DiffTranslatorTests.cs
@@ -113,6 +113,20 @@ namespace SonarLint.VisualStudio.Education.UnitTests.XamlGenerator
             result2.Should().BeEquivalentTo(result1);
         }
 
+        [TestMethod]
+        public void GetDiffXaml_LineHasNoSubPieces_StillCorrectResult()
+        {
+            var input1 = "} if (condition2) {";
+            var input2 = "}\r\n\r\nif (condition2) {";
+
+            var testSubject = CreateTestSubject();
+
+            (string result1, string result2) = testSubject.GetDiffXaml(input1, input2);
+
+            result1.Should().BeEquivalentTo("<Span Style=\"{DynamicResource NonCompliant_Diff}\">\n  <Span Style=\"{DynamicResource Sub_NonCompliant_Diff}\">} </Span>\n  <Span Style=\"{DynamicResource Sub_NonCompliant_Diff}\">if</Span>\n  <Span Style=\"{DynamicResource Sub_NonCompliant_Diff}\"> (</Span>\n  <Span Style=\"{DynamicResource Sub_NonCompliant_Diff}\">condition2</Span>\n  <Span Style=\"{DynamicResource Sub_NonCompliant_Diff}\">) {</Span>\n</Span>\n");
+            result2.Should().BeEquivalentTo("<Span Style=\"{DynamicResource Compliant_Diff}\">\n  <Span Style=\"{DynamicResource Sub_Compliant_Diff}\">}</Span>\n</Span>\n<Span Style=\"{DynamicResource Compliant_Diff}\">if (condition2) {</Span>");
+        }
+
         private static DiffTranslator CreateTestSubject(IXamlWriterFactory xamlWriterFactory = null)
         {
             xamlWriterFactory ??= new XamlWriterFactory();
