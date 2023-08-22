872741e1e5e0252cae775a8f68a22ded2f8ead3c
duncanp-sonar <duncan.pocklington@sonarsource.com>
2023-08-22 10:00:56 +0000

    [Infra] Tweak cleanup script (#4759)
    ... so it doesn't matter which directory it is run from

--- tools/CleanRebuild.cmd
+++ tools/CleanRebuild.cmd
@@ -1,8 +1,8 @@
 @REM this script cleans all of the artifact locations and rebuilds the project
-call cd ..
-(for %%a in (".\build\ProcessJarFiles\tmp" ".\src\Rules\Embedded" ".\src\Integration.Vsix\lib" "%LOCALAPPDATA%\SLVS_Build_DownloadedJars" "%LOCALAPPDATA%\SLVS_CFamily_Build" "%LOCALAPPDATA%\SLVS_TypeScript_Build") do rd /s /q "%%~a")
-call cd ".\build\ProcessJarFiles"
-call msbuild.exe -t:Rebuild
-call cd "..\..\"
-call msbuild.exe "SonarLint.VisualStudio.Integration.sln" -t:Rebuild
-call cd "..\tools"
+
+@REM %~dp0 gives the directory containing this batch file
+
+(for %%a in ("%~dp0..\build\ProcessJarFiles\tmp" "%~dp0..\src\Rules\Embedded" "%~dp0..\src\Integration.Vsix\lib" "%LOCALAPPDATA%\SLVS_Build_DownloadedJars" "%LOCALAPPDATA%\SLVS_CFamily_Build" "%LOCALAPPDATA%\SLVS_TypeScript_Build") do rd /s /q "%%~a")
+
+call msbuild.exe %~dp0..\build\ProcessJarFiles -t:Rebuild
+call msbuild.exe "%~dp0..\SonarLint.VisualStudio.Integration.sln" -t:Rebuild
