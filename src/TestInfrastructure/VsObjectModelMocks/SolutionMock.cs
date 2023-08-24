e1aaba67224a526f96a01fb692f9954f6ffe57a6
duncanp-sonar <duncan.pocklington@sonarsource.com>
2023-08-24 12:53:11 +0000

    [CAYC] Delete unused test helpers (#4786)

--- src/TestInfrastructure/VsObjectModelMocks/SolutionMock.cs
+++ src/TestInfrastructure/VsObjectModelMocks/SolutionMock.cs
@@ -21,9 +21,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using EnvDTE;
 using FluentAssertions;
 using Microsoft.VisualStudio;
