d03cd5a00b9cff79492bf5632615efb729c7a290
georgii-borovinskikh-sonarsource <georgii.borovinskikh@sonarsource.com>
2023-08-29 12:13:24 +0200

    Fix build

--- build/tools/ExtractRuleDescFromJson/PluginRule.cs
+++ build/tools/ExtractRuleDescFromJson/PluginRule.cs
@@ -18,7 +18,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using SonarLint.VisualStudio.Rules;
+using SonarLint.VisualStudio.Core.Analysis;
 
 namespace ExtractRuleDescFromJson;
 
