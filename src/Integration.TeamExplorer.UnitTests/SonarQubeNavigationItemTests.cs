40f59a94d145aa1a19ddb859840b941c1df95547
duncanp-sonar <duncan.pocklington@sonarsource.com>
2023-09-26 18:52:27 +0100

    Remove SonarQubeNavigationItem

--- src/Integration.TeamExplorer.UnitTests/SonarQubeNavigationItemTests.cs
+++ src/Integration.TeamExplorer.UnitTests/SonarQubeNavigationItemTests.cs
@@ -1,71 +1,71 @@
-﻿/*
- * SonarLint for Visual Studio
- * Copyright (C) 2016-2023 SonarSource SA
- * mailto:info AT sonarsource DOT com
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
+﻿///*
+// * SonarLint for Visual Studio
+// * Copyright (C) 2016-2023 SonarSource SA
+// * mailto:info AT sonarsource DOT com
+// *
+// * This program is free software; you can redistribute it and/or
+// * modify it under the terms of the GNU Lesser General Public
+// * License as published by the Free Software Foundation; either
+// * version 3 of the License, or (at your option) any later version.
+// *
+// * This program is distributed in the hope that it will be useful,
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// * Lesser General Public License for more details.
+// *
+// * You should have received a copy of the GNU Lesser General Public License
+// * along with this program; if not, write to the Free Software Foundation,
+// * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// */
 
-using System;
-using FluentAssertions;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SonarLint.VisualStudio.Integration.Resources;
-using SonarLint.VisualStudio.Integration.TeamExplorer;
-using SonarLint.VisualStudio.TestInfrastructure;
+//using System;
+//using FluentAssertions;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using SonarLint.VisualStudio.Integration.Resources;
+//using SonarLint.VisualStudio.Integration.TeamExplorer;
+//using SonarLint.VisualStudio.TestInfrastructure;
 
-namespace SonarLint.VisualStudio.Integration.UnitTests.TeamExplorer
-{
-    [TestClass]
-    public class SonarQubeNavigationItemTests
-    {
-        [TestMethod]
-        public void SonarQubeNavigationItem_Execute()
-        {
-            // Arrange
-            var controller = new ConfigurableTeamExplorerController();
+//namespace SonarLint.VisualStudio.Integration.UnitTests.TeamExplorer
+//{
+//    [TestClass]
+//    public class SonarQubeNavigationItemTests
+//    {
+//        [TestMethod]
+//        public void SonarQubeNavigationItem_Execute()
+//        {
+//            // Arrange
+//            var controller = new ConfigurableTeamExplorerController();
 
-            var testSubject = new SonarQubeNavigationItem(controller);
+//            var testSubject = new SonarQubeNavigationItem(controller);
 
-            // Act
-            testSubject.Execute();
+//            // Act
+//            testSubject.Execute();
 
-            // Assert
-            controller.ShowConnectionsPageCallsCount.Should().Be(1);
-        }
+//            // Assert
+//            controller.ShowConnectionsPageCallsCount.Should().Be(1);
+//        }
 
-        [TestMethod]
-        public void SonarQubeNavigationItem_Ctor()
-        {
-            // Arrange
-            var controller = new ConfigurableTeamExplorerController();
+//        [TestMethod]
+//        public void SonarQubeNavigationItem_Ctor()
+//        {
+//            // Arrange
+//            var controller = new ConfigurableTeamExplorerController();
 
-            // Act
-            var testSubject = new SonarQubeNavigationItem(controller);
+//            // Act
+//            var testSubject = new SonarQubeNavigationItem(controller);
 
-            // Assert
-            testSubject.IsVisible.Should().BeTrue("Nav item should be visible");
-            testSubject.IsEnabled.Should().BeTrue("Nav item should be enabled");
-            testSubject.Text.Should().Be(Strings.TeamExplorerPageTitle, "Unexpected nav text");
+//            // Assert
+//            testSubject.IsVisible.Should().BeTrue("Nav item should be visible");
+//            testSubject.IsEnabled.Should().BeTrue("Nav item should be enabled");
+//            testSubject.Text.Should().Be(Strings.TeamExplorerPageTitle, "Unexpected nav text");
 
-            testSubject.Icon.Should().NotBeNull("Icon should not be null");
-        }
+//            testSubject.Icon.Should().NotBeNull("Icon should not be null");
+//        }
 
-        [TestMethod]
-        public void SonarQubeNavigationItem_Ctor_NullArgChecks()
-        {
-            Exceptions.Expect<ArgumentNullException>(() => new SonarQubeNavigationItem(null));
-        }
-    }
-}
+//        [TestMethod]
+//        public void SonarQubeNavigationItem_Ctor_NullArgChecks()
+//        {
+//            Exceptions.Expect<ArgumentNullException>(() => new SonarQubeNavigationItem(null));
+//        }
+//    }
+//}
--- src/Integration.TeamExplorer/SonarQubeNavigationItem.cs
+++ src/Integration.TeamExplorer/SonarQubeNavigationItem.cs
@@ -1,63 +1,63 @@
-﻿/*
- * SonarLint for Visual Studio
- * Copyright (C) 2016-2023 SonarSource SA
- * mailto:info AT sonarsource DOT com
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
+﻿///*
+// * SonarLint for Visual Studio
+// * Copyright (C) 2016-2023 SonarSource SA
+// * mailto:info AT sonarsource DOT com
+// *
+// * This program is free software; you can redistribute it and/or
+// * modify it under the terms of the GNU Lesser General Public
+// * License as published by the Free Software Foundation; either
+// * version 3 of the License, or (at your option) any later version.
+// *
+// * This program is distributed in the hope that it will be useful,
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// * Lesser General Public License for more details.
+// *
+// * You should have received a copy of the GNU Lesser General Public License
+// * along with this program; if not, write to the Free Software Foundation,
+// * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// */
 
-using System;
-using System.ComponentModel.Composition;
-using System.Windows.Media;
-using Microsoft.TeamFoundation.Controls;
-using Microsoft.TeamFoundation.Controls.WPF.TeamExplorer;
-using SonarLint.VisualStudio.Integration.Resources;
+//using System;
+//using System.ComponentModel.Composition;
+//using System.Windows.Media;
+//using Microsoft.TeamFoundation.Controls;
+//using Microsoft.TeamFoundation.Controls.WPF.TeamExplorer;
+//using SonarLint.VisualStudio.Integration.Resources;
 
-namespace SonarLint.VisualStudio.Integration.TeamExplorer
-{
-    [TeamExplorerNavigationItem(SonarQubeNavigationItem.ItemId, SonarQubeNavigationItem.Priority, TargetPageId = SonarQubePage.PageId)]
-    internal class SonarQubeNavigationItem : TeamExplorerNavigationItemBase
-    {
-        public const string ItemId = "172AF455-5F42-46FC-BFE6-23227A05806B";
-        public const int Priority = TeamExplorerNavigationItemPriority.Settings - 1;
+//namespace SonarLint.VisualStudio.Integration.TeamExplorer
+//{
+//    [TeamExplorerNavigationItem(SonarQubeNavigationItem.ItemId, SonarQubeNavigationItem.Priority, TargetPageId = SonarQubePage.PageId)]
+//    internal class SonarQubeNavigationItem : TeamExplorerNavigationItemBase
+//    {
+//        public const string ItemId = "172AF455-5F42-46FC-BFE6-23227A05806B";
+//        public const int Priority = TeamExplorerNavigationItemPriority.Settings - 1;
 
-        private readonly ITeamExplorerController controller;
+//        private readonly ITeamExplorerController controller;
 
-        [ImportingConstructor]
-        internal SonarQubeNavigationItem([Import] ITeamExplorerController controller)
-        {
-            if (controller == null)
-            {
-                throw new ArgumentNullException(nameof(controller));
-            }
+//        [ImportingConstructor]
+//        internal SonarQubeNavigationItem([Import] ITeamExplorerController controller)
+//        {
+//            if (controller == null)
+//            {
+//                throw new ArgumentNullException(nameof(controller));
+//            }
 
-            this.controller = controller;
+//            this.controller = controller;
 
-            this.Text = Strings.TeamExplorerPageTitle;
-            this.IsVisible = true;
-            this.IsEnabled = true;
+//            this.Text = Strings.TeamExplorerPageTitle;
+//            this.IsVisible = true;
+//            this.IsEnabled = true;
 
-            var image = ResourceHelper.Get<DrawingImage>("SonarQubeServerIcon");
-            this.m_icon = image != null ? new DrawingBrush(image.Drawing) : null;
+//            var image = ResourceHelper.Get<DrawingImage>("SonarQubeServerIcon");
+//            this.m_icon = image != null ? new DrawingBrush(image.Drawing) : null;
 
-            this.m_defaultArgbColorBrush = ResourceHelper.Get<SolidColorBrush>("SQForegroundBrush");
-        }
+//            this.m_defaultArgbColorBrush = ResourceHelper.Get<SolidColorBrush>("SQForegroundBrush");
+//        }
 
-        public override void Execute()
-        {
-            this.controller.ShowSonarQubePage();
-        }
-    }
-}
+//        public override void Execute()
+//        {
+//            this.controller.ShowSonarQubePage();
+//        }
+//    }
+//}
