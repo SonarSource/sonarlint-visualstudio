710e01c3f742925bc7509e0b13c7bffb761af568
Uğraş Ergün <96827714+ugras-ergun-sonarsource@users.noreply.github.com>
2023-10-12 09:53:11 +0200

    Add shared binding Config model (#4927)

---
+++ src/ConnectedMode/Shared/SharedBindingConfigModel.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using Newtonsoft.Json;
+
+namespace SonarLint.VisualStudio.ConnectedMode.Shared
+{
+    public class SharedBindingConfigModel
+    {
+        [JsonProperty("SonarQubeUri", NullValueHandling = NullValueHandling.Ignore)]
+        public string Uri { get; set; }
+
+        [JsonProperty("SonarCloudOrganization", NullValueHandling = NullValueHandling.Ignore)]
+        public string Organization { get; set; }
+
+        [JsonProperty("ProjectKey")]
+        public string ProjectKey { get; set; }
+    }
+}
