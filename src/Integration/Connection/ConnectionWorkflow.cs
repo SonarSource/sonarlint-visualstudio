a47ce6c462cf31817b3423c1a9537daff8080401
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2023-10-31 09:50:00 +0000

    Non-existing shared project key causes SLVS to show a binding error (#4988)
    * Fix `Non-existing shared project key causes SLVS to show a binding error` issue #4987

--- src/Integration/Connection/ConnectionWorkflow.cs
+++ src/Integration/Connection/ConnectionWorkflow.cs
@@ -49,21 +49,24 @@ namespace SonarLint.VisualStudio.Integration.Connection
     {
         private readonly IServiceProvider serviceProvider;
         private readonly IHost host;
+        private readonly string autoBindProjectKey;
         private readonly ICommand parentCommand;
         private readonly IThreadHandling threadHandling;
         private readonly ICredentialStoreService credentialStore;
 
-        public ConnectionWorkflow(IServiceProvider serviceProvider, IHost host, ICommand parentCommand)
-            : this(serviceProvider, host, parentCommand, ThreadHandling.Instance)
+        public ConnectionWorkflow(IServiceProvider serviceProvider, IHost host, string autoBindProjectKey, ICommand parentCommand)
+            : this(serviceProvider, host, autoBindProjectKey, parentCommand, ThreadHandling.Instance)
         { }
 
         internal /* for testing */ ConnectionWorkflow(IServiceProvider serviceProvider,
             IHost host,
+            string autoBindProjectKey,
             ICommand parentCommand,
             IThreadHandling threadHandling)
         {
             this.serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
             this.host = host ?? throw new ArgumentNullException(nameof(host));
+            this.autoBindProjectKey = autoBindProjectKey;
             this.parentCommand = parentCommand ?? throw new ArgumentNullException(nameof(parentCommand));
             this.threadHandling = threadHandling;
 
@@ -132,34 +135,9 @@ namespace SonarLint.VisualStudio.Integration.Connection
                     await this.host.SonarQubeService.ConnectAsync(connection, cancellationToken);
                 }
 
-                if (connection.Organization == null)
-                {
-                    var hasOrgs = await this.host.SonarQubeService.HasOrganizations(cancellationToken);
-                    if (hasOrgs)
-                    {
-                        notifications.ProgressChanged(Strings.ConnectionStepRetrievingOrganizations);
-                        var organizations =
-                            await this.host.SonarQubeService.GetAllOrganizationsAsync(cancellationToken);
+                await HandleOrganizationSelectionAsync(connection, controller, notifications, cancellationToken);
 
-                        connection.Organization = AskUserToSelectOrganizationOnUIThread(organizations);
-                        if (connection.Organization == null) // User clicked cancel
-                        {
-                            AbortWithMessage(notifications, controller, cancellationToken);
-                            return;
-                        }
-                    }
-
-                }
-
-                // Persist the credentials on successful connection to SonarQube, unless
-                // the connection is anonymous
-                if (!string.IsNullOrEmpty(connection.UserName) &&
-                    !string.IsNullOrEmpty(connection.Password.ToUnsecureString()))
-                {
-                    this.credentialStore.WriteCredentials(
-                        connection.ServerUri,
-                        new Credential(connection.UserName, connection.Password.ToUnsecureString()));
-                }
+                PersistCredentials(connection);
 
                 this.ConnectedServer = connection;
 
@@ -167,28 +145,15 @@ namespace SonarLint.VisualStudio.Integration.Connection
                 var projects = await this.host.SonarQubeService.GetAllProjectsAsync(connection.Organization?.Key,
                     cancellationToken);
 
-                // The SonarQube client will limit the number of returned projects to 10K (hard limit on SonarQube side)
-                // but will no longer fail when trying to retrieve more. In the case where the project we want to bind to
-                // is not in the list and the binding was already done and the key is not null then we manually
-                // forge and add a new project to the list.
-                if (!string.IsNullOrWhiteSpace(this.host.VisualStateManager.BoundProjectKey) &&
-                    projects.Count == 10000 &&
-                    !projects.Any(p => p.Key == this.host.VisualStateManager.BoundProjectKey))
-                {
-                    this.host.Logger.WriteLine(
-                        $"The project with key '{this.host.VisualStateManager.BoundProjectKey}' is not part of the first ten thousand projects. The binding process will continue assuming it was found.");
-                    this.host.Logger.WriteLine(
-                        "Note that if the project key does not actually exist on the server the binding will fail at a later stage.");
-
-                    // We have to create a new list because the collection returned by the service as a fixed size
-                    projects = new List<SonarQubeProject>(projects);
-                    // Let's put the new item first in the collection to ease finding it.
-                    projects.Insert(0, new SonarQubeProject(this.host.VisualStateManager.BoundProjectKey,
-                        this.host.VisualStateManager.BoundProjectName ?? this.host.VisualStateManager.BoundProjectKey));
-                }
+                projects = HandleTooManyProjects(projects);
 
                 this.OnProjectsChanged(connection, projects);
                 notifications.ProgressChanged(Strings.ConnectionResultSuccess);
+
+                if (!string.IsNullOrEmpty(autoBindProjectKey) && !projects.Any(project => SonarQubeProject.KeyComparer.Equals(project.Key, autoBindProjectKey)))
+                {
+                    ShowAutobindProjectNotFoundNotificationAndThrow();
+                }
             }
             catch (HttpRequestException e)
             {
@@ -196,13 +161,13 @@ namespace SonarLint.VisualStudio.Integration.Connection
                 // that we would like to show i.e.when the host could not be resolved
                 var innerException = e.InnerException as System.Net.WebException;
                 this.host.Logger.WriteLine(CoreStrings.SonarQubeRequestFailed, e.Message, innerException?.Message);
-                AbortWithMessage(notifications, controller, cancellationToken);
+                AbortWithMessageAndThrow(notifications, controller, cancellationToken);
             }
             catch (TaskCanceledException)
             {
                 // Canceled or timeout
                 this.host.Logger.WriteLine(CoreStrings.SonarQubeRequestTimeoutOrCancelled);
-                AbortWithMessage(notifications, controller, cancellationToken);
+                AbortWithMessageAndThrow(notifications, controller, cancellationToken);
             }
             catch (BindingAbortedException)
             {
@@ -211,11 +176,80 @@ namespace SonarLint.VisualStudio.Integration.Connection
             catch (Exception ex)
             {
                 this.host.Logger.WriteLine(CoreStrings.SonarQubeRequestFailed, ex.Message, null);
-                AbortWithMessage(notifications, controller, cancellationToken);
+                AbortWithMessageAndThrow(notifications, controller, cancellationToken);
             }
         }
 
-        private void AbortWithMessage(IProgressStepExecutionEvents notifications, IProgressController controller,
+        private async Task HandleOrganizationSelectionAsync(ConnectionInformation connection, IProgressController controller,
+            IProgressStepExecutionEvents notifications, CancellationToken cancellationToken)
+        {
+            if (connection.Organization == null)
+            {
+                var hasOrgs = await this.host.SonarQubeService.HasOrganizations(cancellationToken);
+                if (hasOrgs)
+                {
+                    notifications.ProgressChanged(Strings.ConnectionStepRetrievingOrganizations);
+                    var organizations =
+                        await this.host.SonarQubeService.GetAllOrganizationsAsync(cancellationToken);
+
+                    connection.Organization = AskUserToSelectOrganizationOnUIThread(organizations);
+                    if (connection.Organization == null) // User clicked cancel
+                    {
+                        AbortWithMessageAndThrow(notifications, controller, cancellationToken);
+                    }
+                }
+            }
+        }
+
+        private void PersistCredentials(ConnectionInformation connection)
+        {
+            // Persist the credentials on successful connection to SonarQube, unless
+            // the connection is anonymous
+            if (!string.IsNullOrEmpty(connection.UserName) &&
+                !string.IsNullOrEmpty(connection.Password.ToUnsecureString()))
+            {
+                this.credentialStore.WriteCredentials(
+                    connection.ServerUri,
+                    new Credential(connection.UserName, connection.Password.ToUnsecureString()));
+            }
+        }
+
+        private IList<SonarQubeProject> HandleTooManyProjects(IList<SonarQubeProject> projects)
+        {
+            // The SonarQube client will limit the number of returned projects to 10K (hard limit on SonarQube side)
+            // but will no longer fail when trying to retrieve more. In the case where the project we want to bind to
+            // is not in the list and the binding was already done and the key is not null then we manually
+            // forge and add a new project to the list.
+            if (!string.IsNullOrWhiteSpace(this.host.VisualStateManager.BoundProjectKey) &&
+                projects.Count == 10000 &&
+                !projects.Any(p => p.Key == this.host.VisualStateManager.BoundProjectKey))
+            {
+                this.host.Logger.WriteLine(
+                    $"The project with key '{this.host.VisualStateManager.BoundProjectKey}' is not part of the first ten thousand projects. The binding process will continue assuming it was found.");
+                this.host.Logger.WriteLine(
+                    "Note that if the project key does not actually exist on the server the binding will fail at a later stage.");
+
+                // We have to create a new list because the collection returned by the service as a fixed size
+                projects = new List<SonarQubeProject>(projects);
+                // Let's put the new item first in the collection to ease finding it.
+                projects.Insert(0, new SonarQubeProject(this.host.VisualStateManager.BoundProjectKey,
+                    this.host.VisualStateManager.BoundProjectName ?? this.host.VisualStateManager.BoundProjectKey));
+            }
+
+            return projects;
+        }
+
+        private void ShowAutobindProjectNotFoundNotificationAndThrow()
+        {
+            this.host.ActiveSection?.UserNotifications?.ShowNotificationError(
+                string.Format(CultureInfo.CurrentCulture, Strings.BoundProjectNotFound, autoBindProjectKey),
+                NotificationIds.FailedToFindBoundProjectKeyId,
+                host.ActiveSection?.DisconnectCommand);
+
+            throw new BindingAbortedException($"Connection succeeded, but {nameof(autoBindProjectKey)} not found");
+        }
+
+        private void AbortWithMessageAndThrow(IProgressStepExecutionEvents notifications, IProgressController controller,
             CancellationToken cancellationToken)
         {
             notifications.ProgressChanged(cancellationToken.IsCancellationRequested
@@ -224,7 +258,7 @@ namespace SonarLint.VisualStudio.Integration.Connection
             this.host.ActiveSection?.UserNotifications?.ShowNotificationError(Strings.ConnectionFailed,
                 NotificationIds.FailedToConnectId, this.parentCommand);
 
-            AbortWorkflow(controller, cancellationToken);
+            AbortWorkflowAndThrow(controller, cancellationToken);
         }
 
         private SonarQubeOrganization AskUserToSelectOrganizationOnUIThread(IEnumerable<SonarQubeOrganization> organizations)
@@ -245,13 +279,13 @@ namespace SonarLint.VisualStudio.Integration.Connection
 
         #region Helpers
 
-        private void AbortWorkflow(IProgressController controller, CancellationToken token)
+        private void AbortWorkflowAndThrow(IProgressController controller, CancellationToken token)
         {
             bool aborted = controller.TryAbort();
             Debug.Assert(aborted || token.IsCancellationRequested, "Failed to abort the workflow");
 
             this.host.SonarQubeService.Disconnect();
-            throw new BindingAbortedException("Connection failed");
+            throw new BindingAbortedException("Connection failed"); // needed to set ProgressControllerResult to Failed, not possible without an exception (or a refactoring of the progress framework)
         }
 
         #endregion
