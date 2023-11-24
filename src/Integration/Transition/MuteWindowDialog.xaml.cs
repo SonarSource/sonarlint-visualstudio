d8f6200a48e0a194bb6537b091d4b96e5734ff12
Uğraş Ergün <96827714+ugras-ergun-sonarsource@users.noreply.github.com>
2023-11-24 12:23:10 +0100

    exclude tests

--- src/Integration/Transition/MuteWindowDialog.xaml.cs
+++ src/Integration/Transition/MuteWindowDialog.xaml.cs
@@ -18,6 +18,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+using System.Diagnostics.CodeAnalysis;
 using System.Windows;
 using System.Windows.Markup;
 using Microsoft.VisualStudio.PlatformUI;
@@ -29,6 +30,7 @@ namespace SonarLint.VisualStudio.Integration.Transition
     /// Interaction logic for UserControl1.xaml
     /// </summary>
     [ContentProperty(nameof(MuteWindowDialog))]
+    [ExcludeFromCodeCoverage]
     public partial class MuteWindowDialog : DialogWindow
     {
         public MuteWindowDialog(bool showAccept)
