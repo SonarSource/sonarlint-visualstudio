3441d40e583c271782670e135739337bedcc46f7
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2023-11-29 10:21:58 +0100

    Add ComponentKeyGenerator, add FilePathNormalizer reverse conversion (#5062)
    Fixes #5061

---
+++ src/SonarQube.Client.Tests/Helpers/ComponentKeyGeneratorTests.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SonarQube.Client.Helpers;
+
+namespace SonarQube.Client.Tests.Helpers;
+
+[TestClass]
+public class ComponentKeyGeneratorTests
+{
+    [DataTestMethod]
+    [DataRow(@"c:\dir\dir\root\dir\file.cs", @"c:\dir\dir\root\", "ProjectKey", "ProjectKey:dir/file.cs")]
+    [DataRow(@"c:\dir\dir\root\file.cs", @"c:\dir\dir\root\", "ProjectKey", "ProjectKey:file.cs")]
+    [DataRow(@"c:\dir\dir\root\dir1\dir2\file.cs", @"c:\dir\dir\root\", "Project:Key", "Project:Key:dir1/dir2/file.cs")]
+    public void GetComponentKey(string filePath, string rootPath, string projectName, string expected)
+    {
+        ComponentKeyGenerator.GetComponentKey(filePath, rootPath, projectName).Should().Be(expected);
+    }
+
+    [TestMethod]
+    public void GetComponentKey_NonRootedPath_Throws()
+    {
+        Action act = () => { ComponentKeyGenerator.GetComponentKey(@"c:\dir\dir\root\dir\file.cs", @".\non\rooted\", "project"); };
+
+        act.Should().ThrowExactly<ArgumentException>().WithMessage("Invalid root path format");
+    }
+    
+    [TestMethod]
+    public void GetComponentKey_NonFolderPath_Throws()
+    {
+        Action act = () => { ComponentKeyGenerator.GetComponentKey(@"c:\dir\dir\root\dir\file.cs", @"c:\not\folder", "project"); };
+
+        act.Should().ThrowExactly<ArgumentException>().WithMessage("Invalid root path format");
+    }
+    
+    [TestMethod]
+    public void GetComponentKey_NonMatchingRoot_Throws()
+    {
+        Action act = () => { ComponentKeyGenerator.GetComponentKey(@"c:\dir\dir\root\dir\file.cs", @"c:\not\same\folder\", "project"); };
+
+        act.Should().ThrowExactly<ArgumentException>().WithMessage("Local path is not under this root");
+    }
+}
---
+++ src/SonarQube.Client/Helpers/ComponentKeyGenerator.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.IO;
+
+namespace SonarQube.Client.Helpers
+{
+    public static class ComponentKeyGenerator
+    {
+        /// <summary>
+        /// Generates Sonar server component key
+        /// </summary>
+        /// <param name="localFilePath">Path for the local file.</param>
+        /// <param name="projectRootPath">Path for the local root of the project. Server paths are relative to this.</param>
+        /// <param name="projectKey">Sonar server project key</param>
+        /// <returns>Component key in the format projectkey:relativepath</returns>
+        /// <exception cref="ArgumentException">Invalid root format or the local path is not under the root.</exception>
+        public static string GetComponentKey(string localFilePath, string projectRootPath, string projectKey)
+        {
+            if (!Path.IsPathRooted(projectRootPath) || !projectRootPath.EndsWith(Path.DirectorySeparatorChar.ToString()))
+            {
+                throw new ArgumentException("Invalid root path format");
+            }
+
+            if (!localFilePath.StartsWith(projectRootPath))
+            {
+                throw new ArgumentException("Local path is not under this root");
+            }
+            
+            var serverFilePath = FilePathNormalizer.ServerizeWindowsPath(localFilePath.Substring(projectRootPath.Length));
+            return $"{projectKey}:{serverFilePath}";
+        }
+    }
+}
