10859578aca4b07ab756f132a1314a9be66c3d56
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2023-11-29 10:12:06 +0100

    Add ProjectRootCalculator, cleanup PathHelper (#5060)
    Fixes #5058

---
+++ src/ConnectedMode/ProjectRootCalculator.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.Core.Binding;
+using SonarLint.VisualStudio.Core.Helpers;
+using SonarQube.Client;
+
+namespace SonarLint.VisualStudio.ConnectedMode
+{
+    internal interface IProjectRootCalculator
+    {
+        Task<string> CalculateBasedOnLocalPathAsync(string localPath, CancellationToken token);
+    }
+
+    [Export(typeof(IProjectRootCalculator))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    internal class ProjectRootCalculator : IProjectRootCalculator
+    {
+        private readonly ISonarQubeService sonarQubeService;
+        private readonly IConfigurationProvider configurationProvider;
+        private readonly IStatefulServerBranchProvider branchProvider;
+
+        [ImportingConstructor]
+        public ProjectRootCalculator(ISonarQubeService sonarQubeService, IConfigurationProvider configurationProvider, IStatefulServerBranchProvider branchProvider)
+        {
+            this.sonarQubeService = sonarQubeService;
+            this.configurationProvider = configurationProvider;
+            this.branchProvider = branchProvider;
+        }
+
+        public async Task<string> CalculateBasedOnLocalPathAsync(string localPath, CancellationToken token)
+        {
+            var bindingConfiguration = configurationProvider.GetConfiguration();
+
+            if (bindingConfiguration.Mode == SonarLintMode.Standalone)
+            {
+                return null;
+            }
+
+            return PathHelper.CalculateServerRoot(localPath,
+                await sonarQubeService.SearchFilesByNameAsync(bindingConfiguration.Project.ProjectKey,
+                    await branchProvider.GetServerBranchNameAsync(token),
+                    Path.GetFileName(localPath),
+                    token));
+        }
+    }
+}
