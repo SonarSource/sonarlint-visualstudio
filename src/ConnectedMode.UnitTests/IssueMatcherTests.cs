f353a2033c4f2b1116cf221ef48a98960db1e5f6
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2023-12-01 10:05:06 +0100

    Add IssueMatcher (#5067)
    Fixes #5066

---
+++ src/ConnectedMode.UnitTests/IssueMatcherTests.cs
@@ -0,0 +1,162 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using SonarLint.VisualStudio.Core.Suppressions;
+using SonarLint.VisualStudio.TestInfrastructure;
+using SonarQube.Client.Models;
+
+namespace SonarLint.VisualStudio.ConnectedMode.UnitTests;
+
+[TestClass]
+public class IssueMatcherTests
+{
+    [TestMethod]
+    public void MefCtor_CheckIsExported()
+    {
+        MefTestHelpers.CheckTypeCanBeImported<IssueMatcher, IIssueMatcher>();
+    }
+    
+    [TestMethod]
+    public void MefCtor_CheckIsSingleton()
+    {
+        MefTestHelpers.CheckIsSingletonMefComponent<IssueMatcher>();
+    }
+    
+    [DataTestMethod]
+    [DataRow("CorrectRuleId", 1, "CorrectHash", true)] // exact matches
+    [DataRow("correctRULEID", 1, "CorrectHash", true)] // rule-id is case-insensitive
+    [DataRow("CorrectRuleId", 1, "wrong hash", true)] // matches on line
+    [DataRow("CorrectRuleId", 9999, "CorrectHash", true)] // matches on hash only
+    [DataRow("CorrectRuleId", 2, "correcthash", false)] // hash is case-sensitive
+    [DataRow("CorrectRuleId", 2, "wrong hash", false)] // wrong line and hash
+    [DataRow("CorrectRuleId", null, null, false)] // server file issue
+    [DataRow("wrong rule Id", 1, "CorrectHash", false)]
+    public void IsMatch_MatchesBasedOnAllParameters(string serverRuleId, int? serverIssueLine,
+        string serverHash, bool expectedResult)
+    {
+        var issueToMatch = CreateIssueToMatch("CorrectRuleId", 1, "CorrectHash");
+        var serverIssue = CreateServerIssue(serverRuleId, serverIssueLine, serverHash);
+
+        CreateTestSubject().IsLikelyMatch(issueToMatch, serverIssue).Should().Be(expectedResult);
+    }
+
+    [DataTestMethod]
+    [DataRow("CorrectRuleId", null, null, true)] // exact matches
+    [DataRow("CorrectRuleId", null, "hash", true)] // hash should be ignored for file-level issues
+    [DataRow("WrongRuleId", null, null, false)] // wrong rule
+    [DataRow("CorrectRuleId", 1, "hash", false)] // not a file issue
+    [DataRow("CorrectRuleId", 999, null, false)] // not a file issue - should not match a file issue, even though the hash is the same
+    public void IsMatch_FileLevelIssue(string serverRuleId, int? serverIssueLine,
+        string serverHash, bool expectedResult)
+    {
+        // File issues have line number of 0 and an empty hash
+        var issueToMatch = CreateIssueToMatch("CorrectRuleId", null, null);
+        var serverIssue = CreateServerIssue(serverRuleId, serverIssueLine, serverHash);
+
+        CreateTestSubject().IsLikelyMatch(issueToMatch, serverIssue).Should().Be(expectedResult);
+    }
+
+    [TestMethod]
+    // Module-level issues i.e. no file
+    [DataRow(null, null, true)]
+    [DataRow(null, "", true)]
+    [DataRow("", null, true)]
+    [DataRow("", "", true)]
+
+    // Module-level issues should not match non-module-level issues
+    [DataRow(@"any.txt", "", false)]
+    [DataRow(@"any.txt", null, false)]
+    [DataRow("", @"c:\any.txt", false)]
+    [DataRow(null, @"c:\any.txt", false)]
+
+    // File issues
+    [DataRow(@"same.txt", @"c:\same.txt", true)]
+    [DataRow(@"SAME.TXT", @"c:\same.txt", true)]
+    [DataRow(@"same.TXT", @"c:\XXXsame.txt", false)] // partial file name -> should not match
+    [DataRow(@"differentExt.123", @"a:\differentExt.999", false)] // different extension -> should not match
+    [DataRow(@"aaa\partial\file.cs", @"d:\partial\file.cs", false)]
+    // Only matching the local path tail, so the same server path can match multiple local files
+    [DataRow(@"partial\file.cs", @"c:\aaa\partial\file.cs", true)]
+    [DataRow(@"partial\file.cs", @"c:\aaa\bbb\partial\file.cs", true)]
+    [DataRow(@"partial\file.cs", @"c:\aaa\bbb\ccc\partial\file.cs", true)]
+    public void IsMatch_CheckFileComparisons(string serverFilePath, string localFilePath, bool expected)
+    {
+        var issueToMatch = CreateIssueToMatch("111", 0, "hash", filePath: localFilePath);
+
+        var serverIssue = CreateServerIssue("111", 0, "hash", filePath: serverFilePath);
+
+        CreateTestSubject().IsLikelyMatch(issueToMatch, serverIssue).Should().Be(expected);
+    }
+
+    [TestMethod]
+    public void FindMatchOrDefault_FindsFirstMatch()
+    {
+        var ruleId = "111";
+        var startLine = 0;
+        var issueToMatch = CreateIssueToMatch(ruleId, startLine, null, @"c:\root\dir\file.cs");
+        var serverPath = @"dir\file.cs";
+
+        var correctServerIssue = CreateServerIssue(ruleId, startLine, null, serverPath);
+
+        CreateTestSubject().GetFirstLikelyMatchFromSameFileOrNull(issueToMatch, new[]
+        {
+            CreateServerIssue("222", startLine, null, serverPath),
+            CreateServerIssue(ruleId, 111, null, serverPath),
+            correctServerIssue,
+            CreateServerIssue(ruleId, startLine, null, serverPath) // finds only the firs match
+        }).Should().BeSameAs(correctServerIssue);
+    }
+
+    [TestMethod]
+    public void FindMatchOrDefault_NoServerIssues_ReturnsNull()
+    {
+        var issueToMatch = CreateIssueToMatch("1", 1, "1");
+
+        CreateTestSubject().GetFirstLikelyMatchFromSameFileOrNull(issueToMatch, Array.Empty<SonarQubeIssue>()).Should().BeNull();
+    }
+
+    private IssueMatcher CreateTestSubject()
+    {
+        return new IssueMatcher();
+    }
+    
+    private IFilterableIssue CreateIssueToMatch(string ruleId, int? startLine, string lineHash, string filePath = null) =>
+        new TestFilterableIssue
+        {
+            RuleId = ruleId,
+            StartLine = startLine,
+            LineHash = lineHash,
+            FilePath = filePath
+        };
+    
+    private SonarQubeIssue CreateServerIssue(string ruleId, int? startLine, string lineHash,
+        string filePath = null)
+    {
+        var sonarQubeIssue = new SonarQubeIssue(null, filePath, lineHash, null, null, ruleId, false, SonarQubeIssueSeverity.Info,
+            DateTimeOffset.MinValue, DateTimeOffset.MinValue,
+            startLine.HasValue
+                ? new IssueTextRange(startLine.Value, 1, 1, 1)
+                : null,
+            flows: null);
+
+        return sonarQubeIssue;
+    }
+}
---
+++ src/ConnectedMode/IssueMatcher.cs
@@ -0,0 +1,106 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using SonarLint.VisualStudio.Core.Helpers;
+using SonarLint.VisualStudio.Core.Suppressions;
+using SonarQube.Client.Models;
+
+namespace SonarLint.VisualStudio.ConnectedMode
+{
+    internal interface IIssueMatcher
+    {
+        /// <summary>
+        /// This method attempts to match <paramref name="issue"/> with <paramref name="serverIssue"/>.
+        /// </summary>
+        /// <remarks>There's a possibility of False Positive matches: in case false tail-match of file paths, as the project root is not taken into account,
+        /// or when line number matches but line hash doesn't.
+        /// </remarks>
+        /// <param name="issue">Local issue</param>
+        /// <param name="serverIssue">Server issue</param>
+        /// <returns>The best possible match based on rule id, file name (tail matched to the server path), line number and line hash (not checked when line numbers match)</returns>
+        bool IsLikelyMatch(IFilterableIssue issue, SonarQubeIssue serverIssue);
+
+        /// <summary>
+        /// Returns the first likely matching issue. 
+        /// </summary>
+        /// <remarks>For this method to work correctly, all <paramref name="serverIssuesFromSameFile"/>; need to be from the same server file.
+        /// False Positives are possible, since &lt;see cref="IsLikelyMatch"/&gt; can return true for multiple issues in the same file and only the firs one is returned.
+        /// </remarks>
+        /// <param name="issue">Local issue</param>
+        /// <param name="serverIssuesFromSameFile">List of server issues from the same file</param>
+        /// <returns>A matching server issue, if present in the <paramref name="serverIssuesFromSameFile"/> list, or null</returns>
+        SonarQubeIssue GetFirstLikelyMatchFromSameFileOrNull(IFilterableIssue issue, IEnumerable<SonarQubeIssue> serverIssuesFromSameFile);
+    }
+
+    [Export(typeof(IIssueMatcher))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    internal class IssueMatcher : IIssueMatcher
+    {
+        public bool IsLikelyMatch(IFilterableIssue issue, SonarQubeIssue serverIssue)
+        {
+            return IsMatch(issue, serverIssue, true);
+        }
+
+        public SonarQubeIssue GetFirstLikelyMatchFromSameFileOrNull(IFilterableIssue issue, IEnumerable<SonarQubeIssue> serverIssuesFromSameFile)
+        {
+            return serverIssuesFromSameFile?.FirstOrDefault(serverIssue => IsMatch(issue, serverIssue, false));
+        }
+
+        private static bool IsMatch(IFilterableIssue issue, SonarQubeIssue serverIssue, bool checkFilePath)
+        {
+            // File-level issues (i.e. line = null) match if:
+            // 1. Same component, same file, same error code.
+
+            // Non-file-level issues match if:
+            // 1. Same component, same file, same error code, same line hash        // tolerant to line number changing
+            // 2. Same component, same file, same error code, same line             // tolerant to code on the line changing e.g. var rename
+
+            // File-level issues never match non-file-level issues.
+            
+            if (!StringComparer.OrdinalIgnoreCase.Equals(issue.RuleId, serverIssue.RuleId))
+            {
+                return false;
+            }
+
+            if (checkFilePath && !PathHelper.IsServerFileMatch(issue.FilePath, serverIssue.FilePath))
+            {
+                return false;
+            }
+
+            if (!issue.StartLine.HasValue) // i.e. file-level issue
+            {
+                return serverIssue.TextRange == null;
+            }
+            
+            // Non-file level issue
+            
+            return issue.StartLine == serverIssue.TextRange?.StartLine || CompareHash(issue, serverIssue);
+        }
+
+        private static bool CompareHash(IFilterableIssue issue, SonarQubeIssue serverIssue) =>
+            issue.LineHash != null 
+            && serverIssue.Hash != null
+            && StringComparer.Ordinal.Equals(issue.LineHash, serverIssue.Hash);
+    }
+}
