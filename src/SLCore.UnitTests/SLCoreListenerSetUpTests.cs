2013ee02b1393c7b7e3f18a9010b16aae704e604
Uğraş Ergün <96827714+ugras-ergun-sonarsource@users.noreply.github.com>
2023-12-26 16:30:12 +0100

    Added ISLCoreListenerSetUp

---
+++ src/SLCore.UnitTests/SLCoreListenerSetUpTests.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using SonarLint.VisualStudio.SLCore.Core;
+using SonarLint.VisualStudio.TestInfrastructure;
+
+namespace SonarLint.VisualStudio.SLCore.UnitTests
+{
+    [TestClass]
+    public class SLCoreListenerSetUpTests
+    {
+        [TestMethod]
+        [Ignore("Will be enabled when ISLCoreJsonRpc is implemented")]
+        public void MefCtor_CheckIsExported()
+        {
+            MefTestHelpers.CheckTypeCanBeImported<SLCoreListenerSetUp, ISLCoreListenerSetUp>(MefTestHelpers.CreateExport<ISLCoreJsonRpc>());
+        }
+
+        [TestMethod]
+        public void Setup_NoListenerRegistered_DoesNotCallRpcWrapper()
+        {
+            var wrapperMock = new Mock<ISLCoreJsonRpc>();
+
+            var listeners = new ISLCoreListener[] { };
+
+            var testSubject = new SLCoreListenerSetUp(wrapperMock.Object, listeners);
+
+            testSubject.Setup();
+
+            wrapperMock.VerifyNoOtherCalls();
+        }
+
+        [TestMethod]
+        public void Setup_ListenersRegistered_CallRpcWrapper()
+        {
+            var wrapperMock = new Mock<ISLCoreJsonRpc>();
+
+            var listener0 = new Mock<ISLCoreListener>();
+            var listener1 = new Mock<TestListener1>();
+            var listener2 = new Mock<TestListener2>();
+
+            var listeners = new ISLCoreListener[] { listener0.Object, listener1.Object, listener2.Object };
+
+            var testSubject = new SLCoreListenerSetUp(wrapperMock.Object, listeners);
+
+            testSubject.Setup();
+
+            wrapperMock.Verify(w => w.AttachListener(listener0.Object), Times.Once);
+            wrapperMock.Verify(w => w.AttachListener(listener1.Object), Times.Once);
+            wrapperMock.Verify(w => w.AttachListener(listener2.Object), Times.Once);
+            wrapperMock.VerifyNoOtherCalls();
+        }
+
+        public interface TestListener1 : ISLCoreListener { }
+
+        public interface TestListener2 : ISLCoreListener { }
+    }
+}
---
+++ src/SLCore/Core/ISLCoreListenerSetUp.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+
+namespace SonarLint.VisualStudio.SLCore.Core
+{
+    public interface ISLCoreListenerSetUp
+    {
+        void Setup();
+    }
+
+    [Export(typeof(ISLCoreListenerSetUp))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    internal class SLCoreListenerSetUp : ISLCoreListenerSetUp
+    {
+        private readonly ISLCoreJsonRpc wrapper;
+        private readonly IEnumerable<ISLCoreListener> listeners;
+
+        [ImportingConstructor]
+        public SLCoreListenerSetUp(ISLCoreJsonRpc wrapper, [ImportMany] IEnumerable<ISLCoreListener> listeners)
+        {
+            this.wrapper = wrapper;
+            this.listeners = listeners;
+        }
+
+        public void Setup()
+        {
+            foreach (var listener in listeners)
+            {
+                wrapper.AttachListener(listener);
+            }
+        }
+    }
+}
