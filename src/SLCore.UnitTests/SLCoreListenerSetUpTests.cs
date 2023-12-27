3570a1e4ce0ad85ae9c49c087d4bdeed130e39ae
Uğraş Ergün <96827714+ugras-ergun-sonarsource@users.noreply.github.com>
2023-12-27 11:21:32 +0100

    Implement ISLCoreListenerSetUp (#5136)
    Fixes #5133

---
+++ src/SLCore.UnitTests/SLCoreListenerSetUpTests.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using SonarLint.VisualStudio.SLCore.Core;
+using SonarLint.VisualStudio.TestInfrastructure;
+
+namespace SonarLint.VisualStudio.SLCore.UnitTests
+{
+    [TestClass]
+    public class SLCoreListenerSetUpTests
+    {
+        [TestMethod]
+        public void MefCtor_CheckIsExported()
+        {
+            MefTestHelpers.CheckTypeCanBeImported<SLCoreListenerSetUp, ISLCoreListenerSetUp>();
+        }
+
+        [TestMethod]
+        public void Mef_CheckIsSingleton()
+        {
+            MefTestHelpers.CheckIsSingletonMefComponent<SLCoreListenerSetUp>();
+        }
+
+        [TestMethod]
+        public void Setup_NoListenerRegistered_DoesNotCallRpcWrapper()
+        {
+            var wrapperMock = new Mock<ISLCoreJsonRpc>();
+
+            var listeners = new ISLCoreListener[] { };
+
+            var testSubject = new SLCoreListenerSetUp(listeners);
+
+            testSubject.Setup(wrapperMock.Object);
+
+            wrapperMock.VerifyNoOtherCalls();
+        }
+
+        [TestMethod]
+        public void Setup_ListenersRegistered_CallRpcWrapper()
+        {
+            var wrapperMock = new Mock<ISLCoreJsonRpc>();
+
+            var listener0 = Mock.Of<ISLCoreListener>();
+            var listener1 = Mock.Of<TestListener1>();
+            var listener2 = Mock.Of<TestListener2>();
+
+            var listeners = new ISLCoreListener[] { listener0, listener1, listener2 };
+
+            var testSubject = new SLCoreListenerSetUp(listeners);
+
+            testSubject.Setup(wrapperMock.Object);
+
+            wrapperMock.Verify(w => w.AttachListener(listener0), Times.Once);
+            wrapperMock.Verify(w => w.AttachListener(listener1), Times.Once);
+            wrapperMock.Verify(w => w.AttachListener(listener2), Times.Once);
+            wrapperMock.VerifyNoOtherCalls();
+        }
+
+        public interface TestListener1 : ISLCoreListener { }
+
+        public interface TestListener2 : ISLCoreListener { }
+    }
+}
