772ded9c5342b99c47d5ace7d0679cea142ecb1b
Uğraş Ergün <96827714+ugras-ergun-sonarsource@users.noreply.github.com>
2023-12-29 11:37:30 +0100

    Add LoggerListener (#5150)
    Fixes #5148

---
+++ src/SLCore/Listener/LoggerListener.cs
@@ -0,0 +1,73 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.ComponentModel.Composition;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.SLCore.Core;
+
+namespace SonarLint.VisualStudio.SLCore.Listener
+{
+    [Export(typeof(ISLCoreListener))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    public class LoggerListener : ISLCoreListener
+    {
+        private readonly ILogger logger;
+
+        [ImportingConstructor]
+        public LoggerListener(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Log(LogParams parameters)
+        {
+            var message = "[SLCORE] " + parameters.message;
+
+            switch (parameters.level)
+            {
+                case LogLevel.ERROR:
+                case LogLevel.WARN:
+                    logger.WriteLine(message);
+                    break;
+
+                case LogLevel.INFO:
+                case LogLevel.DEBUG:
+                case LogLevel.TRACE:
+                    logger.LogVerbose(message);
+                    break;
+            }
+        }
+    }
+
+    public class LogParams
+    {
+        public string message;
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public LogLevel level;
+    }
+
+    public enum LogLevel
+    {
+        ERROR, WARN, INFO, DEBUG, TRACE
+    }
+}
