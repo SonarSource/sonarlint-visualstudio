b03711ead77c30bac9a3bd506cda502c9622a1f8
Uğraş Ergün <96827714+ugras-ergun-sonarsource@users.noreply.github.com>
2024-01-05 11:03:47 +0100

    Added IConfigurationScopeSLCoreService

---
+++ src/SLCore/Service/Project/IConfigurationScopeSLCoreService.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SonarLint.VisualStudio.SLCore.Core;
+using SonarLint.VisualStudio.SLCore.Service.Project.Models;
+
+namespace SonarLint.VisualStudio.SLCore.Service.Project
+{
+    public interface IConfigurationScopeSLCoreService : ISLCoreService
+    {
+        /// <summary>
+        /// Add configuration scope to SLCORE
+        /// </summary>
+        /// <param name="params"></param>
+        Task DidAddConfigurationScopesAsync(DidAddConfigurationScopesParams @params);
+
+        /// <summary>
+        /// Removes configuration scope from SLCORE
+        /// </summary>
+        /// <param name="params"></param>
+        Task DidRemoveConfigurationScopeAsync(DidRemoveConfigurationScopeParams @params);
+    }
+
+    public class DidRemoveConfigurationScopeParams
+    {
+        public string removeId { get; }
+
+        public DidRemoveConfigurationScopeParams(string removeId)
+        {
+            this.removeId = removeId;
+        }
+    }
+
+    public class DidAddConfigurationScopesParams
+    {
+        public List<ConfigurationScopeDto> addedScopes { get; }
+
+        public DidAddConfigurationScopesParams(List<ConfigurationScopeDto> addedScopes)
+        {
+            this.addedScopes = addedScopes;
+        }
+    }
+}
---
+++ src/SLCore/Service/Project/Models/ConfigurationScopeDto.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace SonarLint.VisualStudio.SLCore.Service.Project.Models
+{
+    public class ConfigurationScopeDto
+    {
+        public string id { get; }
+        public string name { get; }
+        public bool bindable { get; }
+        public BindingConfigurationDto binding { get; }
+
+        public ConfigurationScopeDto(string id, string name, bool bindable, BindingConfigurationDto binding)
+        {
+            this.id = id;
+            this.name = name;
+            this.bindable = bindable;
+            this.binding = binding;
+        }
+    }
+}
