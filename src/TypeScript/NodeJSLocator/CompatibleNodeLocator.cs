dcb7f462869f1be627f55b3c3792b750cdd36054
Georgii Borovinskikh <georgii.borovinskikh@sonarsource.com>
2024-01-16 12:34:02 +0100

    Update embedded JS/TS analyzer 10.11.0.25043

--- src/TypeScript/NodeJSLocator/CompatibleNodeLocator.cs
+++ src/TypeScript/NodeJSLocator/CompatibleNodeLocator.cs
@@ -65,8 +65,8 @@ namespace SonarLint.VisualStudio.TypeScript.NodeJSLocator
 
         internal static bool IsCompatibleVersion(Version nodeVersion)
         {
-            // Minimum supported version 14.17.0
-            return (nodeVersion.Major == 14 && nodeVersion.Minor >= 17) || nodeVersion.Major > 14;
+            // Minimum supported version 16.0.0
+            return nodeVersion.Major >= 16;
         }
     }
 }
