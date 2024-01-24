ff8b92b4645b023b7a495e717be35db85be0b9f7
Uğraş Ergün <96827714+ugras-ergun-sonarsource@users.noreply.github.com>
2024-01-24 11:46:56 +0100

    PR feedback

--- src/SLCore/Service/Rules/IRulesRpcService.cs
+++ src/SLCore/Service/Rules/IRulesRpcService.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * SonarLint for Visual Studio
- * Copyright (C) 2016-2023 SonarSource SA
+ * Copyright (C) 2016-2024 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
@@ -20,21 +20,29 @@
 
 using System.Threading.Tasks;
 using SonarLint.VisualStudio.SLCore.Core;
+using SonarLint.VisualStudio.SLCore.Protocol;
 
 namespace SonarLint.VisualStudio.SLCore.Service.Rules
 {
+    [JsonRpcClassAttribute("rule")]
     public interface IRulesRpcService : ISLCoreService
     {
         /// <summary>
         /// Gets Rule Meta Data from SLCORE
         /// </summary>
-        /// <param name="params"></param>
-        Task<object> GetEffectiveRuleDetailsAsync(GetEffectiveRuleDetailsParams @params);
+        /// <param name="parameters"></param>
+        Task<object> GetEffectiveRuleDetailsAsync(GetEffectiveRuleDetailsParams parameters);
     }
 
     public class GetEffectiveRuleDetailsParams
     {
-        public string configurationScopeId;
-        public string ruleKey;
+        public string configurationScopeId { get; }
+        public string ruleKey { get; }
+
+        public GetEffectiveRuleDetailsParams(string configurationScopeId, string ruleKey)
+        {
+            this.configurationScopeId = configurationScopeId;
+            this.ruleKey = ruleKey;
+        }
     }
 }
