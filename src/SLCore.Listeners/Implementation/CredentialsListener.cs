6a8a0b5235130360c240c9a1495c7bfeb987f8c8
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2024-02-09 09:19:31 +0100

    Add SLCore credentials provider implementation (#5206)
    Fixes #5204

---
+++ src/SLCore.Listeners/Implementation/CredentialsListener.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using SonarLint.VisualStudio.ConnectedMode.Binding;
+using SonarLint.VisualStudio.SLCore.Common.Helpers;
+using SonarLint.VisualStudio.SLCore.Common.Models;
+using SonarLint.VisualStudio.SLCore.Core;
+using SonarLint.VisualStudio.SLCore.Listener.Credentials;
+
+namespace SonarLint.VisualStudio.SLCore.Listeners.Implementation
+{
+    /// <summary>
+    /// Credentials provider for SLCore
+    /// </summary>
+    [Export(typeof(ISLCoreListener))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    internal class CredentialsListener : ICredentialsListener
+    {
+        private readonly ICredentialStoreService credentialStore;
+        private readonly IConnectionIdHelper connectionIdHelper;
+
+        [ImportingConstructor]
+        public CredentialsListener(ICredentialStoreService credentialStore) 
+            : this(credentialStore, new ConnectionIdHelper())
+        {
+        }
+
+        public CredentialsListener(ICredentialStoreService credentialStore, IConnectionIdHelper connectionIdHelper)
+        {
+            this.credentialStore = credentialStore;
+            this.connectionIdHelper = connectionIdHelper;
+        }
+
+        public Task<GetCredentialsResponse> GetCredentialsAsync(GetCredentialsParams parameters)
+        {
+            var serverUri = connectionIdHelper.GetUriFromConnectionId(parameters?.connectionId);
+
+            if (serverUri == null)
+            {
+                return Task.FromResult(GetCredentialsResponse.NoCredentials);
+            }
+
+            var credentials = credentialStore.ReadCredentials(serverUri);
+
+            if (credentials == null)
+            {
+                return Task.FromResult(GetCredentialsResponse.NoCredentials);
+            }
+
+            return Task.FromResult(string.IsNullOrEmpty(credentials.Password)
+                ? new GetCredentialsResponse(new TokenDto(credentials.Username))
+                : new GetCredentialsResponse(new UsernamePasswordDto(credentials.Username, credentials.Password)));
+        }
+    }
+}
