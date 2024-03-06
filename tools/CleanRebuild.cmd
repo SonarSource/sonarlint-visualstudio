7aa35d2448aed8ca39937f8605301ec07e79c097
Uğraş Ergün <96827714+ugras-ergun-sonarsource@users.noreply.github.com>
2024-03-06 12:42:58 +0100

    Cleanup Rule Extraction (#5281)
    Fixes #5279

--- tools/CleanRebuild.cmd
+++ tools/CleanRebuild.cmd
@@ -2,7 +2,7 @@
 
 @REM %~dp0 gives the directory containing this batch file
 
-(for %%a in ("%~dp0..\build\ProcessJarFiles\tmp" "%~dp0..\src\Rules\Embedded" "%~dp0..\src\Integration.Vsix\lib" "%LOCALAPPDATA%\SLVS_Build_DownloadedJars" "%LOCALAPPDATA%\SLVS_CFamily_Build" "%LOCALAPPDATA%\SLVS_TypeScript_Build") do rd /s /q "%%~a")
+(for %%a in ("%~dp0..\src\Integration.Vsix\lib" "%LOCALAPPDATA%\SLVS_Build_DownloadedJars" "%LOCALAPPDATA%\SLVS_CFamily_Build" "%LOCALAPPDATA%\SLVS_TypeScript_Build" "%LOCALAPPDATA%\SLVS_Build_SLOOP") do rd /s /q "%%~a")
 
-call msbuild.exe %~dp0..\build\ProcessJarFiles -t:Rebuild
+call msbuild.exe %~dp0..\build\DownloadDependencies -t:Rebuild
 call msbuild.exe "%~dp0..\SonarLint.VisualStudio.Integration.sln" -t:Rebuild
