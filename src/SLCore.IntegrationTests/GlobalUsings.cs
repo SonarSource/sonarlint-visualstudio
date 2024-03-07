363f63dfff50c8cd253fe12e1f515e40fb9aed4b
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2024-03-07 14:39:25 +0100

    Add RuleDescriptionConversionSmokeTest (#5285)
    Part of #5265

--- src/SLCore.IntegrationTests/GlobalUsings.cs
+++ src/SLCore.IntegrationTests/GlobalUsings.cs
@@ -20,4 +20,7 @@
 
 global using System;
 global using System.Collections.Generic;
+global using System.Threading.Tasks;
+global using FluentAssertions;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using SonarLint.VisualStudio.TestInfrastructure;
