127c2b43ae161f1324b0114ab3ae2b6f68b462c9
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2024-03-15 15:46:24 +0100

    Start Sloop from SonarLintDaemonPackage (#5302)
    Part of #5291

---
+++ src/SLCore/ISLCoreHandleFactory.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.Core.Binding;
+using SonarLint.VisualStudio.SLCore.Configuration;
+using SonarLint.VisualStudio.SLCore.State;
+
+namespace SonarLint.VisualStudio.SLCore;
+
+public interface ISLCoreHandleFactory
+{
+    ISLCoreHandle CreateInstance();
+}
+
+[Export(typeof(ISLCoreHandleFactory))]
+[PartCreationPolicy(CreationPolicy.Shared)]
+internal class SLCoreHandleFactory : ISLCoreHandleFactory
+{
+    private readonly ISLCoreRpcFactory slCoreRpcFactory;
+    private readonly ISLCoreConstantsProvider constantsProvider;
+    private readonly ISLCoreFoldersProvider slCoreFoldersProvider;
+    private readonly IServerConnectionsProvider serverConnectionConfigurationProvider;
+    private readonly ISLCoreEmbeddedPluginJarLocator slCoreEmbeddedPluginJarProvider;
+    private readonly IActiveSolutionBoundTracker activeSolutionBoundTracker;
+    private readonly IConfigScopeUpdater configScopeUpdater;
+    private readonly IThreadHandling threadHandling;
+
+    [ImportingConstructor]
+    public SLCoreHandleFactory(ISLCoreRpcFactory slCoreRpcFactory,
+        ISLCoreConstantsProvider constantsProvider,
+        ISLCoreFoldersProvider slCoreFoldersProvider,
+        IServerConnectionsProvider serverConnectionConfigurationProvider,
+        ISLCoreEmbeddedPluginJarLocator slCoreEmbeddedPluginJarProvider,
+        IActiveSolutionBoundTracker activeSolutionBoundTracker,
+        IConfigScopeUpdater configScopeUpdater,
+        IThreadHandling threadHandling)
+    {
+        this.slCoreRpcFactory = slCoreRpcFactory;
+        this.constantsProvider = constantsProvider;
+        this.slCoreFoldersProvider = slCoreFoldersProvider;
+        this.serverConnectionConfigurationProvider = serverConnectionConfigurationProvider;
+        this.slCoreEmbeddedPluginJarProvider = slCoreEmbeddedPluginJarProvider;
+        this.activeSolutionBoundTracker = activeSolutionBoundTracker;
+        this.configScopeUpdater = configScopeUpdater;
+        this.threadHandling = threadHandling;
+    }
+    
+    public ISLCoreHandle CreateInstance() =>
+        new SLCoreHandle(slCoreRpcFactory,
+            constantsProvider,
+            slCoreFoldersProvider,
+            serverConnectionConfigurationProvider,
+            slCoreEmbeddedPluginJarProvider,
+            activeSolutionBoundTracker,
+            configScopeUpdater,
+            threadHandling);
+}
