083fa01687de884d12e50607d3634045888fd6c5
vnaskos-sonar <168648790+vnaskos-sonar@users.noreply.github.com>
2024-05-16 10:07:42 +0200

    Add common usings into props file for the entire project (production + test code) (#5432)
    Fixes #5426

--- src/Core/SystemAbstractions/Timer/TimerWrapper.cs
+++ src/Core/SystemAbstractions/Timer/TimerWrapper.cs
@@ -18,9 +18,8 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using System;
 using System.Diagnostics.CodeAnalysis;
-using System.Timers;
+using Timer = System.Timers.Timer;
 
 namespace SonarLint.VisualStudio.Core.SystemAbstractions
 {
--- src/TestInfrastructure/VsObjectModelMocks/DTEMock.cs
+++ src/TestInfrastructure/VsObjectModelMocks/DTEMock.cs
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * SonarLint for Visual Studio
  * Copyright (C) 2016-2024 SonarSource SA
  * mailto:info AT sonarsource DOT com
@@ -18,9 +18,9 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using System;
 using EnvDTE;
 using EnvDTE80;
+using Debugger = EnvDTE.Debugger;
 
 namespace SonarLint.VisualStudio.TestInfrastructure
 {
