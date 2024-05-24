a22af767724bb47fe609b08003fd002adf2bab28
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2024-05-24 16:14:19 +0200

    Open in IDE: Replace MessageBox with Tool window Gold bar (#5447)
    Fixes #5443

---
+++ src/IssueViz.UnitTests/OpenInIDE/OpenInIdeNotificationTests.cs
@@ -0,0 +1,95 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.IssueVisualization.OpenInIde;
+using SonarLint.VisualStudio.TestInfrastructure;
+
+namespace SonarLint.VisualStudio.IssueVisualization.UnitTests.OpenInIDE;
+
+[TestClass]
+public class OpenInIdeNotificationTests
+{
+    [TestMethod]
+    public void MefCtor_CheckIsExported()
+    {
+        MefTestHelpers.CheckTypeCanBeImported<OpenInIdeNotification, IOpenInIdeNotification>(
+            MefTestHelpers.CreateExport<IOpenInIdeFailureInfoBar>(),
+            MefTestHelpers.CreateExport<IToolWindowService>());
+    }
+
+    [TestMethod]
+    public void MefCtor_CheckIsSingleton()
+    {
+        MefTestHelpers.CheckIsSingletonMefComponent<OpenInIdeNotification>();
+    }
+
+    [TestMethod]
+    public void UnableToLocateIssue_ShowsMessageBox()
+    {
+        var filePath = "file/path/123";
+        var toolWindowId = Guid.NewGuid();
+        var infoBar = Substitute.For<IOpenInIdeFailureInfoBar>();
+        var toolWindowService = Substitute.For<IToolWindowService>();
+        
+        new OpenInIdeNotification(infoBar, toolWindowService).UnableToLocateIssue(filePath, toolWindowId);
+        
+        VerifyToolWindowOpened(toolWindowService, toolWindowId);
+        VerifyInfoBarShown(infoBar, "Open in IDE. Could not locate issue. Ensure the file (file/path/123) has not been modified", toolWindowId);
+    }
+    
+    [TestMethod]
+    public void UnableToOpenFile_ShowsMessageBox()
+    {
+        var filePath = "file/path/123";
+        var toolWindowId = Guid.NewGuid();
+        var infoBar = Substitute.For<IOpenInIdeFailureInfoBar>();
+        var toolWindowService = Substitute.For<IToolWindowService>();
+        
+        new OpenInIdeNotification(infoBar, toolWindowService).UnableToOpenFile(filePath, toolWindowId);
+        
+        VerifyToolWindowOpened(toolWindowService, toolWindowId);
+        VerifyInfoBarShown(infoBar, "Open in IDE. Could not open File: file/path/123", toolWindowId);
+    }
+    
+    [TestMethod]
+    public void InvalidConfiguration_ShowsMessageBox()
+    {
+        var reason = "reason 123";
+        var infoBar = Substitute.For<IOpenInIdeFailureInfoBar>();
+        var toolWindowId = Guid.NewGuid();
+        var toolWindowService = Substitute.For<IToolWindowService>();
+        
+        new OpenInIdeNotification(infoBar, toolWindowService).InvalidRequest(reason, toolWindowId);
+        
+        VerifyToolWindowOpened(toolWindowService, toolWindowId);
+        VerifyInfoBarShown(infoBar, $"Unable to process Open in IDE request. Reason: reason 123", toolWindowId);
+    }
+
+    private void VerifyToolWindowOpened(IToolWindowService toolWindowService, Guid toolWindowId) =>
+        toolWindowService.Show(toolWindowId);
+    
+    private void VerifyInfoBarShown(IOpenInIdeFailureInfoBar infoBar, string message, Guid toolWindowId) =>
+        infoBar.Received(1).ShowAsync(toolWindowId, message);
+}
---
+++ src/IssueViz/OpenInIde/OpenInIdeNotification.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.ComponentModel.Composition;
+using System.Windows;
+using Microsoft.VisualStudio.Threading;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.Infrastructure.VS;
+
+namespace SonarLint.VisualStudio.IssueVisualization.OpenInIde;
+
+internal interface IOpenInIdeNotification
+{
+    void UnableToLocateIssue(string filePath, Guid toolWindowId);
+    void UnableToOpenFile(string filePath, Guid toolWindowId);
+    void InvalidRequest(string reason, Guid toolWindowId);
+    void Clear();
+}
+
+[Export(typeof(IOpenInIdeNotification))]
+[PartCreationPolicy(CreationPolicy.Shared)]
+internal class OpenInIdeNotification : IOpenInIdeNotification
+{
+    private readonly IToolWindowService toolWindowService;
+    private readonly IOpenInIdeFailureInfoBar infoBar;
+
+    [ImportingConstructor]
+    public OpenInIdeNotification(IOpenInIdeFailureInfoBar infoBar, IToolWindowService toolWindowService)
+    {
+        this.toolWindowService = toolWindowService;
+        this.infoBar = infoBar;
+    }
+
+    public void UnableToLocateIssue(string filePath, Guid toolWindowId) => 
+        Show(string.Format(OpenInIdeResources.Notification_UnableToLocateIssue, filePath), toolWindowId);
+
+    public void UnableToOpenFile(string filePath, Guid toolWindowId) => 
+        Show(string.Format(OpenInIdeResources.Notification_UnableToOpenFile, filePath), toolWindowId);
+    
+    public void InvalidRequest(string reason, Guid toolWindowId) => 
+        Show(string.Format(OpenInIdeResources.Notification_InvalidConfiguration, reason), toolWindowId);
+
+    public void Clear()
+    {
+        infoBar.ClearAsync().Forget();
+    }
+
+    private void Show(string message, Guid toolWindowId)
+    {
+        toolWindowService.Show(toolWindowId);
+        infoBar.ShowAsync(toolWindowId, message).Forget();
+    }
+}
