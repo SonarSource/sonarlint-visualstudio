4be2cd6687cf0d7f98d81003a49efd3dba68df64
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2024-05-30 10:40:53 +0200

    Change BouncyCastle (deprecated) to BouncyCastle.Cryptography 2.4.0 (#5462)
    Fixes #5457

--- src/SonarQube.Client/ChecksumCalculator.cs
+++ src/SonarQube.Client/ChecksumCalculator.cs
@@ -18,7 +18,6 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using System;
 using System.Text;
 using System.Text.RegularExpressions;
 using Org.BouncyCastle.Crypto.Digests;
