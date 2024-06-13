3ae140608bd697518fcf5f5d26d740d6801ae24a
Uğraş Ergün <96827714+ugras-ergun-sonarsource@users.noreply.github.com>
2024-06-13 10:07:17 +0200

    SLVS-1177 - Implement RaiseIssuesListener (#5488)

---
+++ src/SLCore.Listeners.UnitTests/Implementation/AnalysisListenerTests.cs
@@ -0,0 +1,204 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using SonarLint.VisualStudio.Core.Analysis;
+using SonarLint.VisualStudio.SLCore.Common.Models;
+using SonarLint.VisualStudio.SLCore.Core;
+using SonarLint.VisualStudio.SLCore.Listener.Analysis;
+using SonarLint.VisualStudio.SLCore.Listener.Analysis.Models;
+using Language = SonarLint.VisualStudio.Core.Language;
+
+namespace SonarLint.VisualStudio.SLCore.Listeners.UnitTests.Implementation;
+
+[TestClass]
+public class AnalysisListenerTests
+{
+    [TestMethod]
+    public void MefCtor_CheckIsExported()
+    {
+        MefTestHelpers.CheckTypeCanBeImported<AnalysisListener, ISLCoreListener>
+            (MefTestHelpers.CreateExport<IAnalysisService>()
+            , MefTestHelpers.CreateExport<IRaiseIssueParamsToAnalysisIssueConverter>()
+            , MefTestHelpers.CreateExport<IAnalysisStatusNotifierFactory>());
+    }
+
+    [TestMethod]
+    public void MefCtor_CheckIsSingleton()
+    {
+        MefTestHelpers.CheckIsSingletonMefComponent<AnalysisListener>();
+    }
+
+    [TestMethod]
+    public void DidChangeAnalysisReadiness_DoesNothing()
+    {
+        var testSubject = CreateTestSubject();
+
+        var result = testSubject.DidChangeAnalysisReadinessAsync(new DidChangeAnalysisReadinessParams(new List<string> { "id" }, true));
+
+        result.Status.Should().Be(TaskStatus.RanToCompletion);
+    }
+
+    [TestMethod]
+    public void RaiseIssues_AnalysisIDisNull_Ignores()
+    {
+        var raiseIssuesParams = new RaiseIssuesParams("CONFIGURATION_ID", new Dictionary<FileUri, List<RaisedIssueDto>>(), false, null);
+
+        var analysisService = Substitute.For<IAnalysisService>();
+        var raiseIssueParamsToAnalysisIssueConverter = Substitute.For<IRaiseIssueParamsToAnalysisIssueConverter>();
+
+        var testSubject = CreateTestSubject(analysisService, raiseIssueParamsToAnalysisIssueConverter);
+
+        testSubject.RaiseIssues(raiseIssuesParams);
+
+        analysisService.DidNotReceive().PublishIssues(Arg.Any<string>(), Arg.Any<Guid>(), Arg.Any<IEnumerable<IAnalysisIssue>>());
+        raiseIssueParamsToAnalysisIssueConverter.DidNotReceive().GetAnalysisIssues(Arg.Any<RaiseIssuesParams>());
+    }
+
+    [TestMethod]
+    public void RaiseIssues_NoIssues_Ignores()
+    {
+        var issuesByFileUri = new Dictionary<FileUri, List<RaisedIssueDto>> { { new FileUri("file://C:/somefile"), new List<RaisedIssueDto>() } };
+
+        var raiseIssuesParams = new RaiseIssuesParams("CONFIGURATION_ID", issuesByFileUri, false, Guid.NewGuid());
+
+        var analysisService = Substitute.For<IAnalysisService>();
+        var raiseIssueParamsToAnalysisIssueConverter = CreateConverter(raiseIssuesParams, Array.Empty<IAnalysisIssue>());
+
+        var testSubject = CreateTestSubject(analysisService, raiseIssueParamsToAnalysisIssueConverter);
+
+        testSubject.RaiseIssues(raiseIssuesParams);
+
+        analysisService.DidNotReceive().PublishIssues(Arg.Any<string>(), Arg.Any<Guid>(), Arg.Any<IEnumerable<IAnalysisIssue>>());
+        raiseIssueParamsToAnalysisIssueConverter.Received(1).GetAnalysisIssues(raiseIssuesParams);
+    }
+
+    [TestMethod]
+    public void RaiseIssues_NoSupportedLanguages_Ignores()
+    {
+        var issue1 = CreateIssue("csharpsquid:S100");
+        var issue2 = CreateIssue("csharpsquid:S101");
+        var issues = new[] { issue1, issue2 };
+
+        var issuesByFileUri = new Dictionary<FileUri, List<RaisedIssueDto>> { { new FileUri("file://C:/somefile"), new List<RaisedIssueDto>() } };
+
+        var raiseIssuesParams = new RaiseIssuesParams("CONFIGURATION_ID", issuesByFileUri, false, Guid.NewGuid());
+
+        var analysisService = Substitute.For<IAnalysisService>();
+        IRaiseIssueParamsToAnalysisIssueConverter raiseIssueParamsToAnalysisIssueConverter = CreateConverter(raiseIssuesParams, issues);
+
+        var testSubject = CreateTestSubject(analysisService, raiseIssueParamsToAnalysisIssueConverter);
+
+        testSubject.RaiseIssues(raiseIssuesParams);
+
+        analysisService.DidNotReceive().PublishIssues(Arg.Any<string>(), Arg.Any<Guid>(), Arg.Any<IEnumerable<IAnalysisIssue>>());
+        raiseIssueParamsToAnalysisIssueConverter.Received(1).GetAnalysisIssues(raiseIssuesParams);
+    }
+
+    [TestMethod]
+    public void RaiseIssues_HasMoreThanOneFileUri_Throws()
+    {
+        var issuesByFileUri = new Dictionary<FileUri, List<RaisedIssueDto>> { { new FileUri("file://C:/somefile"), new List<RaisedIssueDto>() }, { new FileUri("file://C:/someOtherfile"), new List<RaisedIssueDto>() } };
+        var testSubject = CreateTestSubject();
+
+        var act = () => testSubject.RaiseIssues(new RaiseIssuesParams("CONFIGURATION_ID", issuesByFileUri, false, Guid.NewGuid()));
+
+        act.Should().Throw<InvalidOperationException>();
+    }
+
+    [TestMethod]
+    public void RaiseIssues_HasNoFileUri_Throws()
+    {
+        var testSubject = CreateTestSubject();
+
+        var act = () => testSubject.RaiseIssues(new RaiseIssuesParams("CONFIGURATION_ID", new Dictionary<FileUri, List<RaisedIssueDto>>(), false, Guid.NewGuid()));
+
+        act.Should().Throw<InvalidOperationException>();
+    }
+
+    [DataRow(true)]
+    [DataRow(false)]
+    [TestMethod]
+    public void RaiseIssues_HasIssues_PublishIssues(bool isIntermediatePublication)
+    {
+        var guid = Guid.NewGuid();
+        var issue1 = CreateIssue("csharpsquid:S100");
+        var issue2 = CreateIssue("javascript:S101");
+        var issue3 = CreateIssue("secrets:S1012");
+        var issues = new[] { issue1, issue2, issue3 };
+
+        var issuesByFileUri = new Dictionary<FileUri, List<RaisedIssueDto>> { { new FileUri("file://C:/somefile"), new List<RaisedIssueDto>() } };
+
+        var raiseIssuesParams = new RaiseIssuesParams("CONFIGURATION_ID", issuesByFileUri, isIntermediatePublication, guid);
+
+        var analysisService = Substitute.For<IAnalysisService>();
+        IRaiseIssueParamsToAnalysisIssueConverter raiseIssueParamsToAnalysisIssueConverter = CreateConverter(raiseIssuesParams, issues);
+
+        var analysisStatusNotifierFactory = CreateAnalysisStatusNotifierFactory(out var analysisStatusNotifier);
+
+        var testSubject = CreateTestSubject(analysisService, raiseIssueParamsToAnalysisIssueConverter, analysisStatusNotifierFactory, new List<Language> { Language.Secrets, Language.CSharp });
+
+        testSubject.RaiseIssues(raiseIssuesParams);
+
+        analysisService.Received(1).PublishIssues("C:\\somefile", guid, Arg.Is<IEnumerable<IAnalysisIssue>>(
+           publishedIssues => publishedIssues.Count() == 2 && publishedIssues.ElementAt(0) == issue1 && publishedIssues.ElementAt(1) == issue3));
+        raiseIssueParamsToAnalysisIssueConverter.Received(1).GetAnalysisIssues(raiseIssuesParams);
+
+        if (isIntermediatePublication)
+        {
+            analysisStatusNotifierFactory.DidNotReceive().Create(Arg.Any<string>(), Arg.Any<string>());
+        }
+        else
+        {
+            analysisStatusNotifierFactory.Received(1).Create("SLCoreAnalyzer", "C:\\somefile");
+            analysisStatusNotifier.Received(1).AnalysisFinished(2, TimeSpan.Zero);
+        }
+    }
+
+    private static IRaiseIssueParamsToAnalysisIssueConverter CreateConverter(RaiseIssuesParams raiseIssuesParams, IAnalysisIssue[] issues)
+    {
+        var raiseIssueParamsToAnalysisIssueConverter = Substitute.For<IRaiseIssueParamsToAnalysisIssueConverter>();
+        raiseIssueParamsToAnalysisIssueConverter.GetAnalysisIssues(raiseIssuesParams).Returns(issues);
+        return raiseIssueParamsToAnalysisIssueConverter;
+    }
+
+    private AnalysisListener CreateTestSubject(IAnalysisService analysisService = null,
+        IRaiseIssueParamsToAnalysisIssueConverter raiseIssueParamsToAnalysisIssueConverter = null,
+        IAnalysisStatusNotifierFactory analysisStatusNotifierFactory = null,
+        IEnumerable<Language> languages = null)
+        => new AnalysisListener(analysisService ?? Substitute.For<IAnalysisService>(),
+            raiseIssueParamsToAnalysisIssueConverter ?? Substitute.For<IRaiseIssueParamsToAnalysisIssueConverter>(),
+            analysisStatusNotifierFactory ?? CreateAnalysisStatusNotifierFactory(out _),
+            languages ?? new List<Language>() { Language.Secrets });
+
+    private IAnalysisStatusNotifierFactory CreateAnalysisStatusNotifierFactory(out IAnalysisStatusNotifier analysisStatusNotifier)
+    {
+        var analysisStatusNotifierFactory = Substitute.For<IAnalysisStatusNotifierFactory>();
+        analysisStatusNotifier = Substitute.For<IAnalysisStatusNotifier>();
+        analysisStatusNotifierFactory.Create(Arg.Any<string>(), Arg.Any<string>()).Returns(analysisStatusNotifier);
+        return analysisStatusNotifierFactory;
+    }
+
+    private static IAnalysisIssue CreateIssue(string ruleKey)
+    {
+        var issue1 = Substitute.For<IAnalysisIssue>();
+        issue1.RuleKey.Returns(ruleKey);
+        return issue1;
+    }
+}
--- src/SLCore.Listeners/Implementation/AnalysisListener.cs
+++ src/SLCore.Listeners/Implementation/AnalysisListener.cs
@@ -18,8 +18,9 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-
 using System.ComponentModel.Composition;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.Core.Analysis;
 using SonarLint.VisualStudio.SLCore.Core;
 using SonarLint.VisualStudio.SLCore.Listener.Analysis;
 
@@ -29,6 +30,26 @@ namespace SonarLint.VisualStudio.SLCore.Listeners.Implementation;
 [PartCreationPolicy(CreationPolicy.Shared)]
 internal class AnalysisListener : IAnalysisListener
 {
+    private readonly IEnumerable<Language> supportedLanguages;
+
+    private readonly IAnalysisService analysisService;
+    private readonly IRaiseIssueParamsToAnalysisIssueConverter raiseIssueParamsToAnalysisIssueConverter;
+    private readonly IAnalysisStatusNotifierFactory analysisStatusNotifierFactory;
+
+    [ImportingConstructor]
+    public AnalysisListener(IAnalysisService analysisService, IRaiseIssueParamsToAnalysisIssueConverter raiseIssueParamsToAnalysisIssueConverter, IAnalysisStatusNotifierFactory analysisStatusNotifierFactory)
+       : this(analysisService, raiseIssueParamsToAnalysisIssueConverter, analysisStatusNotifierFactory, new List<Language>() { Language.Secrets })
+    {
+    }
+
+    internal AnalysisListener(IAnalysisService analysisService, IRaiseIssueParamsToAnalysisIssueConverter raiseIssueParamsToAnalysisIssueConverter, IAnalysisStatusNotifierFactory analysisStatusNotifierFactory, IEnumerable<Language> supportedLanguages)
+    {
+        this.supportedLanguages = supportedLanguages;
+        this.analysisService = analysisService;
+        this.raiseIssueParamsToAnalysisIssueConverter = raiseIssueParamsToAnalysisIssueConverter;
+        this.analysisStatusNotifierFactory = analysisStatusNotifierFactory;
+    }
+
     public Task DidChangeAnalysisReadinessAsync(DidChangeAnalysisReadinessParams parameters)
     {
         return Task.CompletedTask;
@@ -36,11 +57,37 @@ internal class AnalysisListener : IAnalysisListener
 
     public void RaiseIssues(RaiseIssuesParams parameters)
     {
-        throw new NotImplementedException();
+        if (parameters.analysisId.HasValue)
+        {
+            var filepath = parameters.issuesByFileUri.Single().Key.LocalPath;
+
+            var issues = GetSupportedLanguageIssues(raiseIssueParamsToAnalysisIssueConverter.GetAnalysisIssues(parameters));
+
+            if (issues.Any())
+            {
+                analysisService.PublishIssues(filepath, parameters.analysisId.Value, issues);
+            }
+            if (!parameters.isIntermediatePublication)
+            {
+                var analysisStatusNotifier = analysisStatusNotifierFactory.Create("SLCoreAnalyzer", filepath);
+                analysisStatusNotifier.AnalysisFinished(issues.Count(), TimeSpan.Zero);
+            }
+        }
+    }
+
+    private IEnumerable<IAnalysisIssue> GetSupportedLanguageIssues(IEnumerable<IAnalysisIssue> issues)
+    {
+        return issues.Where(i => IsSupportedLanguage(i.RuleKey));
+    }
+
+    private bool IsSupportedLanguage(string ruleKey)
+    {
+        return supportedLanguages.Any(l => ruleKey.StartsWith(Language.GetSonarRepoKeyFromLanguage(l)));
     }
 
     public void RaiseHotspots(RaiseHotspotsParams parameters)
     {
-        throw new NotImplementedException();
+        // no-op: We don't have hotspots in Secrets
+        // it will be implemented when we support a language that have hotspots
     }
 }
