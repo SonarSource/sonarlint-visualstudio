0233ee767c8af7420a351ecb451aad1f48c84c5d
Vasileios Naskos <vasileios.naskos@sonarsource.com>
2024-06-19 18:14:50 +0200

    Expand DataTestMethod to separate TestMethods

--- src/SLCore.UnitTests/Common/Helpers/ClientFileDtoFactoryTests.cs
+++ src/SLCore.UnitTests/Common/Helpers/ClientFileDtoFactoryTests.cs
@@ -19,6 +19,7 @@
  */
 
 using SonarLint.VisualStudio.SLCore.Common.Helpers;
+using SonarLint.VisualStudio.SLCore.Common.Models;
 using SonarLint.VisualStudio.SLCore.Listener.Files.Models;
 
 namespace SonarLint.VisualStudio.SLCore.UnitTests.Common.Helpers;
@@ -32,30 +33,55 @@ public class ClientFileDtoFactoryTests
         MefTestHelpers.CheckIsSingletonMefComponent<ClientFileDtoFactory>();
     }
     
-    [DataTestMethod]
-    [DataRow("C:\\Code\\Project\\File1.js", "C:\\", "file:///C:/Code/Project/File1.js", "C:\\Code\\Project\\File1.js", "Code\\Project\\File1.js")]
-    [DataRow("C:\\привет\\project\\file1.js", "C:\\привет\\", "file:///C:/привет/project/file1.js", "C:\\привет\\project\\file1.js", "project\\file1.js")] // supports localized
-    [DataRow("\\\\servername\\work\\project\\file1.js", "\\\\servername\\work\\", "file://servername/work/project/file1.js", "\\\\servername\\work\\project\\file1.js", "project\\file1.js")] // supports UNC
-    [DataRow("C:\\Code\\My Project\\My Favorite File2.js", "C:\\", "file:///C:/Code/My%20Project/My%20Favorite%20File2.js", "C:\\Code\\My Project\\My Favorite File2.js", "Code\\My Project\\My Favorite File2.js")] // supports whitespaces
-    public void Create_ConstructsValidDto(string filePath, string rootPath, string expectedUri, string expectedFsPath, string expectedIdeRelativePath)
+    [TestMethod]
+    public void Create_ConstructsValidDto()
     {
         var testSubject = new ClientFileDtoFactory();
 
-        var result = testSubject.Create(filePath, "CONFIG_SCOPE_ID", rootPath);
+        var result = testSubject.Create("C:\\Code\\Project\\File1.js", "CONFIG_SCOPE_ID", "C:\\");
         
-        ValidateDto(result, expectedUri, expectedFsPath, expectedIdeRelativePath);
+        ValidateDto(result, "C:\\Code\\Project\\File1.js", "Code\\Project\\File1.js");
+    }
+    
+    [TestMethod]
+    public void Create_WithLocalizedPath_ConstructsValidDto()
+    {
+        var testSubject = new ClientFileDtoFactory();
+
+        var result = testSubject.Create("C:\\привет\\project\\file1.js", "CONFIG_SCOPE_ID", "C:\\привет\\");
+        
+        ValidateDto(result,  "C:\\привет\\project\\file1.js", "project\\file1.js");
+    }
+    
+    [TestMethod]
+    public void Create_WithUNCPath_ConstructsValidDto()
+    {
+        var testSubject = new ClientFileDtoFactory();
+
+        var result = testSubject.Create("\\\\servername\\work\\project\\file1.js", "CONFIG_SCOPE_ID", "\\\\servername\\work\\");
+        
+        ValidateDto(result, "\\\\servername\\work\\project\\file1.js", "project\\file1.js");
+    }
+    
+    [TestMethod]
+    public void Create_WithWhitespacesPath_ConstructsValidDto()
+    {
+        var testSubject = new ClientFileDtoFactory();
+
+        var result = testSubject.Create("C:\\Code\\My Project\\My Favorite File2.js", "CONFIG_SCOPE_ID", "C:\\");
+        
+        ValidateDto(result, "C:\\Code\\My Project\\My Favorite File2.js", "Code\\My Project\\My Favorite File2.js");
     }
 
-    private static void ValidateDto(ClientFileDto dto, string uri, string fsPath, string ideRelativePath)
+    private static void ValidateDto(ClientFileDto actual, string expectedFsPath, string expectedIdeRelativePath)
     {
-        dto.uri.ToString().Should().Be(uri);
-        dto.uri.LocalPath.Should().Be(dto.fsPath);
-        dto.fsPath.Should().Be(fsPath);
-        dto.ideRelativePath.Should().Be(ideRelativePath);
-        dto.configScopeId.Should().Be("CONFIG_SCOPE_ID");
-        dto.isTest.Should().BeNull();
-        dto.charset.Should().Be("utf-8");
-        dto.content.Should().BeNull();
+        actual.Should().BeEquivalentTo(new ClientFileDto(
+            new FileUri(expectedFsPath),
+            expectedIdeRelativePath,
+            "CONFIG_SCOPE_ID", 
+            null, 
+            "utf-8", 
+            expectedFsPath));
         
     }
 }
