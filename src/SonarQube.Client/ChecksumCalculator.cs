786167c785c84d5a5dcdad6d79d5b75619a19208
Vasileios Naskos <vasileios.naskos@sonarsource.com>
2024-07-08 12:48:49 +0200

    Revert "Change BouncyCastle (deprecated) to BouncyCastle.Cryptography 2.4.0 (#5462)"

--- src/SonarQube.Client/ChecksumCalculator.cs
+++ src/SonarQube.Client/ChecksumCalculator.cs
@@ -18,6 +18,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+using System;
 using System.Text;
 using System.Text.RegularExpressions;
 using Org.BouncyCastle.Crypto.Digests;
