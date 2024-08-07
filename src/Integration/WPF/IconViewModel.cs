bf1b84ab75d66ea17753cde0541bc97d3f3bdb32
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2024-08-07 16:08:12 +0200

    SLVS-1337 Move ViewModelBase in Core (#5582)

--- src/Integration/WPF/IconViewModel.cs
+++ src/Integration/WPF/IconViewModel.cs
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * SonarLint for Visual Studio
  * Copyright (C) 2016-2024 SonarSource SA
  * mailto:info AT sonarsource DOT com
@@ -18,8 +18,8 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using System.Diagnostics;
 using System.Windows.Controls;
+using SonarLint.VisualStudio.Core.WPF;
 
 namespace SonarLint.VisualStudio.Integration.WPF
 {
