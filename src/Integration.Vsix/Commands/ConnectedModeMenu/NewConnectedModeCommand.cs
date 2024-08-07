07826b4d161281cc5f20ece3c67bda51971e65a0
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2024-08-07 13:52:01 +0200

    SLVS-1327 Add dummy window and menu item (#5579)

---
+++ src/Integration.Vsix/Commands/ConnectedModeMenu/NewConnectedModeCommand.cs
@@ -0,0 +1,15 @@
+﻿using SonarLint.VisualStudio.ConnectedMode.Migration.Wizard;
+
+namespace SonarLint.VisualStudio.Integration.Vsix.Commands.ConnectedModeMenu
+{
+    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+    public class NewConnectedModeCommand : VsCommandBase
+    {
+        internal const int Id = 0x104;
+
+        protected override void InvokeInternal()
+        {
+           new NewConnectedMode().ShowDialog();
+        }
+    }
+}
