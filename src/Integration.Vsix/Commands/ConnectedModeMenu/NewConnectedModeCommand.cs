7bdc0c6f272fcef3c56ab4e0a092cf4eb3d1702c
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2024-08-09 13:14:42 +0200

    SLVS-1330 Create the server selection window for the new connected mode (#5586)

--- src/Integration.Vsix/Commands/ConnectedModeMenu/NewConnectedModeCommand.cs
+++ src/Integration.Vsix/Commands/ConnectedModeMenu/NewConnectedModeCommand.cs
@@ -1,15 +1,22 @@
 ﻿using SonarLint.VisualStudio.ConnectedMode.Migration.Wizard;
+using SonarLint.VisualStudio.Core;
 
 namespace SonarLint.VisualStudio.Integration.Vsix.Commands.ConnectedModeMenu
 {
     [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
     public class NewConnectedModeCommand : VsCommandBase
     {
+        private readonly IBrowserService browserService;
         internal const int Id = 0x104;
+
+        public NewConnectedModeCommand(IBrowserService browserService)
+        {
+            this.browserService = browserService;
+        }
 
         protected override void InvokeInternal()
         {
-           new NewConnectedMode().ShowDialog();
+           new NewConnectedMode(browserService).ShowDialog();
         }
     }
 }
