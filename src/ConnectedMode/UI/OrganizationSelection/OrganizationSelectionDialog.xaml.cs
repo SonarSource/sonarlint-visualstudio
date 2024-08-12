9fb60300e4c16cf5c2eeafd4077f0c74c3914d01
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2024-08-12 16:27:10 +0200

    SLVS-1331 Create Organization Selection dialog (#5589)

---
+++ src/ConnectedMode/UI/OrganizationSelection/OrganizationSelectionDialog.xaml.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Diagnostics.CodeAnalysis;
+using System.Windows;
+
+namespace SonarLint.VisualStudio.ConnectedMode.UI.OrganizationSelection;
+
+[ExcludeFromCodeCoverage]
+public partial class OrganizationSelectionDialog : Window
+{
+    public OrganizationSelectionDialog(IReadOnlyList<OrganizationDisplay> organizations)
+    {
+        ViewModel = new OrganizationSelectionViewModel(organizations);
+        Owner = Application.Current.MainWindow;
+        InitializeComponent();
+    }
+
+    public OrganizationSelectionViewModel ViewModel { get; private set; }
+
+    private void OnSelectFromList(object sender, RoutedEventArgs e)
+    {
+        DialogResult = true;
+        ShowSelectedKey();
+    }
+
+    private void ManualOrganizationSelection(object sender, RoutedEventArgs e)
+    {
+        ViewModel.SelectedOrganization = null;
+        var manualOrganizationSelectionDialog = new ManualOrganizationSelectionDialog();
+        manualOrganizationSelectionDialog.Owner = this;
+        var manualSelection = manualOrganizationSelectionDialog.ShowDialog();
+        ViewModel.SelectedOrganization =new OrganizationDisplay(manualOrganizationSelectionDialog.ViewModel.OrganizationKey, null);
+        if (manualSelection is true)
+        {
+            DialogResult = true;
+            ShowSelectedKey();
+        }
+    }
+
+    private void ShowSelectedKey()
+    {
+        MessageBox.Show(ViewModel.SelectedOrganization.Key);
+    }
+}
+
