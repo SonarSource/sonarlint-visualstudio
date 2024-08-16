81c128346231ddcb000f8f5b529c84cc85114763
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2024-08-16 16:23:45 +0200

    SLVS-1346 Remove window used for development (#5603)

--- src/Integration.Vsix/Commands/ConnectedModeMenu/NewConnectedModeCommand.cs
+++ src/Integration.Vsix/Commands/ConnectedModeMenu/NewConnectedModeCommand.cs
@@ -1,5 +1,27 @@
-﻿using SonarLint.VisualStudio.ConnectedMode.Migration.Wizard;
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Windows;
+using SonarLint.VisualStudio.ConnectedMode.UI.ManageBinding;
 using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.ConnectedMode.UI;
 
 namespace SonarLint.VisualStudio.Integration.Vsix.Commands.ConnectedModeMenu
 {
@@ -16,7 +38,7 @@ namespace SonarLint.VisualStudio.Integration.Vsix.Commands.ConnectedModeMenu
 
         protected override void InvokeInternal()
         {
-           new NewConnectedMode(browserService).ShowDialog();
+           new ManageBindingDialog(browserService, new SolutionInfoModel("Sample Project VS 2022", SolutionType.Solution)).ShowDialog(Application.Current.MainWindow);
         }
     }
 }
