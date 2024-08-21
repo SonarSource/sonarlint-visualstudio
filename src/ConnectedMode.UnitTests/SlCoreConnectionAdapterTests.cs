14edf2d7a965e9f2ddd968e6a93bf77b173bc26a
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2024-08-21 13:06:19 +0200

    SLVS-1374 Add connection validation adapter

---
+++ src/ConnectedMode.UnitTests/SlCoreConnectionAdapterTests.cs
@@ -0,0 +1,194 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.SLCore;
+using SonarLint.VisualStudio.SLCore.Common.Models;
+using SonarLint.VisualStudio.SLCore.Core;
+using SonarLint.VisualStudio.SLCore.Protocol;
+using SonarLint.VisualStudio.SLCore.Service.Connection;
+using SonarLint.VisualStudio.TestInfrastructure;
+
+namespace SonarLint.VisualStudio.ConnectedMode.UnitTests;
+
+[TestClass]
+public class SlCoreConnectionAdapterTests
+{
+    private SlCoreConnectionAdapter testSubject;
+    private ISLCoreServiceProvider slCoreServiceProvider;
+    private IThreadHandling threadHandling;
+    private ILogger logger;
+    private IConnectionConfigurationSLCoreService connectionConfigurationSlCoreService;
+    private ConnectionInfo sonarCloudConnectionInfo;
+    private ConnectionInfo sonarQubeConnectionInfo;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        slCoreServiceProvider = Substitute.For<ISLCoreServiceProvider>();
+        threadHandling = Substitute.For<IThreadHandling>();
+        logger = Substitute.For<ILogger>();
+        connectionConfigurationSlCoreService = Substitute.For<IConnectionConfigurationSLCoreService>();
+        testSubject = new SlCoreConnectionAdapter(slCoreServiceProvider, threadHandling, logger);
+
+        SetupConnection();
+    }
+
+    [TestMethod]
+    public async Task ValidateConnectionAsync_SwitchesToBackgroundThread()
+    {
+        await testSubject.ValidateConnectionAsync(sonarQubeConnectionInfo, "myToken");
+
+        Received.InOrder(() =>
+        {
+            threadHandling.SwitchToBackgroundThread();
+            slCoreServiceProvider.TryGetTransientService(out IConnectionConfigurationSLCoreService _);
+        });
+    }
+
+    [TestMethod]
+    public async Task ValidateConnectionAsync_GettingConnectionConfigurationSLCoreServiceFails_ReturnsUnsuccessfulResponseAndLogs()
+    {
+        slCoreServiceProvider.TryGetTransientService(out IConnectionConfigurationSLCoreService _).Returns(false);
+
+        var response = await testSubject.ValidateConnectionAsync(sonarQubeConnectionInfo, "myToken");
+
+        logger.Received(1).LogVerbose($"[{nameof(IConnectionConfigurationSLCoreService)}] {SLCoreStrings.ServiceProviderNotInitialized}");
+        response.success.Should().BeFalse();
+        response.message.Should().Be(Resources.ValidateCredentials_Fails);
+    }
+
+    [TestMethod]
+    public async Task ValidateConnectionAsync_ConnectionToSonarQubeWithToken_CallsValidateConnectionWithCorrectParams()
+    {
+        var token = "myToken";
+
+        await testSubject.ValidateConnectionAsync(sonarQubeConnectionInfo, token);
+
+        await connectionConfigurationSlCoreService.Received(1)
+            .ValidateConnectionAsync(Arg.Is<ValidateConnectionParams>(x => IsExpectedSonarQubeConnectionParams(x, token)));
+    }
+
+    [TestMethod]
+    public async Task ValidateConnectionAsync_ConnectionToSonarQubeWithCredentials_CallsValidateConnectionWithCorrectParams()
+    {
+        var username = "username";
+        var password = "password";
+
+        await testSubject.ValidateConnectionAsync(sonarQubeConnectionInfo, username, password);
+
+        await connectionConfigurationSlCoreService.Received(1)
+            .ValidateConnectionAsync(Arg.Is<ValidateConnectionParams>(x => IsExpectedSonarQubeConnectionParams(x, username, password)));
+    }
+
+    [TestMethod]
+    public async Task ValidateConnectionAsync_ConnectionToSonarCloudWithToken_CallsValidateConnectionWithCorrectParams()
+    {
+        var token = "myToken";
+
+        await testSubject.ValidateConnectionAsync(sonarCloudConnectionInfo, token);
+
+        await connectionConfigurationSlCoreService.Received(1)
+            .ValidateConnectionAsync(Arg.Is<ValidateConnectionParams>(x => IsExpectedSonarCloudConnectionParams(x, token)));
+    }
+
+    [TestMethod]
+    public async Task ValidateConnectionAsync_ConnectionToSonarCloudWithCredentials_CallsValidateConnectionWithCorrectParams()
+    {
+        var username = "username";
+        var password = "password";
+
+        await testSubject.ValidateConnectionAsync(sonarCloudConnectionInfo, username, password);
+
+        await connectionConfigurationSlCoreService.Received(1)
+            .ValidateConnectionAsync(Arg.Is<ValidateConnectionParams>(x => IsExpectedSonarCloudConnectionParams(x, username, password)));
+    }
+
+    [TestMethod]
+    [DataRow(true, "success")]
+    [DataRow(false, "failure")]
+    public async Task ValidateConnectionAsync_ReturnsResponseFromSlCore(bool success, string message)
+    {
+        var expectedResponse = new ValidateConnectionResponse(success, message);
+        connectionConfigurationSlCoreService.ValidateConnectionAsync(Arg.Any<ValidateConnectionParams>()).Returns(expectedResponse);
+
+        var response = await testSubject.ValidateConnectionAsync(sonarCloudConnectionInfo, "token");
+
+        response.Should().BeEquivalentTo(expectedResponse);
+    }
+
+    [TestMethod]
+    public async Task ValidateConnectionAsync_SlCoreValidationThrowsException_ReturnsUnsuccessfulResponse()
+    {
+        connectionConfigurationSlCoreService.When(x => x.ValidateConnectionAsync(Arg.Any<ValidateConnectionParams>()))
+            .Do(x => throw new Exception("test"));
+
+        var response = await testSubject.ValidateConnectionAsync(sonarCloudConnectionInfo, "token");
+
+        response.success.Should().BeFalse();
+        response.message.Should().Be(Resources.ValidateCredentials_Fails);
+    }
+
+    private bool IsExpectedSonarQubeConnectionParams(ValidateConnectionParams receivedParams, string token)
+    {
+        var transientSonarQubeDto = receivedParams.transientConnection.Left;
+        return transientSonarQubeDto.serverUrl == sonarQubeConnectionInfo.Id && IsExpectedCredentials(transientSonarQubeDto.credentials, token);
+    }
+
+    private bool IsExpectedSonarQubeConnectionParams(ValidateConnectionParams receivedParams, string username, string password)
+    {
+        var transientSonarQubeDto = receivedParams.transientConnection.Left;
+        return transientSonarQubeDto.serverUrl == sonarQubeConnectionInfo.Id && IsExpectedCredentials(transientSonarQubeDto.credentials, username, password);
+    }
+
+    private static bool IsExpectedCredentials(Either<TokenDto, UsernamePasswordDto> credentials, string token)
+    {
+        return credentials.Left.token == token;
+    }
+
+    private static bool IsExpectedCredentials(Either<TokenDto, UsernamePasswordDto> credentials, string username, string password)
+    {
+        return credentials.Right.username == username && credentials.Right.password == password;
+    }
+
+    private bool IsExpectedSonarCloudConnectionParams(ValidateConnectionParams receivedParams, string token)
+    {
+        var transientSonarCloudDto = receivedParams.transientConnection.Right;
+        return transientSonarCloudDto.organization == sonarCloudConnectionInfo.Id && IsExpectedCredentials(transientSonarCloudDto.credentials, token);
+    }
+
+    private bool IsExpectedSonarCloudConnectionParams(ValidateConnectionParams receivedParams, string username, string password)
+    {
+        var transientSonarCloudDto = receivedParams.transientConnection.Right;
+        return transientSonarCloudDto.organization == sonarCloudConnectionInfo.Id && IsExpectedCredentials(transientSonarCloudDto.credentials, username, password);
+    }
+
+    private void SetupConnection()
+    {
+        sonarCloudConnectionInfo = new ConnectionInfo("myOrg", ConnectionServerType.SonarCloud);
+        sonarQubeConnectionInfo = new ConnectionInfo("http://localhost:9000", ConnectionServerType.SonarQube);
+        threadHandling.SwitchToBackgroundThread().Returns(new NoOpThreadHandler.NoOpAwaitable());
+        slCoreServiceProvider.TryGetTransientService(out IConnectionConfigurationSLCoreService _).Returns(x =>
+        {
+            x[0] = connectionConfigurationSlCoreService;
+            return true;
+        });
+    }
+}
---
+++ src/ConnectedMode/SlCoreConnectionAdapter.cs
@@ -0,0 +1,106 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.ComponentModel.Composition;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.SLCore;
+using SonarLint.VisualStudio.SLCore.Common.Models;
+using SonarLint.VisualStudio.SLCore.Core;
+using SonarLint.VisualStudio.SLCore.Protocol;
+using SonarLint.VisualStudio.SLCore.Service.Connection;
+using SonarLint.VisualStudio.SLCore.Service.Connection.Models;
+
+namespace SonarLint.VisualStudio.ConnectedMode;
+
+public interface ISlCoreConnectionAdapter
+{
+    Task<ValidateConnectionResponse> ValidateConnectionAsync(ConnectionInfo connectionInfo, string token);
+    Task<ValidateConnectionResponse> ValidateConnectionAsync(ConnectionInfo connectionInfo, string username, string password);
+}
+
+[Export(typeof(ISlCoreConnectionAdapter))]
+public class SlCoreConnectionAdapter : ISlCoreConnectionAdapter
+{
+    private readonly ISLCoreServiceProvider serviceProvider;
+    private readonly IThreadHandling threadHandling;
+    private readonly ILogger logger;
+
+    [ImportingConstructor]
+    public SlCoreConnectionAdapter(ISLCoreServiceProvider serviceProvider, IThreadHandling threadHandling, ILogger logger)
+    {
+        this.serviceProvider = serviceProvider;
+        this.threadHandling = threadHandling;
+        this.logger = logger;
+    }
+
+    public async Task<ValidateConnectionResponse> ValidateConnectionAsync(ConnectionInfo connectionInfo, string token)
+    {
+        var validateConnectionParams = GetValidateConnectionParams(connectionInfo, GetEitherForToken(token));
+        return await ValidateConnectionAsync(validateConnectionParams);
+    }
+
+    public async Task<ValidateConnectionResponse> ValidateConnectionAsync(ConnectionInfo connectionInfo, string username, string password)
+    {
+        var validateConnectionParams = GetValidateConnectionParams(connectionInfo, GetEitherForUsernamePassword(username, password));
+        return await ValidateConnectionAsync(validateConnectionParams);
+    }
+
+    private async Task<ValidateConnectionResponse> ValidateConnectionAsync(ValidateConnectionParams validateConnectionParams)
+    {
+        await threadHandling.SwitchToBackgroundThread();
+
+        if (!serviceProvider.TryGetTransientService(out IConnectionConfigurationSLCoreService connectionConfigurationSlCoreService))
+        {
+            logger.LogVerbose($"[{nameof(IConnectionConfigurationSLCoreService)}] {SLCoreStrings.ServiceProviderNotInitialized}");
+            return new ValidateConnectionResponse(false, Resources.ValidateCredentials_Fails);
+        }
+
+        try
+        {
+            return await connectionConfigurationSlCoreService.ValidateConnectionAsync(validateConnectionParams);
+        }
+        catch (Exception)
+        {
+            return new ValidateConnectionResponse(false, Resources.ValidateCredentials_Fails);
+        }
+    }
+
+    private static ValidateConnectionParams GetValidateConnectionParams(ConnectionInfo connectionInfo, Either<TokenDto, UsernamePasswordDto> credentials)
+    {
+        return new ValidateConnectionParams(GetTransientConnectionDto(connectionInfo, credentials));
+    }
+
+    private static Either<TransientSonarQubeConnectionDto, TransientSonarCloudConnectionDto> GetTransientConnectionDto(ConnectionInfo connectionInfo, Either<TokenDto, UsernamePasswordDto> credentials)
+    {
+        return connectionInfo.ServerType == ConnectionServerType.SonarQube
+            ? Either<TransientSonarQubeConnectionDto, TransientSonarCloudConnectionDto>.CreateLeft(new TransientSonarQubeConnectionDto(connectionInfo.Id, credentials))
+            : Either<TransientSonarQubeConnectionDto, TransientSonarCloudConnectionDto>.CreateRight(new TransientSonarCloudConnectionDto(connectionInfo.Id, credentials));
+    }
+
+    private static Either<TokenDto, UsernamePasswordDto> GetEitherForToken(string token)
+    {
+        return Either<TokenDto, UsernamePasswordDto>.CreateLeft(new TokenDto(token));
+    }
+
+    private static Either<TokenDto, UsernamePasswordDto> GetEitherForUsernamePassword(string username, string password)
+    {
+        return Either<TokenDto, UsernamePasswordDto>.CreateRight(new UsernamePasswordDto(username, password));
+    }
+}
