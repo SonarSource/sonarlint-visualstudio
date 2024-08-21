81697a4feefbe5923fe34960fcab7c60209da342
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2024-08-21 15:42:40 +0200

    SLVS-1397 Add ServerConnection model (#5614)

---
+++ src/Core.UnitTests/Binding/ServerConnectionTests.cs
@@ -0,0 +1,105 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using SonarLint.VisualStudio.Core.Binding;
+using ICredentials = SonarLint.VisualStudio.Core.Binding.ICredentials;
+
+namespace SonarLint.VisualStudio.Core.UnitTests.Binding;
+
+[TestClass]
+public class ServerConnectionTests
+{
+    private static readonly Uri localhost = new Uri("http://localhost:5000");
+    private static readonly string org = "myOrg";
+    
+    [TestMethod]
+    public void Ctor_SonarCloud_NullOrganization_Throws()
+    {
+        Action act = () => new ServerConnection.SonarCloud(null);
+
+        act.Should().Throw<ArgumentNullException>();
+    }
+    
+    [TestMethod]
+    public void Ctor_SonarCloud_NullSettings_SetDefault()
+    {
+        var sonarCloud = new ServerConnection.SonarCloud(org, null);
+
+        sonarCloud.Settings.Should().BeSameAs(ServerConnection.DefaultSettings);
+    }
+    
+    [TestMethod]
+    public void Ctor_SonarCloud_NullCredentials_SetsNull()
+    {
+        var sonarCloud = new ServerConnection.SonarCloud(org, credentials: null);
+
+        sonarCloud.Credentials.Should().BeNull();
+    }
+    
+    [TestMethod]
+    public void Ctor_SonarCloud_SetsProperties()
+    {
+        var serverConnectionSettings = new ServerConnectionSettings(false);
+        var credentials = Substitute.For<ICredentials>();
+        var sonarCloud = new ServerConnection.SonarCloud(org, serverConnectionSettings, credentials);
+
+        sonarCloud.Id.Should().BeSameAs(org);
+        sonarCloud.OrganizationKey.Should().BeSameAs(org);
+        sonarCloud.Settings.Should().BeSameAs(serverConnectionSettings);
+        sonarCloud.Credentials.Should().BeSameAs(credentials);
+    }
+    
+    [TestMethod]
+    public void Ctor_SonarQube_NullUri_Throws()
+    {
+        Action act = () => new ServerConnection.SonarQube(null);
+
+        act.Should().Throw<ArgumentNullException>();
+    }
+    
+    [TestMethod]
+    public void Ctor_SonarQube_NullSettings_SetDefault()
+    {
+        var sonarQube = new ServerConnection.SonarQube(localhost, null);
+
+        sonarQube.Settings.Should().BeSameAs(ServerConnection.DefaultSettings);
+    }
+    
+    [TestMethod]
+    public void Ctor_SonarQube_NullCredentials_SetsNull()
+    {
+        var sonarQube = new ServerConnection.SonarQube(localhost, credentials: null);
+
+        sonarQube.Credentials.Should().BeNull();
+    }
+    
+    [TestMethod]
+    public void Ctor_SonarQube_SetsProperties()
+    {
+        var serverConnectionSettings = new ServerConnectionSettings(false);
+        var credentials = Substitute.For<ICredentials>();
+        var sonarQube = new ServerConnection.SonarQube(localhost, serverConnectionSettings, credentials);
+
+        sonarQube.Id.Should().Be(localhost.ToString());
+        sonarQube.ServerUri.Should().BeSameAs(localhost);
+        sonarQube.Settings.Should().BeSameAs(serverConnectionSettings);
+        sonarQube.Credentials.Should().BeSameAs(credentials);
+    }
+}
---
+++ src/Core/Binding/ServerConnection.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace SonarLint.VisualStudio.Core.Binding;
+
+public abstract class ServerConnection
+{
+    internal static readonly ServerConnectionSettings DefaultSettings = new(true);
+    
+    public string Id { get; }
+    public ServerConnectionSettings Settings { get; }
+    public ICredentials Credentials { get; set; }
+
+    private ServerConnection(string id, ServerConnectionSettings settings = null, ICredentials credentials = null)
+    {
+        Id = id ?? throw new ArgumentNullException(nameof(id));
+        Settings = settings ?? DefaultSettings;
+        Credentials = credentials;
+    }
+    
+    public sealed class SonarCloud(string organizationKey, ServerConnectionSettings settings = null, ICredentials credentials = null)
+        : ServerConnection(organizationKey, settings, credentials)
+    {
+        public string OrganizationKey { get; } = organizationKey;
+    }
+    
+    public sealed class SonarQube(Uri serverUri, ServerConnectionSettings settings = null, ICredentials credentials = null)
+        : ServerConnection(serverUri?.ToString(), settings, credentials)
+    {
+        public Uri ServerUri { get; } = serverUri;
+    }
+}
