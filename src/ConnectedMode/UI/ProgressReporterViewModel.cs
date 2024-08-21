acb9ceb16714e9cdf736e0ac9cc093c4cb29c2ea
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2024-08-21 17:08:28 +0200

    SLVS-1413 Add component to show progress and warnings.

---
+++ src/ConnectedMode/UI/ProgressReporterViewModel.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using SonarLint.VisualStudio.Core.WPF;
+
+namespace SonarLint.VisualStudio.ConnectedMode.UI;
+
+public class ProgressReporterViewModel : ViewModelBase
+{
+    private string progressStatus;
+    private string warning;
+
+    public string ProgressStatus
+    {
+        get => progressStatus;
+        set
+        {
+            progressStatus = value;
+            RaisePropertyChanged();
+            RaisePropertyChanged(nameof(IsOperationInProgress));
+        }
+    }
+
+    public string Warning
+    {
+        get => warning;
+        set
+        {
+            warning = value;
+            RaisePropertyChanged();
+            RaisePropertyChanged(nameof(HasWarning));
+        }
+    }
+
+    public bool IsOperationInProgress => !string.IsNullOrEmpty(ProgressStatus);
+    public bool HasWarning => !string.IsNullOrEmpty(Warning);
+}
