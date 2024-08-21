c67814e69961670427daaa3c8ec7e2b7fdab91c0
Vasileios Naskos <168648790+vnaskos-sonar@users.noreply.github.com>
2024-08-21 12:20:17 +0200

    SLVS-1409 Add fuzzySearchProjects & getAllProjects to ConnectionRpcService (#5612)

---
+++ src/SLCore.UnitTests/Service/Connection/FuzzySearchProjectsParamsTests.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using Newtonsoft.Json;
+using SonarLint.VisualStudio.SLCore.Service.Connection;
+using SonarLint.VisualStudio.SLCore.Service.Connection.Models;
+
+namespace SonarLint.VisualStudio.SLCore.UnitTests.Service.Connection;
+
+[TestClass]
+public class FuzzySearchProjectsParamsTests
+{
+    [TestMethod]
+    public void Serialize_AsExpected()
+    {
+        var testSubject = new FuzzySearchProjectsParams("sq-connection", "myproj");
+
+        const string expectedString = """
+                                      {
+                                        "connectionId": "sq-connection",
+                                        "searchText": "myproj"
+                                      }
+                                      """;
+
+        var serializedString = JsonConvert.SerializeObject(testSubject, Formatting.Indented);
+
+        serializedString.Should().Be(expectedString);
+    }
+}
---
+++ src/SLCore.UnitTests/Service/Connection/GetAllProjectsResponseTests.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using Newtonsoft.Json;
+using SonarLint.VisualStudio.SLCore.Service.Connection;
+using SonarLint.VisualStudio.SLCore.Service.Connection.Models;
+
+namespace SonarLint.VisualStudio.SLCore.UnitTests.Service.Connection;
+
+[TestClass]
+public class GetAllProjectsResponseTests
+{
+    [TestMethod]
+    public void Serialize_AsExpected()
+    {
+        var sonarProjects = new List<SonarProjectDto>
+        {
+            new("proj-key-1", "Project 1"),
+            new("proj-key-2", "Project 2"),
+        };
+
+        var testSubject = new GetAllProjectsResponse(sonarProjects);
+
+        const string expectedString = """
+                                      {
+                                        "sonarProjects": [
+                                          {
+                                            "key": "proj-key-1",
+                                            "name": "Project 1"
+                                          },
+                                          {
+                                            "key": "proj-key-2",
+                                            "name": "Project 2"
+                                          }
+                                        ]
+                                      }
+                                      """;
+
+        var serializedString = JsonConvert.SerializeObject(testSubject, Formatting.Indented);
+
+        serializedString.Should().Be(expectedString);
+    }
+}
