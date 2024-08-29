078f6f11313723045357f843088c1a9f701db513
Jayadeep Kinavoor Madam <jayadeep.kinavoormadam@sonarsource.com>
2024-08-29 15:02:02 +0000

    BUILD-6086: build test artifact for release
    Signed-off-by: Jayadeep Kinavoor Madam <jayadeep.kinavoormadam@sonarsource.com>

--- src/AssemblyInfo.Shared.cs
+++ src/AssemblyInfo.Shared.cs
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyInformationalVersion("Version:8.3.0.0 Branch:not-set Sha1:not-set Configuration:not-set")]
 [assembly: AssemblyConfiguration("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyProduct("SonarLint for Visual Studio")]
+[assembly: AssemblyProduct("SonarLint for Visual Studio Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyCompany("SonarSource")]
