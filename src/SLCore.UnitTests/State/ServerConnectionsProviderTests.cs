5f25a1c3f0230b1f6746671e559086449993f9eb
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2024-09-30 12:15:20 +0200

    SLVS-1474  Update ServerConnectionsProvider to use the ServerConnectionsRepository instead of the ISolutionBindingRepository.
    Now we have a separate file for storing the server connections, so we no longer need to look at the bindings.
    
    The ServerConnectionsProvider now works as an adapter: converts the ServerConnection model to the DTO expected by slCore.

--- src/SLCore.UnitTests/State/ServerConnectionsProviderTests.cs
+++ src/SLCore.UnitTests/State/ServerConnectionsProviderTests.cs
@@ -19,7 +19,6 @@
  */
 
 using SonarLint.VisualStudio.Core.Binding;
-using SonarLint.VisualStudio.SLCore.Common.Helpers;
 using SonarLint.VisualStudio.SLCore.Service.Connection.Models;
 using SonarLint.VisualStudio.SLCore.State;
 
@@ -32,8 +31,7 @@ public class ServerConnectionsProviderTests
     public void MefCtor_CheckIsExported()
     {
         MefTestHelpers.CheckTypeCanBeImported<ServerConnectionsProvider, IServerConnectionsProvider>(
-            MefTestHelpers.CreateExport<ISolutionBindingRepository>(),
-            MefTestHelpers.CreateExport<IConnectionIdHelper>());
+            MefTestHelpers.CreateExport<IServerConnectionsRepository>());
     }
 
     [TestMethod]
@@ -45,77 +43,85 @@ public class ServerConnectionsProviderTests
     [TestMethod]
     public void GetServerConnections_CorrectlyReturnsSonarQubeConnection()
     {
-        const string connectionId = "connectionId";
         const string servierUriString = "http://localhost/";
         var serverUri = new Uri(servierUriString);
-        var binding = new BoundServerProject("solution", "project", new ServerConnection.SonarQube(serverUri));
-        var solutionBindingRepository = SetUpBindingRepository(binding);
-        var connectionIdHelper = Substitute.For<IConnectionIdHelper>();
-        connectionIdHelper.GetConnectionIdFromServerConnection(Arg.Is<ServerConnection>(s => s.ServerUri == serverUri)).Returns(connectionId);
-        var testSubject = CreateTestSubject(solutionBindingRepository, connectionIdHelper);
+        var connection = new ServerConnection.SonarQube(serverUri);
+        var serverConnectionsRepository = SetServerConnectionsRepository(succeeded: true, connection);
+        var testSubject = CreateTestSubject(serverConnectionsRepository);
 
         var serverConnections = testSubject.GetServerConnections();
 
         serverConnections.Should().HaveCount(1);
-        serverConnections[connectionId].Should().BeOfType<SonarQubeConnectionConfigurationDto>().Which.serverUrl.Should().Be(servierUriString);
+        serverConnections[connection.Id].Should().BeOfType<SonarQubeConnectionConfigurationDto>().Which.serverUrl.Should().Be(servierUriString);
     }
     
     [TestMethod]
     public void GetServerConnections_CorrectlyReturnsSonarCloudConnection()
     {
-        const string connectionId = "https://sonarcloud.io/organizations/org";
-        var serverUri = new Uri("https://sonarcloud.io/");
         const string organizationKey = "org";
-        var binding = new BoundServerProject("solution", "project", new ServerConnection.SonarCloud(organizationKey));
-        var solutionBindingRepository = SetUpBindingRepository(binding);
-        var connectionIdHelper = Substitute.For<IConnectionIdHelper>();
-        connectionIdHelper.GetConnectionIdFromServerConnection(Arg.Is<ServerConnection>(s => s.ServerUri.Equals(serverUri) && s.Id == connectionId)).Returns(connectionId);
-        var testSubject = CreateTestSubject(solutionBindingRepository, connectionIdHelper);
+        var connection = new ServerConnection.SonarCloud(organizationKey);
+        var serverConnectionsRepository = SetServerConnectionsRepository(succeeded: true, connection);
+        var testSubject = CreateTestSubject(serverConnectionsRepository);
 
         var serverConnections = testSubject.GetServerConnections();
 
         serverConnections.Should().HaveCount(1);
-        serverConnections[connectionId].Should().BeOfType<SonarCloudConnectionConfigurationDto>().Which.organization.Should().Be(organizationKey);
+        serverConnections[connection.Id].Should().BeOfType<SonarCloudConnectionConfigurationDto>().Which.organization.Should().Be(organizationKey);
     }
     
     [TestMethod]
     public void GetServerConnections_CorrectlyHandlesMultipleConnections()
     {
-        var bindingSQ1 = new BoundServerProject("solution1", "project1", new ServerConnection.SonarQube(new Uri("http://localhost/")));
-        var bindingSQ2 = new BoundServerProject("solution2", "project2", new ServerConnection.SonarQube(new Uri("https://next.sonarqube.org/sonarqube/")));
-        var bindingSC = new BoundServerProject("solution3", "project3", new ServerConnection.SonarCloud("myorg"));
-        var solutionBindingRepository = SetUpBindingRepository(bindingSQ1, bindingSQ2, bindingSC);
-        var connectionIdHelper = Substitute.ForPartsOf<ConnectionIdHelper>();
-        var testSubject = CreateTestSubject(solutionBindingRepository, connectionIdHelper);
+        var connectionSQ1 = new ServerConnection.SonarQube(new Uri("http://localhost/"));
+        var connectionSQ2 =new ServerConnection.SonarQube(new Uri("https://next.sonarqube.org/sonarqube/"));
+        var connectionSC = new ServerConnection.SonarCloud("myorg");
+        var serverConnectionsRepository = SetServerConnectionsRepository(succeeded:true, connectionSQ1, connectionSQ2, connectionSC);
+        var testSubject = CreateTestSubject(serverConnectionsRepository);
 
         var serverConnections = testSubject.GetServerConnections();
 
         serverConnections.Should().HaveCount(3);
-        serverConnections["sc|https://sonarcloud.io/organizations/myorg"].Should().BeOfType<SonarCloudConnectionConfigurationDto>();
-        serverConnections["sq|http://localhost/"].Should().BeOfType<SonarQubeConnectionConfigurationDto>();
-        serverConnections["sq|https://next.sonarqube.org/sonarqube/"].Should().BeOfType<SonarQubeConnectionConfigurationDto>();
+        serverConnections["https://sonarcloud.io/organizations/myorg"].Should().BeOfType<SonarCloudConnectionConfigurationDto>();
+        serverConnections["http://localhost/"].Should().BeOfType<SonarQubeConnectionConfigurationDto>();
+        serverConnections["https://next.sonarqube.org/sonarqube/"].Should().BeOfType<SonarQubeConnectionConfigurationDto>();
     }
     
     [TestMethod]
     public void GetServerConnections_CorrectlyHandlesNoConnections()
     {
-        var solutionBindingRepository = SetUpBindingRepository();
-        var connectionIdHelper = Substitute.ForPartsOf<ConnectionIdHelper>();
-        var testSubject = CreateTestSubject(solutionBindingRepository, connectionIdHelper);
+        var serverConnectionsRepository = SetServerConnectionsRepository(succeeded: true);
+        var testSubject = CreateTestSubject(serverConnectionsRepository);
 
         var serverConnections = testSubject.GetServerConnections();
 
         serverConnections.Should().HaveCount(0);
     }
 
-    private static ISolutionBindingRepository SetUpBindingRepository(
-        params BoundServerProject[] bindings)
+    [TestMethod]
+    public void GetServerConnections_ConnectionsCouldNotBeRead_ReturnsNoConnection()
     {
-        var bindingRepository = Substitute.For<ISolutionBindingRepository>();
-        bindingRepository.List().Returns(bindings);
+        var serverConnectionsRepository = SetServerConnectionsRepository(succeeded:false);
+        var testSubject = CreateTestSubject(serverConnectionsRepository);
+
+        var serverConnections = testSubject.GetServerConnections();
+
+        serverConnections.Should().HaveCount(0);
+    }
+
+    private static IServerConnectionsRepository SetServerConnectionsRepository(bool succeeded,
+        params ServerConnection[] serverConnections)
+    {
+        var bindingRepository = Substitute.For<IServerConnectionsRepository>();
+        bindingRepository.TryGetAll(out var _).Returns(callInfo =>
+        {
+            callInfo[0] = serverConnections;
+            return succeeded;
+        });
+
+
         return bindingRepository;
     }
 
-    private static IServerConnectionsProvider CreateTestSubject(ISolutionBindingRepository bindingRepository, IConnectionIdHelper connectionIdHelper) => 
-        new ServerConnectionsProvider(bindingRepository, connectionIdHelper);
+    private static IServerConnectionsProvider CreateTestSubject(IServerConnectionsRepository serverConnectionsRepository) => 
+        new ServerConnectionsProvider(serverConnectionsRepository);
 }
