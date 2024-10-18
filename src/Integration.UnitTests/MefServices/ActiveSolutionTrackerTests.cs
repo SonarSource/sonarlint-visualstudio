572ad82c291d0bb2ba4a5e37a00f1d4e0022ebcd
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2024-10-18 10:27:07 +0200

    SLVS-1524 Add Solution name tracking to ActiveSolutionTracker (#5756)
    [SLVS-1524](https://sonarsource.atlassian.net/browse/SLVS-1524)
    
    Part of SLVS-1496
    
    [SLVS-1524]:
    https://sonarsource.atlassian.net/browse/SLVS-1524?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/Integration.UnitTests/MefServices/ActiveSolutionTrackerTests.cs
+++ src/Integration.UnitTests/MefServices/ActiveSolutionTrackerTests.cs
@@ -18,121 +18,122 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using FluentAssertions;
 using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
 using SonarLint.VisualStudio.Core;
 using SonarLint.VisualStudio.TestInfrastructure;
 
-namespace SonarLint.VisualStudio.Integration.UnitTests
+namespace SonarLint.VisualStudio.Integration.UnitTests.MefServices;
+
+[TestClass]
+public class ActiveSolutionTrackerTests
 {
-    [TestClass]
-    public class ActiveSolutionTrackerTests
+    private ConfigurableServiceProvider serviceProvider;
+    private SolutionMock solutionMock;
+    private ISolutionInfoProvider solutionInfoProvider;
+
+    [TestInitialize]
+    public void TestInitialize()
     {
-        private ConfigurableServiceProvider serviceProvider;
-        private SolutionMock solutionMock;
+        this.serviceProvider = new ConfigurableServiceProvider();
+        this.solutionMock = new SolutionMock();
+        this.serviceProvider.RegisterService(typeof(SVsSolution), this.solutionMock);
+        solutionInfoProvider = Substitute.For<ISolutionInfoProvider>();
+        solutionInfoProvider.GetSolutionName().Returns((string)null);
+    }
 
-        [TestInitialize]
-        public void TestInitialize()
-        {
-            this.serviceProvider = new ConfigurableServiceProvider();
-            this.solutionMock = new SolutionMock();
-            this.serviceProvider.RegisterService(typeof(SVsSolution), this.solutionMock);
-        }
+    [TestMethod]
+    public void ActiveSolutionTracker_Dispose()
+    {
+        // Arrange
+        int counter = 0;
+        var testSubject = CreateTestSubject();
+        testSubject.ActiveSolutionChanged += (o, e) => counter++;
+        testSubject.Dispose();
 
-        [TestMethod]
-        public void ActiveSolutionTracker_Dispose()
-        {
-            // Arrange
-            int counter = 0;
-            var testSubject = CreateTestSubject();
-            testSubject.ActiveSolutionChanged += (o, e) => counter++;
-            testSubject.Dispose();
+        // Act
+        this.solutionMock.SimulateSolutionClose();
+        this.solutionMock.SimulateSolutionOpen();
 
-            // Act
-            this.solutionMock.SimulateSolutionClose();
-            this.solutionMock.SimulateSolutionOpen();
+        // Assert
+        counter.Should().Be(0, nameof(testSubject.ActiveSolutionChanged) + " was not expected to be raised since disposed");
+        solutionInfoProvider.ReceivedCalls().Should().BeEmpty();
+    }
 
-            // Assert
-            counter.Should().Be(0, nameof(testSubject.ActiveSolutionChanged) + " was not expected to be raised since disposed");
-        }
+    [TestMethod]
+    public void ActiveSolutionTracker_RaiseEventOnSolutionOpen()
+    {
+        // Arrange
+        int counter = 0;
+        ActiveSolutionChangedEventArgs args = null;
+        var testSubject = CreateTestSubject();
+        testSubject.ActiveSolutionChanged += (o, e) => { counter++; args = e; };
+        solutionInfoProvider.GetSolutionName().Returns("name123");
 
-        [TestMethod]
-        public void ActiveSolutionTracker_RaiseEventOnSolutionOpen()
-        {
-            // Arrange
-            int counter = 0;
-            bool isSolutionOpenArg = false;
-            var testSubject = CreateTestSubject();
-            testSubject.ActiveSolutionChanged += (o, e) => { counter++; isSolutionOpenArg = e.IsSolutionOpen; };
+        // Act
+        this.solutionMock.SimulateSolutionOpen();
 
-            // Act
-            this.solutionMock.SimulateSolutionOpen();
+        // Assert
+        counter.Should().Be(1, nameof(testSubject.ActiveSolutionChanged) + " was expected to be raised");
+        args.Should().BeEquivalentTo(new ActiveSolutionChangedEventArgs(true, "name123"));
+    }
 
-            // Assert
-            counter.Should().Be(1, nameof(testSubject.ActiveSolutionChanged) + " was expected to be raised");
-            isSolutionOpenArg.Should().BeTrue();
-        }
+    [TestMethod]
+    public void ActiveSolutionTracker_RaiseEventOnSolutionClose()
+    {
+        // Arrange
+        int counter = 0;
+        ActiveSolutionChangedEventArgs args = null;
+        var testSubject = CreateTestSubject();
+        testSubject.ActiveSolutionChanged += (o, e) => { counter++; args = e; };
 
-        [TestMethod]
-        public void ActiveSolutionTracker_RaiseEventOnSolutionClose()
-        {
-            // Arrange
-            int counter = 0;
-            bool isSolutionOpenEventArg = true;
-            var testSubject = CreateTestSubject();
-            testSubject.ActiveSolutionChanged += (o, e) => { counter++; isSolutionOpenEventArg = e.IsSolutionOpen; };
+        // Act
+        this.solutionMock.SimulateSolutionClose();
 
-            // Act
-            this.solutionMock.SimulateSolutionClose();
+        // Assert
+        counter.Should().Be(1, nameof(testSubject.ActiveSolutionChanged) + " was expected to be raised");
+        args.Should().BeEquivalentTo(new ActiveSolutionChangedEventArgs(false, null));
+    }
 
-            // Assert
-            counter.Should().Be(1, nameof(testSubject.ActiveSolutionChanged) + " was expected to be raised");
-            isSolutionOpenEventArg.Should().BeFalse();
-        }
+    [TestMethod]
+    public void ActiveSolutionTracker_RaiseEventOnFolderOpen()
+    {
+        // Arrange
+        int counter = 0;
+        ActiveSolutionChangedEventArgs args = null;
+        var testSubject = CreateTestSubject();
+        testSubject.ActiveSolutionChanged += (o, e) => { counter++; args = e; };
+        solutionInfoProvider.GetSolutionName().Returns("name123");
 
-        [TestMethod]
-        public void ActiveSolutionTracker_RaiseEventOnFolderOpen()
-        {
-            // Arrange
-            int counter = 0;
-            bool isSolutionOpenArg = false;
-            var testSubject = CreateTestSubject();
-            testSubject.ActiveSolutionChanged += (o, e) => { counter++; isSolutionOpenArg = e.IsSolutionOpen; };
+        // Act
+        solutionMock.SimulateFolderOpen();
 
-            // Act
-            solutionMock.SimulateFolderOpen();
+        // Assert
+        counter.Should().Be(1, nameof(testSubject.ActiveSolutionChanged) + " was expected to be raised");
+        args.Should().BeEquivalentTo(new ActiveSolutionChangedEventArgs(true, "name123"));
+    }
 
-            // Assert
-            counter.Should().Be(1, nameof(testSubject.ActiveSolutionChanged) + " was expected to be raised");
-            isSolutionOpenArg.Should().BeTrue();
-        }
+    [TestMethod]
+    [DataRow(true)]
+    [DataRow(false)]
+    public void FolderWorkspaceInitializedEvent_RaiseEventOnProjectOpenedIfFolderWorkspace(bool isFolderWorkspace)
+    {
+        // Arrange
+        int counter = 0;
+        var testSubject = CreateTestSubject(isFolderWorkspace);
+        testSubject.FolderWorkspaceInitialized += (o, e) => { counter++; };
 
-        [TestMethod]
-        [DataRow(true)]
-        [DataRow(false)]
-        public void FolderWorkspaceInitializedEvent_RaiseEventOnProjectOpenedIfFolderWorkspace(bool isFolderWorkspace)
-        {
-            // Arrange
-            int counter = 0;
-            var testSubject = CreateTestSubject(isFolderWorkspace);
-            testSubject.FolderWorkspaceInitialized += (o, e) => { counter++; };
+        // Act
+        this.solutionMock.SimulateProjectOpen(null);
 
-            // Act
-            this.solutionMock.SimulateProjectOpen(null);
+        // Assert
+        counter.Should().Be(isFolderWorkspace ? 1 : 0);
+    }
 
-            // Assert
-            counter.Should().Be(isFolderWorkspace ? 1 : 0);
-        }
-
-        private ActiveSolutionTracker CreateTestSubject(bool isFolderWorkspace = false)
-        {
-            var folderWorkspaceFolder = new Mock<IFolderWorkspaceService>();
-            folderWorkspaceFolder.Setup(x => x.IsFolderWorkspace()).Returns(isFolderWorkspace);
+    private ActiveSolutionTracker CreateTestSubject(bool isFolderWorkspace = false)
+    {
+        var folderWorkspaceFolder = Substitute.For<IFolderWorkspaceService>();
+        folderWorkspaceFolder.IsFolderWorkspace().Returns(isFolderWorkspace);
           
-            return new ActiveSolutionTracker(serviceProvider, folderWorkspaceFolder.Object);
-        }
-
+        return new ActiveSolutionTracker(serviceProvider, folderWorkspaceFolder, solutionInfoProvider);
     }
 }
