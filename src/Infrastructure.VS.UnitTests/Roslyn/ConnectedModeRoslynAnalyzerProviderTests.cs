18630174afcb0cb1b1c1b56dfd099342c317a094
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2024-10-18 09:05:02 +0200

    Add a basic implementation of the IConnectedModeRoslynAnalyzerProvider Part of SLVS-1496

---
+++ src/Infrastructure.VS.UnitTests/Roslyn/ConnectedModeRoslynAnalyzerProviderTests.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using SonarLint.VisualStudio.Core.Binding;
+using SonarLint.VisualStudio.Infrastructure.VS.Roslyn;
+
+namespace SonarLint.VisualStudio.Infrastructure.VS.UnitTests.Roslyn;
+
+[TestClass]
+public class ConnectedModeRoslynAnalyzerProviderTests
+{
+    private ConnectedModeRoslynAnalyzerProvider testSubject;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        testSubject = new ConnectedModeRoslynAnalyzerProvider();
+    }
+
+    [TestMethod]
+    public void MefCtor_CheckIsExported()
+    {
+        MefTestHelpers.CheckTypeCanBeImported<ConnectedModeRoslynAnalyzerProvider, IConnectedModeRoslynAnalyzerProvider>();
+    }
+
+    [TestMethod]
+    public void MefCtor_IsSingleton()
+    {
+        MefTestHelpers.CheckIsSingletonMefComponent<ConnectedModeRoslynAnalyzerProvider>();
+    }
+
+    [TestMethod]
+    public void GetOrNull_ReturnsNull()
+    {
+        var analyzer = testSubject.GetOrNull(new ServerConnection.SonarCloud("myOrg"));
+
+        analyzer.Should().BeNull();
+    }
+}
