09344648c4bf6ec8a358e58ef620f344a194b42b
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2024-10-21 10:20:40 +0200

    SLVS-1517 Implement IEmbeddedAnalyzerProvider (#5757)
    This targets the [SLVS-1514
    PR](https://github.com/SonarSource/sonarlint-visualstudio/pull/5755),
    because the location to the embedded analyzers changed in that PR, so
    the provider should look in the updated location.

---
+++ src/Infrastructure.VS.UnitTests/Roslyn/EmbeddedRoslynAnalyzerProviderTests.cs
@@ -0,0 +1,126 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Immutable;
+using System.IO;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.Infrastructure.VS.Roslyn;
+
+namespace SonarLint.VisualStudio.Infrastructure.VS.UnitTests.Roslyn;
+
+[TestClass]
+public class EmbeddedRoslynAnalyzerProviderTests
+{
+    private const string AnalyzersPath = "C:\\somepath";
+    private readonly IAnalyzerAssemblyLoader analyzerAssemblyLoader = Substitute.For<IAnalyzerAssemblyLoader>();
+    private EmbeddedRoslynAnalyzerProvider testSubject;
+    private IEmbeddedRoslynAnalyzersLocator locator;
+    private IAnalyzerAssemblyLoaderFactory analyzerAssemblyLoaderFactory;
+    private ILogger logger;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        locator = Substitute.For<IEmbeddedRoslynAnalyzersLocator>();
+        analyzerAssemblyLoaderFactory = Substitute.For<IAnalyzerAssemblyLoaderFactory>();
+        logger = Substitute.For<ILogger>();
+
+        testSubject = new EmbeddedRoslynAnalyzerProvider(locator, analyzerAssemblyLoaderFactory, logger);
+        MockServices();
+    }
+
+    [TestMethod]
+    public void MefCtor_CheckIsExported()
+    {
+        MefTestHelpers.CheckTypeCanBeImported<EmbeddedRoslynAnalyzerProvider, IEmbeddedRoslynAnalyzerProvider>(
+            MefTestHelpers.CreateExport<IEmbeddedRoslynAnalyzersLocator>(),
+            MefTestHelpers.CreateExport<ILogger>());
+    }
+
+    [TestMethod]
+    public void MefCtor_IsSingleton()
+    {
+        MefTestHelpers.CheckIsSingletonMefComponent<EmbeddedRoslynAnalyzerProvider>();
+    }
+
+    [TestMethod]
+    public void Get_GetsAnalyzersFromExpectedLocation()
+    {
+        testSubject.Get();
+
+        locator.Received(1).GetAnalyzerFullPaths();
+    }
+
+    [TestMethod]
+    public void Get_AnalyzerFilesExist_ReturnsAnalyzerFileReference()
+    {
+        locator.GetAnalyzerFullPaths().Returns([GetAnalyzerFullPath("analyzer1.dll"), GetAnalyzerFullPath("analyzer2.dll")]);
+
+        var analyzerFileReferences = testSubject.Get();
+
+        analyzerAssemblyLoaderFactory.Received(1).Create();
+        analyzerFileReferences.Should().NotBeNull();
+        analyzerFileReferences.Length.Should().Be(2);
+        ContainsExpectedAnalyzerFileReference(analyzerFileReferences, GetAnalyzerFullPath("analyzer1.dll"));
+        ContainsExpectedAnalyzerFileReference(analyzerFileReferences, GetAnalyzerFullPath("analyzer2.dll"));
+    }
+
+    [TestMethod]
+    public void Get_AnalyzerFilesDoNotExist_ReturnsLogsAndThrows()
+    {
+        locator.GetAnalyzerFullPaths().Returns([]);
+
+        Action act = () => testSubject.Get();
+
+        act.Should().Throw<InvalidOperationException>().WithMessage(Resources.EmbeddedRoslynAnalyzersNotFound);
+        logger.Received(1).LogVerbose(Resources.EmbeddedRoslynAnalyzersNotFound);
+        analyzerAssemblyLoaderFactory.DidNotReceive().Create();
+    }
+
+    [TestMethod]
+    public void Get_CachesAnalyzerFileReferences()
+    {
+        testSubject.Get();
+        testSubject.Get();
+
+        analyzerAssemblyLoaderFactory.Received(1).Create();
+        locator.Received(1).GetAnalyzerFullPaths();
+    }
+
+    private static string GetAnalyzerFullPath(string analyzerFile)
+    {
+        return Path.Combine(AnalyzersPath, analyzerFile);
+    }
+
+    private static void ContainsExpectedAnalyzerFileReference(
+        ImmutableArray<AnalyzerFileReference> analyzerFileReference,
+        string analyzerPath)
+    {
+        analyzerFileReference.Should().Contain(analyzerFile => analyzerFile.FullPath == analyzerPath);
+    }
+
+    private void MockServices()
+    {
+        locator.GetAnalyzerFullPaths().Returns([GetAnalyzerFullPath("analyzer1.dll")]);
+        analyzerAssemblyLoaderFactory.Create().Returns(analyzerAssemblyLoader);
+    }
+}
---
+++ src/Integration.Vsix.UnitTests/EmbeddedAnalyzers/EmbeddedRoslynAnalyzersLocatorTests.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.IO;
+using System.IO.Abstractions;
+using SonarLint.VisualStudio.Infrastructure.VS.Roslyn;
+using SonarLint.VisualStudio.Integration.Vsix.EmbeddedAnalyzers;
+using SonarLint.VisualStudio.Integration.Vsix.Helpers;
+
+namespace SonarLint.VisualStudio.Integration.UnitTests.EmbeddedAnalyzers;
+
+[TestClass]
+public class EmbeddedRoslynAnalyzersLocatorTests
+{
+    private const string PathInsideVsix = "C:\\somePath";
+
+    private EmbeddedRoslynAnalyzersLocator testSubject;
+    private IVsixRootLocator vsixRootLocator;
+    private IFileSystem fileSystem;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        vsixRootLocator = Substitute.For<IVsixRootLocator>();
+        fileSystem = Substitute.For<IFileSystem>();
+        testSubject = new EmbeddedRoslynAnalyzersLocator(vsixRootLocator, fileSystem);
+    }
+
+    [TestMethod]
+    public void MefCtor_CheckIsExported()
+    {
+        MefTestHelpers.CheckTypeCanBeImported<EmbeddedRoslynAnalyzersLocator, IEmbeddedRoslynAnalyzersLocator>(
+            MefTestHelpers.CreateExport<IVsixRootLocator>());
+    }
+
+    [TestMethod]
+    public void MefCtor_IsSingleton()
+    {
+        MefTestHelpers.CheckIsSingletonMefComponent<EmbeddedRoslynAnalyzersLocator>();
+    }
+
+    [TestMethod]
+    public void GetAnalyzerPaths_AnalyzersExists_ReturnsFullPathsToAnalyzers()
+    {
+        string[] expectedPaths =
+        [
+            GetAnalyzerFullPath(PathInsideVsix, "analyzer1.dll"),
+            GetAnalyzerFullPath(PathInsideVsix, "analyzer2.dll")
+        ];
+        fileSystem.Directory.GetFiles(Arg.Any<string>(), Arg.Any<string>()).Returns(expectedPaths);
+
+        var paths = testSubject.GetAnalyzerFullPaths();
+
+        paths.Should().BeEquivalentTo(expectedPaths);
+    }
+
+    [TestMethod]
+    public void GetAnalyzerPaths_SearchesForFilesInsideVsix()
+    {
+        vsixRootLocator.GetVsixRoot().Returns(PathInsideVsix);
+
+        testSubject.GetAnalyzerFullPaths();
+
+        fileSystem.Directory.Received(1).GetFiles(Path.Combine(PathInsideVsix, "EmbeddedRoslynAnalyzers"), "*.dll");
+    }
+
+    private static string GetAnalyzerFullPath(string pathInsideVsix, string analyzerFile)
+    {
+        return Path.Combine(pathInsideVsix, analyzerFile);
+    }
+}
