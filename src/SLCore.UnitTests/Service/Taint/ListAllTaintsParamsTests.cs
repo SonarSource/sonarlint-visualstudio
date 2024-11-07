39e8c31f7a5fc378dd524189b17abe1150c3965e
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2024-11-07 17:37:14 +0100

    SLVS-1590 Add SLCore rpc contracts for taint issues (#5808)

---
+++ src/SLCore.UnitTests/Service/Taint/ListAllTaintsParamsTests.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using Newtonsoft.Json;
+using SonarLint.VisualStudio.SLCore.Service.Taint;
+
+namespace SonarLint.VisualStudio.SLCore.UnitTests.Service.Taint;
+
+[TestClass]
+public class ListAllTaintsParamsTests
+{
+    [DataTestMethod]
+    [DataRow("scope 1", false)]
+    [DataRow("other scope 2", true)]
+    public void Serialized_AsExpected(string configurationScopeId, bool shouldRefresh)
+    {
+        var expected = $$"""{"configurationScopeId":"{{configurationScopeId}}","shouldRefresh":{{shouldRefresh.ToString().ToLower()}}}""";
+
+        var listAllTaintsParams = new ListAllTaintsParams(configurationScopeId, shouldRefresh);
+
+        JsonConvert.SerializeObject(listAllTaintsParams).Should().Be(expected);
+    }
+}
