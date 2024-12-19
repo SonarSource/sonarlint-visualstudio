7255987c66df2dd8cdc785e2016d5b3a00fa45fc
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2024-12-19 14:02:19 +0100

    SLVS-1709 Cleanup CFamily build (#5908)
    [SLVS-1709](https://sonarsource.atlassian.net/browse/SLVS-1709)
    
    
    
    [SLVS-1709]:
    https://sonarsource.atlassian.net/browse/SLVS-1709?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- tools/CleanRebuild.cmd
+++ tools/CleanRebuild.cmd
@@ -2,7 +2,7 @@
 
 @REM %~dp0 gives the directory containing this batch file
 
-(for %%a in ("%~dp0..\src\Integration.Vsix\lib" "%LOCALAPPDATA%\SLVS_Build_DownloadedJars" "%LOCALAPPDATA%\SLVS_CFamily_Build" "%LOCALAPPDATA%\SLVS_TypeScript_Build" "%LOCALAPPDATA%\SLVS_Build_SLOOP" "%LOCALAPPDATA%\SLVS_Build_Dotnet") do rd /s /q "%%~a")
+(for %%a in ("%LOCALAPPDATA%\SLVS_Build_DownloadedJars" "%LOCALAPPDATA%\SLVS_Build_SLOOP" "%LOCALAPPDATA%\SLVS_Build_Dotnet") do rd /s /q "%%~a")
 
 call msbuild.exe %~dp0..\build\DownloadDependencies -t:Rebuild
 call msbuild.exe "%~dp0..\SonarLint.VisualStudio.Integration.sln" -t:Rebuild
