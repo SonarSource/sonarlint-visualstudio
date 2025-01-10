ff78b705f3d51b79d0adf698b9900916f4340b45
Vasileios Naskos <168648790+vnaskos-sonar@users.noreply.github.com>
2025-01-10 14:08:30 +0100

    SLVS-1660 Create integration tests for CFamily (#5933)

---
+++ src/SLCore.IntegrationTests/Resources/CFamilyIssues.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // TODO: This is an issue
+    cout << "Hello CMake." << endl;
+    
+    int* a = NULL; // Some more issues
+    
+    return 0;
+}
