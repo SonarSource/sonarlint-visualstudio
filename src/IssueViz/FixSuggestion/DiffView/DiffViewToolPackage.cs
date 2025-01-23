d2c3b36841edc8d55a8a1369e708a4f01f64b08f
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2025-01-23 10:22:11 +0100

    SLVS-1744 Add a diff view window (#5958)

---
+++ src/IssueViz/FixSuggestion/DiffView/DiffViewToolPackage.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text.Differencing;
+
+namespace SonarLint.VisualStudio.IssueVisualization.FixSuggestion.DiffView
+{
+    [ExcludeFromCodeCoverage]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [ProvideToolWindow(typeof(DiffViewToolWindowPane))]
+    [Guid(PackageGuidString)]
+    public sealed class DiffViewToolPackage : AsyncPackage
+    {
+        public const string PackageGuidString = "DD11524E-BB8A-4BFF-B693-949686EBD9E3";
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress) =>
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+        protected override WindowPane InstantiateToolWindow(Type toolWindowType)
+        {
+            if (toolWindowType != typeof(DiffViewToolWindowPane) || GetService(typeof(SComponentModel)) is not IComponentModel componentModel)
+            {
+                return base.InstantiateToolWindow(toolWindowType);
+            }
+
+            var differenceBufferFactoryService = componentModel.GetService<IDifferenceBufferFactoryService>();
+            var differenceViewerFactoryService = componentModel.GetService<IWpfDifferenceViewerFactoryService>();
+
+            return new DiffViewToolWindowPane(differenceBufferFactoryService, differenceViewerFactoryService);
+        }
+    }
+}
---
+++ src/IssueViz/FixSuggestion/DiffView/DiffViewWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Diagnostics.CodeAnalysis;
+using System.Windows;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Differencing;
+
+namespace SonarLint.VisualStudio.IssueVisualization.FixSuggestion.DiffView;
+
+[ExcludeFromCodeCoverage] // UI, not really unit-testable
+public sealed partial class DiffViewWindow : Window
+{
+    private readonly IDifferenceBufferFactoryService differenceBufferFactoryService;
+    private readonly IWpfDifferenceViewerFactoryService wpfDifferenceViewerFactoryService;
+
+    public DiffViewWindow(
+        IDifferenceBufferFactoryService differenceBufferFactoryService,
+        IWpfDifferenceViewerFactoryService wpfDifferenceViewerFactoryService)
+    {
+        this.differenceBufferFactoryService = differenceBufferFactoryService;
+        this.wpfDifferenceViewerFactoryService = wpfDifferenceViewerFactoryService;
+        InitializeComponent();
+    }
+
+    public void InitializeDifferenceViewer(FixSuggestionDetails fixSuggestionDetails, ITextBuffer before, ITextBuffer after)
+    {
+        Title = BuildTitle(fixSuggestionDetails);
+        var differenceBuffer = differenceBufferFactoryService.CreateDifferenceBuffer(before, after);
+        var differenceViewer = wpfDifferenceViewerFactoryService.CreateDifferenceView(differenceBuffer);
+
+        DiffGrid.Children.Clear();
+        DiffGrid.Children.Add(differenceViewer.VisualElement);
+    }
+
+    private static string BuildTitle(FixSuggestionDetails fixSuggestionDetails) =>
+        string.Format(IssueVisualization.Resources.DiffViewWindow_Title, fixSuggestionDetails.ChangeIndex, fixSuggestionDetails.TotalChangesFixes, fixSuggestionDetails.FileName);
+
+    private void OnAccept(object sender, RoutedEventArgs e) => DialogResult = true;
+
+    private void OnDecline(object sender, RoutedEventArgs e) => DialogResult = false;
+}
