5b0e36b668c4b7c2196b3adb41d170e0d5c3aafc
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2025-02-04 08:57:20 +0100

    SLVS-1825 Add the PluginInfo to the language class.
    Reason: there are multiple sources of truth for the same info and that info is spread in different clases. Additinally, the plugin info is disconnected from the language info.Instead they should be grouped together in one place.

---
+++ src/Core.UnitTests/PluginInfoTests.cs
@@ -0,0 +1,19 @@
+﻿namespace SonarLint.VisualStudio.Core.UnitTests;
+
+[TestClass]
+public class PluginInfoTests
+{
+    [TestMethod]
+    public void PluginInfo_NullPluginKey_ThrowsException()
+    {
+        Action act = () => new PluginInfo(null, "pattern");
+        act.Should().ThrowExactly<ArgumentNullException>().And.ParamName.Should().Be("pluginKey");
+    }
+
+    [TestMethod]
+    public void PluginInfo_NullFilePattern_DoesNotThrow()
+    {
+        Action act = () => new PluginInfo("csharp", null);
+        act.Should().NotThrow<ArgumentNullException>();
+    }
+}
