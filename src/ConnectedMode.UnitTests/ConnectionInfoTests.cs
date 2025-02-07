d4877ee607b40f268211f979970ef1409a4fb5fe
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2025-02-07 15:44:02 +0100

    SLVS-1817 Adapt UI model to support multi-region for SQC (#6002)
    [SLVS-1817](https://sonarsource.atlassian.net/browse/SLVS-1817)
    
    
    
    [SLVS-1817]:
    https://sonarsource.atlassian.net/browse/SLVS-1817?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/ConnectedMode.UnitTests/ConnectionInfoTests.cs
+++ src/ConnectedMode.UnitTests/ConnectionInfoTests.cs
@@ -18,8 +18,6 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using SonarLint.VisualStudio.ConnectedMode.UI.Resources;
-using SonarLint.VisualStudio.Core;
 using SonarLint.VisualStudio.Core.Binding;
 
 namespace SonarLint.VisualStudio.ConnectedMode.UnitTests;
@@ -27,6 +25,32 @@ namespace SonarLint.VisualStudio.ConnectedMode.UnitTests;
 [TestClass]
 public class ConnectionInfoTests
 {
+    [TestMethod]
+    public void Ctor_SonarCloudWithNoRegion_DefaultsToEu()
+    {
+        var connectionInfo = new ConnectionInfo("myOrg", ConnectionServerType.SonarCloud);
+
+        connectionInfo.CloudServerRegion.Should().Be(CloudServerRegion.Eu);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(GetCloudServerRegions), DynamicDataSourceType.Method)]
+    public void Ctor_SonarCloudWithRegion_SetsRegion(CloudServerRegion region)
+    {
+        var connectionInfo = new ConnectionInfo("myOrg", ConnectionServerType.SonarCloud, region);
+
+        connectionInfo.CloudServerRegion.Should().Be(region);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(GetCloudServerRegions), DynamicDataSourceType.Method)]
+    public void Ctor_SonarQubeWithRegion_IgnoresRegion(CloudServerRegion region)
+    {
+        var connectionInfo = new ConnectionInfo("myOrg", ConnectionServerType.SonarQube, region);
+
+        connectionInfo.CloudServerRegion.Should().BeNull();
+    }
+
     [TestMethod]
     public void FromServerConnection_ShouldReturnConnectionInfoWithSameId()
     {
@@ -38,13 +62,25 @@ public class ConnectionInfoTests
     }
 
     [TestMethod]
-    public void FromServerConnection_WithSonarCloud_ShouldReturnConnectionInfo()
+    public void FromServerConnection_WithSonarCloudForEuRegion_ShouldReturnConnectionInfo()
     {
         var sonarCloudServerConnection = new ServerConnection.SonarCloud("organization");
 
         var connectionInfo = ConnectionInfo.From(sonarCloudServerConnection);
 
         connectionInfo.ServerType.Should().Be(ConnectionServerType.SonarCloud);
+        connectionInfo.CloudServerRegion.Should().Be(CloudServerRegion.Eu);
+    }
+
+    [TestMethod]
+    public void FromServerConnection_WithSonarCloudForUsRegion_ShouldReturnConnectionInfo()
+    {
+        var sonarCloudServerConnection = new ServerConnection.SonarCloud("organization", CloudServerRegion.Us);
+
+        var connectionInfo = ConnectionInfo.From(sonarCloudServerConnection);
+
+        connectionInfo.ServerType.Should().Be(ConnectionServerType.SonarCloud);
+        connectionInfo.CloudServerRegion.Should().Be(CloudServerRegion.Us);
     }
 
     [TestMethod]
@@ -58,11 +94,19 @@ public class ConnectionInfoTests
     }
 
     [TestMethod]
-    public void GetIdForTransientConnection_SonarCloudWithNullId_ReturnsSonarCloudUrl()
+    public void GetIdForTransientConnection_SonarCloudWithNullIdAndNoRegion_ReturnsSonarCloudUrlForEuRegion()
     {
         var connectionInfo = new ConnectionInfo(null, ConnectionServerType.SonarCloud);
 
-        connectionInfo.GetIdForTransientConnection().Should().Be(CoreStrings.SonarCloudUrl);
+        connectionInfo.GetIdForTransientConnection().Should().Be(CloudServerRegion.Eu.Url.ToString());
+    }
+
+    [TestMethod]
+    public void GetIdForTransientConnection_SonarCloudWithNullIdAndUsRegion_ReturnsSonarCloudUrlForUsRegion()
+    {
+        var connectionInfo = new ConnectionInfo(null, ConnectionServerType.SonarCloud, CloudServerRegion.Us);
+
+        connectionInfo.GetIdForTransientConnection().Should().Be(CloudServerRegion.Us.Url.ToString());
     }
 
     [TestMethod]
@@ -83,4 +127,6 @@ public class ConnectionInfoTests
 
         connectionInfo.GetIdForTransientConnection().Should().Be(id);
     }
+
+    public static IEnumerable<object[]> GetCloudServerRegions() => [[CloudServerRegion.Eu], [CloudServerRegion.Us],];
 }
