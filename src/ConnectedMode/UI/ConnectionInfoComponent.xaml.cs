12095f50a28f90be629db99e0732b6266bd825ca
Georgii Borovinskikh <georgii.borovinskikh@sonarsource.com>
2025-02-10 10:58:47 +0100

    Add reusable ConnectionIconComponent, replace duplicate layout with it

--- src/ConnectedMode/UI/ConnectionInfoComponent.xaml.cs
+++ src/ConnectedMode/UI/ConnectionInfoComponent.xaml.cs
@@ -30,7 +30,6 @@ public sealed partial class ConnectionInfoComponent : UserControl
     public static readonly DependencyProperty ConnectionInfoProp = DependencyProperty.Register(nameof(ConnectionInfo), typeof(ConnectionInfo), typeof(ConnectionInfoComponent), new PropertyMetadata(OnConnectionInfoSet));
     public static readonly DependencyProperty FontWeightProp = DependencyProperty.Register(nameof(TextFontWeight), typeof(FontWeight), typeof(ConnectionInfoComponent), new PropertyMetadata(FontWeights.DemiBold));
     public static readonly DependencyProperty TextAndIconVerticalAlignmentProp = DependencyProperty.Register(nameof(TextAndIconVerticalAlignment), typeof(VerticalAlignment), typeof(ConnectionInfoComponent), new PropertyMetadata(VerticalAlignment.Bottom));
-    public static readonly DependencyProperty ImageMarginProp = DependencyProperty.Register(nameof(ImageMargin), typeof(Thickness), typeof(ConnectionInfoComponent), new PropertyMetadata(new Thickness(-5,-5,0,-5)));
 
     public ConnectionInfoComponent()
     {
@@ -54,13 +53,6 @@ public sealed partial class ConnectionInfoComponent : UserControl
         get => (VerticalAlignment)GetValue(TextAndIconVerticalAlignmentProp);
         set => SetValue(TextAndIconVerticalAlignmentProp, value);
     }
-
-    public Thickness ImageMargin
-    {
-        get => (Thickness)GetValue(ImageMarginProp);
-        set => SetValue(ImageMarginProp, value);
-    }
-
 
     private static void OnConnectionInfoSet(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
