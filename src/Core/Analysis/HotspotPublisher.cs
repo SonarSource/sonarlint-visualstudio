31334a52c2cecd7d1b34e18b95adba4be41fffb9
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2025-02-12 11:23:54 +0100

    SLVS-1752 Stop ignoring issues that are raised by SlCore when AnalysisId is null (#6016)

--- src/Core/Analysis/HotspotPublisher.cs
+++ src/Core/Analysis/HotspotPublisher.cs
@@ -24,15 +24,14 @@ namespace SonarLint.VisualStudio.Core.Analysis;
 
 [Export(typeof(IHotspotPublisher))]
 [PartCreationPolicy(CreationPolicy.Shared)]
-[method:ImportingConstructor]
-internal class HotspotPublisher(IIssueConsumerStorage issueConsumerStorage) : IHotspotPublisher
+[method: ImportingConstructor]
+internal class HotspotPublisher(IIssueConsumerStorage issueConsumerStorage) : FindingsPublisherBase, IHotspotPublisher
 {
     public string FindingsType => CoreStrings.FindingType_Hotspot;
 
-    public void Publish(string filePath, Guid analysisId, IEnumerable<IAnalysisIssue> findings)
+    public void Publish(string filePath, Guid? analysisId, IEnumerable<IAnalysisIssue> findings)
     {
-        if (issueConsumerStorage.TryGet(filePath, out var currentAnalysisId, out var issueConsumer)
-            && analysisId == currentAnalysisId)
+        if (issueConsumerStorage.TryGet(filePath, out var currentAnalysisId, out var issueConsumer) && IsValidAnalysisId(analysisId, currentAnalysisId))
         {
             issueConsumer.SetHotspots(filePath, findings);
         }
