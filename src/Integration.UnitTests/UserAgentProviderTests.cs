a7a1c38aaa4b6d4abaf53417a1c3d452f5bb23a3
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2025-02-13 15:10:57 +0100

    SLVS-1832 Include the new brand name in the User-Agent (#6023)
    [SLVS-1832](https://sonarsource.atlassian.net/browse/SLVS-1832)
    
    
    
    [SLVS-1832]:
    https://sonarsource.atlassian.net/browse/SLVS-1832?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ src/Integration.UnitTests/UserAgentProviderTests.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using SonarLint.VisualStudio.Core.VsInfo;
+using SonarLint.VisualStudio.Integration.Service;
+using SonarLint.VisualStudio.TestInfrastructure;
+
+namespace SonarLint.VisualStudio.Integration.UnitTests;
+
+[TestClass]
+public class UserAgentProviderTests
+{
+    private IVsInfoProvider vsInfoProvider;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        vsInfoProvider = Substitute.For<IVsInfoProvider>();
+    }
+
+    [TestMethod]
+    public void MefCtor_CheckIsExported() =>
+        MefTestHelpers.CheckTypeCanBeImported<UserAgentProvider, IUserAgentProvider>(
+            MefTestHelpers.CreateExport<IVsInfoProvider>());
+
+    [TestMethod]
+    public void MefCtor_CheckIsSingleton() =>
+        MefTestHelpers.CheckIsSingletonMefComponent<UserAgentProvider>();
+
+    [TestMethod]
+    public void UserAgent_ReturnsExpectedFormat()
+    {
+        vsInfoProvider.Version.DisplayVersion.Returns("1.2.3.4");
+        vsInfoProvider.Version.DisplayName.Returns("Vi Es Pro 2042");
+
+        new UserAgentProvider(vsInfoProvider).UserAgent.Should().Be($"SonarQube for IDE (SonarLint) - Visual Studio {VersionHelper.SonarLintVersion} - Vi Es Pro 2042 1.2.3.4");
+    }
+
+    [TestMethod]
+    public void UserAgent_VsVersionUnavailable_ReturnsExpectedFormatWithDefaults()
+    {
+        vsInfoProvider.Version.Returns((IVsVersion)null);
+
+        new UserAgentProvider(vsInfoProvider).UserAgent.Should().Be($"SonarQube for IDE (SonarLint) - Visual Studio {VersionHelper.SonarLintVersion} - VisualStudio version unknown");
+    }
+
+    [TestMethod]
+    public void UserAgent_ValueIsCached()
+    {
+        var testSubject = new UserAgentProvider(vsInfoProvider);
+
+        _ = testSubject.UserAgent;
+        _ = testSubject.UserAgent;
+        _ = testSubject.UserAgent;
+
+        _ = vsInfoProvider.Received(1).Version;
+        _ = vsInfoProvider.Version.Received(1).DisplayName;
+        _ = vsInfoProvider.Version.Received(1).DisplayVersion;
+    }
+}
---
+++ src/Integration/UserAgentProvider.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.ComponentModel.Composition;
+using SonarLint.VisualStudio.Core.VsInfo;
+using SonarLint.VisualStudio.Integration.Service;
+
+namespace SonarLint.VisualStudio.Integration;
+
+public interface IUserAgentProvider
+{
+    string UserAgent { get; }
+}
+
+[Export(typeof(IUserAgentProvider))]
+[PartCreationPolicy(CreationPolicy.Shared)]
+[method: ImportingConstructor]
+public class UserAgentProvider(IVsInfoProvider vsInfoProvider) : IUserAgentProvider
+{
+    public string UserAgent { get; } = GetUserAgent(vsInfoProvider);
+
+    private static string GetUserAgent(IVsInfoProvider vsInfoProvider)
+    {
+        var vsVersionInfo = vsInfoProvider.Version;
+        var vsProductName = vsVersionInfo?.DisplayName ?? "VisualStudio";
+        var vsProductVersion = vsVersionInfo?.DisplayVersion ?? "version unknown";
+
+        return $"SonarQube for IDE (SonarLint) - Visual Studio {VersionHelper.SonarLintVersion} - {vsProductName} {vsProductVersion}";
+    }
+}
