1e040a4b99b8560e90b3b59091460c54ac7050f5
Georgii Borovinskikh <georgii.borovinskikh@sonarsource.com>
2025-02-13 12:36:10 +0100

    re-enable parallelization

--- src/TestInfrastructure/TestParallelization.cs
+++ src/TestInfrastructure/TestParallelization.cs
@@ -18,4 +18,4 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-[assembly:Parallelize(Workers = 1, Scope = ExecutionScope.ClassLevel)] // method level is not safe as multiple test classes rely on global setup (for example, SLCore integration tests)
+[assembly:Parallelize(Workers = 4, Scope = ExecutionScope.ClassLevel)] // method level is not safe as multiple test classes rely on global setup (for example, SLCore integration tests)
