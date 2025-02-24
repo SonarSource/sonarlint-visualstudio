af1d112b0f0ec9449e143faef0f5abd5721fa40c
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2025-02-24 09:01:46 +0100

    SLVS-1884 Add RPC method OpenUrlInBrowser, its models and implementation.

---
+++ src/SLCore.UnitTests/Listener/Visualization/OpenUrlInBrowserParamsTests.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using SonarLint.VisualStudio.SLCore.Listener.Visualization.Models;
+
+namespace SonarLint.VisualStudio.SLCore.UnitTests.Listener.Visualization;
+
+[TestClass]
+public class OpenUrlInBrowserParamsTests
+{
+    [TestMethod]
+    public void Serialize_AsExpected()
+    {
+        var openUrlInBrowserParams = new OpenUrlInBrowserParams("http://localhost:9000/home");
+        var expected = """
+                       {
+                         "url": "http://localhost:9000/home"
+                       }
+                       """;
+
+        var actual = JsonConvert.SerializeObject(openUrlInBrowserParams, Formatting.Indented);
+
+        actual.Should().Be(expected);
+    }
+
+    [TestMethod]
+    public void Deserialize_AsExpected()
+    {
+        var expected = new OpenUrlInBrowserParams("http://localhost:9000/home");
+        var serialized = """
+                         {
+                           "url": "http://localhost:9000/home"
+                         }
+                         """;
+
+        var actual = JsonConvert.DeserializeObject<OpenUrlInBrowserParams>(serialized);
+
+        actual.Should().BeEquivalentTo(expected, options => options.ComparingByMembers<OpenUrlInBrowserParams>());
+    }
+}
