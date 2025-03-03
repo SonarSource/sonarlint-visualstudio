dcec597c9b49ea1fa53aaa7943b7859566ae4993
Vasileios Naskos <168648790+vnaskos-sonar@users.noreply.github.com>
2025-03-03 12:48:50 +0100

    SLVS-1857 Clean up non-roslyn server sent events (#6064)
    Targets the [clean up for non-roslyn quality
    profiles](https://github.com/SonarSource/sonarlint-visualstudio/pull/6060)
    
    ---------
    
    Co-authored-by: Georgii Borovinskikh <georgii.borovinskikh@sonarsource.com>

--- src/SonarQube.Client.Tests/Requests/Api/V9_4/GetSonarLintEventStreamTests.cs
+++ src/SonarQube.Client.Tests/Requests/Api/V9_4/GetSonarLintEventStreamTests.cs
@@ -18,47 +18,41 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using System;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using FluentAssertions;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using SonarQube.Client.Api.V9_4;
 using SonarQube.Client.Logging;
 using SonarQube.Client.Tests.Infra;
 
-namespace SonarQube.Client.Tests.Requests.Api.V9_4
+namespace SonarQube.Client.Tests.Requests.Api.V9_4;
+
+[TestClass]
+public class GetSonarLintEventStreamTests
 {
-    [TestClass]
-    public class GetSonarLintEventStreamTests
+    [TestMethod]
+    public async Task InvokeAsync_ReturnsCorrectStream()
     {
-        [TestMethod]
-        public async Task InvokeAsync_ReturnsCorrectStream()
-        {
-            using var testedStream = new MemoryStream(Encoding.UTF8.GetBytes("hello this is a test"));
-            var messageHandler = new Mock<HttpClientHandler>();
-            using var httpClient = new HttpClient(messageHandler.Object) { BaseAddress = new Uri("http://localhost") };
+        using var testedStream = new MemoryStream(Encoding.UTF8.GetBytes("hello this is a test"));
+        var messageHandler = new Mock<HttpClientHandler>();
+        using var httpClient = new HttpClient(messageHandler.Object) { BaseAddress = new Uri("http://localhost") };
 
-            MocksHelper.SetupHttpRequest(
-                messageHandler,
-                requestRelativePath: "api/push/sonarlint_events?languages=cs%2Cvbnet%2Ccpp%2Cc%2Cjs%2Cts%2Csecrets%2Ccss%2Cweb%2Ctsql&projectKeys=someproj",
-                responseMessage: new HttpResponseMessage { Content = new StreamContent(testedStream) },
-                headers: MediaTypeHeaderValue.Parse("text/event-stream"));
+        MocksHelper.SetupHttpRequest(
+            messageHandler,
+            requestRelativePath: "api/push/sonarlint_events?languages=cs%2Cvbnet&projectKeys=someproj",
+            responseMessage: new HttpResponseMessage { Content = new StreamContent(testedStream) },
+            headers: MediaTypeHeaderValue.Parse("text/event-stream"));
 
-            var testSubject = new GetSonarLintEventStream { ProjectKey = "someproj", Logger = Mock.Of<ILogger>() };
+        var testSubject = new GetSonarLintEventStream { ProjectKey = "someproj", Logger = Mock.Of<ILogger>() };
 
-            using var response = await testSubject.InvokeAsync(httpClient, CancellationToken.None);
-            response.Should().NotBeNull();
-            messageHandler.VerifyAll();
+        using var response = await testSubject.InvokeAsync(httpClient, CancellationToken.None);
+        response.Should().NotBeNull();
+        messageHandler.VerifyAll();
 
-            using var reader = new StreamReader(response, Encoding.UTF8);
-            var responseString = await reader.ReadToEndAsync();
-            responseString.Should().Be("hello this is a test");
-        }
+        using var reader = new StreamReader(response, Encoding.UTF8);
+        var responseString = await reader.ReadToEndAsync();
+        responseString.Should().Be("hello this is a test");
     }
 }
