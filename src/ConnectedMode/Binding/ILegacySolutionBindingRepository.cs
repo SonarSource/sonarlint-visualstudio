96d5ae2018f7de8b722a2a6a19535298239410e7
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2025-03-04 16:50:39 +0100

    SLVS-1910 Rename file to match class

--- src/ConnectedMode/Binding/ILegacySolutionBindingRepository.cs
+++ src/ConnectedMode/Binding/ILegacySolutionBindingRepository.cs
@@ -20,14 +20,13 @@
 
 using SonarLint.VisualStudio.Core.Binding;
 
-namespace SonarLint.VisualStudio.ConnectedMode.Binding
+namespace SonarLint.VisualStudio.ConnectedMode.Binding;
+
+internal interface ILegacySolutionBindingRepository
 {
-    internal interface ILegacySolutionBindingRepository
-    {
-        /// <summary>
-        /// Retrieves solution binding information
-        /// </summary>
-        /// <returns>Can be null if not bound</returns>
-        BoundSonarQubeProject Read(string configFilePath);
-    }
+    /// <summary>
+    /// Retrieves solution binding information
+    /// </summary>
+    /// <returns>Can be null if not bound</returns>
+    BoundSonarQubeProject Read(string configFilePath);
 }
