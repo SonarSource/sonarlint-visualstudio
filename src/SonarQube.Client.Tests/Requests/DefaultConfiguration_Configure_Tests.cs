9e4126fb07bfe37bc2ff42885b3a27e5d7710d50
Georgii Borovinskikh <georgii.borovinskikh@sonarsource.com>
2025-03-13 16:25:16 +0100

    Remove unused methods and models from ISonarQubeService

--- src/SonarQube.Client.Tests/Requests/DefaultConfiguration_Configure_Tests.cs
+++ src/SonarQube.Client.Tests/Requests/DefaultConfiguration_Configure_Tests.cs
@@ -34,40 +34,23 @@ public class DefaultConfiguration_Configure_Tests
 
         var expected = new[]
         {
-            "Registered SonarQube.Client.Api.V2_10.GetPluginsRequest for 2.1",
-            "Registered SonarQube.Client.Api.V2_10.GetProjectsRequest for 2.1",
             "Registered SonarQube.Client.Api.V2_10.GetVersionRequest for 2.1",
             "Registered SonarQube.Client.Api.V2_60.GetPropertiesRequest for 2.6",
             "Registered SonarQube.Client.Api.V3_30.ValidateCredentialsRequest for 3.3",
             "Registered SonarQube.Client.Api.V5_10.GetIssuesRequest for 5.1",
-            "Registered SonarQube.Client.Api.V5_10.GetLanguagesRequest for 5.1",
-            "Registered SonarQube.Client.Api.V5_20.GetQualityProfileChangeLogRequest for 5.2",
             "Registered SonarQube.Client.Api.V5_20.GetQualityProfilesRequest for 5.2",
-            "Registered SonarQube.Client.Api.V5_20.GetRoslynExportProfileRequest for 5.2",
-            "Registered SonarQube.Client.Api.V5_40.GetModulesRequest for 5.4",
             "Registered SonarQube.Client.Api.V5_50.GetRulesRequest for 5.5",
-            "Registered SonarQube.Client.Api.V5_50.DownloadStaticFile for 5.5",
-            "Registered SonarQube.Client.Api.V6_20.GetOrganizationsRequest for 6.2",
-            "Registered SonarQube.Client.Api.V6_20.GetProjectsRequest for 6.2",
-            "Registered SonarQube.Client.Api.V6_30.GetPluginsRequest for 6.3",
             "Registered SonarQube.Client.Api.V6_30.GetPropertiesRequest for 6.3",
-            "Registered SonarQube.Client.Api.V6_50.GetQualityProfileChangeLogRequest for 6.5",
             "Registered SonarQube.Client.Api.V6_50.GetQualityProfilesRequest for 6.5",
             "Registered SonarQube.Client.Api.V6_60.GetNotificationsRequest for 6.6",
-            "Registered SonarQube.Client.Api.V6_60.GetRoslynExportProfileRequest for 6.6",
             "Registered SonarQube.Client.Api.V6_60.GetProjectBranchesRequest for 6.6",
-            "Registered SonarQube.Client.Api.V7_00.GetOrganizationsRequest for 7.0",
             "Registered SonarQube.Client.Api.V7_20.GetIssuesRequestWrapper`1[SonarQube.Client.Api.V7_20.GetIssuesWithComponentSonarQubeRequest] for 7.2",
-            "Registered SonarQube.Client.Api.V8_6.GetHotspotRequest for 8.6",
             "Registered SonarQube.Client.Api.V7_20.GetExclusionsRequest for 7.2",
             "Registered SonarQube.Client.Api.V9_4.GetSonarLintEventStream for 9.4",
             "Registered SonarQube.Client.Api.V9_7.SearchHotspotRequest for 9.7",
             "Registered SonarQube.Client.Api.V10_2.SearchHotspotRequest for 10.2",
             "Registered SonarQube.Client.Api.V10_2.GetRulesWithCCTRequest for 10.2",
-            "Registered SonarQube.Client.Api.V9_9.TransitionIssueRequestWithWontFix for 9.9",
-            "Registered SonarQube.Client.Api.V10_4.TransitionIssueRequestWithAccept for 10.4",
-            "Registered SonarQube.Client.Api.V9_9.CommentIssueRequest for 9.9",
-            "Registered SonarQube.Client.Api.V9_9.SearchFilesByNameRequest for 9.9"
+            "Registered SonarQube.Client.Api.V9_9.SearchFilesByNameRequest for 9.9",
         };
 
         DefaultConfiguration.ConfigureSonarQube(new RequestFactory(logger));
@@ -83,30 +66,20 @@ public class DefaultConfiguration_Configure_Tests
     {
         var logger = new TestLogger();
 
-        var expected = new string[]
+        var expected = new[]
         {
             "Registered SonarQube.Client.Api.V2_10.GetVersionRequest",
             "Registered SonarQube.Client.Api.V3_30.ValidateCredentialsRequest",
-            "Registered SonarQube.Client.Api.V5_10.GetLanguagesRequest",
-            "Registered SonarQube.Client.Api.V5_40.GetModulesRequest",
             "Registered SonarQube.Client.Api.V10_2.GetRulesWithCCTRequest",
-            "Registered SonarQube.Client.Api.V5_50.DownloadStaticFile",
-            "Registered SonarQube.Client.Api.V6_20.GetProjectsRequest",
-            "Registered SonarQube.Client.Api.V6_30.GetPluginsRequest",
             "Registered SonarQube.Client.Api.V6_30.GetPropertiesRequest",
-            "Registered SonarQube.Client.Api.V6_50.GetQualityProfileChangeLogRequest",
             "Registered SonarQube.Client.Api.V6_50.GetQualityProfilesRequest",
             "Registered SonarQube.Client.Api.V6_60.GetNotificationsRequest",
-            "Registered SonarQube.Client.Api.V6_60.GetRoslynExportProfileRequest",
             "Registered SonarQube.Client.Api.V6_60.GetProjectBranchesRequest",
-            "Registered SonarQube.Client.Api.V7_00.GetOrganizationsRequest",
             "Registered SonarQube.Client.Api.V7_20.GetIssuesRequestWrapper`1[SonarQube.Client.Api.V7_20.GetIssuesWithComponentSonarCloudRequest]",
-            "Registered SonarQube.Client.Api.V8_6.GetHotspotRequest",
             "Registered SonarQube.Client.Api.V7_20.GetExclusionsRequest",
             "Registered SonarQube.Client.Api.V9_7.SearchHotspotRequest",
-            "Registered SonarQube.Client.Api.V10_4.TransitionIssueRequestWithAccept",
-            "Registered SonarQube.Client.Api.V9_9.CommentIssueRequest",
             "Registered SonarQube.Client.Api.V9_9.SearchFilesByNameRequest"
+
         };
 
         DefaultConfiguration.ConfigureSonarCloud(new UnversionedRequestFactory(logger));
@@ -123,27 +96,16 @@ public class DefaultConfiguration_Configure_Tests
         var testSubject = DefaultConfiguration.ConfigureSonarQube(new RequestFactory(new TestLogger()));
         var serverInfo = new ServerInfo(null /* latest */, ServerType.SonarQube);
 
-        testSubject.Create<IDownloadStaticFile>(serverInfo).Should().NotBeNull();
-        testSubject.Create<IGetHotspotRequest>(serverInfo).Should().NotBeNull();
         testSubject.Create<IGetIssuesRequest>(serverInfo).Should().NotBeNull();
-        testSubject.Create<IGetLanguagesRequest>(serverInfo).Should().NotBeNull();
-        testSubject.Create<IGetModulesRequest>(serverInfo).Should().NotBeNull();
         testSubject.Create<IGetNotificationsRequest>(serverInfo).Should().NotBeNull();
-        testSubject.Create<IGetOrganizationsRequest>(serverInfo).Should().NotBeNull();
-        testSubject.Create<IGetPluginsRequest>(serverInfo).Should().NotBeNull();
-        testSubject.Create<IGetProjectsRequest>(serverInfo).Should().NotBeNull();
         testSubject.Create<IGetPropertiesRequest>(serverInfo).Should().NotBeNull();
-        testSubject.Create<IGetQualityProfileChangeLogRequest>(serverInfo).Should().NotBeNull();
         testSubject.Create<IGetQualityProfilesRequest>(serverInfo).Should().NotBeNull();
-        testSubject.Create<IGetRoslynExportProfileRequest>(serverInfo).Should().NotBeNull();
         testSubject.Create<IGetRulesRequest>(serverInfo).Should().NotBeNull();
         testSubject.Create<IGetVersionRequest>(serverInfo).Should().NotBeNull();
         testSubject.Create<IValidateCredentialsRequest>(serverInfo).Should().NotBeNull();
         testSubject.Create<IGetProjectBranchesRequest>(serverInfo).Should().NotBeNull();
         testSubject.Create<IGetExclusionsRequest>(serverInfo).Should().NotBeNull();
         testSubject.Create<IGetSonarLintEventStream>(serverInfo).Should().NotBeNull();
-        testSubject.Create<ITransitionIssueRequest>(serverInfo).Should().NotBeNull();
-        testSubject.Create<ICommentIssueRequest>(serverInfo).Should().NotBeNull();
         testSubject.Create<ISearchFilesByNameRequest>(serverInfo).Should().NotBeNull();
     }
 
@@ -153,26 +115,15 @@ public class DefaultConfiguration_Configure_Tests
         var testSubject = DefaultConfiguration.ConfigureSonarCloud(new UnversionedRequestFactory(new TestLogger()));
         var serverInfo = new ServerInfo(null /* latest */, ServerType.SonarQube);
 
-        testSubject.Create<IDownloadStaticFile>(serverInfo).Should().NotBeNull();
-        testSubject.Create<IGetHotspotRequest>(serverInfo).Should().NotBeNull();
         testSubject.Create<IGetIssuesRequest>(serverInfo).Should().NotBeNull();
-        testSubject.Create<IGetLanguagesRequest>(serverInfo).Should().NotBeNull();
-        testSubject.Create<IGetModulesRequest>(serverInfo).Should().NotBeNull();
         testSubject.Create<IGetNotificationsRequest>(serverInfo).Should().NotBeNull();
-        testSubject.Create<IGetOrganizationsRequest>(serverInfo).Should().NotBeNull();
-        testSubject.Create<IGetPluginsRequest>(serverInfo).Should().NotBeNull();
-        testSubject.Create<IGetProjectsRequest>(serverInfo).Should().NotBeNull();
         testSubject.Create<IGetPropertiesRequest>(serverInfo).Should().NotBeNull();
-        testSubject.Create<IGetQualityProfileChangeLogRequest>(serverInfo).Should().NotBeNull();
         testSubject.Create<IGetQualityProfilesRequest>(serverInfo).Should().NotBeNull();
-        testSubject.Create<IGetRoslynExportProfileRequest>(serverInfo).Should().NotBeNull();
         testSubject.Create<IGetRulesRequest>(serverInfo).Should().NotBeNull();
         testSubject.Create<IGetVersionRequest>(serverInfo).Should().NotBeNull();
         testSubject.Create<IValidateCredentialsRequest>(serverInfo).Should().NotBeNull();
         testSubject.Create<IGetProjectBranchesRequest>(serverInfo).Should().NotBeNull();
         testSubject.Create<IGetExclusionsRequest>(serverInfo).Should().NotBeNull();
-        testSubject.Create<ITransitionIssueRequest>(serverInfo).Should().NotBeNull();
-        testSubject.Create<ICommentIssueRequest>(serverInfo).Should().NotBeNull();
         testSubject.Create<ISearchFilesByNameRequest>(serverInfo).Should().NotBeNull();
     }
 
--- src/SonarQube.Client/Api/DefaultConfiguration.cs
+++ src/SonarQube.Client/Api/DefaultConfiguration.cs
@@ -18,7 +18,18 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+using SonarQube.Client.Api.V10_2;
+using SonarQube.Client.Api.V2_10;
+using SonarQube.Client.Api.V3_30;
+using SonarQube.Client.Api.V5_50;
+using SonarQube.Client.Api.V6_30;
+using SonarQube.Client.Api.V6_50;
+using SonarQube.Client.Api.V6_60;
+using SonarQube.Client.Api.V7_20;
+using SonarQube.Client.Api.V9_4;
+using SonarQube.Client.Api.V9_9;
 using SonarQube.Client.Requests;
+using GetIssuesRequest = SonarQube.Client.Api.V5_10.GetIssuesRequest;
 
 namespace SonarQube.Client.Api;
 
@@ -27,40 +38,23 @@ internal static class DefaultConfiguration
     public static RequestFactory ConfigureSonarQube(RequestFactory requestFactory)
     {
         requestFactory
-            .RegisterRequest<IGetPluginsRequest, V2_10.GetPluginsRequest>("2.1")
-            .RegisterRequest<IGetProjectsRequest, V2_10.GetProjectsRequest>("2.1")
-            .RegisterRequest<IGetVersionRequest, V2_10.GetVersionRequest>("2.1")
+            .RegisterRequest<IGetVersionRequest, GetVersionRequest>("2.1")
             .RegisterRequest<IGetPropertiesRequest, V2_60.GetPropertiesRequest>("2.6")
-            .RegisterRequest<IValidateCredentialsRequest, V3_30.ValidateCredentialsRequest>("3.3")
-            .RegisterRequest<IGetIssuesRequest, V5_10.GetIssuesRequest>("5.1")
-            .RegisterRequest<IGetLanguagesRequest, V5_10.GetLanguagesRequest>("5.1")
-            .RegisterRequest<IGetQualityProfileChangeLogRequest, V5_20.GetQualityProfileChangeLogRequest>("5.2")
+            .RegisterRequest<IValidateCredentialsRequest, ValidateCredentialsRequest>("3.3")
+            .RegisterRequest<IGetIssuesRequest, GetIssuesRequest>("5.1")
             .RegisterRequest<IGetQualityProfilesRequest, V5_20.GetQualityProfilesRequest>("5.2")
-            .RegisterRequest<IGetRoslynExportProfileRequest, V5_20.GetRoslynExportProfileRequest>("5.2")
-            .RegisterRequest<IGetModulesRequest, V5_40.GetModulesRequest>("5.4")
-            .RegisterRequest<IGetRulesRequest, V5_50.GetRulesRequest>("5.5")
-            .RegisterRequest<IDownloadStaticFile, V5_50.DownloadStaticFile>("5.5")
-            .RegisterRequest<IGetOrganizationsRequest, V6_20.GetOrganizationsRequest>("6.2")
-            .RegisterRequest<IGetProjectsRequest, V6_20.GetProjectsRequest>("6.2")
-            .RegisterRequest<IGetPluginsRequest, V6_30.GetPluginsRequest>("6.3")
-            .RegisterRequest<IGetPropertiesRequest, V6_30.GetPropertiesRequest>("6.3")
-            .RegisterRequest<IGetQualityProfileChangeLogRequest, V6_50.GetQualityProfileChangeLogRequest>("6.5")
-            .RegisterRequest<IGetQualityProfilesRequest, V6_50.GetQualityProfilesRequest>("6.5")
-            .RegisterRequest<IGetNotificationsRequest, V6_60.GetNotificationsRequest>("6.6")
-            .RegisterRequest<IGetRoslynExportProfileRequest, V6_60.GetRoslynExportProfileRequest>("6.6")
-            .RegisterRequest<IGetProjectBranchesRequest, V6_60.GetProjectBranchesRequest>("6.6")
-            .RegisterRequest<IGetOrganizationsRequest, V7_00.GetOrganizationsRequest>("7.0")
-            .RegisterRequest<IGetIssuesRequest, V7_20.GetIssuesRequestWrapper<V7_20.GetIssuesWithComponentSonarQubeRequest>>("7.2")
-            .RegisterRequest<IGetHotspotRequest, V8_6.GetHotspotRequest>("8.6")
-            .RegisterRequest<IGetExclusionsRequest, V7_20.GetExclusionsRequest>("7.2")
-            .RegisterRequest<IGetSonarLintEventStream, V9_4.GetSonarLintEventStream>("9.4")
+            .RegisterRequest<IGetRulesRequest, GetRulesRequest>("5.5")
+            .RegisterRequest<IGetPropertiesRequest, GetPropertiesRequest>("6.3")
+            .RegisterRequest<IGetQualityProfilesRequest, GetQualityProfilesRequest>("6.5")
+            .RegisterRequest<IGetNotificationsRequest, GetNotificationsRequest>("6.6")
+            .RegisterRequest<IGetProjectBranchesRequest, GetProjectBranchesRequest>("6.6")
+            .RegisterRequest<IGetIssuesRequest, GetIssuesRequestWrapper<GetIssuesWithComponentSonarQubeRequest>>("7.2")
+            .RegisterRequest<IGetExclusionsRequest, GetExclusionsRequest>("7.2")
+            .RegisterRequest<IGetSonarLintEventStream, GetSonarLintEventStream>("9.4")
             .RegisterRequest<ISearchHotspotRequest, V9_7.SearchHotspotRequest>("9.7")
-            .RegisterRequest<ISearchHotspotRequest, V10_2.SearchHotspotRequest>("10.2")
-            .RegisterRequest<IGetRulesRequest, V10_2.GetRulesWithCCTRequest>("10.2")
-            .RegisterRequest<ITransitionIssueRequest, V9_9.TransitionIssueRequestWithWontFix>("9.9")
-            .RegisterRequest<ITransitionIssueRequest, V10_4.TransitionIssueRequestWithAccept>("10.4")
-            .RegisterRequest<ICommentIssueRequest, V9_9.CommentIssueRequest>("9.9")
-            .RegisterRequest<ISearchFilesByNameRequest, V9_9.SearchFilesByNameRequest>("9.9");
+            .RegisterRequest<ISearchHotspotRequest, SearchHotspotRequest>("10.2")
+            .RegisterRequest<IGetRulesRequest, GetRulesWithCCTRequest>("10.2")
+            .RegisterRequest<ISearchFilesByNameRequest, SearchFilesByNameRequest>("9.9");
 
         return requestFactory;
     }
@@ -68,28 +62,17 @@ internal static class DefaultConfiguration
     public static UnversionedRequestFactory ConfigureSonarCloud(UnversionedRequestFactory requestFactory)
     {
         requestFactory
-            .RegisterRequest<IGetVersionRequest, V2_10.GetVersionRequest>()
-            .RegisterRequest<IValidateCredentialsRequest, V3_30.ValidateCredentialsRequest>()
-            .RegisterRequest<IGetLanguagesRequest, V5_10.GetLanguagesRequest>()
-            .RegisterRequest<IGetModulesRequest, V5_40.GetModulesRequest>()
-            .RegisterRequest<IGetRulesRequest, V10_2.GetRulesWithCCTRequest>()
-            .RegisterRequest<IDownloadStaticFile, V5_50.DownloadStaticFile>()
-            .RegisterRequest<IGetProjectsRequest, V6_20.GetProjectsRequest>()
-            .RegisterRequest<IGetPluginsRequest, V6_30.GetPluginsRequest>()
-            .RegisterRequest<IGetPropertiesRequest, V6_30.GetPropertiesRequest>()
-            .RegisterRequest<IGetQualityProfileChangeLogRequest, V6_50.GetQualityProfileChangeLogRequest>()
-            .RegisterRequest<IGetQualityProfilesRequest, V6_50.GetQualityProfilesRequest>()
-            .RegisterRequest<IGetNotificationsRequest, V6_60.GetNotificationsRequest>()
-            .RegisterRequest<IGetRoslynExportProfileRequest, V6_60.GetRoslynExportProfileRequest>()
-            .RegisterRequest<IGetProjectBranchesRequest, V6_60.GetProjectBranchesRequest>()
-            .RegisterRequest<IGetOrganizationsRequest, V7_00.GetOrganizationsRequest>()
-            .RegisterRequest<IGetIssuesRequest, V7_20.GetIssuesRequestWrapper<V7_20.GetIssuesWithComponentSonarCloudRequest>>()
-            .RegisterRequest<IGetHotspotRequest, V8_6.GetHotspotRequest>()
-            .RegisterRequest<IGetExclusionsRequest, V7_20.GetExclusionsRequest>()
+            .RegisterRequest<IGetVersionRequest, GetVersionRequest>()
+            .RegisterRequest<IValidateCredentialsRequest, ValidateCredentialsRequest>()
+            .RegisterRequest<IGetRulesRequest, GetRulesWithCCTRequest>()
+            .RegisterRequest<IGetPropertiesRequest, GetPropertiesRequest>()
+            .RegisterRequest<IGetQualityProfilesRequest, GetQualityProfilesRequest>()
+            .RegisterRequest<IGetNotificationsRequest, GetNotificationsRequest>()
+            .RegisterRequest<IGetProjectBranchesRequest, GetProjectBranchesRequest>()
+            .RegisterRequest<IGetIssuesRequest, GetIssuesRequestWrapper<GetIssuesWithComponentSonarCloudRequest>>()
+            .RegisterRequest<IGetExclusionsRequest, GetExclusionsRequest>()
             .RegisterRequest<ISearchHotspotRequest, V9_7.SearchHotspotRequest>()
-            .RegisterRequest<ITransitionIssueRequest, V10_4.TransitionIssueRequestWithAccept>()
-            .RegisterRequest<ICommentIssueRequest, V9_9.CommentIssueRequest>()
-            .RegisterRequest<ISearchFilesByNameRequest, V9_9.SearchFilesByNameRequest>();
+            .RegisterRequest<ISearchFilesByNameRequest, SearchFilesByNameRequest>();
 
         return requestFactory;
     }
