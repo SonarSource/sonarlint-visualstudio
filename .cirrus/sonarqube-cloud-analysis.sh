d2923a9097ea7b3fb8e5b92fcf5ff5232bae2054
Vasileios Naskos <vasileios.naskos@sonarsource.com>
2025-03-14 16:31:39 +0100

    SLVS-1923 Fix: SonarCloud project new code period is stuck on version 8.3

--- .cirrus/sonarqube-cloud-analysis.sh
+++ .cirrus/sonarqube-cloud-analysis.sh
@@ -2,7 +2,10 @@
 
 set -xeo pipefail
 
+version=$(grep -oP '<MainVersion>\K[^<]+' ./build/Version.props)
+
 SONAR_PARAMS=(
+  -v:"${version}"
   -k:"${CIRRUS_REPO_NAME}"
   -o:"sonarsource"
   -d:sonar.host.url="${SONAR_URL}"
@@ -14,20 +17,18 @@ SONAR_PARAMS=(
   -d:sonar.scanner.scanAll=false
 )
 
-version=$(grep -oP '<MainVersion>\K[^<]+' ./build/Version.props)
-
 if [ "$CIRRUS_BRANCH" == "master" ] && [ -z "$CIRRUS_PR" ]; then
   echo '======= Analyze master branch'
-  dotnet sonarscanner begin -v:"${version}" "${SONAR_PARAMS[@]}"
+  dotnet sonarscanner begin "${SONAR_PARAMS[@]}"
 
 elif [ -n "$CIRRUS_PR" ]; then
   echo '======= Analyze pull request'
-  dotnet sonarscanner begin -v:"${version}" "${SONAR_PARAMS[@]}" \
+  dotnet sonarscanner begin "${SONAR_PARAMS[@]}" \
     -d:sonar.pullrequest.key="${CIRRUS_PR}" \
     -d:sonar.pullrequest.branch="${CIRRUS_BRANCH}" \
     -d:sonar.pullrequest.base="${CIRRUS_BASE_BRANCH}"
 
 else
     echo '======= Analyze branch'
-    dotnet sonarscanner begin -v:"${version}" "${SONAR_PARAMS[@]}" -d:sonar.branch.name="${CIRRUS_BRANCH}"
+    dotnet sonarscanner begin "${SONAR_PARAMS[@]}" -d:sonar.branch.name="${CIRRUS_BRANCH}"
 fi
