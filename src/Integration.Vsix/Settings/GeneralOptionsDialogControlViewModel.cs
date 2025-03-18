ca2cc3fbbf9a88c1e2b38a88f25729ae1042d3f6
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2025-03-18 10:20:12 +0000

    SLVS-1515 Cleanup TeamExplorer & Progress left over components (#6107)
    [SLVS-1515](https://sonarsource.atlassian.net/browse/SLVS-1515)
    
    
    
    [SLVS-1515]:
    https://sonarsource.atlassian.net/browse/SLVS-1515?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/Integration.Vsix/Settings/GeneralOptionsDialogControlViewModel.cs
+++ src/Integration.Vsix/Settings/GeneralOptionsDialogControlViewModel.cs
@@ -20,7 +20,8 @@
 
 using System.Windows.Input;
 using SonarLint.VisualStudio.Core;
-using SonarLint.VisualStudio.Progress.MVVM;
+using SonarLint.VisualStudio.Core.WPF;
+using SonarLint.VisualStudio.Integration.WPF;
 
 namespace SonarLint.VisualStudio.Integration.Vsix.Settings;
 
