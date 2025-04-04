903dc4e1d5a77927877b724275d5a03986fb6c18
Vasileios Naskos <vasileios.naskos@sonarsource.com>
2025-04-04 11:52:53 +0200

    Add tests for Rules and FileExclusions

--- src/Core.UnitTests/UserRuleSettings/AnalysisSettingsTests.cs
+++ src/Core.UnitTests/UserRuleSettings/AnalysisSettingsTests.cs
@@ -7,6 +7,64 @@ namespace SonarLint.VisualStudio.Core.UnitTests.UserRuleSettings;
 public class AnalysisSettingsTests
 {
     [TestMethod]
+    public void AnalysisSettings_SerializesCorrectly()
+    {
+        var settings = new AnalysisSettings
+        {
+            Rules =
+            {
+                { "typescript:S2685", new RuleConfig { Level = RuleLevel.On, Parameters = new Dictionary<string, string> { { "key1", "value1" } }, } }
+            },
+            FileExclusions = ["file1.cpp", "**/obj/*", "file2.cpp"]
+        };
+        const string expectedJson =
+            """
+            {
+              "sonarlint.rules": {
+                "typescript:S2685": {
+                  "level": "On",
+                  "parameters": {
+                    "key1": "value1"
+                  }
+                }
+              },
+              "sonarlint.analysisExcludesStandalone": "file1.cpp,**/obj/*,file2.cpp"
+            }
+            """;
+
+        var json = JsonConvert.SerializeObject(settings, Formatting.Indented);
+
+        json.Should().Be(expectedJson);
+    }
+
+    [TestMethod]
+    public void AnalysisSettings_DeserializesCorrectly()
+    {
+        const string json = """
+                            {
+                              "sonarlint.rules": {
+                                "typescript:S2685": {
+                                  "level": "On",
+                                  "parameters": {
+                                    "key1": "value1"
+                                  }
+                                }
+                              },
+                              "sonarlint.analysisExcludesStandalone": "file1.cpp,**/obj/*,file2.cpp"
+                            }
+                            """;
+
+        var settings = JsonConvert.DeserializeObject<AnalysisSettings>(json);
+
+        settings.Rules.Should().BeEquivalentTo(
+            new Dictionary<string, RuleConfig>
+            {
+                { "typescript:S2685", new RuleConfig { Level = RuleLevel.On, Parameters = new Dictionary<string, string> { { "key1", "value1" } }, } }
+            });
+        settings.FileExclusions.Should().BeEquivalentTo("file1.cpp", "**/obj/*", "file2.cpp");
+    }
+
+    [TestMethod]
     public void AnalysisSettings_FileExclusions_SerializesCorrectly()
     {
         var settings = new AnalysisSettings { FileExclusions = ["file1.cpp", "**/obj/*", "file2.cpp"] };
