6ac0208e1297035774c2308e0f8705d42463b2f3
Vasileios Naskos <168648790+vnaskos-sonar@users.noreply.github.com>
2025-04-07 09:07:26 +0200

    SLVS-1997 Parse settings.json comma separated sonarlint.analysisExcludesStandalone (#6129)

---
+++ src/Core.UnitTests/UserRuleSettings/AnalysisSettingsTests.cs
@@ -0,0 +1,143 @@
+﻿using Newtonsoft.Json;
+using SonarLint.VisualStudio.Core.UserRuleSettings;
+
+namespace SonarLint.VisualStudio.Core.UnitTests.UserRuleSettings;
+
+[TestClass]
+public class AnalysisSettingsTests
+{
+    [TestMethod]
+    public void AnalysisSettings_SerializesCorrectly()
+    {
+        var settings = new AnalysisSettings
+        {
+            Rules =
+            {
+                { "typescript:S2685", new RuleConfig { Level = RuleLevel.On, Parameters = new Dictionary<string, string> { { "key1", "value1" } } } }
+            },
+            FileExclusions = ["file1.cpp", "**/obj/*", "file2.cpp"]
+        };
+        const string expectedJson =
+            """
+            {
+              "sonarlint.rules": {
+                "typescript:S2685": {
+                  "level": "On",
+                  "parameters": {
+                    "key1": "value1"
+                  }
+                }
+              },
+              "sonarlint.analysisExcludesStandalone": "file1.cpp,**/obj/*,file2.cpp"
+            }
+            """;
+
+        var json = JsonConvert.SerializeObject(settings, Formatting.Indented);
+
+        json.Should().Be(expectedJson);
+    }
+
+    [TestMethod]
+    public void AnalysisSettings_DeserializesCorrectly()
+    {
+        const string json = """
+                            {
+                              "sonarlint.rules": {
+                                "typescript:S2685": {
+                                  "level": "On",
+                                  "parameters": {
+                                    "key1": "value1"
+                                  }
+                                }
+                              },
+                              "sonarlint.analysisExcludesStandalone": "file1.cpp,**/obj/*,file2.cpp"
+                            }
+                            """;
+
+        var settings = JsonConvert.DeserializeObject<AnalysisSettings>(json);
+
+        settings.Rules.Should().BeEquivalentTo(
+            new Dictionary<string, RuleConfig>
+            {
+                { "typescript:S2685", new RuleConfig { Level = RuleLevel.On, Parameters = new Dictionary<string, string> { { "key1", "value1" } } } }
+            });
+        settings.FileExclusions.Should().BeEquivalentTo("file1.cpp", "**/obj/*", "file2.cpp");
+    }
+
+    [TestMethod]
+    public void AnalysisSettings_FileExclusions_SerializesCorrectly()
+    {
+        var settings = new AnalysisSettings { FileExclusions = ["file1.cpp", "**/obj/*", "file2.cpp"] };
+        const string expectedJson =
+            """
+            {
+              "sonarlint.rules": {},
+              "sonarlint.analysisExcludesStandalone": "file1.cpp,**/obj/*,file2.cpp"
+            }
+            """;
+
+        var json = JsonConvert.SerializeObject(settings, Formatting.Indented);
+
+        json.Should().Be(expectedJson);
+    }
+
+    [TestMethod]
+    public void AnalysisSettings_FileExclusions_SerializesAndIgnoresIfNotPresent()
+    {
+        var settings = new AnalysisSettings();
+        const string expectedJson =
+            """
+            {
+              "sonarlint.rules": {}
+            }
+            """;
+
+        var json = JsonConvert.SerializeObject(settings, Formatting.Indented);
+
+        json.Should().Be(expectedJson);
+    }
+
+    [TestMethod]
+    public void AnalysisSettings_FileExclusions_DeserializesCorrectly()
+    {
+        const string json = """
+                            {
+                              "sonarlint.rules": {},
+                              "sonarlint.analysisExcludesStandalone": "file1.cpp,**/obj/*,,file2.cpp"
+                            }
+                            """;
+
+        var settings = JsonConvert.DeserializeObject<AnalysisSettings>(json);
+
+        settings.FileExclusions.Should().BeEquivalentTo("file1.cpp", "**/obj/*", "file2.cpp");
+    }
+
+    [TestMethod]
+    public void AnalysisSettings_FileExclusions_DeserializesAndIgnoresIfNotPresent()
+    {
+        const string json = """
+                            {
+                              "sonarlint.rules": {}
+                            }
+                            """;
+
+        var settings = JsonConvert.DeserializeObject<AnalysisSettings>(json);
+
+        settings.FileExclusions.Should().BeNull();
+    }
+
+    [TestMethod]
+    public void AnalysisSettings_DeserializesAndIgnoresIfNotString()
+    {
+        const string json = """
+                            {
+                              "sonarlint.rules": {},
+                              "sonarlint.analysisExcludesStandalone": 12
+                            }
+                            """;
+
+        var settings = JsonConvert.DeserializeObject<AnalysisSettings>(json);
+
+        settings.FileExclusions.Should().BeNull();
+    }
+}
---
+++ src/Core/Helpers/CommaSeparatedStringArrayConverter.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace SonarLint.VisualStudio.Core.Helpers;
+
+public class CommaSeparatedStringArrayConverter : JsonConverter
+{
+    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+    {
+        if (value is not string[] strings)
+        {
+            return;
+        }
+
+        writer.WriteValue(string.Join(",", strings));
+    }
+
+    public override object ReadJson(
+        JsonReader reader,
+        Type objectType,
+        object existingValue,
+        JsonSerializer serializer)
+    {
+        var value = reader.Value as string;
+        return value?.Split([','], StringSplitOptions.RemoveEmptyEntries);
+    }
+
+    public override bool CanConvert(Type objectType) => objectType == typeof(string[]);
+}
