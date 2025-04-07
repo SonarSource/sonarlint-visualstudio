61d7f11c90b0da21bdb247e898695432b67a1f9a
Vasileios Naskos <168648790+vnaskos-sonar@users.noreply.github.com>
2025-04-07 09:07:26 +0200

    SLVS-1997 Parse settings.json comma separated sonarlint.analysisExcludesStandalone (#6129)

--- src/Core/UserRuleSettings/AnalysisSettingsSerializer.cs
+++ src/Core/UserRuleSettings/AnalysisSettingsSerializer.cs
@@ -21,64 +21,55 @@
 using System.IO.Abstractions;
 using Newtonsoft.Json;
 
-namespace SonarLint.VisualStudio.Core.UserRuleSettings
+namespace SonarLint.VisualStudio.Core.UserRuleSettings;
+
+public interface IAnalysisSettingsSerializer
 {
-    public interface IAnalysisSettingsSerializer
+    AnalysisSettings SafeLoad(string filePath);
+    void SafeSave(string filePath, AnalysisSettings data);
+}
+
+/// <summary>
+/// Loads and saves rules settings to disc.
+/// Logs user-friendly messages and suppresses non-critical exceptions.
+/// </summary>
+public class AnalysisSettingsSerializer(IFileSystem fileSystem, ILogger logger) : IAnalysisSettingsSerializer
+{
+    private readonly IFileSystem fileSystem = fileSystem ?? throw new ArgumentNullException(nameof(fileSystem));
+    private readonly ILogger logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+    public AnalysisSettings SafeLoad(string filePath)
     {
-        AnalysisSettings SafeLoad(string filePath);
-        void SafeSave(string filePath, AnalysisSettings data);
+        if (!fileSystem.File.Exists(filePath))
+        {
+            logger?.WriteLine(CoreStrings.Settings_NoSettingsFile, filePath);
+            return null;
+        }
+
+        try
+        {
+            logger?.WriteLine(CoreStrings.Settings_LoadedSettingsFile, filePath);
+            var data = fileSystem.File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<AnalysisSettings>(data);
+        }
+        catch (Exception ex) when (!ErrorHandler.IsCriticalException(ex))
+        {
+            logger?.WriteLine(CoreStrings.Settings_ErrorLoadingSettingsFile, filePath, ex.Message);
+            return null;
+        }
     }
 
-    /// <summary>
-    /// Loads and saves rules settings to disc.
-    /// Logs user-friendly messages and suppresses non-critical exceptions.
-    /// </summary>
-    public class AnalysisSettingsSerializer : IAnalysisSettingsSerializer
+    public void SafeSave(string filePath, AnalysisSettings data)
     {
-        private readonly IFileSystem fileSystem;
-        private readonly ILogger logger;
-
-        public AnalysisSettingsSerializer(IFileSystem fileSystem, ILogger logger)
+        try
         {
-            this.fileSystem = fileSystem ?? throw new ArgumentNullException(nameof(fileSystem));
-            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            var dataAsText = JsonConvert.SerializeObject(data, Formatting.Indented);
+            fileSystem.File.WriteAllText(filePath, dataAsText);
+            logger?.WriteLine(CoreStrings.Settings_SavedSettingsFile, filePath);
         }
-
-        public AnalysisSettings SafeLoad(string filePath)
+        catch (Exception ex) when (!ErrorHandler.IsCriticalException(ex))
         {
-            AnalysisSettings settings = null;
-            if (!fileSystem.File.Exists(filePath))
-            {
-                logger?.WriteLine(CoreStrings.Settings_NoSettingsFile, filePath);
-            }
-            else
-            {
-                try
-                {
-                    logger?.WriteLine(CoreStrings.Settings_LoadedSettingsFile, filePath);
-                    var data = fileSystem.File.ReadAllText(filePath);
-                    settings = JsonConvert.DeserializeObject<AnalysisSettings>(data);
-                }
-                catch (Exception ex) when (!ErrorHandler.IsCriticalException(ex))
-                {
-                    logger?.WriteLine(CoreStrings.Settings_ErrorLoadingSettingsFile, filePath, ex.Message);
-                }
-            }
-            return settings;
-        }
-
-        public void SafeSave(string filePath, AnalysisSettings data)
-        {
-            try
-            {
-                string dataAsText = JsonConvert.SerializeObject(data, Formatting.Indented);
-                fileSystem.File.WriteAllText(filePath, dataAsText);
-                logger?.WriteLine(CoreStrings.Settings_SavedSettingsFile, filePath);
-            }
-            catch (Exception ex) when (!ErrorHandler.IsCriticalException(ex))
-            {
-                logger?.WriteLine(CoreStrings.Settings_ErrorSavingSettingsFile, filePath, ex.Message);
-            }
+            logger?.WriteLine(CoreStrings.Settings_ErrorSavingSettingsFile, filePath, ex.Message);
         }
     }
 }
