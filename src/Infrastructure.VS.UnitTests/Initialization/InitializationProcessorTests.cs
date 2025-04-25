931b293734f327adf5e09d01a4034a00ecbea687
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2025-04-25 10:54:11 +0200

    SLVS-2072 Add initialization interfaces & helpers (#6162)
    [SLVS-2072](https://sonarsource.atlassian.net/browse/SLVS-2072)
    
    
    
    [SLVS-2072]:
    https://sonarsource.atlassian.net/browse/SLVS-2072?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ src/Infrastructure.VS.UnitTests/Initialization/InitializationProcessorTests.cs
@@ -0,0 +1,173 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using NSubstitute.ExceptionExtensions;
+using NSubstitute.ReceivedExtensions;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.Core.Initialization;
+using SonarLint.VisualStudio.Core.Synchronization;
+using SonarLint.VisualStudio.Infrastructure.VS.Initialization;
+
+namespace SonarLint.VisualStudio.Infrastructure.VS.UnitTests.Initialization;
+
+[TestClass]
+public class InitializationProcessorTests
+{
+    private IAsyncLockFactory asyncLockFactory;
+    private IThreadHandling threadHandling;
+    private TestLogger testLogger;
+    private InitializationProcessor testSubject;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        asyncLockFactory = Substitute.For<IAsyncLockFactory>();
+        threadHandling = Substitute.For<IThreadHandling>();
+        threadHandling.RunOnBackgroundThread(Arg.Any<Func<Task<int>>>()).Returns(info => info.Arg<Func<Task<int>>>().Invoke());
+        testLogger = new TestLogger();
+        testSubject = new InitializationProcessor(asyncLockFactory, threadHandling, testLogger);
+    }
+
+    [TestMethod]
+    public void MefCtor_CheckIsExported() =>
+        MefTestHelpers.CheckTypeCanBeImported<InitializationProcessor, IInitializationProcessor>(
+            MefTestHelpers.CreateExport<IAsyncLockFactory>(),
+            MefTestHelpers.CreateExport<IThreadHandling>(),
+            MefTestHelpers.CreateExport<ILogger>());
+
+    [TestMethod]
+    public void MefCtor_CheckIsNonShared() => MefTestHelpers.CheckIsNonSharedMefComponent<InitializationProcessor>();
+
+    [TestMethod]
+    public void IsFinalized_NotStarted_ReturnsFalse() =>
+        testSubject.IsFinalized.Should().BeFalse();
+
+    [TestMethod]
+    public async Task IsFinalized_StartedButNotFinished_ReturnsFalse()
+    {
+        var barrier = new TaskCompletionSource<byte>();
+        var initialization = Substitute.For<Func<IThreadHandling, Task>>();
+        initialization.Invoke(threadHandling).Returns(barrier.Task);
+
+        var initializationProcessTask = testSubject.InitializeAsync(default, [], initialization);
+        testSubject.IsFinalized.Should().BeFalse();
+
+        barrier.SetResult(1);
+        await initializationProcessTask;
+        testSubject.IsFinalized.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public async Task InitializeAsync_NoDependencies_Completes()
+    {
+        var initialization = Substitute.For<Func<IThreadHandling, Task>>();
+
+        await testSubject.InitializeAsync(default, [], initialization);
+
+        testSubject.IsFinalized.Should().BeTrue();
+        Received.InOrder(() =>
+        {
+            var asyncLock = asyncLockFactory.Create();
+            threadHandling.RunOnBackgroundThread(Arg.Any<Func<Task<int>>>());
+            var acquireAsync = asyncLock.AcquireAsync().GetAwaiter().GetResult();
+            initialization.Invoke(threadHandling);
+            acquireAsync.Dispose();
+        });
+    }
+
+    [TestMethod]
+    public async Task InitializeAsync_CompletesOnlyOnce()
+    {
+        var dependency1 = Substitute.For<IRequireInitialization>();
+        var dependency2 = Substitute.For<IRequireInitialization>();
+        var initialization = Substitute.For<Func<IThreadHandling, Task>>();
+
+        await testSubject.InitializeAsync(default, [dependency1, dependency2], initialization);
+        await testSubject.InitializeAsync(default, [dependency1, dependency2], initialization);
+        await testSubject.InitializeAsync(default, [dependency1, dependency2], initialization);
+
+        testSubject.IsFinalized.Should().BeTrue();
+        Received.InOrder(() =>
+        {
+            var asyncLock = asyncLockFactory.Create();
+            threadHandling.RunOnBackgroundThread(Arg.Any<Func<Task<int>>>());
+            var acquireAsync = asyncLock.AcquireAsync().GetAwaiter().GetResult();
+            dependency1.InitializeAsync();
+            dependency2.InitializeAsync();
+            initialization.Invoke(threadHandling);
+            acquireAsync.Dispose();
+        });
+    }
+
+    [TestMethod]
+    public async Task InitializeAsync_CompletesOnlyOnceFromMultipleThreads()
+    {
+        var dependency1 = Substitute.For<IRequireInitialization>();
+        var dependency2 = Substitute.For<IRequireInitialization>();
+        var lockImplementation = new AsyncLock();
+        asyncLockFactory.Create().AcquireAsync().Returns(_ => lockImplementation.AcquireAsync());
+        var initialization = Substitute.For<Func<IThreadHandling, Task>>();
+
+        await Task.WhenAll(
+            Enumerable.Range(1, 20)
+                .AsParallel()
+                .Select(_ => testSubject.InitializeAsync(default, [dependency1, dependency2], initialization)));
+
+        testSubject.IsFinalized.Should().BeTrue();
+        asyncLockFactory.Create().Received(Quantity.Within(2, 20)).AcquireAsync();
+        initialization.ReceivedWithAnyArgs(1).Invoke(default);
+        dependency1.Received(1).InitializeAsync();
+        dependency2.Received(1).InitializeAsync();
+    }
+
+    [TestMethod]
+    public void InitializeAsync_DependencyThrows_ThrowsAndExecutesOnlyOnce()
+    {
+        var dependency = Substitute.For<IRequireInitialization>();
+        dependency.InitializeAsync().ThrowsAsync(new InvalidOperationException("My Failed Dependency"));
+        var initialization = Substitute.For<Func<IThreadHandling, Task>>();
+
+        var act = () => testSubject.InitializeAsync("Owner 1", [dependency], initialization);
+        act.Should().ThrowAsync<InvalidOperationException>();
+        act.Should().ThrowAsync<InvalidOperationException>();
+        act.Should().ThrowAsync<InvalidOperationException>();
+
+        testSubject.IsFinalized.Should().BeTrue();
+        dependency.Received(1).InitializeAsync();
+        initialization.DidNotReceiveWithAnyArgs().Invoke(default);
+        testLogger.OutputStrings.Last().Should().ContainAll("Owner 1", "My Failed Dependency");
+    }
+
+    [TestMethod]
+    public void InitializeAsync_InitializationThrows_ThrowsAndExecutesOnlyOnce()
+    {
+        var initialization = Substitute.For<Func<IThreadHandling, Task>>();
+        initialization.Invoke(threadHandling).ThrowsAsync(new InvalidOperationException("My Failed Operation"));
+
+        var act = () => testSubject.InitializeAsync("Owner 1", [], initialization);
+        act.Should().ThrowAsync<InvalidOperationException>();
+        act.Should().ThrowAsync<InvalidOperationException>();
+        act.Should().ThrowAsync<InvalidOperationException>();
+
+        testSubject.IsFinalized.Should().BeTrue();
+        initialization.ReceivedWithAnyArgs(1).Invoke(default);
+        testLogger.OutputStrings.Last().Should().ContainAll("Owner 1", "My Failed Operation");
+    }
+}
---
+++ src/Infrastructure.VS/Initialization/InitializationProcessor.cs
@@ -0,0 +1,134 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.ComponentModel.Composition;
+using System.Runtime.ExceptionServices;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.Core.Initialization;
+using SonarLint.VisualStudio.Core.Synchronization;
+
+namespace SonarLint.VisualStudio.Infrastructure.VS.Initialization;
+
+[Export(typeof(IInitializationProcessor))]
+[PartCreationPolicy(CreationPolicy.NonShared)]
+[method: ImportingConstructor]
+public class InitializationProcessor(
+    IAsyncLockFactory asyncLockFactory,
+    IThreadHandling threadHandling,
+    ILogger logger) : IInitializationProcessor
+{
+    private readonly IAsyncLock initializationProcessLock = asyncLockFactory.Create();
+    private readonly InitializationStateManager state = new();
+
+    public bool IsFinalized => state.InitializationState.IsInitialized;
+
+    public Task InitializeAsync(
+        string owner,
+        IReadOnlyCollection<IRequireInitialization> dependencies,
+        Func<IThreadHandling, Task> initialization) =>
+        !CheckInitialized()
+            ? threadHandling.RunOnBackgroundThread(() => InitializeInternalAsync(owner, dependencies, initialization))
+            : Task.CompletedTask;
+
+    private async Task InitializeInternalAsync(string owner, IReadOnlyCollection<IRequireInitialization> dependencies, Func<IThreadHandling, Task> initialization)
+    {
+        using (await initializationProcessLock.AcquireAsync())
+        {
+            if (CheckInitialized())
+            {
+                return;
+            }
+
+            await DoInitializationAsync(owner, dependencies, initialization);
+        }
+    }
+
+    private async Task DoInitializationAsync(string owner, IReadOnlyCollection<IRequireInitialization> dependencies, Func<IThreadHandling, Task> initialization)
+    {
+        var loggerContext = new MessageLevelContext { VerboseContext = [owner] };
+        try
+        {
+            var initialThread = threadHandling.CheckAccess();
+            logger.LogVerbose(loggerContext, Resources.InitializationProcessor_Start);
+            await Task.WhenAll(dependencies.Select(x => x.InitializeAsync()));
+            await initialization(threadHandling);
+            Debug.Assert(initialThread == threadHandling.CheckAccess(), "Thread switching should not happen");
+
+            state.InitializationState = InitializationState.Success();
+            logger.LogVerbose(loggerContext, Resources.InitializationProcessor_Finish);
+        }
+        catch (Exception e)
+        {
+            logger.WriteLine(loggerContext, e.ToString());
+            state.InitializationState = InitializationState.Failure(ExceptionDispatchInfo.Capture(e));
+            throw;
+        }
+    }
+
+    private bool CheckInitialized()
+    {
+        var initializationState = state.InitializationState;
+        initializationState.ThrowIfFailedInitialization();
+        return initializationState.IsInitialized;
+    }
+
+    private sealed class InitializationStateManager
+    {
+        private readonly object updateLock = new();
+        private InitializationState initializationState;
+
+        public InitializationState InitializationState
+        {
+            get
+            {
+                lock (updateLock)
+                {
+                    return initializationState;
+                }
+            }
+            set
+            {
+                lock (updateLock)
+                {
+                    initializationState = value;
+                }
+            }
+        }
+    }
+
+    private readonly struct InitializationState
+    {
+        private readonly ExceptionDispatchInfo initializationException;
+
+        public static InitializationState Success() => new(true, null);
+
+        public static InitializationState Failure(ExceptionDispatchInfo initializationException) => new(true, initializationException);
+
+        private InitializationState(bool isInitialized, ExceptionDispatchInfo initializationException)
+        {
+            IsInitialized = isInitialized;
+            this.initializationException = initializationException;
+        }
+
+        public bool IsInitialized { get; }
+
+        public void ThrowIfFailedInitialization() => initializationException?.Throw();
+    }
+}
