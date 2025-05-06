61878c5a6f180f3630ac6dfd952ff1bad56c09d5
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2025-05-06 11:38:56 +0200

    SLVS-2091 Fix compilation errors after rebase

--- src/Integration.Vsix/Settings/SolutionSettings/SolutionSettingsDialog.xaml.cs
+++ src/Integration.Vsix/Settings/SolutionSettings/SolutionSettingsDialog.xaml.cs
@@ -51,7 +51,7 @@ internal sealed partial class SolutionSettingsDialog : Window
     private void OpenFile(object sender, RequestNavigateEventArgs e)
     {
         // TODO by https://sonarsource.atlassian.net/browse/SLVS-2109: open the solution level settings.json
-        DocumentOpener.OpenDocumentInVs(serviceProvider, userSettingsProvider.SettingsFilePath);
+        DocumentOpener.OpenDocumentInVs(serviceProvider, userSettingsProvider.GlobalAnalysisSettingsFilePath);
         ApplyAndClose();
     }
 
