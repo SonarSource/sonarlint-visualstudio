c972b6fbb4b98fd173a11dff2cd4cd1587a1d81b
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2025-05-15 12:50:59 +0200

    SLVS-2139 Add RPC methods

---
+++ src/SLCore.UnitTests/Service/Hotspot/CheckStatusChangePermittedParamsTests.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using SonarLint.VisualStudio.SLCore.Service.Hotspot;
+
+namespace SonarLint.VisualStudio.SLCore.UnitTests.Service.Hotspot;
+
+[TestClass]
+public class CheckStatusChangePermittedParamsTests
+{
+    [TestMethod]
+    public void Serialized_AsExpected()
+    {
+        const string expected = """
+                                {
+                                  "connectionId": "CONNECTION_ID",
+                                  "hotspotKey": "hotspotKey"
+                                }
+                                """;
+
+        var checkStatusChangePermittedParams = new CheckStatusChangePermittedParams("CONNECTION_ID", "hotspotKey");
+
+        JsonConvert.SerializeObject(checkStatusChangePermittedParams, Formatting.Indented).Should().Be(expected);
+    }
+}
