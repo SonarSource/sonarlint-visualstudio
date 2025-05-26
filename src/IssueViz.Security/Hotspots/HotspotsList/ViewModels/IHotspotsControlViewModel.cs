0b9a75cdefa4015c7ca9df010aa0e027bbebd92e
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2025-05-26 09:01:20 +0000

    SLVS-2124 Allow hotspots to be filtered by location (#6222)

---
+++ src/IssueViz.Security/Hotspots/HotspotsList/ViewModels/IHotspotsControlViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using SonarLint.VisualStudio.Core.Analysis;
+using SonarLint.VisualStudio.IssueVisualization.IssueVisualizationControl.ViewModels.Commands;
+
+namespace SonarLint.VisualStudio.IssueVisualization.Security.Hotspots.HotspotsList.ViewModels;
+
+internal interface IHotspotsControlViewModel : IDisposable
+{
+    ObservableCollection<IHotspotViewModel> Hotspots { get; }
+    IHotspotViewModel SelectedHotspot { get; }
+
+    LocationFilterViewModel SelectedLocationFilter { get; set; }
+    ObservableCollection<LocationFilterViewModel> LocationFilters { get; }
+
+    ICommand NavigateCommand { get; }
+
+    INavigateToRuleDescriptionCommand NavigateToRuleDescriptionCommand { get; }
+
+    bool IsCloud { get; }
+
+    Task<IEnumerable<HotspotStatus>> GetAllowedStatusesAsync();
+
+    Task ChangeHotspotStatusAsync(HotspotStatus newStatus);
+}
+
+public enum LocationFilter
+{
+    CurrentDocument,
+    OpenDocuments,
+}
