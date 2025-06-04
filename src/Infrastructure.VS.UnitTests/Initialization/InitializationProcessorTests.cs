091b638bbe50a582eba8204972e53c6a2eda7096
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2025-06-04 15:16:03 +0200

    SLVS-2162 Block initialization to increase the chance of multiple threads trying to initialize at the same time

--- src/Infrastructure.VS.UnitTests/Initialization/InitializationProcessorTests.cs
+++ src/Infrastructure.VS.UnitTests/Initialization/InitializationProcessorTests.cs
@@ -112,17 +112,22 @@ public class InitializationProcessorTests
     {
         var dependency1 = Substitute.For<IRequireInitialization>();
         var dependency2 = Substitute.For<IRequireInitialization>();
+        var manualResetEvent = new ManualResetEvent(false);
+        initialization.When(x => x.Invoke(Arg.Any<IThreadHandling>())).Do(x =>
+        {
+            // block initialization to increase the chance of multiple threads trying to initialize at the same time
+            manualResetEvent.WaitOne(50);
+        });
         var lockImplementation = new AsyncLock();
         asyncLockFactory.Create().AcquireAsync().Returns(_ => lockImplementation.AcquireAsync());
         var testSubject = testSubjectFactory.Create<InitializationProcessorTests>([dependency1, dependency2], initialization);
 
-        await Task.WhenAll(
-            Enumerable.Range(1, 20)
-                .AsParallel()
-                .Select(_ => testSubject.InitializeAsync()));
+        var tasks = Enumerable.Range(1, 20).AsParallel().Select(_ => testSubject.InitializeAsync()).ToArray();
+        manualResetEvent.Set();
+        await Task.WhenAll(tasks);
 
         testSubject.IsFinalized.Should().BeTrue();
-        asyncLockFactory.Create().Received(Quantity.Within(1, 20)).AcquireAsync();
+        asyncLockFactory.Create().Received(Quantity.Within(2, 20)).AcquireAsync();
         initialization.ReceivedWithAnyArgs(1).Invoke(default);
         dependency1.InitializationProcessor.Received(1).InitializeAsync();
         dependency2.InitializationProcessor.Received(1).InitializeAsync();
