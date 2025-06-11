f0a619ee2fa8330de403b9d65d0aaa49474217fa
Georgii Borovinskikh <georgii.borovinskikh@sonarsource.com>
2025-06-11 15:25:39 +0200

    SLVS-2041, SLVS-2166 Update compilation database on config scope change

---
+++ src/Integration.Vsix.UnitTests/CFamily/ActiveCompilationDatabaseTrackerTests.cs
@@ -0,0 +1,387 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using NSubstitute.ReturnsExtensions;
+using SonarLint.VisualStudio.CFamily;
+using SonarLint.VisualStudio.CFamily.CMake;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.Core.CFamily;
+using SonarLint.VisualStudio.Core.ConfigurationScope;
+using SonarLint.VisualStudio.Core.Initialization;
+using SonarLint.VisualStudio.Core.Synchronization;
+using SonarLint.VisualStudio.Integration.Vsix.CFamily;
+using SonarLint.VisualStudio.SLCore.Core;
+using SonarLint.VisualStudio.SLCore.Service.Analysis;
+
+namespace SonarLint.VisualStudio.Integration.UnitTests.CFamily;
+
+[TestClass]
+public class ActiveCompilationDatabaseTrackerTests
+{
+    private const string DefaultConfigScope = "scope1";
+    private const string CmakeJson = "cmake.json";
+    private const string VcxJson = "vcx.json";
+    private ICMakeCompilationDatabaseLocator cMakeCompilationDatabaseLocator;
+    private IActiveVcxCompilationDatabase activeVcxCompilationDatabase;
+    private IActiveConfigScopeTracker activeConfigScopeTracker;
+    private IInitializationProcessorFactory initializationProcessorFactory;
+    private ICFamilyAnalysisConfigurationSLCoreService cFamilyAnalysisConfigurationSlCore;
+    private IAsyncLockFactory asyncLockFactory;
+    private IThreadHandling threadHandling;
+    private TestLogger testLogger;
+    private ISLCoreServiceProvider serviceProvider;
+    private IAsyncLock asyncLock;
+
+    private readonly IRequireInitialization[] initializationDependencies = [];
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        cMakeCompilationDatabaseLocator = Substitute.For<ICMakeCompilationDatabaseLocator>();
+        activeVcxCompilationDatabase = Substitute.For<IActiveVcxCompilationDatabase>();
+        activeConfigScopeTracker = Substitute.For<IActiveConfigScopeTracker>();
+        initializationProcessorFactory = Substitute.For<IInitializationProcessorFactory>();
+        cFamilyAnalysisConfigurationSlCore = Substitute.For<ICFamilyAnalysisConfigurationSLCoreService>();
+        asyncLockFactory = Substitute.For<IAsyncLockFactory>();
+        threadHandling = Substitute.ForPartsOf<NoOpThreadHandler>();
+        testLogger = Substitute.ForPartsOf<TestLogger>();
+        serviceProvider = Substitute.For<ISLCoreServiceProvider>();
+        SetUpServiceProvider();
+        asyncLock = Substitute.For<IAsyncLock>();
+        asyncLockFactory.Create().Returns(asyncLock);
+    }
+
+    [TestMethod]
+    public void MefCtor_CheckIsExported() =>
+        MefTestHelpers.CheckTypeCanBeImported<ActiveCompilationDatabaseTracker, IActiveCompilationDatabaseTracker>(
+            MefTestHelpers.CreateExport<ICMakeCompilationDatabaseLocator>(),
+            MefTestHelpers.CreateExport<IActiveVcxCompilationDatabase>(),
+            MefTestHelpers.CreateExport<IActiveConfigScopeTracker>(),
+            MefTestHelpers.CreateExport<IInitializationProcessorFactory>(),
+            MefTestHelpers.CreateExport<IAsyncLockFactory>(),
+            MefTestHelpers.CreateExport<IThreadHandling>(),
+            MefTestHelpers.CreateExport<ISLCoreServiceProvider>());
+
+    [TestMethod]
+    public void MefCtor_CheckIsSingleton() => MefTestHelpers.CheckIsSingletonMefComponent<ActiveCompilationDatabaseTracker>();
+
+    [TestMethod]
+    public void Ctor_RunsInitialization()
+    {
+        var testSubject = CreateAndInitializeTestSubject();
+
+        Received.InOrder(() =>
+        {
+            initializationProcessorFactory.Create<ActiveCompilationDatabaseTracker>(Arg.Is<IReadOnlyCollection<IRequireInitialization>>(x => x.SequenceEqual(initializationDependencies)),
+                Arg.Any<Func<IThreadHandling, Task>>());
+            testSubject.InitializationProcessor.InitializeAsync();
+            activeVcxCompilationDatabase.EnsureDatabaseInitializedAsync();
+            activeConfigScopeTracker.CurrentConfigurationScopeChanged += Arg.Any<EventHandler<ConfigurationScopeChangedEventArgs>>();
+            var releaser = asyncLock.AcquireAsync().GetAwaiter().GetResult();
+            releaser.Dispose();
+            testSubject.InitializationProcessor.InitializeAsync(); // called by CreateAndInitializeTestSubject
+        });
+        testSubject.DatabasePath.Should().BeNull();
+        VerifyOnBackgroundThreadLockTakenAndReleased(1);
+    }
+
+    [TestMethod]
+    public void Ctor_WhenUninitialized_DoesNotReactToConfigScopeChangedEvents()
+    {
+        var testSubject = CreateUninitializedTestSubject(out var barrier);
+
+        RaiseEventNewConfigScope();
+        _ = activeConfigScopeTracker.DidNotReceiveWithAnyArgs().Current;
+
+        barrier.SetResult(1);
+        testSubject.InitializationProcessor.InitializeAsync().GetAwaiter().GetResult();
+
+        _ = activeConfigScopeTracker.Received(1).Current;
+        RaiseEventNewConfigScope();
+        _ = activeConfigScopeTracker.Received(2).Current;
+    }
+
+    [TestMethod]
+    public void DisposedBeforeInitialized_InitializationDoesNothing()
+    {
+        var testSubject = CreateUninitializedTestSubject(out var barrier);
+        testSubject.Dispose();
+
+        barrier.SetResult(1);
+        testSubject.InitializationProcessor.InitializeAsync().GetAwaiter().GetResult();
+
+        activeVcxCompilationDatabase.DidNotReceiveWithAnyArgs().EnsureDatabaseInitializedAsync();
+        activeVcxCompilationDatabase.DidNotReceiveWithAnyArgs().DropDatabaseAsync();
+        activeConfigScopeTracker.DidNotReceiveWithAnyArgs().CurrentConfigurationScopeChanged += Arg.Any<EventHandler<ConfigurationScopeChangedEventArgs>>();
+        activeConfigScopeTracker.DidNotReceiveWithAnyArgs().CurrentConfigurationScopeChanged -= Arg.Any<EventHandler<ConfigurationScopeChangedEventArgs>>();
+        _ = activeConfigScopeTracker.DidNotReceiveWithAnyArgs().Current;
+    }
+
+    [TestMethod]
+    public void Ctor_ConfigScopeExists_InitializesWithCMakeDatabase()
+    {
+        SetCurrentConfiguration(DefaultConfigScope,CmakeJson, null);
+
+        var testSubject = CreateAndInitializeTestSubject();
+
+        testSubject.DatabasePath.Should().Be(CmakeJson);
+        VerifyCalledService(DefaultConfigScope, CmakeJson);
+        VerifyOnBackgroundThreadLockTakenAndReleased(1);
+    }
+
+    [TestMethod]
+    public void Ctor_ConfigScopeExists_CMakeDatabaseNotAvailable_InitializesWithVcxDatabase()
+    {
+        SetCurrentConfiguration(DefaultConfigScope,null, VcxJson);
+
+        var testSubject = CreateAndInitializeTestSubject();
+
+        testSubject.DatabasePath.Should().Be(VcxJson);
+        VerifyCalledService(DefaultConfigScope, VcxJson);
+        VerifyOnBackgroundThreadLockTakenAndReleased(1);
+    }
+
+    [TestMethod]
+    public void Ctor_NoConfigurationScope_DoesNotUpdateSLCore()
+    {
+        activeConfigScopeTracker.Current.ReturnsNull();
+        cMakeCompilationDatabaseLocator.Locate().ReturnsNull();
+
+        var testSubject = CreateAndInitializeTestSubject();
+
+        testSubject.DatabasePath.Should().BeNull();
+        cFamilyAnalysisConfigurationSlCore.DidNotReceiveWithAnyArgs().DidChangePathToCompileCommands(default);
+    }
+
+    [TestMethod]
+    public void CurrentConfigurationScopeChanged_NoConfigurationScope_DoesNotUpdateSLCore()
+    {
+        var testSubject = CreateAndInitializeTestSubject();
+        activeConfigScopeTracker.Current.ReturnsNull();
+        cMakeCompilationDatabaseLocator.Locate().ReturnsNull();
+
+        RaiseEventNewConfigScope();
+
+        testSubject.DatabasePath.Should().BeNull();
+        cFamilyAnalysisConfigurationSlCore.DidNotReceiveWithAnyArgs().DidChangePathToCompileCommands(default);
+        VerifyOnBackgroundThreadLockTakenAndReleased(1 + 1);
+    }
+
+    [TestMethod]
+    public void CurrentConfigurationScopeChanged_PrefersCMakeDatabase_WhenAvailable()
+    {
+        var testSubject = CreateAndInitializeTestSubject();
+        SetCurrentConfiguration(DefaultConfigScope,CmakeJson, null);
+
+        RaiseEventNewConfigScope();
+
+        testSubject.DatabasePath.Should().Be(CmakeJson);
+        VerifyCalledService(DefaultConfigScope, CmakeJson);
+        VerifyOnBackgroundThreadLockTakenAndReleased(1 + 1);
+    }
+
+    [TestMethod]
+    public void CurrentConfigurationScopeChanged_FallsBackToVcxDatabase_WhenCMakeNotAvailable()
+    {
+        var testSubject = CreateAndInitializeTestSubject();
+        SetCurrentConfiguration(DefaultConfigScope,null, VcxJson);
+
+        RaiseEventNewConfigScope();
+
+        testSubject.DatabasePath.Should().Be(VcxJson);
+        VerifyCalledService(DefaultConfigScope, VcxJson);
+        VerifyOnBackgroundThreadLockTakenAndReleased(1 + 1);
+    }
+
+    [TestMethod]
+    public void CurrentConfigurationScopeChanged_SameConfigurationScopeId_UpdateSLCore()
+    {
+        SetCurrentConfiguration(DefaultConfigScope,CmakeJson, null);
+        var testSubject = CreateAndInitializeTestSubject();
+        testSubject.DatabasePath.Should().Be(CmakeJson); // sanity check
+        VerifyOnBackgroundThreadLockTakenAndReleased(1);
+
+        SetCurrentConfiguration(DefaultConfigScope, null, VcxJson);
+        RaiseEventNewConfigScope();
+
+        testSubject.DatabasePath.Should().Be(VcxJson);
+        VerifyCalledService(DefaultConfigScope, VcxJson);
+        VerifyOnBackgroundThreadLockTakenAndReleased(1 + 1);
+    }
+
+    [TestMethod]
+    public void CurrentConfigurationScopeChanged_DifferentConfigurationScope_UpdateSLCore()
+    {
+        SetCurrentConfiguration(DefaultConfigScope,CmakeJson, null);
+        var testSubject = CreateAndInitializeTestSubject();
+        testSubject.DatabasePath.Should().Be(CmakeJson); // sanity check
+        VerifyOnBackgroundThreadLockTakenAndReleased(1);
+
+        const string scope2 = "scope2";
+        SetCurrentConfiguration(scope2, null, VcxJson);
+        RaiseEventNewConfigScope();
+
+        testSubject.DatabasePath.Should().Be(VcxJson);
+        VerifyCalledService(scope2, VcxJson);
+        VerifyOnBackgroundThreadLockTakenAndReleased(1 + 1);
+    }
+
+    [TestMethod]
+    public void CurrentConfigurationScopeChanged_SameConfigurationScopeId_NotRedeclaredButUpdated_DoesNothing()
+    {
+        SetCurrentConfiguration(DefaultConfigScope,CmakeJson, null);
+        var testSubject = CreateAndInitializeTestSubject();
+        testSubject.DatabasePath.Should().Be(CmakeJson); // sanity check
+        VerifyOnBackgroundThreadLockTakenAndReleased(1);
+
+        cFamilyAnalysisConfigurationSlCore.ClearReceivedCalls();
+        cMakeCompilationDatabaseLocator.ClearReceivedCalls();
+        activeVcxCompilationDatabase.ClearReceivedCalls();
+        activeConfigScopeTracker.CurrentConfigurationScopeChanged += Raise.EventWith<ConfigurationScopeChangedEventArgs>(new(false));
+
+        testSubject.DatabasePath.Should().Be(CmakeJson);
+        cFamilyAnalysisConfigurationSlCore.DidNotReceiveWithAnyArgs().DidChangePathToCompileCommands(default);
+        cMakeCompilationDatabaseLocator.DidNotReceiveWithAnyArgs().Locate();
+        _ = activeVcxCompilationDatabase.DidNotReceiveWithAnyArgs().DatabasePath;
+        VerifyOnBackgroundThreadLockTakenAndReleased(1); // still one invocation, as the event is just ignored
+    }
+
+    [TestMethod]
+    public void CurrentConfigurationScopeChanged_ServiceNotAvailable_SetsToNull()
+    {
+        SetCurrentConfiguration(DefaultConfigScope,CmakeJson, null);
+        var testSubject = CreateAndInitializeTestSubject();
+
+        serviceProvider.TryGetTransientService(out ICFamilyAnalysisConfigurationSLCoreService _).Returns(false);
+        const string scope2 = "scope2";
+        SetCurrentConfiguration(scope2, null, VcxJson);
+        RaiseEventNewConfigScope();
+
+        testSubject.DatabasePath.Should().BeNull();
+        cFamilyAnalysisConfigurationSlCore.DidNotReceive().DidChangePathToCompileCommands(Arg.Is<DidChangePathToCompileCommandsParams>(x => x.configurationScopeId == scope2));
+        VerifyOnBackgroundThreadLockTakenAndReleased(1 + 1);
+    }
+
+    [TestMethod]
+    public void CurrentConfigurationScopeChanged_ServiceNotAvailableAndThenAvailable_Recovers()
+    {
+        SetCurrentConfiguration(DefaultConfigScope,CmakeJson, null);
+        var testSubject = CreateAndInitializeTestSubject();
+        serviceProvider.TryGetTransientService(out ICFamilyAnalysisConfigurationSLCoreService _).Returns(false);
+
+        const string scope2 = "scope2";
+        SetCurrentConfiguration(scope2, null, VcxJson);
+        RaiseEventNewConfigScope();
+        testSubject.DatabasePath.Should().BeNull(); // sanity check
+        SetUpServiceProvider();
+        // recovery to the same id causes an update despite the optimization to ignore updates to the same id
+        SetCurrentConfiguration(DefaultConfigScope, CmakeJson, null);
+        RaiseEventNewConfigScope();
+
+        testSubject.DatabasePath.Should().Be(CmakeJson);
+        VerifyOnBackgroundThreadLockTakenAndReleased(1 + 1 + 1);
+    }
+
+    [TestMethod]
+    public void Dispose_UnsubscribesAndDisposes()
+    {
+        var testSubject = CreateAndInitializeTestSubject();
+
+        testSubject.Dispose();
+        testSubject.Dispose();
+        testSubject.Dispose();
+
+        Received.InOrder(() =>
+        {
+            activeConfigScopeTracker.CurrentConfigurationScopeChanged -= Arg.Any<EventHandler<ConfigurationScopeChangedEventArgs>>();
+            threadHandling.Run(Arg.Any<Func<Task<int>>>());
+            activeVcxCompilationDatabase.DropDatabaseAsync();
+            activeVcxCompilationDatabase.Dispose();
+            asyncLock.Dispose();
+        });
+    }
+
+    private void RaiseEventNewConfigScope() =>
+        activeConfigScopeTracker.CurrentConfigurationScopeChanged += Raise.EventWith<ConfigurationScopeChangedEventArgs>(new(true));
+
+    private void SetCurrentConfiguration(string configurationScopeId, string cmakePath, string vcxPath)
+    {
+        var newConfigScope = new ConfigurationScope(configurationScopeId);
+        activeConfigScopeTracker.Current.Returns(newConfigScope);
+        cMakeCompilationDatabaseLocator.Locate().Returns(cmakePath);
+        activeVcxCompilationDatabase.DatabasePath.Returns(vcxPath);
+    }
+
+    private void VerifyCalledService(string configurationScopeId, string databasePath) =>
+        cFamilyAnalysisConfigurationSlCore.Received()
+            .DidChangePathToCompileCommands(Arg.Is<DidChangePathToCompileCommandsParams>(
+                x => x.configurationScopeId == configurationScopeId && x.pathToCompileCommands == databasePath));
+
+
+    private void VerifyOnBackgroundThreadLockTakenAndReleased(int count) =>
+        Received.InOrder(() =>
+        {
+            for (var i = 0; i < count; i++)
+            {
+                threadHandling.RunOnBackgroundThread(Arg.Any<Func<Task<int>>>());
+                var acquireAsync = asyncLock.AcquireAsync().GetAwaiter().GetResult();
+                acquireAsync.Dispose();
+            }
+        });
+
+    private void SetUpServiceProvider() =>
+        serviceProvider.TryGetTransientService(out Arg.Any<ICFamilyAnalysisConfigurationSLCoreService>()).Returns(info =>
+        {
+            info[0] = cFamilyAnalysisConfigurationSlCore;
+            return true;
+        });
+
+    private ActiveCompilationDatabaseTracker CreateUninitializedTestSubject(out TaskCompletionSource<byte> barrier)
+    {
+        var tcs = barrier = new TaskCompletionSource<byte>();
+        initializationProcessorFactory = MockableInitializationProcessor.CreateFactory<ActiveCompilationDatabaseTracker>(threadHandling, testLogger, processor =>
+        {
+            MockableInitializationProcessor.ConfigureWithWait(processor, tcs);
+        });
+        return new ActiveCompilationDatabaseTracker(
+            cMakeCompilationDatabaseLocator,
+            activeVcxCompilationDatabase,
+            activeConfigScopeTracker,
+            initializationProcessorFactory,
+            asyncLockFactory,
+            threadHandling,
+            serviceProvider);
+    }
+
+    private ActiveCompilationDatabaseTracker CreateAndInitializeTestSubject()
+    {
+        initializationProcessorFactory = MockableInitializationProcessor.CreateFactory<ActiveCompilationDatabaseTracker>(threadHandling, testLogger);
+        var tracker = new ActiveCompilationDatabaseTracker(
+            cMakeCompilationDatabaseLocator,
+            activeVcxCompilationDatabase,
+            activeConfigScopeTracker,
+            initializationProcessorFactory,
+            asyncLockFactory,
+            threadHandling,
+            serviceProvider);
+        tracker.InitializationProcessor.InitializeAsync().GetAwaiter().GetResult();
+        return tracker;
+    }
+}
---
+++ src/Integration.Vsix/CFamily/ActiveCompilationDatabaseTracker.cs
@@ -0,0 +1,129 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Threading;
+using SonarLint.VisualStudio.CFamily;
+using SonarLint.VisualStudio.CFamily.CMake;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.Core.CFamily;
+using SonarLint.VisualStudio.Core.ConfigurationScope;
+using SonarLint.VisualStudio.Core.Initialization;
+using SonarLint.VisualStudio.Core.Synchronization;
+using SonarLint.VisualStudio.Infrastructure.VS.Initialization;
+using SonarLint.VisualStudio.SLCore.Core;
+using SonarLint.VisualStudio.SLCore.Service.Analysis;
+
+namespace SonarLint.VisualStudio.Integration.Vsix.CFamily;
+
+[Export(typeof(IActiveCompilationDatabaseTracker))]
+[PartCreationPolicy(CreationPolicy.Shared)]
+internal sealed class ActiveCompilationDatabaseTracker : IActiveCompilationDatabaseTracker
+{
+    private readonly ICMakeCompilationDatabaseLocator cMakeCompilationDatabaseLocator;
+    private readonly IActiveVcxCompilationDatabase activeVcxCompilationDatabase;
+    private readonly IActiveConfigScopeTracker activeConfigScopeTracker;
+    private readonly ISLCoreServiceProvider serviceProvider;
+    private readonly IThreadHandling threadHandling;
+    private readonly IAsyncLock asyncLock;
+    private bool isDisposed;
+    private string lastConfigScopeId;
+
+    public IInitializationProcessor InitializationProcessor { get; }
+    public string DatabasePath { get; private set; }
+
+    [ImportingConstructor]
+    public ActiveCompilationDatabaseTracker(
+        ICMakeCompilationDatabaseLocator cMakeCompilationDatabaseLocator,
+        IActiveVcxCompilationDatabase activeVcxCompilationDatabase,
+        IActiveConfigScopeTracker activeConfigScopeTracker,
+        IInitializationProcessorFactory initializationProcessorFactory,
+        IAsyncLockFactory asyncLockFactory,
+        IThreadHandling threadHandling,
+        ISLCoreServiceProvider serviceProvider)
+    {
+        this.cMakeCompilationDatabaseLocator = cMakeCompilationDatabaseLocator;
+        this.activeVcxCompilationDatabase = activeVcxCompilationDatabase;
+        this.activeConfigScopeTracker = activeConfigScopeTracker;
+        this.threadHandling = threadHandling;
+        this.serviceProvider = serviceProvider;
+        asyncLock = asyncLockFactory.Create();
+        InitializationProcessor = initializationProcessorFactory.CreateAndStart<ActiveCompilationDatabaseTracker>([],
+            async () =>
+            {
+                if (isDisposed)
+                {
+                    return;
+                }
+                await activeVcxCompilationDatabase.EnsureDatabaseInitializedAsync();
+                activeConfigScopeTracker.CurrentConfigurationScopeChanged += ActiveConfigScopeTracker_CurrentConfigurationScopeChanged;
+                await HandleConfigScopeEventAsync();
+            });
+    }
+
+    private void ActiveConfigScopeTracker_CurrentConfigurationScopeChanged(object sender, ConfigurationScopeChangedEventArgs e)
+    {
+        if (!e.DefinitionChanged)
+        {
+            return;
+        }
+        threadHandling.RunOnBackgroundThread(HandleConfigScopeEventAsync).Forget();
+    }
+
+    private async Task HandleConfigScopeEventAsync()
+    {
+        using (await asyncLock.AcquireAsync())
+        {
+            if (activeConfigScopeTracker.Current is { Id: { } currentConfigScopeId } && serviceProvider.TryGetTransientService(out ICFamilyAnalysisConfigurationSLCoreService cFamilyAnalysisConfiguration))
+            {
+                lastConfigScopeId = currentConfigScopeId;
+                DatabasePath = cMakeCompilationDatabaseLocator.Locate() ?? activeVcxCompilationDatabase.DatabasePath;
+                cFamilyAnalysisConfiguration.DidChangePathToCompileCommands(new(currentConfigScopeId, DatabasePath));
+            }
+            else
+            {
+                lastConfigScopeId = null;
+                DatabasePath = null;
+            }
+        }
+    }
+
+    public void Dispose()
+    {
+        if (isDisposed)
+        {
+            return;
+        }
+
+        if (InitializationProcessor.IsFinalized)
+        {
+            activeConfigScopeTracker.CurrentConfigurationScopeChanged -= ActiveConfigScopeTracker_CurrentConfigurationScopeChanged;
+            threadHandling.Run(async () =>
+            {
+                await activeVcxCompilationDatabase.DropDatabaseAsync();
+                return 0;
+            });
+        }
+
+        activeVcxCompilationDatabase.Dispose();
+        asyncLock.Dispose();
+        isDisposed = true;
+    }
+}
