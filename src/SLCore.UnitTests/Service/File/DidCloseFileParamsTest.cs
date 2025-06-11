6b65880f643a5a8237950ac594e19f48bbe347a7
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2025-06-11 11:24:21 +0200

    SLVS-2034 Add RPC methods `didOpenFile` and `didCloseFile`

---
+++ src/SLCore.UnitTests/Service/File/DidCloseFileParamsTest.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using SonarLint.VisualStudio.SLCore.Common.Models;
+using SonarLint.VisualStudio.SLCore.Service.File;
+
+namespace SonarLint.VisualStudio.SLCore.UnitTests.Service.File;
+
+[TestClass]
+public class DidCloseFileParamsTest
+{
+    [TestMethod]
+    public void Serialize_AsExpected()
+    {
+        var testSubject = new DidCloseFileParams("some ConfigScope", new FileUri("file:///c:/Users/test/project1/Baz.cs"));
+
+        const string expectedString = """
+                                      {
+                                        "configurationScopeId": "some ConfigScope",
+                                        "fileUri": "file:///c:/Users/test/project1/Baz.cs"
+                                      }
+                                      """;
+
+        var serializedString = JsonConvert.SerializeObject(testSubject, Formatting.Indented);
+
+        serializedString.Should().Be(expectedString);
+    }
+}
