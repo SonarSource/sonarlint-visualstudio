35ff90c91a7e546ded2d8bdf1c514f5dfecec91a
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2025-06-13 17:34:22 +0200

    SLVS-2046 Drop IAnalyzerOptions

--- src/Core/Analysis/IAnalysisRequester.cs
+++ src/Core/Analysis/IAnalysisRequester.cs
@@ -18,9 +18,6 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using System;
-using System.Collections.Generic;
-
 namespace SonarLint.VisualStudio.Core.Analysis
 {
     public interface IAnalysisRequester
@@ -33,25 +30,18 @@ namespace SonarLint.VisualStudio.Core.Analysis
         /// <summary>
         /// Called to request that analysis is performed
         /// </summary>
-        /// <param name="analyzerOptions">Any analyzer-specific options. Can be null.</param>
-        /// <param name="filePaths">List of specific files to analyze. Can be null, in which case all files will be analyzed.</param>
+        /// <param name="filePaths">List of specific files to analyze. Can be null, in which case all opened files will be analyzed.</param>
         /// <remarks>There are no guarantees about whether the analysis is performed before the method
         /// returns or not.</remarks>
-        void RequestAnalysis(IAnalyzerOptions analyzerOptions, params string[] filePaths);
+        void RequestAnalysis(params string[] filePaths);
     }
 
     public class AnalysisRequestEventArgs : EventArgs
     {
-        public AnalysisRequestEventArgs(IAnalyzerOptions analyzerOptions, IEnumerable<string> filePaths)
+        public AnalysisRequestEventArgs(IEnumerable<string> filePaths)
         {
-            Options = analyzerOptions;
             FilePaths = filePaths;
         }
-
-        /// <summary>
-        /// Analyzer-specific options (optional)
-        /// </summary>
-        public IAnalyzerOptions Options { get; }
 
         /// <summary>
         /// The list of files to analyze. Null/empty = analyze all files
@@ -61,8 +51,6 @@ namespace SonarLint.VisualStudio.Core.Analysis
 
     public static class AnalysisRequesterExtensions
     {
-        public static void RequestAnalysis(this IAnalysisRequester analysisRequester, params string[] filePaths)
-             => analysisRequester.RequestAnalysis(null, filePaths);
+        public static void RequestAnalysis(this IAnalysisRequester analysisRequester, params string[] filePaths) => analysisRequester.RequestAnalysis(filePaths);
     }
-
 }
