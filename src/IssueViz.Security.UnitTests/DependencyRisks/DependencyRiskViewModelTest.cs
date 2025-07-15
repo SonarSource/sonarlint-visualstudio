1f66010b3f0389c708ad8ba78843329b43db9417
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2025-07-15 11:58:22 +0200

    SLVS-2370 Add view models

---
+++ src/IssueViz.Security.UnitTests/DependencyRisks/DependencyRiskViewModelTest.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.ComponentModel;
+using SonarLint.VisualStudio.Core.Analysis;
+using SonarLint.VisualStudio.IssueVisualization.Security.DependencyRisks;
+
+namespace SonarLint.VisualStudio.IssueVisualization.Security.UnitTests.DependencyRisks;
+
+[TestClass]
+public class DependencyRiskViewModelTest
+{
+    private DependencyRiskViewModel testSubject;
+
+    [TestInitialize]
+    public void Initialize() => testSubject = new DependencyRiskViewModel();
+
+    [TestMethod]
+    public void Type_Set_RaisesPropertyChanged()
+    {
+        var eventHandler = Substitute.For<PropertyChangedEventHandler>();
+        testSubject.PropertyChanged += eventHandler;
+
+        testSubject.Type = DependencyRiskType.ProhibitedLicense;
+
+        eventHandler.Received(1).Invoke(Arg.Any<object>(), Arg.Is<PropertyChangedEventArgs>(p => p.PropertyName == nameof(testSubject.Type)));
+    }
+
+    [TestMethod]
+    public void PackageName_Set_RaisesPropertyChanged()
+    {
+        var eventHandler = Substitute.For<PropertyChangedEventHandler>();
+        testSubject.PropertyChanged += eventHandler;
+
+        testSubject.PackageName = "System.Xaml";
+
+        eventHandler.Received(1).Invoke(Arg.Any<object>(), Arg.Is<PropertyChangedEventArgs>(p => p.PropertyName == nameof(testSubject.PackageName)));
+    }
+
+    [TestMethod]
+    public void PackageVersion_Set_RaisesPropertyChanged()
+    {
+        var eventHandler = Substitute.For<PropertyChangedEventHandler>();
+        testSubject.PropertyChanged += eventHandler;
+
+        testSubject.PackageVersion = "1.00";
+
+        eventHandler.Received(1).Invoke(Arg.Any<object>(), Arg.Is<PropertyChangedEventArgs>(p => p.PropertyName == nameof(testSubject.PackageVersion)));
+    }
+
+    [TestMethod]
+    public void ImpactSeverity_Set_RaisesPropertyChanged()
+    {
+        var eventHandler = Substitute.For<PropertyChangedEventHandler>();
+        testSubject.PropertyChanged += eventHandler;
+
+        testSubject.ImpactSeverity = DependencyRiskImpactSeverity.Blocker;
+
+        eventHandler.Received(1).Invoke(Arg.Any<object>(), Arg.Is<PropertyChangedEventArgs>(p => p.PropertyName == nameof(testSubject.ImpactSeverity)));
+    }
+}
---
+++ src/IssueViz.Security.UnitTests/DependencyRisks/GroupDependencyRiskViewModelTest.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using SonarLint.VisualStudio.IssueVisualization.Security.DependencyRisks;
+
+namespace SonarLint.VisualStudio.IssueVisualization.Security.UnitTests.DependencyRisks;
+
+[TestClass]
+public class GroupDependencyRiskViewModelTest
+{
+    private GroupDependencyRiskViewModel testSubject;
+
+    [TestInitialize]
+    public void Initialize() => testSubject = new GroupDependencyRiskViewModel();
+
+    [TestMethod]
+    public void Title_ReturnsExpectedResource() => testSubject.Title.Should().Be(Resources.DependencyRisksGroupTitle);
+
+    [TestMethod]
+    public void Risks_ContainsExpectedNumberOfItems() => testSubject.Risks.Should().HaveCount(3);
+}
