8112feb2051cdf0423ed537b5324d1e2490fee49
Georgii Borovinskikh <georgii.borovinskikh@sonarsource.com>
2025-07-15 09:57:52 +0200

    SLVS-2384 Fix threading in mef-importing ctor of FileRenameEventSource

--- src/IssueViz.UnitTests/Editor/FileRenamesEventSourceTests.cs
+++ src/IssueViz.UnitTests/Editor/FileRenamesEventSourceTests.cs
@@ -18,88 +18,111 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using FluentAssertions;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.Core.Initialization;
+using SonarLint.VisualStudio.Infrastructure.VS;
 using SonarLint.VisualStudio.IssueVisualization.Editor;
+using SonarLint.VisualStudio.TestInfrastructure;
 
 namespace SonarLint.VisualStudio.IssueVisualization.UnitTests.Editor
 {
     [TestClass]
     public class FileRenamesEventSourceTests
     {
-        private FileRenamesEventSource testSubject;
-        private Mock<IVsTrackProjectDocuments2> trackProjectDocumentsMock;
+        private IVsTrackProjectDocuments2 trackProjectDocuments;
+        private IVsUIServiceOperation serviceOperation;
+        private IInitializationProcessorFactory initializationProcessorFactory;
+        private IThreadHandling threadHandling;
         private uint cookie;
 
         [TestInitialize]
         public void TestInitialize()
         {
-            trackProjectDocumentsMock = new Mock<IVsTrackProjectDocuments2>();
-
-            var serviceProviderMock = new Mock<IServiceProvider>();
-            serviceProviderMock
-                .Setup(x => x.GetService(typeof(SVsTrackProjectDocuments)))
-                .Returns(trackProjectDocumentsMock.Object);
-
-            testSubject = new FileRenamesEventSource(serviceProviderMock.Object);
-
-            cookie = 0;
-            trackProjectDocumentsMock.Setup(x => x.AdviseTrackProjectDocumentsEvents(testSubject, out cookie));
+            trackProjectDocuments = Substitute.For<IVsTrackProjectDocuments2>();
+            serviceOperation = Substitute.For<IVsUIServiceOperation>();
+            serviceOperation
+                .When(x => x.Execute<SVsTrackProjectDocuments, IVsTrackProjectDocuments2>(Arg.Any<Action<IVsTrackProjectDocuments2>>()))
+                .Do(call =>
+                {
+                    var action = (Action<IVsTrackProjectDocuments2>)call.Args()[0];
+                    action(trackProjectDocuments);
+                });
+            threadHandling = Substitute.ForPartsOf<NoOpThreadHandler>();
         }
 
         [TestMethod]
-        public void Ctor_RegisterToAdviseTrackProjectDocumentsEvents()
+        public void MefCtor_CheckIsExported() =>
+            MefTestHelpers.CheckTypeCanBeImported<FileRenamesEventSource, IFileRenamesEventSource>(
+                MefTestHelpers.CreateExport<IVsUIServiceOperation>(),
+                MefTestHelpers.CreateExport<IInitializationProcessorFactory>());
+
+        [TestMethod]
+        public void MefCtor_CheckIsSingleton() => MefTestHelpers.CheckIsSingletonMefComponent<FileRenamesEventSource>();
+
+        [TestMethod]
+        public void Ctor_RegisterToAdviseTrackProjectDocumentsEvents_InOrder()
         {
-            trackProjectDocumentsMock.Verify(x => x.AdviseTrackProjectDocumentsEvents(testSubject, out cookie), Times.Once);
+            var testSubject = CreateAndInitializeTestSubject();
+
+            Received.InOrder(() =>
+            {
+                initializationProcessorFactory.Create<FileRenamesEventSource>(Arg.Is<IReadOnlyCollection<IRequireInitialization>>(x => x.Count == 0), Arg.Any<Func<IThreadHandling, Task>>());
+                testSubject.InitializationProcessor.InitializeAsync();
+                serviceOperation.Execute<SVsTrackProjectDocuments, IVsTrackProjectDocuments2>(Arg.Any<Action<IVsTrackProjectDocuments2>>());
+                trackProjectDocuments.AdviseTrackProjectDocumentsEvents(testSubject, out _);
+                testSubject.InitializationProcessor.InitializeAsync(); // called by CreateAndInitializeTestSubject
+            });
         }
 
         [TestMethod]
         public void Dispose_UnregisterFromAdviseTrackProjectDocumentsEvents()
         {
-            trackProjectDocumentsMock.Verify(x=> x.UnadviseTrackProjectDocumentsEvents(It.IsAny<uint>()), Times.Never);
+            cookie = 1234;
+            trackProjectDocuments.AdviseTrackProjectDocumentsEvents(Arg.Any<IVsTrackProjectDocumentsEvents2>(), out Arg.Any<uint>())
+                .Returns(info =>
+                {
+                    info[1] = cookie;
+                    return VSConstants.S_OK;
+                });
+            trackProjectDocuments.UnadviseTrackProjectDocumentsEvents(cookie).Returns(VSConstants.S_OK);
 
+            var testSubject = CreateAndInitializeTestSubject();
+            testSubject.Dispose();
             testSubject.Dispose();
 
-            trackProjectDocumentsMock.Verify(x => x.UnadviseTrackProjectDocumentsEvents(cookie), Times.Once);
+            trackProjectDocuments.Received(1).UnadviseTrackProjectDocumentsEvents(cookie);
         }
 
         [TestMethod]
         public void AfterDocumentsRenamed_NoSubscribers_NoException()
         {
-            Action act = () => RaiseDocumentsRenamed(new Dictionary<string, string> {{"old name", "new name"}});
+            var testSubject = CreateAndInitializeTestSubject();
+            Action act = () => SimulateDocumentsRenamed(testSubject, new Dictionary<string, string> { { "old name", "new name" } });
             act.Should().NotThrow();
         }
 
         [TestMethod]
         public void AfterDocumentsRenamed_HasSubscribers_RaisesEvent()
         {
-            var eventHandler = new Mock<EventHandler<FilesRenamedEventArgs>>();
-            testSubject.FilesRenamed += eventHandler.Object;
+            var testSubject = CreateAndInitializeTestSubject();
+            var eventHandler = Substitute.For<EventHandler<FilesRenamedEventArgs>>();
+            testSubject.FilesRenamed += eventHandler;
 
-            var renamedFiles = new Dictionary<string, string>
-            {
-                {"old name1", "new name1"},
-                {"old name2", "new name2"}
-            };
+            var renamedFiles = new Dictionary<string, string> { { "old name1", "new name1" }, { "old name2", "new name2" } };
 
-            RaiseDocumentsRenamed(renamedFiles);
+            SimulateDocumentsRenamed(testSubject, renamedFiles);
 
-            eventHandler.Verify(
-                x => x(testSubject, It.Is((FilesRenamedEventArgs args) =>
-                        args.OldNewFilePaths.Count == renamedFiles.Count &&
-                        args.OldNewFilePaths.All(arg => renamedFiles.ContainsKey(arg.Key) && renamedFiles[arg.Key] == arg.Value))),
-                    Times.Once());
+            eventHandler.Received(1).Invoke(testSubject, Arg.Is<FilesRenamedEventArgs>(args =>
+                args.OldNewFilePaths.Count == renamedFiles.Count &&
+                args.OldNewFilePaths.All(arg => renamedFiles.ContainsKey(arg.Key) && renamedFiles[arg.Key] == arg.Value)));
         }
 
         [TestMethod]
         public void IVsTrackProjectDocumentsEvents2_AllMethodsReturnOkStatus()
         {
+            var testSubject = CreateAndInitializeTestSubject();
             var vsTrackProjectDocumentsEvents2 = testSubject as IVsTrackProjectDocumentsEvents2;
 
             var notImplementedMethods = typeof(IVsTrackProjectDocumentsEvents2)
@@ -114,15 +137,60 @@ namespace SonarLint.VisualStudio.IssueVisualization.UnitTests.Editor
             }
         }
 
+        [TestMethod]
+        public void Initialization_DoesNotAdviseEventsBeforeInitialization()
+        {
+            var testSubject = CreateUninitializedTestSubject(out var barrier);
+
+            trackProjectDocuments.DidNotReceiveWithAnyArgs().AdviseTrackProjectDocumentsEvents(default, out _);
+
+            barrier.SetResult(1);
+            testSubject.InitializationProcessor.InitializeAsync().GetAwaiter().GetResult();
+
+            serviceOperation.Received(1).Execute<SVsTrackProjectDocuments, IVsTrackProjectDocuments2>(Arg.Any<Action<IVsTrackProjectDocuments2>>());
+            trackProjectDocuments.Received(1).AdviseTrackProjectDocumentsEvents(testSubject, out _);
+        }
+
+        [TestMethod]
+        public void Initialization_DoesNotAdviseEventsIfAlreadyDisposed()
+        {
+            var testSubject = CreateUninitializedTestSubject(out var barrier);
+
+            trackProjectDocuments.DidNotReceiveWithAnyArgs().AdviseTrackProjectDocumentsEvents(default, out _);
+
+            testSubject.Dispose();
+            barrier.SetResult(1);
+            testSubject.InitializationProcessor.InitializeAsync().GetAwaiter().GetResult();
+
+            trackProjectDocuments.DidNotReceiveWithAnyArgs().AdviseTrackProjectDocumentsEvents(default, out _);
+            trackProjectDocuments.DidNotReceiveWithAnyArgs().UnadviseTrackProjectDocumentsEvents(default);
+        }
+
+        private FileRenamesEventSource CreateAndInitializeTestSubject()
+        {
+            initializationProcessorFactory = MockableInitializationProcessor.CreateFactory<FileRenamesEventSource>(threadHandling, Substitute.For<ILogger>());
+            var testSubject = new FileRenamesEventSource(initializationProcessorFactory, serviceOperation);
+            testSubject.InitializationProcessor.InitializeAsync().GetAwaiter().GetResult();
+            return testSubject;
+        }
+
+        private FileRenamesEventSource CreateUninitializedTestSubject(out TaskCompletionSource<byte> barrier)
+        {
+            var tcs = barrier = new TaskCompletionSource<byte>();
+            initializationProcessorFactory = MockableInitializationProcessor.CreateFactory<FileRenamesEventSource>(threadHandling, Substitute.For<ILogger>(),
+                processor => MockableInitializationProcessor.ConfigureWithWait(processor, tcs));
+            return new FileRenamesEventSource(initializationProcessorFactory, serviceOperation);
+        }
+
+        private static void SimulateDocumentsRenamed(FileRenamesEventSource testSubject, IDictionary<string, string> oldNewFilePaths)
+        {
+            (testSubject as IVsTrackProjectDocumentsEvents2).OnAfterRenameFiles(0, oldNewFilePaths.Count, null, new int[0],
+                oldNewFilePaths.Keys.ToArray(), oldNewFilePaths.Values.ToArray(), null);
+        }
+
         public static object GetDefaultValue(Type type)
         {
             return type.IsValueType ? Activator.CreateInstance(type) : null;
-        }
-
-        private void RaiseDocumentsRenamed(IDictionary<string, string> oldNewFilePaths)
-        {
-            (testSubject as IVsTrackProjectDocumentsEvents2).OnAfterRenameFiles(0, 0, null, new int[0],
-                oldNewFilePaths.Keys.ToArray(), oldNewFilePaths.Values.ToArray(), null);
         }
     }
 }
