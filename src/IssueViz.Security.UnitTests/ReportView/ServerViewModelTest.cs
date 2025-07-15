fd17ff461dd4fd6a3cd6399b8440a8d9676c533d
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2025-07-15 17:45:27 +0200

    SLVS-2386 Extract logic for detecting the sever information into a base class, so it can be reused.

---
+++ src/IssueViz.Security.UnitTests/ReportView/ServerViewModelTest.cs
@@ -0,0 +1,100 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using SonarLint.VisualStudio.Core.Binding;
+using SonarLint.VisualStudio.IssueVisualization.Security.ReportView;
+
+namespace SonarLint.VisualStudio.IssueVisualization.Security.UnitTests.ReportView;
+
+[TestClass]
+public class ServerViewModelTest
+{
+    private IActiveSolutionBoundTracker activeSolutionBoundTracker;
+    private ServerViewModel testSubject;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        activeSolutionBoundTracker = Substitute.For<IActiveSolutionBoundTracker>();
+        testSubject = CreateTestSubject();
+    }
+
+    [TestMethod]
+    public void Ctor_RegistersToEvents() => activeSolutionBoundTracker.ReceivedWithAnyArgs(1).SolutionBindingChanged += Arg.Any<EventHandler<ActiveSolutionBindingEventArgs>>();
+
+    [TestMethod]
+    public void Ctor_InitializesIsCloud()
+    {
+        activeSolutionBoundTracker.CurrentConfiguration.Returns(CreateBindingConfiguration(new ServerConnection.SonarCloud("myOrg"), SonarLintMode.Connected));
+        activeSolutionBoundTracker.ClearReceivedCalls();
+
+        var hotspotsControlViewModel = CreateTestSubject();
+
+        _ = activeSolutionBoundTracker.Received(1).CurrentConfiguration;
+        hotspotsControlViewModel.IsCloud.Should().BeTrue();
+    }
+
+    [TestMethod]
+    [DataRow(SonarLintMode.Connected)]
+    [DataRow(SonarLintMode.LegacyConnected)]
+    public void SolutionBindingChanged_BindingToCloud_IsCloudIsTrue(SonarLintMode sonarLintMode)
+    {
+        var cloudBindingConfiguration = CreateBindingConfiguration(new ServerConnection.SonarCloud("my org"), sonarLintMode);
+
+        activeSolutionBoundTracker.SolutionBindingChanged += Raise.EventWith(new ActiveSolutionBindingEventArgs(cloudBindingConfiguration));
+
+        testSubject.IsCloud.Should().BeTrue();
+    }
+
+    [TestMethod]
+    [DataRow(SonarLintMode.Connected)]
+    [DataRow(SonarLintMode.LegacyConnected)]
+    public void SolutionBindingChanged_BindingToServer_IsCloudIsFalse(SonarLintMode sonarLintMode)
+    {
+        var cloudBindingConfiguration = CreateBindingConfiguration(new ServerConnection.SonarQube(new Uri("C:\\")), sonarLintMode);
+
+        activeSolutionBoundTracker.SolutionBindingChanged += Raise.EventWith(new ActiveSolutionBindingEventArgs(cloudBindingConfiguration));
+
+        testSubject.IsCloud.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void SolutionBindingChanged_Standalone_IsCloudIsFalse()
+    {
+        var cloudBindingConfiguration = new BindingConfiguration(null, SonarLintMode.Standalone, string.Empty);
+
+        activeSolutionBoundTracker.SolutionBindingChanged += Raise.EventWith(new ActiveSolutionBindingEventArgs(cloudBindingConfiguration));
+
+        testSubject.IsCloud.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void Dispose_UnsubscribesFromActiveSolutionBoundTrackerEvents()
+    {
+        testSubject.Dispose();
+
+        activeSolutionBoundTracker.ReceivedWithAnyArgs(1).SolutionBindingChanged -= Arg.Any<EventHandler<ActiveSolutionBindingEventArgs>>();
+    }
+
+    private ServerViewModel CreateTestSubject() => new(activeSolutionBoundTracker);
+
+    private static BindingConfiguration CreateBindingConfiguration(ServerConnection serverConnection, SonarLintMode mode) =>
+        new(new BoundServerProject("my solution", "my project", serverConnection), mode, string.Empty);
+}
---
+++ src/IssueViz.Security/ReportView/ServerViewModel.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using SonarLint.VisualStudio.Core.Binding;
+using SonarLint.VisualStudio.Core.WPF;
+
+namespace SonarLint.VisualStudio.IssueVisualization.Security.ReportView;
+
+internal class ServerViewModel : ViewModelBase, IDisposable
+{
+    private readonly IActiveSolutionBoundTracker activeSolutionBoundTracker;
+    private bool isCloud;
+
+    public bool IsCloud
+    {
+        get => isCloud;
+        private set
+        {
+            isCloud = value;
+            RaisePropertyChanged();
+        }
+    }
+
+    public ServerViewModel(IActiveSolutionBoundTracker activeSolutionBoundTracker)
+    {
+        this.activeSolutionBoundTracker = activeSolutionBoundTracker;
+        activeSolutionBoundTracker.SolutionBindingChanged += OnSolutionBindingChanged;
+        IsCloud = IsCurrentConfigurationToCloud(this.activeSolutionBoundTracker.CurrentConfiguration);
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposing)
+        {
+            return;
+        }
+        activeSolutionBoundTracker.SolutionBindingChanged -= OnSolutionBindingChanged;
+    }
+
+    private void OnSolutionBindingChanged(object sender, ActiveSolutionBindingEventArgs args) => IsCloud = IsCurrentConfigurationToCloud(args.Configuration);
+
+    private static bool IsCurrentConfigurationToCloud(BindingConfiguration bindingConfiguration) => bindingConfiguration?.Project?.ServerConnection is ServerConnection.SonarCloud;
+}
