6b82dd84de76003000f449277f253c4d8672801c
Gabriela Trutan <gabriela.trutan@sonarsource.com>
2025-07-21 12:50:42 +0000

    SLVS-2371 Refresh dependency risks in UI when store is updated (#6334)
    [SLVS-2371](https://sonarsource.atlassian.net/browse/SLVS-2371)
    
    
    
    [SLVS-2371]:
    https://sonarsource.atlassian.net/browse/SLVS-2371?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/IssueViz.Security.UnitTests/ReportView/ServerViewModelTest.cs
+++ src/IssueViz.Security.UnitTests/ReportView/ServerViewModelTest.cs
@@ -19,6 +19,7 @@
  */
 
 using SonarLint.VisualStudio.Core.Binding;
+using SonarLint.VisualStudio.IssueVisualization.Security.DependencyRisks;
 using SonarLint.VisualStudio.IssueVisualization.Security.ReportView;
 
 namespace SonarLint.VisualStudio.IssueVisualization.Security.UnitTests.ReportView;
@@ -93,7 +94,7 @@ public class ServerViewModelTest
         activeSolutionBoundTracker.ReceivedWithAnyArgs(1).SolutionBindingChanged -= Arg.Any<EventHandler<ActiveSolutionBindingEventArgs>>();
     }
 
-    private ServerViewModel CreateTestSubject() => new ReportViewModel(activeSolutionBoundTracker);
+    private ServerViewModel CreateTestSubject() => new ReportViewModel(activeSolutionBoundTracker, Substitute.For<IDependencyRisksStore>());
 
     private static BindingConfiguration CreateBindingConfiguration(ServerConnection serverConnection, SonarLintMode mode) =>
         new(new BoundServerProject("my solution", "my project", serverConnection), mode, string.Empty);
