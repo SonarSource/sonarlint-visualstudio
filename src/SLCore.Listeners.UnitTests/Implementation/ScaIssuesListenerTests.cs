71efddc05eea1b488836fefa551fa3c6f3f90534
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2025-07-23 10:25:13 +0200

    SLVS-2368 Update dependency risks store (#6340)
    [SLVS-2368](https://sonarsource.atlassian.net/browse/SLVS-2368)
    
    
    
    [SLVS-2368]:
    https://sonarsource.atlassian.net/browse/SLVS-2368?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/SLCore.Listeners.UnitTests/Implementation/ScaIssuesListenerTests.cs
+++ src/SLCore.Listeners.UnitTests/Implementation/ScaIssuesListenerTests.cs
@@ -18,16 +18,116 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+using SonarLint.VisualStudio.Core;
+using SonarLint.VisualStudio.IssueVisualization.Security.DependencyRisks;
+using SonarLint.VisualStudio.SLCore.Common.Models;
 using SonarLint.VisualStudio.SLCore.Core;
+using SonarLint.VisualStudio.SLCore.Listener.SCA;
 
 namespace SonarLint.VisualStudio.SLCore.Listeners.UnitTests.Implementation;
 
 [TestClass]
 public class ScaIssuesListenerTests
 {
+    private ScaIssuesListener testSubject;
+    private IDependencyRisksStore dependencyRisksStore;
+    private IScaIssueDtoToDependencyRiskConverter converter;
+    private ILogger logger;
+
+    private const string StoreConfigScopeId = "store-config-scope-id";
+    private const string ParamsConfigScopeId = "params-config-scope-id";
+    private const string ConfigScopeId = "config-scope-id";
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        dependencyRisksStore = Substitute.For<IDependencyRisksStore>();
+        converter = Substitute.For<IScaIssueDtoToDependencyRiskConverter>();
+        logger = Substitute.For<ILogger>();
+
+        testSubject = new ScaIssuesListener(dependencyRisksStore, converter, logger);
+    }
+
     [TestMethod]
-    public void MefCtor_CheckIsExported() => MefTestHelpers.CheckTypeCanBeImported<ScaIssuesListener, ISLCoreListener>();
+    public void MefCtor_CheckIsExported() =>
+        MefTestHelpers.CheckTypeCanBeImported<ScaIssuesListener, ISLCoreListener>(
+            MefTestHelpers.CreateExport<IDependencyRisksStore>(),
+            MefTestHelpers.CreateExport<IScaIssueDtoToDependencyRiskConverter>(),
+            MefTestHelpers.CreateExport<ILogger>());
 
     [TestMethod]
     public void MefCtor_CheckIsSingleton() => MefTestHelpers.CheckIsSingletonMefComponent<ScaIssuesListener>();
+
+    [TestMethod]
+    public void DidChangeScaIssues_ConfigurationScopeMismatch_LogsWarningAndDoesNotUpdateStore()
+    {
+        dependencyRisksStore.CurrentConfigurationScope.Returns(StoreConfigScopeId);
+        var parameters = CreateParams(ParamsConfigScopeId);
+
+        testSubject.DidChangeScaIssues(parameters);
+
+        dependencyRisksStore.DidNotReceiveWithAnyArgs().Set(default, default);
+        converter.DidNotReceiveWithAnyArgs().Convert(default);
+    }
+
+    [TestMethod]
+    public void DidChangeScaIssues_ConfigurationScopeMatches_UpdatesStoreWithConvertedIssues()
+    {
+        dependencyRisksStore.CurrentConfigurationScope.Returns(ConfigScopeId);
+        var (scaIssue1, dependencyRisk1) = CreateScaIssueAndDependencyRisk();
+        var (scaIssue2, dependencyRisk2) = CreateScaIssueAndDependencyRisk();
+        var (scaIssue3, dependencyRisk3) = CreateScaIssueAndDependencyRisk();
+        var parameters = CreateParams(ConfigScopeId, [scaIssue1, scaIssue2], [scaIssue3]);
+
+        testSubject.DidChangeScaIssues(parameters);
+
+        dependencyRisksStore.Received(1).Set(
+            Arg.Is<IDependencyRisk[]>(issues =>
+                issues.Contains(dependencyRisk1) &&
+                issues.Contains(dependencyRisk2) &&
+                issues.Contains(dependencyRisk3)),
+            ConfigScopeId);
+        logger.DidNotReceiveWithAnyArgs().WriteLine(default, default, default);
+    }
+
+    [TestMethod]
+    public void DidChangeScaIssues_EmptyLists_UpdatesStoreWithEmptyList()
+    {
+        dependencyRisksStore.CurrentConfigurationScope.Returns(ConfigScopeId);
+
+        var parameters = CreateParams(ConfigScopeId);
+
+        testSubject.DidChangeScaIssues(parameters);
+
+        converter.DidNotReceiveWithAnyArgs().Convert(default!);
+        dependencyRisksStore.Received(1).Set(
+            Arg.Is<IEnumerable<IDependencyRisk>>(issues => !issues.Any()),
+            ConfigScopeId);
+    }
+
+    private (ScaIssueDto scaIssue, IDependencyRisk dependencyRisk) CreateScaIssueAndDependencyRisk()
+    {
+        var scaIssue = new ScaIssueDto(
+            Guid.NewGuid(),
+            default,
+            default,
+            default,
+            string.Empty,
+            string.Empty,
+            []);
+        var dependencyRisk = Substitute.For<IDependencyRisk>();
+        converter.Convert(scaIssue).Returns(dependencyRisk);
+
+        return (scaIssue, dependencyRisk);
+    }
+
+    private static DidChangeScaIssuesParams CreateParams(
+        string configScopeId,
+        List<ScaIssueDto> addedScaIssues = null,
+        List<ScaIssueDto> updatedScaIssues = null) =>
+        new(
+            configScopeId,
+            [Guid.NewGuid()],
+            addedScaIssues ?? [],
+            updatedScaIssues ?? []);
 }
