da4df24fab4a1ca7cd4f39eab5e1c4f88791870e
Georgii Borovinskikh <georgii.borovinskikh@sonarsource.com>
2025-08-11 12:39:29 +0200

    Add tests for compilation provider

--- src/RoslynAnalyzerServer/Analysis/SonarRoslynProjectCompilationProvider.cs
+++ src/RoslynAnalyzerServer/Analysis/SonarRoslynProjectCompilationProvider.cs
@@ -1,22 +1,22 @@
-﻿/*
- * SonarLint for Visual Studio
- * Copyright (C) 2016-2025 SonarSource SA
- * mailto:info AT sonarsource DOT com
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
+﻿// /*
+//  * SonarLint for Visual Studio
+//  * Copyright (C) 2016-2025 SonarSource SA
+//  * mailto:info AT sonarsource DOT com
+//  *
+//  * This program is free software; you can redistribute it and/or
+//  * modify it under the terms of the GNU Lesser General Public
+//  * License as published by the Free Software Foundation; either
+//  * version 3 of the License, or (at your option) any later version.
+//  *
+//  * This program is distributed in the hope that it will be useful,
+//  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  * Lesser General Public License for more details.
+//  *
+//  * You should have received a copy of the GNU Lesser General Public License
+//  * along with this program; if not, write to the Free Software Foundation,
+//  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//  */
 
 using System.Collections.Immutable;
 using System.ComponentModel.Composition;
@@ -27,14 +27,6 @@ using SonarLint.VisualStudio.Core;
 using SonarLint.VisualStudio.RoslynAnalyzerServer.Analysis.Wrappers;
 
 namespace SonarLint.VisualStudio.RoslynAnalyzerServer.Analysis;
-
-internal interface ISonarRoslynProjectCompilationProvider
-{
-    Task<ISonarRoslynCompilationWithAnalyzersWrapper> GetProjectCompilationAsync(
-        ISonarRoslynProjectWrapper project,
-        ImmutableDictionary<Language, SonarRoslynAnalysisConfiguration> sonarRoslynAnalysisConfigurations,
-        CancellationToken token);
-}
 
 [Export(typeof(ISonarRoslynProjectCompilationProvider))]
 [PartCreationPolicy(CreationPolicy.Shared)]
@@ -91,7 +83,7 @@ internal class SonarRoslynProjectCompilationProvider(ILogger logger) : ISonarRos
     }
 
     private void OnAnalyzerException(Exception arg1, DiagnosticAnalyzer arg2, Diagnostic arg3) =>
-        logger.WriteLine(
-            new MessageLevelContext { Context = ["Roslyn Analyzer", arg2.GetType().Name, arg3.Id] },
+        logger.LogVerbose(
+            new MessageLevelContext { Context = ["Roslyn Analyzer Exception", arg2.GetType().Name, arg3.Id] },
             arg1.ToString());
 }
