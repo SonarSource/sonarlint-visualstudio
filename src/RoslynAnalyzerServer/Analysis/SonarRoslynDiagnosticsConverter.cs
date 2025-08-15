7041e1ed9c0c9a3ab0b2c4525d6320e58eb4bbd2
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2025-08-15 12:20:26 +0200

    SLVS-2465 Add analysis engine to connect with Roslyn APIs (#6368)
    [SLVS-2465](https://sonarsource.atlassian.net/browse/SLVS-2465)
    
    Part of
    <!--
      Only for standalone PRs without Jira issue in the PR title:
        * Replace this comment with Epic ID to create a new Task in Jira
    * Replace this comment with Issue ID to create a new Sub-Task in Jira
    * Ignore or delete this note to create a new Task in Jira without a
    parent
    -->
    
    [SLVS-2465]:
    https://sonarsource.atlassian.net/browse/SLVS-2465?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ src/RoslynAnalyzerServer/Analysis/SonarRoslynDiagnosticsConverter.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.ComponentModel.Composition;
+using Microsoft.CodeAnalysis;
+using SonarLint.VisualStudio.Core;
+
+namespace SonarLint.VisualStudio.RoslynAnalyzerServer.Analysis;
+
+[Export(typeof(IDiagnosticToRoslynIssueConverter))]
+[PartCreationPolicy(CreationPolicy.Shared)]
+public class DiagnosticToRoslynIssueConverter : IDiagnosticToRoslynIssueConverter
+{
+    public RoslynIssue ConvertToSonarDiagnostic(Diagnostic diagnostic, Language language)
+    {
+        var fileLinePositionSpan = diagnostic.Location.GetMappedLineSpan();
+
+        var textRange = new RoslynIssueTextRange(
+            fileLinePositionSpan.StartLinePosition.Line + 1, // roslyn lines are 0-based, while we use 1-based
+            fileLinePositionSpan.EndLinePosition.Line + 1, // roslyn lines are 0-based, while we use 1-based
+            fileLinePositionSpan.StartLinePosition.Character,
+            fileLinePositionSpan.EndLinePosition.Character);
+
+        var location = new RoslynIssueLocation(
+            diagnostic.GetMessage(),
+            fileLinePositionSpan.Path,
+            textRange);
+
+        // todo SLVS-2427 quick fixes
+        // todo SLVS-2428 secondary locations
+        return new RoslynIssue(
+            SonarCompositeRuleId.GetFullErrorCode(language.RepoInfo.Key, diagnostic.Id),
+            location);
+    }
+}
---
+++ src/RoslynAnalyzerServer/Analysis/Wrappers/RoslynCompilationWrapper.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using SonarLint.VisualStudio.Core;
+
+namespace SonarLint.VisualStudio.RoslynAnalyzerServer.Analysis.Wrappers;
+
+[ExcludeFromCodeCoverage] // todo SLVS-2466 add roslyn 'integration' tests using AdHocWorkspace
+internal class RoslynCompilationWrapper(Compilation roslynCompilation) : IRoslynCompilationWrapper
+{
+    public CompilationOptions RoslynCompilationOptions => roslynCompilation.Options;
+    public Language Language { get; } = roslynCompilation.Language switch
+    {
+        LanguageNames.CSharp => Language.CSharp,
+        LanguageNames.VisualBasic => Language.VBNET,
+        _ => throw new ArgumentOutOfRangeException(nameof(roslynCompilation)),
+    };
+
+    public IRoslynCompilationWrapper WithOptions(CompilationOptions withSpecificDiagnosticOptions) =>
+        new RoslynCompilationWrapper(roslynCompilation.WithOptions(withSpecificDiagnosticOptions));
+
+    public IRoslynCompilationWithAnalyzersWrapper WithAnalyzers(ImmutableArray<DiagnosticAnalyzer> analyzers, CompilationWithAnalyzersOptions compilationWithAnalyzersOptions) =>
+        new RoslynCompilationWithAnalyzersWrapper(roslynCompilation.WithAnalyzers(analyzers, compilationWithAnalyzersOptions), Language);
+}
