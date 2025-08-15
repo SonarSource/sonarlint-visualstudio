f6313ba73d8214f4a8cbd3cd3e296242069dbb26
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2025-08-15 12:20:26 +0200

    SLVS-2465 Add analysis engine to connect with Roslyn APIs (#6368)
    [SLVS-2465](https://sonarsource.atlassian.net/browse/SLVS-2465)
    
    Part of
    <!--
      Only for standalone PRs without Jira issue in the PR title:
        * Replace this comment with Epic ID to create a new Task in Jira
    * Replace this comment with Issue ID to create a new Sub-Task in Jira
    * Ignore or delete this note to create a new Task in Jira without a
    parent
    -->
    
    [SLVS-2465]:
    https://sonarsource.atlassian.net/browse/SLVS-2465?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ src/RoslynAnalyzerServer/Analysis/SequentialRoslynAnalysisEngine.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Immutable;
+using System.ComponentModel.Composition;
+using System.IO;
+using SonarLint.VisualStudio.Core;
+
+namespace SonarLint.VisualStudio.RoslynAnalyzerServer.Analysis;
+
+[Export(typeof(IRoslynAnalysisEngine))]
+[PartCreationPolicy(CreationPolicy.Shared)]
+[method: ImportingConstructor]
+internal class SequentialRoslynAnalysisEngine(
+    IDiagnosticToRoslynIssueConverter issueConverter,
+    IRoslynProjectCompilationProvider projectCompilationProvider,
+    ILogger logger) : IRoslynAnalysisEngine
+{
+    private readonly ILogger logger = logger.ForContext("Roslyn Analysis", "Engine");
+
+    public async Task<IEnumerable<RoslynIssue>> AnalyzeAsync(
+        List<RoslynProjectAnalysisRequest> projectsAnalysis,
+        ImmutableDictionary<Language, RoslynAnalysisConfiguration> sonarRoslynAnalysisConfigurations,
+        CancellationToken token)
+    {
+        var uniqueDiagnostics = new HashSet<RoslynIssue>(DiagnosticDuplicatesComparer.Instance);
+        foreach (var projectAnalysisCommands in projectsAnalysis)
+        {
+            var compilationWithAnalyzers = await projectCompilationProvider.GetProjectCompilationAsync(projectAnalysisCommands.Project, sonarRoslynAnalysisConfigurations, token);
+
+            // todo SLVS-2467 issue streaming
+            foreach (var analysisCommand in projectAnalysisCommands.AnalysisCommands)
+            {
+                var issues = await analysisCommand.ExecuteAsync(compilationWithAnalyzers, token);
+
+                foreach (var diagnostic in issues.Select(d => issueConverter.ConvertToSonarDiagnostic(d, compilationWithAnalyzers.Language)))
+                {
+                    // todo SLVS-2468 improve issue merging
+                    if (!uniqueDiagnostics.Add(diagnostic))
+                    {
+                        logger.LogVerbose("Duplicate diagnostic discarded ID: {0}, File: {1}, Line: {2}", diagnostic.RuleKey, Path.GetFileName(diagnostic.PrimaryLocation.FilePath), diagnostic.PrimaryLocation.TextRange.StartLine);
+                    }
+                }
+            }
+        }
+
+        return uniqueDiagnostics;
+    }
+}
