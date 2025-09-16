d3af700ee28667ce68ecc1ddd24c7e9faa39d71d
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2025-09-16 17:02:35 +0200

    SLVS-2550 Improve debounce action internals to not rely on TaskCanceledException (#6404)

---
+++ src/Integration.Vsix/SonarLintTagger/ResettableOneShotTimer.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace SonarLint.VisualStudio.Integration.Vsix.SonarLintTagger;
+
+internal interface IResettableOneShotTimer : IDisposable
+{
+    void Reset();
+
+    event EventHandler Elapsed;
+}
+
+internal sealed class ResettableOneShotTimer : IResettableOneShotTimer
+{
+    private readonly Timer timer;
+    private readonly long timerDurationMs;
+
+    public ResettableOneShotTimer(TimeSpan timerTimeSpan)
+    {
+        timerDurationMs = (long)timerTimeSpan.TotalMilliseconds;
+        timer = new Timer(TimerAction, null, Timeout.Infinite, Timeout.Infinite);
+    }
+
+    private void TimerAction(object state) => Elapsed?.Invoke(this, EventArgs.Empty);
+
+    public void Reset() => timer.Change(timerDurationMs, Timeout.Infinite);
+
+    public event EventHandler Elapsed;
+
+    public void Dispose() => timer?.Dispose();
+}
