e1d07450b50855d9ee1939a2d067018743a62366
Georgii Borovinskikh <georgii.borovinskikh@sonarsource.com>
2025-11-06 12:51:03 +0100

    fix

--- src/IssueViz.Security.UnitTests/ReportView/Issues/IssuesReportViewModelTests.cs
+++ src/IssueViz.Security.UnitTests/ReportView/Issues/IssuesReportViewModelTests.cs
@@ -50,7 +50,7 @@ public class IssuesReportViewModelTests
     [TestMethod]
     public void MefCtor_CheckIsExported() =>
         MefTestHelpers.CheckTypeCanBeImported<IssuesReportViewModel, IIssuesReportViewModel>(
-            MefTestHelpers.CreateExport<IIssuesStore>(),
+            MefTestHelpers.CreateExport<ILocalIssuesStore>(),
             MefTestHelpers.CreateExport<IShowInBrowserService>(),
             MefTestHelpers.CreateExport<IThreadHandling>()
         );
