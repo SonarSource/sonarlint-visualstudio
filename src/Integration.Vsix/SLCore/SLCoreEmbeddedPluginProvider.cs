aa6136863dce40185a08edcc09c05c51d4fb529a
Georgii Borovinskikh <georgii.borovinskikh@sonarsource.com>
2025-12-11 15:08:21 +0100

    upd

--- src/Integration.Vsix/SLCore/SLCoreEmbeddedPluginProvider.cs
+++ src/Integration.Vsix/SLCore/SLCoreEmbeddedPluginProvider.cs
@@ -89,7 +89,7 @@ public class SLCoreEmbeddedPluginProvider : ISLCoreEmbeddedPluginProvider
         var connectedModeEmbeddedPluginPathsByKey = new Dictionary<string, string>();
         var embeddedPluginFilePaths = ListJarFiles();
 
-        foreach (var plugin in GetAllEmbeddedPluginInfos())
+        foreach (var plugin in GetAllEmbeddedPluginInfos().Where(x => x.IsEnabledInStandaloneMode))
         {
             if (connectedModeEmbeddedPluginPathsByKey.ContainsKey(plugin.Key))
             {
