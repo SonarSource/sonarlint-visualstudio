1bd0d09779988d00a832e62d526e3ddff0dc55ac
tom-shafir-sonarsource <tom.shafir@sonarsource.com>
2026-03-05 14:37:28 +0100

    SLVS-2867 Create models for the service

---
+++ src/SLCore/Service/Plugin/Models/ArtifactSourceDto.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2025 SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace SonarLint.VisualStudio.SLCore.Service.Plugin.Models;
+
+/// <summary>
+/// Describes where an analyzer plugin artifact was obtained from.
+/// </summary>
+public enum ArtifactSourceDto
+{
+    /// <summary>
+    /// The plugin is bundled with the IDE extension.
+    /// </summary>
+    EMBEDDED,
+
+    /// <summary>
+    /// The plugin was downloaded on demand from an external source (e.g. binaries.sonarsource.com).
+    /// </summary>
+    ON_DEMAND,
+
+    /// <summary>
+    /// The plugin was synchronized from a SonarQube Server connection.
+    /// </summary>
+    SONARQUBE_SERVER,
+
+    /// <summary>
+    /// The plugin was synchronized from a SonarQube Cloud connection.
+    /// </summary>
+    SONARQUBE_CLOUD
+}
---
+++ src/SLCore/Service/Plugin/Models/PluginStatusDto.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2025 SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace SonarLint.VisualStudio.SLCore.Service.Plugin.Models;
+
+/// <param name="pluginName">human-readable name of the language/analyzer (e.g. "Java", "C/C++/Objective-C")</param>
+/// <param name="state">current lifecycle state of the plugin in the backend</param>
+/// <param name="source">where the plugin artifact came from; null when the plugin is not available</param>
+/// <param name="actualVersion">version of the plugin that is currently in use; null when the plugin is not loaded</param>
+/// <param name="overriddenVersion">a local plugin version that was superseded by the one obtained via SQS/SQC sync, if any; null when no override is in effect</param>
+public record PluginStatusDto(string pluginName, PluginStateDto state, ArtifactSourceDto? source, string? actualVersion, string? overriddenVersion);
