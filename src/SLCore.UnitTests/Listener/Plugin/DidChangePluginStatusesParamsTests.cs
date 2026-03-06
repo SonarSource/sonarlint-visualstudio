e923c4204d4516c5113c8868d70840f8f020b27c
tom-shafir-sonarsource <tom.shafir@sonarsource.com>
2026-03-06 15:27:19 +0100

    SLVS-2867 Create RPC models for PluginRpcService and didChangePluginStatuses listener

---
+++ src/SLCore.UnitTests/Listener/Plugin/DidChangePluginStatusesParamsTests.cs
@@ -0,0 +1,80 @@
+/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2025 SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using Newtonsoft.Json;
+using SonarLint.VisualStudio.SLCore.Listener.Plugin;
+using SonarLint.VisualStudio.SLCore.Service.Plugin.Models;
+
+namespace SonarLint.VisualStudio.SLCore.UnitTests.Listener.Plugin;
+
+[TestClass]
+public class DidChangePluginStatusesParamsTests
+{
+    [TestMethod]
+    public void Deserialized_AsExpected()
+    {
+        var expected = new DidChangePluginStatusesParams(
+            pluginStatuses:
+            [
+                new PluginStatusDto(
+                    pluginName: "Java",
+                    state: PluginStateDto.ACTIVE,
+                    source: ArtifactSourceDto.EMBEDDED,
+                    actualVersion: "1.2.3",
+                    overriddenVersion: null),
+                new PluginStatusDto(
+                    pluginName: "C/C++/Objective-C",
+                    state: PluginStateDto.SYNCED,
+                    source: ArtifactSourceDto.SONARQUBE_SERVER,
+                    actualVersion: "4.5.6",
+                    overriddenVersion: "3.0.0")
+            ]);
+
+        const string serialized = """
+                                  {
+                                    pluginStatuses: [
+                                      {
+                                        pluginName: "Java",
+                                        state: "ACTIVE",
+                                        source: "EMBEDDED",
+                                        actualVersion: "1.2.3",
+                                        overriddenVersion: null
+                                      },
+                                      {
+                                        pluginName: "C/C++/Objective-C",
+                                        state: "SYNCED",
+                                        source: "SONARQUBE_SERVER",
+                                        actualVersion: "4.5.6",
+                                        overriddenVersion: "3.0.0"
+                                      }
+                                    ]
+                                  }
+                                  """;
+
+        var actual = JsonConvert.DeserializeObject<DidChangePluginStatusesParams>(serialized);
+
+        actual
+            .Should()
+            .BeEquivalentTo(expected,
+                options =>
+                    options
+                        .ComparingByMembers<DidChangePluginStatusesParams>());
+    }
+}
