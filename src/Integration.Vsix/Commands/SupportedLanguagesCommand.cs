caa20ec498be4b91aec39a66e26f94eb28c4bf08
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2026-03-09 13:27:53 +0100

    SLVS-2868 Create Plugin status dialog window (#6646)
    Part of 
    <!-- 
      Only for standalone PRs without Jira issue in the PR title: 
        * Replace this comment with Epic ID to create a new Task in Jira
    * Replace this comment with Issue ID to create a new Sub-Task in Jira
    * Ignore or delete this note to create a new Task in Jira without a
    parent
    -->

---
+++ src/Integration.Vsix/Commands/SupportedLanguagesCommand.cs
@@ -0,0 +1,38 @@
+/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2025 SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Diagnostics.CodeAnalysis;
+using System.Windows;
+using SonarLint.VisualStudio.ConnectedMode.UI;
+using SonarLint.VisualStudio.Integration.Vsix.SupportedLanguages;
+
+namespace SonarLint.VisualStudio.Integration.Vsix.Commands;
+
+[ExcludeFromCodeCoverage]
+internal class SupportedLanguagesCommand : VsCommandBase
+{
+    internal const int Id = 0x1029;
+
+    protected override void InvokeInternal()
+    {
+        var supportedLanguagesWindow = new SupportedLanguagesDialogWindow();
+        supportedLanguagesWindow.ShowDialog(Application.Current.MainWindow);
+    }
+}
