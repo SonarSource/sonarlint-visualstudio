e56fb376b63a3fb4bf15ce4ec0c1792b83aa6595
Georgii Borovinskikh <117642191+georgii-borovinskikh-sonarsource@users.noreply.github.com>
2026-03-12 14:49:39 +0100

    SLVS-2881 Add pragma analyzer and codefix (#6648)
    Introduce DiagnosticAwarePragmaAnalyzer (SQ0079) that detects unused
    #pragma warning disable directives, and
    PragmaWarningDisableCodeFixProvider that offers removal as a code fix.
    Includes integration tests.
    
    Part of SLVS-2855

---
+++ src/RoslynAnalyzerServer.IntegrationTests/Analysis/Pragma/DiagnosticAwarePragmaAnalyzerTests.cs
@@ -0,0 +1,358 @@
+/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2025 SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using SonarLint.VisualStudio.RoslynAnalyzerServer.Analysis.Pragma;
+using static SonarLint.VisualStudio.RoslynAnalyzerServer.IntegrationTests.Analysis.Pragma.PragmaTestHelper;
+
+namespace SonarLint.VisualStudio.RoslynAnalyzerServer.IntegrationTests.Analysis.Pragma;
+
+[TestClass]
+public class DiagnosticAwarePragmaAnalyzerTests
+{
+    [TestMethod]
+    public async Task AnalyzeSyntaxTree_NoPragmas_NoDiagnosticRaised()
+    {
+        var (tree, testIssues, supportedIds) = GetPragmaDiagnosticsForMarkedSource(
+            """
+            class Foo { }
+            """);
+
+        var results = await GetPragmaDiagnosticsAsync(tree, testIssues, supportedIds);
+
+        results.Should().BeEmpty();
+    }
+
+    [TestMethod]
+    public async Task AnalyzeSyntaxTree_PragmaWithMatchingDiagnostic_NoDiagnosticRaised()
+    {
+        var (tree, testIssues, supportedIds) = GetPragmaDiagnosticsForMarkedSource(
+            """
+            #pragma warning disable S1234
+            class Foo
+            {
+                //SimulateIssue:S1234
+            }
+            #pragma warning restore S1234
+            """);
+
+        var results = await GetPragmaDiagnosticsAsync(tree, testIssues, supportedIds);
+
+        results.Should().BeEmpty();
+    }
+
+    [TestMethod]
+    public async Task AnalyzeSyntaxTree_PragmaWithNoMatchingDiagnostic_DiagnosticRaised()
+    {
+        var (tree, testIssues, supportedIds) = GetPragmaDiagnosticsForMarkedSource(
+            """
+            #pragma warning disable S1234
+            class Foo { }
+            #pragma warning restore S1234
+            """);
+
+        var results = await GetPragmaDiagnosticsAsync(tree, testIssues, supportedIds);
+
+        AssertExpectedPragmaDiagnostics(results, ("S1234", 0), ("S1234", 2));
+        AssertPairedPragmaProperties(results, 0, 2);
+    }
+
+    [TestMethod]
+    public async Task AnalyzeSyntaxTree_MultiIdPragmaWithPartialMatch_DiagnosticRaisedOnlyForUnmatchedId()
+    {
+        var (tree, testIssues, supportedIds) = GetPragmaDiagnosticsForMarkedSource(
+            """
+            #pragma warning disable S1234, S5678
+            class Foo
+            {
+                //SimulateIssue:S1234
+            }
+            #pragma warning restore S1234, S5678
+            """);
+
+        var results = await GetPragmaDiagnosticsAsync(tree, testIssues, supportedIds);
+
+        AssertExpectedPragmaDiagnostics(results, ("S5678", 0), ("S5678", 5));
+        AssertPairedPragmaProperties(results, 0, 5);
+    }
+
+    [TestMethod]
+    public async Task AnalyzeSyntaxTree_UnmatchDiagnosticNotSupported()
+    {
+        var (tree, testIssues, supportedIds) = GetPragmaDiagnosticsForMarkedSource(
+            """
+            #pragma warning disable S1234, S5678
+            class Foo
+            {
+                //SimulateIssue:S1234
+            }
+            #pragma warning restore S1234, S5678
+            """);
+
+        var results = await GetPragmaDiagnosticsAsync(tree, testIssues, supportedIds.Remove("S5678"));
+
+        results.Should().BeEmpty();
+    }
+
+    [TestMethod]
+    public async Task AnalyzeSyntaxTree_MultiPragmaWith_DiagnosticRaisedOnlyForUnmatchedId()
+    {
+        var (tree, testIssues, supportedIds) = GetPragmaDiagnosticsForMarkedSource(
+            """
+            #pragma warning disable S1234
+            #pragma warning disable S5678
+            class Foo
+            {
+                //SimulateIssue:S1234
+            }
+            #pragma warning restore S1234
+            #pragma warning restore S5678
+            """);
+
+        var results = await GetPragmaDiagnosticsAsync(tree, testIssues, supportedIds);
+
+        AssertExpectedPragmaDiagnostics(results, ("S5678", 1), ("S5678", 7));
+        AssertPairedPragmaProperties(results, 1, 7);
+    }
+
+    [TestMethod]
+    public async Task AnalyzeSyntaxTree_MultiIdPragmaWithFullMatch_DiagnosticRaisedOnlyForAll()
+    {
+        var (tree, testIssues, supportedIds) = GetPragmaDiagnosticsForMarkedSource(
+            """
+            #pragma warning disable S1234, S5678
+            class Foo { }
+            #pragma warning restore S1234, S5678
+            """);
+
+        var results = await GetPragmaDiagnosticsAsync(tree, testIssues, supportedIds);
+
+        AssertExpectedPragmaDiagnostics(results, ("S1234", 0), ("S5678", 0), ("S1234", 2), ("S5678", 2));
+        AssertPairedPragmaProperties(results, 0, 2);
+    }
+
+    [TestMethod]
+    public async Task AnalyzeSyntaxTree_PragmaForUnsupportedId_NoDiagnosticRaised()
+    {
+        var tree = CSharpSyntaxTree.ParseText(
+            """
+            #pragma warning disable CS0168
+            class Foo { }
+            #pragma warning restore CS0168
+            """);
+
+        var results = await GetPragmaDiagnosticsAsync(
+            tree,
+            ImmutableArray<Diagnostic>.Empty,
+            ImmutableHashSet<string>.Empty);
+
+        results.Should().BeEmpty();
+    }
+
+    [TestMethod]
+    public async Task AnalyzeSyntaxTree_DisableWithoutRestore_DiagnosticRaised()
+    {
+        var (tree, testIssues, supportedIds) = GetPragmaDiagnosticsForMarkedSource(
+            """
+            #pragma warning disable S1234
+            #pragma warning disable S5678
+            #pragma warning disable S1234
+            class Foo { }
+            """);
+
+        var results = await GetPragmaDiagnosticsAsync(tree, testIssues, supportedIds);
+
+        AssertExpectedPragmaDiagnostics(results, ("S1234", 0), ("S5678", 1), ("S1234", 2));
+        AssertSinglePragmaProperties(results);
+    }
+
+    [TestMethod]
+    public async Task AnalyzeSyntaxTree_RestoreWithoutDisable_DiagnosticRaised()
+    {
+        var (tree, testIssues, supportedIds) = GetPragmaDiagnosticsForMarkedSource(
+            """
+            class Foo { }
+            #pragma warning restore S1234
+            #pragma warning restore S5678
+            #pragma warning restore S1234
+            """);
+
+        var results = await GetPragmaDiagnosticsAsync(tree, testIssues, supportedIds);
+
+        AssertExpectedPragmaDiagnostics(results, ("S1234", 1), ("S5678", 2), ("S1234", 3));
+        AssertSinglePragmaProperties(results);
+    }
+
+    [TestMethod]
+    public async Task AnalyzeSyntaxTree_NestedPragmasSameId_InnerMatchedOuterReported()
+    {
+        var (tree, testIssues, supportedIds) = GetPragmaDiagnosticsForMarkedSource(
+            """
+            #pragma warning disable S1234
+            #pragma warning disable S1234
+            class Foo
+            {
+                //SimulateIssue:S1234
+            }
+            #pragma warning restore S1234
+            #pragma warning restore S1234
+            """);
+
+        var results = await GetPragmaDiagnosticsAsync(tree, testIssues, supportedIds);
+
+        AssertExpectedPragmaDiagnostics(results, ("S1234", 0), ("S1234", 7));
+        AssertPairedPragmaProperties(results, 0, 7);
+    }
+
+    [TestMethod]
+    public async Task AnalyzeSyntaxTree_DiagnosticOutsidePragmaRegion_DiagnosticRaised()
+    {
+        var (tree, testIssues, supportedIds) = GetPragmaDiagnosticsForMarkedSource(
+            """
+            class Bar
+            {
+                //SimulateIssue:S1234
+            }
+            #pragma warning disable S1234
+            class Foo { }
+            #pragma warning restore S1234
+            """);
+
+        var results = await GetPragmaDiagnosticsAsync(tree, testIssues, supportedIds);
+
+        AssertExpectedPragmaDiagnostics(results, ("S1234", 4), ("S1234", 6));
+        AssertPairedPragmaProperties(results, 4, 6);
+    }
+
+    [TestMethod]
+    public async Task AnalyzeSyntaxTree_MultipleDiagnosticsInRegion_NoDiagnosticRaised()
+    {
+        var (tree, testIssues, supportedIds) = GetPragmaDiagnosticsForMarkedSource(
+            """
+            #pragma warning disable S1234
+            class Foo
+            {
+                //SimulateIssue:S1234
+            }
+            class Bar
+            {
+                //SimulateIssue:S1234
+            }
+            #pragma warning restore S1234
+            """);
+
+        var results = await GetPragmaDiagnosticsAsync(tree, testIssues, supportedIds);
+
+        results.Should().BeEmpty();
+    }
+
+    [TestMethod]
+    public async Task AnalyzeSyntaxTree_NewRegionWithoutDiagnostic_DiagnosticRaised()
+    {
+        var (tree, testIssues, supportedIds) = GetPragmaDiagnosticsForMarkedSource(
+            """
+            #pragma warning disable S1234
+            class Foo
+            {
+                //SimulateIssue:S1234
+            }
+            #pragma warning restore S1234
+            #pragma warning disable S1234
+            """);
+
+        var results = await GetPragmaDiagnosticsAsync(tree, testIssues, supportedIds);
+
+        AssertExpectedPragmaDiagnostics(results, ("S1234", 6));
+        AssertSinglePragmaProperties(results);
+    }
+
+    [TestMethod]
+    public async Task AnalyzeSyntaxTree_MoreRestoresThanDisables_DiagnosticRaised()
+    {
+        var (tree, testIssues, supportedIds) = GetPragmaDiagnosticsForMarkedSource(
+            """
+            #pragma warning disable S1234
+            class Foo
+            {
+                //SimulateIssue:S1234
+            }
+            #pragma warning restore S1234
+            #pragma warning restore S1234
+            """);
+
+        var results = await GetPragmaDiagnosticsAsync(tree, testIssues, supportedIds);
+
+        AssertExpectedPragmaDiagnostics(results, ("S1234", 6));
+        AssertSinglePragmaProperties(results);
+    }
+
+    [TestMethod]
+    public async Task AnalyzeSyntaxTree_SyntaxErrors_NoDiagnosticRaised()
+    {
+        var (tree, testIssues, supportedIds) = GetPragmaDiagnosticsForMarkedSource(
+            """
+            #pragma warning disable S1234
+            class Foo {
+            #pragma warning restore S1234
+            """);
+
+        var results = await GetPragmaDiagnosticsAsync(tree, testIssues, supportedIds);
+
+        results.Should().BeEmpty();
+    }
+
+    private static void AssertExpectedPragmaDiagnostics(
+        ImmutableArray<Diagnostic> results,
+        params (string ruleId, int line)[] expected)
+    {
+        var actual = results.Select(d => (
+            ruleId: d.Properties[DiagnosticAwarePragmaAnalyzer.ReportedDiagnosticId],
+            line: d.Location.GetLineSpan().StartLinePosition.Line));
+
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    private static void AssertPairedPragmaProperties(
+        ImmutableArray<Diagnostic> results,
+        int disableLine,
+        int restoreLine)
+    {
+        foreach (var diagnostic in results)
+        {
+            diagnostic.AdditionalLocations.Should().HaveCount(2);
+            diagnostic.AdditionalLocations[0].GetLineSpan().StartLinePosition.Line.Should().Be(disableLine);
+            diagnostic.AdditionalLocations[1].GetLineSpan().StartLinePosition.Line.Should().Be(restoreLine);
+            diagnostic.Properties["0"].Should().Be("#pragma warning disable");
+            diagnostic.Properties["1"].Should().Be("#pragma warning restore");
+        }
+    }
+
+    private static void AssertSinglePragmaProperties(ImmutableArray<Diagnostic> results)
+    {
+        foreach (var diagnostic in results)
+        {
+            diagnostic.AdditionalLocations.Should().BeEmpty();
+            diagnostic.Properties.ContainsKey("0").Should().BeFalse();
+            diagnostic.Properties.ContainsKey("1").Should().BeFalse();
+        }
+    }
+}
---
+++ src/RoslynAnalyzerServer/Analysis/Pragma/PragmaWarningDisableCodeFixProvider.cs
@@ -0,0 +1,113 @@
+/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2016-2025 SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Text;
+using SonarLint.VisualStudio.Core;
+using Document = Microsoft.CodeAnalysis.Document;
+
+namespace SonarLint.VisualStudio.RoslynAnalyzerServer.Analysis.Pragma;
+
+[ExportCodeFixProvider(LanguageNames.CSharp)]
+public class PragmaWarningDisableCodeFixProvider : CodeFixProvider
+{
+    public override ImmutableArray<string> FixableDiagnosticIds { get; } =
+        ImmutableArray.Create(AdditionalRules.UnusedPragmaRuleKey);
+
+    public override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+    public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+    {
+        var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+        if (root == null)
+        {
+            return;
+        }
+
+        foreach (var diagnostic in context.Diagnostics)
+        {
+            if (FindPragmaIdentifier(root, diagnostic.Location.SourceSpan) == null)
+            {
+                continue;
+            }
+
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    Resources.SQVSPRAGMACodeFixTitle,
+                    _ => RemoveIdentifierFromPragmaAsync(context.Document, root, diagnostic),
+                    equivalenceKey: AdditionalRules.UnusedPragmaRuleKey),
+                diagnostic);
+        }
+    }
+
+    private static Task<Document> RemoveIdentifierFromPragmaAsync(
+        Document document,
+        SyntaxNode root,
+        Diagnostic diagnostic)
+    {
+        var allLocations = new HashSet<Location> { diagnostic.Location };
+        allLocations.UnionWith(diagnostic.AdditionalLocations);
+
+        var editor = new SyntaxEditor(root, document.Project.Solution.Workspace);
+
+        foreach (var loc in allLocations)
+        {
+            var found = FindPragmaIdentifier(root, loc.SourceSpan);
+            if (found == null)
+            {
+                continue;
+            }
+            var (identifier, pragma) = found.Value;
+
+            if (pragma.ErrorCodes.Count == 1)
+            {
+                editor.RemoveNode(pragma);
+            }
+            else
+            {
+                editor.RemoveNode(identifier);
+            }
+        }
+
+        return Task.FromResult(document.WithSyntaxRoot(editor.GetChangedRoot()));
+    }
+
+    private static (IdentifierNameSyntax, PragmaWarningDirectiveTriviaSyntax)? FindPragmaIdentifier(
+        SyntaxNode root, TextSpan span)
+    {
+        var node = root.FindNode(span, findInsideTrivia: true, getInnermostNodeForTie: true);
+        if (node is not IdentifierNameSyntax identifier)
+        {
+            return null;
+        }
+
+        if (identifier.Parent is not PragmaWarningDirectiveTriviaSyntax pragma)
+        {
+            return null;
+        }
+
+        return (identifier, pragma);
+    }
+}
